6d5cafbcb74e43062be804ccd02aa31c919f0de6
Paul Romano <romano7@mit.edu>
2011-01-23 05:05:58 +0000

    Changed allocation of main arrays, many formatting and structural changes.

--- src/main.f90
+++ src/main.f90
@@ -1,9 +1,11 @@
 program main
 
-  use global, only: cells, surfaces, materials, inputfile
-  use fileio, only: read_input, read_command_line
-  use output, only: title, message, warning, error
-  use geometry, only: sense, cell_contains
+  use global
+  use fileio,      only: read_input, read_command_line, read_count
+  use output,      only: title, echo_input, message, warning, error
+  use geometry,    only: sense, cell_contains
+  use mcnp_random, only: RN_init_problem, rang, RN_init_particle
+  use source,      only: init_source
 
   implicit none
 
@@ -13,14 +15,75 @@ program main
   real(8) :: point(3)
   integer :: s(4)
 
+  ! Print the OpenMC title and version/date/time information
   call title()
-  
+
+  ! Initialize random number generator
+  call RN_init_problem( 3, 0_8, 0_8, 0_8, 0 )
+
+  ! Read command line arguments
   call read_command_line()
+
+  ! Read input file -- make a first pass through the file to count
+  ! cells, surfaces, etc in order to allocate arrays, then do a second
+  ! pass to actually read values
+  call read_count(inputfile)
   call read_input(inputfile)
+  call echo_input()
 
-  point = (/ 4.0, 0.0, 3.1 /)
+  ! create source particles
+  call init_source()
 
-  call cell_contains( cells(1), point, .true. )
+  ! start problem
+  ! call run_problem()
 
+
+contains
+
+!=====================================================================
+! RUN_PROBLEM encompasses all the main logic where iterations are
+! performed over the cycles and histories.
+!=====================================================================
+
+  subroutine run_problem()
+
+    integer :: i, j
+    integer :: i_cycle
+    integer :: i_particle
+    type(Neutron), pointer :: particle => null()
+
+    CYCLE_LOOP: do i_cycle = 1, n_cycles
+       
+       ! Set all tallies to zero
+
+       HISTORY_LOOP: do j = 1, n_particles
+
+          ! grab source particle from bank
+          ! particle => get_source_particle()
+
+          ! set random number seed
+          i_particle = (i-1)*n_particles + j
+          call RN_init_particle(int(i_particle,8))
+
+          ! transport particle
+          ! call transport(particle)
+
+       end do HISTORY_LOOP
+
+       ! Collect results and statistics
+
+       ! print cycle information
+
+    end do CYCLE_LOOP
+
+    ! Collect all tallies and print
+
+    ! print run time
+
+    call free_memory()
+    
+  end subroutine run_problem
 
 end program main
+
+
--- src/types.f90
+++ src/types.f90
@@ -2,46 +2,8 @@ module types
 
   implicit none
 
-  type :: Material
-    real(8) :: Sigma_f          ! Macro fission xs
-    real(8) :: Sigma_s          ! Macro scattering xs
-    real(8) :: Sigma_a          ! Macro absorption xs
-    real(8) :: Sigma_t          ! Macro total xs
-    real(8) :: Sigma_nf         ! Macro nu-fission xs 
-  end type Material
-  
-  type :: region
-    real(8) :: xyz_min(3)       ! Minimum coordinates of region
-    real(8) :: xyz_max(3)       ! Maximum coordinates of region
-    integer  :: mat             ! Material in region
-  end type region
-
-  type :: bank
-     real(8) :: xyz(3)          ! location
-     integer  :: uid            ! Unique ID
-  end type bank
-  
-  type :: particle
-    real(8) :: xyz(3)           ! location
-    real(8) :: uvw(3)           ! directional cosines
-    real(8) :: wgt              ! particle weight
-    integer  :: ijk(3)          ! interval in global mesh
-    integer  :: local_ijk(3)    ! interval in local mesh
-    integer  :: uid             ! Unique ID
-  end type particle
-  
-  type :: fissionBankPtr
-    type(bank), pointer :: ptr  ! Used for sorting fission bank
-  end type fissionBankPtr
-  
-  ! For each basic type (Cell, Surface, and Material), we have
-  ! corresponding types CellList, etc that have an extra pointer for a
-  ! linked list. As the input file is being read, the list types are
-  ! used since they can be dynamically allocated. Once the input file
-  ! is complete, the linked list is converted to a normal array.
-
   type :: Surface
-     integer :: id
+     integer :: uid
      integer :: type
      real(8), allocatable :: coeffs(:)
      integer, allocatable :: neighbor_pos(:)
@@ -49,29 +11,12 @@ module types
      integer :: bc
   end type Surface
 
-  type :: SurfaceList
-     integer :: id
-     integer :: type
-     real(8), allocatable :: coeffs(:)
-     integer, allocatable :: neighbor_pos(:)
-     integer, allocatable :: neighbor_neg(:)
-     integer :: bc
-     type(SurfaceList), pointer :: next
-  end type SurfaceList
-
   type :: Cell
-     integer :: id
+     integer :: uid
      integer :: n_items
      integer, allocatable :: boundary_list(:)
      integer :: material
   end type Cell
-
-  type :: CellList
-     integer :: id
-     integer, allocatable :: boundary_list(:)
-     integer :: material
-     type(CellList), pointer :: next
-  end type CellList
 
   type :: Neutron
     integer  :: uid    ! Unique ID
@@ -80,5 +25,28 @@ module types
     real(8)  :: wgt    ! particle weight
   end type Neutron
 
+  type :: Bank
+     integer :: uid    ! Unique ID
+     real(8) :: xyz(3) ! Location of bank particle
+  end type Bank
+
+  type :: Isotope
+     integer :: uid     ! unique identifier
+     integer :: zaid    ! ZAID, e.g. 92235
+     character(3) :: xs ! cross section identifier, e.g. 70c
+     real(8) :: density ! density in atom/b-cm
+  end type Isotope
+
+  type :: Material
+     integer :: uid
+     integer :: n_isotopes
+     type(Isotope), allocatable :: isotopes(:)
+  end type Material
+
+  type :: ExtSource
+     integer :: uid                     ! unique identifier
+     integer :: type                    ! type of source, e.g. 'box' or 'cell'
+     real(8), allocatable :: values(:)  ! values for particular source type
+  end type ExtSource
 
 end module types

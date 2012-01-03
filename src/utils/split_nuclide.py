f453f4ea8cdd39039173eb4632b799b92fc981b3
Paul Romano <romano7@gmail.com>
2012-01-03 18:00:32 -0500

    Added element to nuclide splitting script.

---
+++ src/utils/split_nuclide.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import sys
+
+if len(sys.argv) != 3:
+    print "Must supply element and atom/b-cm"
+    exit
+
+element = sys.argv[1]
+ao = float(sys.argv[2])
+
+for line in open('abundances_modified.txt','r'):
+    words = line.split()
+    if words[1] == element:
+        print('<nuclide name="{0}-{1}" ao="{2}" />'.format(
+                element, words[2], float(words[3])*ao*0.01))
+
+    

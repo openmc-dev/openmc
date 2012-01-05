5d8685523a5a5e88afa09d861662201e52adc6e6
itayhorin <itayhorin@gmail.com>
2012-01-04 17:20:33 -0800

    added hdf5 link and output a sample file

--- src/cmfd_utils.F90
+++ src/cmfd_utils.F90
@@ -509,4 +509,30 @@ contains
 
   end function get_reflector_albedo
 
+!===============================================================================
+! WRITE_HDF5 writes an hdf5 output file with the cmfd object for restarts 
+!===============================================================================
+
+  subroutine write_hdf5()
+
+     use hdf5 ! This module contains all necessary modules
+
+     character(LEN=8), parameter :: filename = "filef.h5" ! File name
+     integer(HID_T) :: file_id                            ! File identifier
+     integer     ::   error  ! Error flag
+
+!    Initialize FORTRAN interface.
+     call h5open_f (error)
+
+     ! Create a new file using default properties.
+     call h5fcreate_f(filename, H5F_ACC_TRUNC_F, file_id, error)
+
+     ! Terminate access to the file.
+     call h5fclose_f(file_id, error)
+
+!    Close FORTRAN interface.
+     call h5close_f(error)
+
+  end subroutine write_hdf5
+
 end module cmfd_utils

725bb38e4a0eaebebe4bb63c2e4b4216795587e8
Paul Romano <romano7@gmail.com>
2012-02-02 13:09:32 -0500

    Extended length of data that can be read in xml-fortran.

--- src/xml-fortran/xmlreader.f90
+++ src/xml-fortran/xmlreader.f90
@@ -21,7 +21,7 @@ program xmlreader
    logical                                :: endtag
    character(len=80), dimension(1:2,1:20) :: attribs
    integer                                :: noattribs
-   character(len=200), dimension(1:100)   :: data
+   character(len=200), dimension(1:1000)  :: data
    integer                                :: nodata
    integer                                :: i
    integer                                :: j
@@ -569,7 +569,7 @@ subroutine write_prolog
   &   '   logical                                :: endtag'    , &
   &   '   character(len=80), dimension(1:2,1:20) :: attribs'   , &
   &   '   integer                                :: noattribs' , &
-  &   '   character(len=200), dimension(1:100)   :: data'      , &
+  &   '   character(len=200), dimension(1:1000)  :: data'      , &
   &   '   integer                                :: nodata'
 
    write( lusubs, '(a)' ) &

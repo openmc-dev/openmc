934f0459245a65648082c98e6ed6bfee647a7396
itayhorin <itayhorin@gmail.com>
2012-02-07 11:18:48 -0800

    added more room for pin-by-pin mesh when reading input line

--- src/xml-fortran/xmlreader.f90
+++ src/xml-fortran/xmlreader.f90
@@ -21,7 +21,7 @@ program xmlreader
    logical                                :: endtag
    character(len=80), dimension(1:2,1:20) :: attribs
    integer                                :: noattribs
-   character(len=200), dimension(1:1000)  :: data
+   character(len=100000), dimension(1:1000)  :: data
    integer                                :: nodata
    integer                                :: i
    integer                                :: j
@@ -292,6 +292,7 @@ program xmlreader
    !
    write( luend, '(a)' ) &
       & '   if ( present(errout) ) errout = error', &
+      & '   call xml_close(info)', &
       & 'end subroutine', &
       & ' '
    write( luwritp, '(a)' ) &
@@ -569,7 +570,7 @@ subroutine write_prolog
   &   '   logical                                :: endtag'    , &
   &   '   character(len=80), dimension(1:2,1:20) :: attribs'   , &
   &   '   integer                                :: noattribs' , &
-  &   '   character(len=200), dimension(1:1000)  :: data'      , &
+  &   '   character(len=100000), dimension(1:1000)  :: data'      , &
   &   '   integer                                :: nodata'
 
    write( lusubs, '(a)' ) &

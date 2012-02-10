51168ad1ca3224598909fb87a8b13b82e8a79a70
Bryan Herman <bherman@kilkenny.bw01.mit.edu>
2012-02-10 12:25:46 -0800

    prod matrix now built in parallel

--- src/cmfd_prod_operator.F90
+++ src/cmfd_prod_operator.F90
@@ -35,11 +35,10 @@ contains
     call get_F_indices(this)
 
     ! set up M operator
-    call MatCreateSeqAIJ(PETSC_COMM_SELF,this%n,this%n,this%nnz,               &
-   &                     PETSC_NULL_INTEGER,this%F,ierr)
+    call MatCreateMPIAIJ(PETSC_COMM_WORLD,PETSC_DECIDE,PETSC_DECIDE,this%n,    &
+   & this%n,this%nnz,PETSC_NULL_INTEGER,this%nnz,PETSC_NULL_INTEGER,this%F,ierr)
     call MatSetOption(this%F,MAT_NEW_NONZERO_LOCATIONS,PETSC_TRUE,ierr)
     call MatSetOption(this%F,MAT_IGNORE_ZERO_ENTRIES,PETSC_TRUE,ierr)
-    call MatSetOption(this%F,MAT_USE_HASH_TABLE,PETSC_TRUE,ierr)
 
   end subroutine init_F_operator
 
@@ -77,7 +76,7 @@ contains
 
   subroutine build_prod_matrix(this)
 
-    use global, only: cmfd,cmfd_coremap
+    use global, only: cmfd,cmfd_coremap,mpi_err
 
     type(prod_operator) :: this
 
@@ -90,56 +89,50 @@ contains
     integer :: gmat_idx           ! index in matrix for energy group g
     integer :: hmat_idx           ! index in matrix for energy group h
     integer :: ierr               ! Petsc error code
+    integer :: row_start            ! the first local row on the processor
+    integer :: row_finish           ! the last local row on the processor
+    integer :: irow                 ! iteration counter over row
     real(8) :: nfissxs            ! nufission cross section h-->g
     real(8) :: val                ! temporary variable for nfissxs
 
-    ! initialize matrix for building
+    ! get row bounds for this processor
+    call MatGetOwnershipRange(this%F,row_start,row_finish,ierr)
+
+    ! begin iteration loops
+    ROWS: do irow = row_start,row_finish-1
+
+      ! get indices for that row
+      call matrix_to_indices(irow,g,i,j,k)
+
+      ! check if not including reflector
+      if (cmfd_coremap) then
+
+        ! check if at a reflector
+        if (cmfd % coremap(i,j,k) == 99999) then
+          cycle
+        end if
+
+      end if
+
+      ! loop around all other groups 
+      NFISS: do h = 1,ng
+
+        ! get cell data
+        nfissxs = cmfd%nfissxs(h,g,i,j,k)
+
+        ! get matrix column location
+        call indices_to_matrix(h,i,j,k,hmat_idx)
+
+        ! reocrd value in matrix
+        val = nfissxs
+        call MatSetValue(this%F,irow,hmat_idx-1,val,INSERT_VALUES,ierr)
+
+      end do NFISS
+
+    end do ROWS 
+
+   ! assemble matrix 
     call MatAssemblyBegin(this%F,MAT_FLUSH_ASSEMBLY,ierr)
-
-    ! begin loop around energy groups and spatial indices
-    ZLOOP:  do k = 1,nz
-
-      YLOOP: do j = 1,ny
-
-        XLOOP: do i = 1,nx
-
-          ! check if not including reflector
-          if (cmfd_coremap) then
-
-            ! check if at a reflector
-            if (cmfd % coremap(i,j,k) == 99999) then
-              cycle
-            end if
-
-          end if
-
-          GROUP: do g = 1,ng
-
-            NFISS: do h = 1,ng
-
-              ! get cell data
-              nfissxs = cmfd%nfissxs(h,g,i,j,k)
-
-              ! get matrix location
-              gmat_idx = get_matrix_idx(g,i,j,k)
-              hmat_idx = get_matrix_idx(h,i,j,k)
-
-              ! reocrd value in matrix
-              val = nfissxs
-              call MatSetValue(this%F,gmat_idx-1,hmat_idx-1,val,INSERT_VALUES, &
-             &                 ierr)
-
-            end do NFISS
-
-          end do GROUP
-
-        end do XLOOP
-
-      end do YLOOP
-
-    end do ZLOOP
-
-    ! finalize matrix assembly
     call MatAssemblyEnd(this%F,MAT_FINAL_ASSEMBLY,ierr)
 
     ! print out operator to file
@@ -148,40 +141,53 @@ contains
   end subroutine build_prod_matrix
 
 !===============================================================================
-! GET_MATRIX_IDX takes (g,x,y,z) indices and computes location in matrix 
+! INDICES_TO_MATRIX takes (x,y,z,g) indices and computes location in matrix 
 !===============================================================================
 
-  function get_matrix_idx(g,i,j,k)
-
+  subroutine indices_to_matrix(g,i,j,k,matidx)
+  
     use global, only: cmfd,cmfd_coremap
 
-    ! arguments
-    integer :: get_matrix_idx  ! the index location in matrix
+    integer :: matidx         ! the index location in matrix
     integer :: i               ! current x index
     integer :: j               ! current y index
     integer :: k               ! current z index
     integer :: g               ! current group index
-
-    ! local variables
-    integer :: nidx            ! index in matrix
-
+    
     ! check if coremap is used
     if (cmfd_coremap) then
-
+    
       ! get idx from core map
-      nidx = ng*(cmfd % coremap(i,j,k)) - (ng - g)
+      matidx = ng*(cmfd % coremap(i,j,k)) - (ng - g)
 
     else
 
       ! compute index
-      nidx = g + ng*(i - 1) + ng*nx*(j - 1) + ng*nx*ny*(k - 1)
+      matidx = g + ng*(i - 1) + ng*nx*(j - 1) + ng*nx*ny*(k - 1)
 
     end if
 
-    ! record value to function
-    get_matrix_idx = nidx
+  end subroutine indices_to_matrix
 
-  end function get_matrix_idx
+!===============================================================================
+! MATRIX_TO_INDICES 
+!===============================================================================
+
+  subroutine matrix_to_indices(irow,g,i,j,k)
+
+    integer :: i                    ! iteration counter for x
+    integer :: j                    ! iteration counter for y
+    integer :: k                    ! iteration counter for z
+    integer :: g                    ! iteration counter for groups
+    integer :: irow                 ! iteration counter over row (0 reference)
+
+    ! compute indices
+    g = irow/(nx*ny*nz) + 1
+    i = mod(irow,nx*ny*nz)/(ny*nz) + 1
+    j = mod(irow,ny*nz)/nz + 1
+    k = mod(irow,nz) + 1
+
+  end subroutine matrix_to_indices
 
 !===============================================================================
 ! PRINT_F_OPERATOR 
@@ -196,11 +202,12 @@ contains
     type(prod_operator) :: this
 
     ! write out matrix in binary file (debugging)
-    call PetscViewerBinaryOpen(PETSC_COMM_SELF,trim(path_input)//'prodmat.bin' &
+    call PetscViewerBinaryOpen(PETSC_COMM_WORLD,trim(path_input)//'prodmat.bin'&
    &     ,FILE_MODE_WRITE,viewer,ierr)
     call MatView(this%F,viewer,ierr)
     call PetscViewerDestroy(viewer,ierr)
 
+stop
   end subroutine print_F_operator
 
 !==============================================================================

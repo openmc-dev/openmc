36463aeabeaf5de107e765d8e830f8c10be63b75
itayhorin <itayhorin@gmail.com>
2012-08-28 14:56:36 -0400

    Added absolute uncertainty for error bars as well as score information. The filter labels are incorrect still, and the plots are not yet pretty or saved to a file.

--- src/utils/tally_conv.py
+++ src/utils/tally_conv.py
@@ -16,8 +16,8 @@ files.sort()
 # Initialize arrays as needed
 mean = [None for x in range(len(files))]
 uncert = [None for x in range(len(files))]
-#tally_score = []
-#tally_filter = []
+scoreType = [None for x in range(len(files))]
+filterType = [None for x in range(len(files))]
 active_batches = [None for x in range(len(files))]
 
 for i_batch in xrange(len(files)):
@@ -35,6 +35,8 @@ for i_batch in xrange(len(files)):
     # Increase the dimensionality of our main variables
     mean[i_batch] = [None for x in range(len(sp.tallies))]
     uncert[i_batch] = [None for x in range(len(sp.tallies))]
+    scoreType[i_batch] = [None for x in range(len(sp.tallies))]    
+    filterType[i_batch] = [None for x in range(len(sp.tallies))]    
     
     # Calculate t-value for 95% two-sided CI
     n = sp.current_batch - sp.n_inactive
@@ -48,12 +50,17 @@ for i_batch in xrange(len(files)):
         # Resize the 2nd dimension      
         mean[i_batch][i_tally] = [None for x in range(t.n_score_bins)]
         uncert[i_batch][i_tally] = [None for x in range(t.n_score_bins)]
+        scoreType[i_batch][i_tally] = [None for x in range(t.n_score_bins)]
+        filterType[i_batch][i_tally] = [None for x in range(t.n_score_bins)]
         
         for i_score in range(t.n_score_bins):
             # Resize the 3rd dimension            
             mean[i_batch][i_tally][i_score] = \
                 [None for x in range(t.n_filter_bins)]
             uncert[i_batch][i_tally][i_score] = \
+                [None for x in range(t.n_filter_bins)]
+            scoreType[i_batch][i_tally][i_score] = t.scores[i_score]  
+            filterType[i_batch][i_tally][i_score] = \
                 [None for x in range(t.n_filter_bins)]
             for i_filter in range(t.n_filter_bins):
                 s, s2 = sp._get_double(2)
@@ -64,20 +71,23 @@ for i_batch in xrange(len(files)):
                 else:
                     relative_error = 0.0
                 uncert[i_batch][i_tally][i_score][i_filter] = relative_error
-       
-        # Still need to get some information just to be able to build labels
-            
+            #scoreType[i_batch][i_tally][i_score][i_filter] = t.scores[i_score]            
+            # Not quite right:
+            filterType[i_batch][i_tally][i_score][i_filter] = t.filters[i_filter]
+
 
 # Reorder the data lists in to a list order more conducive for plotting:
 # The indexing should be: [tally][score][filter][batch]
 meanPlot = [None for x in range(len(mean[0]))] # Set to the number of tallies
 uncertPlot = [None for x in range(len(mean[0]))] # Set to the number of tallies
+absUncertPlot = [None for x in range(len(mean[0]))] # Set to the number of tallies
 
 # Get and set the correct sizes for the rest of the dimensions
 for i_tally in range(len(meanPlot)):
     # Set 2nd (score) dimension
     meanPlot[i_tally] = [None for x in range(len(mean[0][i_tally]))]
     uncertPlot[i_tally] = [None for x in range(len(mean[0][i_tally]))]
+    absUncertPlot[i_tally] = [None for x in range(len(mean[0][i_tally]))]
     
     for i_score in range(len(meanPlot[i_tally])):
         # Set 3rd (filter) dimension
@@ -85,12 +95,16 @@ for i_tally in range(len(meanPlot)):
             [None for x in range(len(mean[0][i_tally][i_score]))]
         uncertPlot[i_tally][i_score] = \
             [None for x in range(len(mean[0][i_tally][i_score]))]
+        absUncertPlot[i_tally][i_score] = \
+            [None for x in range(len(mean[0][i_tally][i_score]))]
         
         for i_filter in range(len(meanPlot[i_tally][i_score])):
             # Set 4th (batch) dimension
             meanPlot[i_tally][i_score][i_filter] = \
                 [None for x in range(len(mean))]
             uncertPlot[i_tally][i_score][i_filter] = \
+                [None for x in range(len(mean))]
+            absUncertPlot[i_tally][i_score][i_filter] = \
                 [None for x in range(len(mean))]
         
 # Now rearrange the data as suitable
@@ -102,9 +116,14 @@ for i_batch in range(len(mean)):
                     mean[i_batch][i_tally][i_score][i_filter]
                 uncertPlot[i_tally][i_score][i_filter][i_batch] = \
                     uncert[i_batch][i_tally][i_score][i_filter]
+                absUncertPlot[i_tally][i_score][i_filter][i_batch] = \
+                    uncert[i_batch][i_tally][i_score][i_filter] * \
+                    mean[i_batch][i_tally][i_score][i_filter]
 
 # Set plotting constants
 xLabel = "Active Batches"
+xLabel = xLabel.title() # not necessary for now, but is left in to handle if 
+# the previous line changes
 
 # Begin plotting
 for i_tally in range(len(meanPlot)):
@@ -113,7 +132,7 @@ for i_tally in range(len(meanPlot)):
     
     for i_score in range(len(meanPlot[i_tally])):
         # Set score string
-        scoreStr = "" #Not in place until I get that from statePoint above
+        scoreStr = scoreType[i_batch][i_tally][i_score]
                 
         for i_filter in range(len(meanPlot[i_tally][i_score])):
             # Set filter string
@@ -121,19 +140,21 @@ for i_tally in range(len(meanPlot)):
             
             # set Title 
             title = tallyStr + " for " + filterStr
+            title = title.title()
             # set yLabel
             yLabel = scoreStr
+            yLabel = yLabel.title()
             
-            # Plot mean with error bars
+            # Plot mean with absolute error bars
             plt.errorbar(active_batches, \
                 meanPlot[i_tally][i_score][i_filter][:], \
-                uncertPlot[i_tally][i_score][i_filter][:])
+                absUncertPlot[i_tally][i_score][i_filter][:])
             plt.xlabel(xLabel)
             plt.ylabel(yLabel)
             plt.title(title)
             plt.show()
             
-            # Plot mean with error bars
+            # Plot relative uncertainty
             plt.plot(active_batches, \
                 uncertPlot[i_tally][i_score][i_filter][:])
             plt.xlabel(xLabel)

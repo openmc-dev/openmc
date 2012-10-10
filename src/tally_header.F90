f000cd7afb895179594ea83026b3aad939a3761f
Paul Romano <paul.k.romano@gmail.com>
2012-10-10 13:59:17 -0400

    Modified use of n_realizations. There is a now a global variable n_realizations for global tallies, and the TallyObject type also has a n_realizations attribute.

--- src/tally_header.F90
+++ src/tally_header.F90
@@ -105,6 +105,9 @@ module tally_header
      ! reset property - allows a tally to be reset after every batch
      logical :: reset = .false.
 
+     ! Number of realizations of tally random variables
+     integer :: n_realizations = 0
+
   end type TallyObject
 
 end module tally_header

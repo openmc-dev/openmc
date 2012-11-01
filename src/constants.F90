bc829372190180e1a50ed6b0d8d99e86bcf99618
Paul Romano <paul.k.romano@gmail.com>
2012-11-01 09:11:39 -0400

    Incremented REVISION_STATEPOINT in constants module.

--- src/constants.F90
+++ src/constants.F90
@@ -12,7 +12,7 @@ module constants
 
   ! Revision numbers for binary files
   integer, parameter :: REVISION_SOURCE     = 1
-  integer, parameter :: REVISION_STATEPOINT = 4
+  integer, parameter :: REVISION_STATEPOINT = 5
 
   ! ============================================================================
   ! ADJUSTABLE PARAMETERS 

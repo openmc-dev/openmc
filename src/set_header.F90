ea35be8ed0cbae26439657ea809e108e68d42d2a
Paul Romano <paul.k.romano@gmail.com>
2013-01-18 21:51:49 -0500

    Added character list and set.

--- src/set_header.F90
+++ src/set_header.F90
@@ -11,6 +11,7 @@ module set_header
 ! gigantic sets where performance is critical.
 !===============================================================================
 
+  use constants, only: MAX_WORD_LEN
   use list_header
 
   implicit none
@@ -31,6 +32,17 @@ module set_header
     procedure :: size => set_size_int
   end type SetInt
 
+  type :: SetChar
+    private
+    type(ListChar) :: elements
+  contains
+    procedure :: add => set_add_char
+    procedure :: contains => set_contains_char
+    procedure :: get_item => set_get_item_char
+    procedure :: remove => set_remove_char
+    procedure :: size => set_size_char
+  end type SetChar
+
 contains
 
 !===============================================================================
@@ -39,7 +51,7 @@ contains
 
   subroutine set_add_int(this, data)
     class(SetInt) :: this
-    integer :: data
+    integer       :: data
 
     if (.not. this % elements % contains(data)) then
       call this % elements % append(data)
@@ -47,24 +59,44 @@ contains
 
   end subroutine set_add_int
 
+  subroutine set_add_char(this, data)
+    class(SetChar) :: this
+    character(*)   :: data
+
+    if (.not. this % elements % contains(data)) then
+      call this % elements % append(data)
+    end if
+
+  end subroutine set_add_char
+
 !===============================================================================
 ! SET_CONTAINS determines if a specified item is in a set
 !===============================================================================
 
   function set_contains_int(this, data) result(in_set)
     class(SetInt) :: this
-    integer :: data
-    logical :: in_set
+    integer       :: data
+    logical       :: in_set
 
     in_set = this % elements % contains(data)
 
   end function set_contains_int
+
+  function set_contains_char(this, data) result(in_set)
+    class(SetChar) :: this
+    character(*)   :: data
+    logical        :: in_set
+
+    in_set = this % elements % contains(data)
+
+  end function set_contains_char
 
 !===============================================================================
 ! SET_GET_ITEM returns the i-th item in the set
 !===============================================================================
 
   function set_get_item_int(this, i_list) result(data)
+
     class(SetInt) :: this
     integer :: i_list
     integer :: data
@@ -73,29 +105,59 @@ contains
 
   end function set_get_item_int
 
+  function set_get_item_char(this, i_list) result(data)
+
+    class(SetChar)          :: this
+    integer                 :: i_list
+    character(MAX_WORD_LEN) :: data
+
+    data = this % elements % get_item(i_list)
+
+  end function set_get_item_char
+
 !===============================================================================
 ! SET_REMOVE removes the specified item from the set. If it is not in the set,
 ! no action is taken.
 !===============================================================================
 
   subroutine set_remove_int(this, data)
+
     class(SetInt) :: this
-    integer :: data
+    integer       :: data
 
     call this % elements % remove(data)
 
   end subroutine set_remove_int
+
+  subroutine set_remove_char(this, data)
+
+    class(SetChar) :: this
+    character(*)   :: data
+
+    call this % elements % remove(data)
+
+  end subroutine set_remove_char
 
 !===============================================================================
 ! SET_SIZE returns the number of elements in the set
 !===============================================================================
 
   function set_size_int(this) result(size)
+
     class(SetInt) :: this
-    integer :: size
+    integer       :: size
 
     size = this % elements % size()
 
   end function set_size_int
+
+  function set_size_char(this) result(size)
+
+    class(SetChar) :: this
+    integer        :: size
+
+    size = this % elements % size()
+
+  end function set_size_char
 
 end module set_header

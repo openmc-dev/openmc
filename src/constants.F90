5d5425b0500e33d399b15702e7ec1611980d49b5
Paul Romano <paul.k.romano@gmail.com>
2013-02-20 19:08:28 -0500

    Added combined estimate of k-effective to state point.

--- src/constants.F90
+++ src/constants.F90
@@ -12,7 +12,7 @@ module constants
 
   ! Revision numbers for binary files
   integer, parameter :: REVISION_SOURCE     = 1
-  integer, parameter :: REVISION_STATEPOINT = 7
+  integer, parameter :: REVISION_STATEPOINT = 8
 
   ! ============================================================================
   ! ADJUSTABLE PARAMETERS 

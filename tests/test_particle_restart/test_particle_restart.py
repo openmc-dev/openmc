fba2ee42c385c5842d48af7c7f2e516d7b2a7440
itayhorin <itayhorin@gmail.com>
2013-04-09 05:26:14 -0700

    particle restart test uses -s option, return codes not equal to 0

--- tests/test_particle_restart/test_particle_restart.py
+++ tests/test_particle_restart/test_particle_restart.py
@@ -13,13 +13,14 @@ def test_run():
     proc = Popen([pwd + '/../../src/openmc'], stderr=STDOUT, stdout=PIPE)
     returncode = proc.wait()
     print(proc.communicate()[0])
-    assert returncode == 0
+    assert returncode != 0
 
 def test_run_restart():
-    proc = Popen([pwd + '/../../src/openmc'], stderr=STDOUT, stdout=PIPE)
+    proc = Popen([pwd + '/../../src/openmc -s particle_0.binary'], 
+           stderr=STDOUT, stdout=PIPE)
     returncode = proc.wait()
     print(proc.communicate()[0])
-    assert returncode == 0
+    assert returncode != 0
 
 def test_created_restart():
     assert os.path.exists(pwd + '/particle_0.binary')

4842ef7ec9cee4a5574eeb38092ba59e532ee0a9
itayhorin <itayhorin@gmail.com>
2013-04-21 23:19:39 -0400

    Adjust physical constants to match CODATA 2010.

--- src/constants.F90
+++ src/constants.F90
@@ -48,13 +48,16 @@ module constants
   ! ============================================================================
   ! PHYSICAL CONSTANTS
 
+  ! Values here are from the Committee on Data for Science and Technology
+  ! (CODATA) 2010 recommendation (doi:10.1103/RevModPhys.84.1527).
+
   real(8), parameter ::            &
        PI           = 3.1415926535898_8, & ! pi
-       MASS_NEUTRON = 1.0086649156,      & ! mass of a neutron
-       MASS_PROTON  = 1.00727646677,     & ! mass of a proton
-       AMU          = 1.66053873e-27,    & ! 1 amu in kg
-       N_AVOGADRO   = 0.602214179,       & ! Avogadro's number in 10^24/mol
-       K_BOLTZMANN  = 8.617342e-11,      & ! Boltzmann constant in MeV/K
+       MASS_NEUTRON = 1.008664916,       & ! mass of a neutron in amu
+       MASS_PROTON  = 1.007276466812,    & ! mass of a proton in amu
+       AMU          = 1.660538921e-27,   & ! 1 amu in kg
+       N_AVOGADRO   = 0.602214129,       & ! Avogadro's number in 10^24/mol
+       K_BOLTZMANN  = 8.6173324e-11,     & ! Boltzmann constant in MeV/K
        INFINITY     = huge(0.0_8),       & ! positive infinity
        ZERO         = 0.0_8,             &
        ONE          = 1.0_8,             &

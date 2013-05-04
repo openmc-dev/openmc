57e3e3f2a34ed16ac89c72782c320ddff4bac4a5
Bryan Herman <bherman@mit.edu>
2013-05-04 13:25:45 -0700

    added some code to read in source info from HDF5 source file

--- src/initialize.F90
+++ src/initialize.F90
@@ -122,7 +122,8 @@ contains
       ! If this is a restart run, load the state point data and binary source
       ! file
 #ifdef HDF5
-      if (restart_run) call hdf5_load_state_point()
+!     if (restart_run) call hdf5_load_state_point()
+      if (restart_run) call load_state_point()
 #else
       if (restart_run) call load_state_point()
 #endif

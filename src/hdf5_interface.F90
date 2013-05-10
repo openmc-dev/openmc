cc590d78d3f46bc5e024464ef31d9cf01015b115
Bryan Herman <bherman@mit.edu>
2013-05-10 15:24:51 -0700

    implemented statepoint file writing for hdf5

--- src/hdf5_interface.F90
+++ src/hdf5_interface.F90
@@ -20,8 +20,8 @@ module hdf5_interface
 
   implicit none
 
-    integer(HID_T) :: hdf5_fh
-    integer(HID_T) :: temp_group 
+    integer(HID_T)  :: hdf5_fh
+    integer(HID_T)  :: temp_group 
 
 #ifdef HDF5
 
@@ -1315,19 +1315,6 @@ contains
 
   end subroutine hdf5_parallel_file_create
 
-!===============================================================================
-! HDF5_PARALLEL_FILE_CLOSE
-!===============================================================================
-
-  subroutine hdf5_parallel_file_close(file_id)
-
-    integer(HID_T) :: file_id
-
-    ! Close property list and the file
-    call h5fclose_f(file_id, hdf5_err)
-
-  end subroutine hdf5_parallel_file_close
-
 #endif
 
 !===============================================================================
@@ -1345,10 +1332,8 @@ contains
 
     ! Either create or open group
     if (status) then
-      print *,'OPENING GROUP'
       call h5gopen_f(hdf5_fh, trim(group), temp_group, hdf5_err)
     else
-      print *,'CREATING GROUP'
       call h5gcreate_f(hdf5_fh, trim(group), temp_group, hdf5_err)
     end if
 
--- src/output_interface.F90
+++ src/output_interface.F90
@@ -1,6 +1,8 @@
 module output_interface
 
   use constants
+  use global
+  use tally_header,  only: TallyResult
 
 #ifdef HDF5
   use hdf5_interface
@@ -39,9 +41,10 @@ contains
 #ifdef HDF5
 # ifdef MPI
     if (trim(fh_str) == 'state_point') then
-      if (master) call hdf5_file_create(trim(trim(filename) // '.h5'), &
-                       hdf5_fh)
+      if(master) call hdf5_file_create(trim(trim(filename) // '.h5'), &
+                      hdf5_fh)
    else
+     print *,'Opening source'
      call hdf5_parallel_file_create(trim(filename) // '.h5', &
           hdf5_fh)
     endif
@@ -83,9 +86,9 @@ contains
 #ifdef HDF5
 # ifdef MPI
     if (trim(fh_str) == 'state_point') then
-      if (master) call hdf5_file_close(hdf5_fh)
+     if(master) call hdf5_file_close(hdf5_fh)
    else
-     call hdf5_parallel_file_close(hdf5_fh)
+     call hdf5_file_close(hdf5_fh)
     endif
 # else
      call hdf5_file_close(hdf5_fh)
@@ -265,5 +268,134 @@ contains
 #endif
 
   end subroutine write_string
+
+!===============================================================================
+! WRITE_GLOBAL_TALLIES
+!===============================================================================
+
+  subroutine write_tally_result(buffer, name, group, length)
+
+    character(*),      intent(in), optional :: group
+    character(*),      intent(in)           :: name
+    integer,           intent(in)           :: length
+    type(TallyResult), intent(in)           :: buffer(length)
+
+    integer          :: hdf5_err
+    integer(HSIZE_T) :: dims(1)
+    integer(HID_T)   :: dspace
+    integer(HID_T)   :: dset
+    type(c_ptr)      :: f_ptr
+
+#ifdef HDF5
+
+    ! Open up sub-group if present
+    if (present(group)) then
+      call hdf5_open_group(group)
+    else
+      temp_group = hdf5_fh
+    end if
+
+    ! Set overall size of vector to write
+    dims(1) = length
+
+    ! Create up a dataspace for size
+    call h5screate_simple_f(1, dims, dspace, hdf5_err)
+
+    ! Create the dataset
+    call h5dcreate_f(temp_group, name, hdf5_tallyresult_t, dspace, dset, &
+         hdf5_err)
+
+    ! Set pointer to first value and write
+    f_ptr = c_loc(buffer(1))
+    call h5dwrite_f(dset, hdf5_tallyresult_t, f_ptr, hdf5_err)
+
+    ! Close ids
+    call h5dclose_f(dset, hdf5_err)
+    call h5sclose_f(dspace, hdf5_err)
+    if (present(group)) then
+      call hdf5_close_group()
+    end if
+
+#elif MPI
+
+#else
+
+#endif 
+   
+  end subroutine write_tally_result
+
+!===============================================================================
+! WRITE_SOURCE_BANK
+!===============================================================================
+
+  subroutine write_source_bank()
+
+#ifdef HDF5
+    integer(HSIZE_T) :: dims(1)
+    integer(HID_T)   :: dset
+    integer(HID_T)   :: dspace
+    integer(HID_T)   :: memspace
+    integer(HID_T)   :: plist
+    integer          :: rank
+    integer(8)       :: offset(1)
+    type(c_ptr)      :: f_ptr
+#endif
+
+#ifdef HDF5
+# ifdef MPI
+
+    ! Set size of total dataspace for all procs and rank
+    dims(1) = n_particles
+    rank = 1
+
+    ! Create that dataspace
+    call h5screate_simple_f(rank, dims, dspace, hdf5_err)
+
+    ! Create the dataset for that dataspace
+    call h5dcreate_f(hdf5_fh, "source", hdf5_bank_t, dspace, dset, hdf5_err)
+
+    ! Close the dataspace
+    call h5sclose_f(dspace, hdf5_err)
+
+    ! Create another data space but for each proc individually
+    dims(1) = work
+    call h5screate_simple_f(rank, dims, memspace, hdf5_err)
+
+    ! Get the individual local proc dataspace
+    call h5dget_space_f(dset, dspace, hdf5_err)
+
+    ! Select hyperslab for this dataspace
+    offset(1) = bank_first - 1_8
+    call h5sselect_hyperslab_f(dspace, H5S_SELECT_SET_F, offset, dims, hdf5_err)
+
+    ! Set up the property list for parallel writing
+    call h5pcreate_f(H5P_DATASET_XFER_F, plist, hdf5_err)
+    call h5pset_dxpl_mpio_f(plist, H5FD_MPIO_COLLECTIVE_F, hdf5_err)
+
+    ! Set up pointer to data
+    f_ptr = c_loc(source_bank(1))
+
+    ! Write data to file in parallel
+    call h5dwrite_f(dset, hdf5_bank_t, f_ptr, hdf5_err, &
+         file_space_id = dspace, mem_space_id = memspace, &
+         xfer_prp = plist)
+
+    ! Close all ids
+    call h5sclose_f(dspace, hdf5_err)
+    call h5sclose_f(memspace, hdf5_err)
+    call h5dclose_f(dset, hdf5_err)
+    call h5pclose_f(plist, hdf5_err)
+
+# else
+
+# endif 
+
+#elif MPI
+
+#else
+
+#endif
+
+  end subroutine write_source_bank
 
 end module output_interface

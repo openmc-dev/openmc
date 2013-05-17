28f2da847b339cf1f0234341fe6b224e5d89dbbc
Bryan Herman <bherman@mit.edu>
2013-05-17 11:02:09 -0400

    added a lot of comments and intents

--- src/mpi_interface.F90
+++ src/mpi_interface.F90
@@ -6,21 +6,22 @@ module mpi_interface
 #ifdef MPI
   use mpi
 
-
   implicit none
 
-! integer :: mpi_fh
+  integer :: mpi_fh ! MPI file handle
+
+  ! TODO: eventually move mpi_err to here and use this for all other MPI calls
 
 contains
 
 !===============================================================================
-! MPI_CREATE_FILE
+! MPI_CREATE_FILE creates a file using MPI file I/O
 !===============================================================================
 
   subroutine mpi_create_file(filename, fh)
 
-    character(*) :: filename
-    integer, intent(in)     :: fh
+    character(*), intent(in)    :: filename ! name of file to create
+    integer,      intent(inout) :: fh       ! file handle
 
     ! Create the file
     call MPI_FILE_OPEN(MPI_COMM_WORLD, filename, MPI_MODE_CREATE + &
@@ -29,14 +30,14 @@ contains
   end subroutine mpi_create_file
 
 !===============================================================================
-! MPI_OPEN_FILE
+! MPI_OPEN_FILE opens a file using MPI file I/O
 !===============================================================================
 
   subroutine mpi_open_file(filename, fh, mode)
 
-    character(*), intent(in) :: filename
-    character(*), intent(in) :: mode
-    integer,      intent(in) :: fh
+    character(*), intent(in)    :: filename ! name of file to open
+    character(*), intent(in)    :: mode     ! open 'r' read, 'rw' read/write
+    integer,      intent(inout) :: fh       ! file handle
 
     integer :: open_mode
 
@@ -53,25 +54,25 @@ contains
   end subroutine mpi_open_file
 
 !===============================================================================
-! MPI_CLOSE_FILE
+! MPI_CLOSE_FILE closes a file using MPI file I/O
 !===============================================================================
 
   subroutine mpi_close_file(fh)
 
-    integer, intent(in) :: fh
+    integer, intent(inout) :: fh ! file handle
 
     call MPI_FILE_CLOSE(fh, mpi_err)
 
   end subroutine mpi_close_file
 
 !===============================================================================
-! MPI_WRITE_INTEGER
+! MPI_WRITE_INTEGER writes integer scalar data using MPI File I/O
 !===============================================================================
 
   subroutine mpi_write_integer(fh, buffer)
 
-    integer, intent(in) :: fh
-    integer, intent(in) :: buffer
+    integer, intent(in) :: fh     ! file handle
+    integer, intent(in) :: buffer ! data to write
 
     call MPI_FILE_WRITE(fh, buffer, 1, MPI_INTEGER, &
          MPI_STATUS_IGNORE, mpi_err) 
@@ -79,14 +80,14 @@ contains
   end subroutine mpi_write_integer
 
 !===============================================================================
-! MPI_WRITE_INTEGER_1DARRAY
+! MPI_WRITE_INTEGER_1DARRAY writes integer 1-D array data using MPI File I/O
 !===============================================================================
 
   subroutine mpi_write_integer_1Darray(fh, buffer, length)
 
-    integer, intent(in) :: fh
-    integer, intent(in) :: length
-    integer, intent(in) :: buffer(:)
+    integer, intent(in) :: fh        ! file handle
+    integer, intent(in) :: length    ! length of array
+    integer, intent(in) :: buffer(:) ! data to write
 
     call MPI_FILE_WRITE(fh, buffer, length, MPI_INTEGER, &
          MPI_STATUS_IGNORE, mpi_err)
@@ -94,13 +95,13 @@ contains
   end subroutine mpi_write_integer_1Darray
 
 !===============================================================================
-! MPI_WRITE_LONG
+! MPI_WRITE_LONG writes long integer scalar data using MPI file I/O
 !===============================================================================
 
   subroutine mpi_write_long(fh, buffer)
 
-    integer,    intent(in) :: fh
-    integer(8), intent(in) :: buffer
+    integer,    intent(in) :: fh     ! file handle
+    integer(8), intent(in) :: buffer ! data to write
 
     call MPI_FILE_WRITE(fh, buffer, 1, MPI_INTEGER8, &
          MPI_STATUS_IGNORE, mpi_err)
@@ -108,13 +109,13 @@ contains
   end subroutine mpi_write_long
 
 !===============================================================================
-! MPI_WRITE_DOUBLE
+! MPI_WRITE_DOUBLE writes double precision scalar data using MPI file I/O
 !===============================================================================
 
   subroutine mpi_write_double(fh, buffer)
 
-    integer, intent(in) :: fh
-    real(8), intent(in) :: buffer
+    integer, intent(in) :: fh     ! file handle
+    real(8), intent(in) :: buffer ! data to write
 
     call MPI_FILE_WRITE(fh, buffer, 1, MPI_REAL8, &
          MPI_STATUS_IGNORE, mpi_err)
@@ -122,14 +123,14 @@ contains
   end subroutine mpi_write_double
 
 !===============================================================================
-! MPI_WRITE_DOUBLE_1DARRAY
+! MPI_WRITE_DOUBLE_1DARRAY writes double precision 1-D array using MPI file I/O
 !===============================================================================
 
   subroutine mpi_write_double_1Darray(fh, buffer, length)
 
-    integer, intent(in) :: fh
-    integer, intent(in) :: length
-    real(8), intent(in) :: buffer(:)
+    integer, intent(in) :: fh        ! file handle
+    integer, intent(in) :: length    ! length of array
+    real(8), intent(in) :: buffer(:) ! data to write
 
     call MPI_FILE_WRITE(fh, buffer, length, MPI_REAL8, &
          MPI_STATUS_IGNORE, mpi_err)
@@ -137,14 +138,14 @@ contains
   end subroutine mpi_write_double_1Darray
 
 !===============================================================================
-! MPI_WRITE_STRING
+! MPI_WRITE_STRING writes string data using MPI file I/O
 !===============================================================================
 
   subroutine mpi_write_string(fh, buffer, length)
 
-    character(*), intent(in) :: buffer
-    integer,      intent(in) :: fh
-    integer,      intent(in) :: length
+    character(*), intent(in) :: buffer ! data to write
+    integer,      intent(in) :: fh     ! file handle
+    integer,      intent(in) :: length ! length of data
 
     call MPI_FILE_WRITE(fh, buffer, length, MPI_CHARACTER, &
          MPI_STATUS_IGNORE, mpi_err)
@@ -152,13 +153,13 @@ contains
   end subroutine mpi_write_string
 
 !===============================================================================
-! MPI_READ_INTEGER
+! MPI_READ_INTEGER reads integer scalar data using MPI file I/O
 !===============================================================================
 
   subroutine mpi_read_integer(fh, buffer)
 
-    integer, intent(in) :: fh
-    integer, intent(inout) :: buffer
+    integer, intent(in)    :: fh     ! file handle
+    integer, intent(inout) :: buffer ! read data to here
 
     call MPI_FILE_READ(fh, buffer, 1, MPI_INTEGER, &
          MPI_STATUS_IGNORE, mpi_err) 
@@ -166,14 +167,14 @@ contains
   end subroutine mpi_read_integer
 
 !===============================================================================
-! MPI_READ_INTEGER_1DARRAY
+! MPI_READ_INTEGER_1DARRAY reads integer 1-D array using MPI file I/O
 !===============================================================================
 
   subroutine mpi_read_integer_1Darray(fh, buffer, length)
 
-    integer, intent(in) :: fh
-    integer, intent(in) :: length
-    integer, intent(inout) :: buffer(:)
+    integer, intent(in)    :: fh        ! file handle
+    integer, intent(in)    :: length    ! length of array
+    integer, intent(inout) :: buffer(:) ! read data to here
 
     call MPI_FILE_READ(fh, buffer, length, MPI_INTEGER, &
          MPI_STATUS_IGNORE, mpi_err)
@@ -181,13 +182,13 @@ contains
   end subroutine mpi_read_integer_1Darray
 
 !===============================================================================
-! MPI_READ_LONG
+! MPI_READ_LONG reads long integer scalar data using MPI file I/O
 !===============================================================================
 
   subroutine mpi_read_long(fh, buffer)
 
-    integer,    intent(in) :: fh
-    integer(8), intent(inout) :: buffer
+    integer,    intent(in)    :: fh     ! file handle
+    integer(8), intent(inout) :: buffer ! read data to here
 
     call MPI_FILE_READ(fh, buffer, 1, MPI_INTEGER8, &
          MPI_STATUS_IGNORE, mpi_err)
@@ -195,13 +196,13 @@ contains
   end subroutine mpi_read_long
 
 !===============================================================================
-! MPI_READ_DOUBLE
+! MPI_READ_DOUBLE reads double precision scalar data using MPI file I/O
 !===============================================================================
 
   subroutine mpi_read_double(fh, buffer)
 
-    integer, intent(in) :: fh
-    real(8), intent(inout) :: buffer
+    integer, intent(in)    :: fh     ! file handle
+    real(8), intent(inout) :: buffer ! read data to here
 
     call MPI_FILE_READ(fh, buffer, 1, MPI_REAL8, &
          MPI_STATUS_IGNORE, mpi_err)
@@ -209,14 +210,14 @@ contains
   end subroutine mpi_read_double
 
 !===============================================================================
-! MPI_READ_DOUBLE_1DARRAY
+! MPI_READ_DOUBLE_1DARRAY reads double precision 1-D array using MPI file I/O
 !===============================================================================
 
   subroutine mpi_read_double_1Darray(fh, buffer, length)
 
-    integer, intent(in) :: fh
-    integer, intent(in) :: length
-    real(8), intent(inout) :: buffer(:)
+    integer, intent(in)    :: fh        ! file handle
+    integer, intent(in)    :: length    ! length of array
+    real(8), intent(inout) :: buffer(:) ! read data to here
 
     call MPI_FILE_READ(fh, buffer, length, MPI_REAL8, &
          MPI_STATUS_IGNORE, mpi_err)
@@ -224,14 +225,14 @@ contains
   end subroutine mpi_read_double_1Darray
 
 !===============================================================================
-! MPI_READ_STRING
+! MPI_READ_STRING reads string data using MPI file I/O
 !===============================================================================
 
   subroutine mpi_read_string(fh, buffer, length)
 
-    character(*), intent(inout) :: buffer
-    integer,      intent(in) :: fh
-    integer,      intent(in) :: length
+    character(*), intent(inout) :: buffer ! read data to here
+    integer,      intent(in)    :: fh     ! file handle
+    integer,      intent(in)    :: length ! length of string
 
     call MPI_FILE_READ(fh, buffer, length, MPI_CHARACTER, &
          MPI_STATUS_IGNORE, mpi_err)

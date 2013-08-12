2400436239cc58c6ccf0b036775910969bb6223b
itayhorin <itayhorin@gmail.com>
2013-08-12 17:44:29 -0400

    updated survival_biasing test

--- tests/test_survival_biasing/test_survival_biasing.py
+++ tests/test_survival_biasing/test_survival_biasing.py
@@ -2,32 +2,32 @@
 
 import os
 from subprocess import Popen, STDOUT, PIPE
+import filecmp
 
 pwd = os.path.dirname(__file__)
 
 def setup(): 
-    # Change directory
     os.putenv('PWD', pwd)
     os.chdir(pwd)
 
 def test_run():
-    # Run the simulation
     proc = Popen([pwd + '/../../src/openmc'], stderr=STDOUT, stdout=PIPE)
     returncode = proc.wait()
-
-    # Display stdout
     print(proc.communicate()[0])
-
-    # Make sure simulation ran to completion
     assert returncode == 0
 
 def test_created_statepoint():
-    # Make sure that statepoint file was created
     assert os.path.exists(pwd + '/statepoint.10.binary')
 
+def test_results():
+    os.system('python results.py')
+    compare = filecmp.cmp('results_test.dat', 'results_true.dat')
+    if not compare:
+      os.system('cp results_test.dat results_error.dat')
+    assert compare
+
 def teardown():
-    # Remove output files
-    output = [pwd + '/statepoint.10.binary']
+    output = [pwd + '/statepoint.10.binary', pwd + '/results_test.dat']
     for f in output:
         if os.path.exists(f):
             os.remove(f)

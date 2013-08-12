e903b1906d728b2d10718e06460a9021b814cac5
itayhorin <itayhorin@gmail.com>
2013-08-12 16:21:54 -0400

    updated score_flux test

--- tests/test_score_nuscatter/test_score_nuscatter.py
+++ tests/test_score_nuscatter/test_score_nuscatter.py
@@ -2,6 +2,7 @@
 
 import os
 from subprocess import Popen, STDOUT, PIPE
+import filecmp
 
 pwd = os.path.dirname(__file__)
 
@@ -21,8 +22,16 @@ def test_statepoint_exists():
 def test_output_exists():
     assert os.path.exists(pwd + '/tallies.out')
 
+def test_results():
+    os.system('python results.py')
+    compare = filecmp.cmp('results_test.dat', 'results_true.dat')
+    if not compare:
+      os.system('cp results_test.dat results_error.dat')
+    assert compare
+
 def teardown():
-    output = [pwd + '/statepoint.10.binary', pwd + '/tallies.out']
+    output = [pwd + '/statepoint.10.binary', pwd + '/tallies.out', 
+              pwd + '/results_test.dat']
     for f in output:
         if os.path.exists(f):
             os.remove(f)

b4daaca3cee60f9a44f69cd66140ae99bc22143a
itayhorin <itayhorin@gmail.com>
2013-08-12 13:17:03 -0400

    added new results test for basic test

---
+++ tests/update_results.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import os
+import sys
+from subprocess import Popen, STDOUT, PIPE
+
+# Loop around directories
+for root, dirs, files in os.walk('.'):
+
+  # Don't continue if not prefixed by test
+  if root[2:6] != 'test':
+    continue
+ 
+  # Go into that directory
+  os.chdir(root)
+  pwd = os.path.abspath(os.path.dirname('settings.xml'))
+  os.putenv('PWD', pwd)
+
+  # Run openmc
+  proc = Popen(['../../src/openmc'])
+  returncode = proc.wait()
+
+  # Process results
+  os.system('python results.py') 
+  os.system('mv results_test.dat results_true.dat')
+  os.system('rm *.binary')
+
+  # Go back a directory
+  os.chdir('..')
+
+  exit()

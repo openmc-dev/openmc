a522e0d040bb7ad93522725616141d61021e7570
Bryan Herman <bherman@mit.edu>
2013-09-02 10:45:11 -0400

    removed setup script and added plugins to run_tests script

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -6,6 +6,7 @@ import sys
 import nose
 import glob
 from subprocess import call
+from nose_mpi import NoseMPI
 
 # set mpiexec path
 mpiexec = '/opt/mpich/3.0.4-gnu/bin/mpiexec'
@@ -108,7 +109,7 @@ if run_gfortran:
     os.rename(pwd + '/../src/openmc-gfortran', pwd + '/../src/openmc')
     result_gfortran = nose.run(argv=[
         'run_tests.py', '-v', '--exclude', 'test_compile',
-        '--exclude', 'cmfd'])
+        '--exclude', 'cmfd'], addplugins=[NoseMPI()])
     os.rename(pwd + '/../src/openmc', pwd + '/../src/openmc-gfortran')
     if not result_gfortran:
         sys.stdout.write('\nDid not pass gfortran tests\n')
@@ -122,7 +123,7 @@ if run_gfortran_dbg:
     os.rename(pwd + '/../src/openmc-gfortran-dbg', pwd + '/../src/openmc')
     result_gfortran_dbg = nose.run(argv=[
         'run_tests.py', '-v', '--exclude', 'test_compile',
-        '--exclude', 'cmfd'])
+        '--exclude', 'cmfd'], addplugins=[NoseMPI()])
     os.rename(pwd + '/../src/openmc', pwd + '/../src/openmc-gfortran-dbg')
     if not result_gfortran_dbg:
         sys.stdout.write('\nDid not pass gfortran-dbg tests\n')
@@ -136,7 +137,7 @@ if run_gfortran_opt:
     os.rename(pwd + '/../src/openmc-gfortran-opt', pwd + '/../src/openmc')
     result_gfortran_opt = nose.run(argv=[
         'run_tests.py', '-v', '--exclude', 'test_compile',
-        '--exclude', 'cmfd'])
+        '--exclude', 'cmfd'], addplugins=[NoseMPI()])
     os.rename(pwd + '/../src/openmc', pwd + '/../src/openmc-gfortran-opt')
     if not result_gfortran_opt:
         sys.stdout.write('\nDid not pass gfortran-opt tests\n')
@@ -150,7 +151,7 @@ if run_gfortran_hdf5:
     os.rename(pwd + '/../src/openmc-gfortran-hdf5', pwd + '/../src/openmc')
     result_gfortran_hdf5 = nose.run(argv=[
         'run_tests.py', '-v', '--exclude', 'test_compile',
-        '--exclude', 'cmfd'])
+        '--exclude', 'cmfd'], addplugins=[NoseMPI()])
     os.rename(pwd + '/../src/openmc', pwd + '/../src/openmc-gfortran-hdf5')
     if not result_gfortran_hdf5:
         sys.stdout.write('\nDid not pass gfortran-hdf5 tests\n')
@@ -164,7 +165,8 @@ if run_gfortran_mpi:
     os.rename(pwd + '/../src/openmc-gfortran-mpi', pwd + '/../src/openmc')
     result_gfortran_mpi = nose.run(argv=[
         'run_tests.py', '-v', '--exclude', 'test_compile',
-        '--exclude', 'cmfd', '--mpi-np', '3', '--mpi-exec', mpiexec])
+        '--exclude', 'cmfd', '--mpi-np', '3', '--mpi-exec', mpiexec],
+        addplugins=[NoseMPI()])
     os.rename(pwd + '/../src/openmc', pwd + '/../src/openmc-gfortran-mpi')
     if not result_gfortran_mpi:
         sys.stdout.write('\nDid not pass gfortran-mpi tests\n')
@@ -179,7 +181,8 @@ if run_gfortran_phdf5:
     os.rename(pwd + '/../src/openmc-gfortran-phdf5', pwd + '/../src/openmc')
     result_gfortran_phdf5 = nose.run(argv=[
         'run_tests.py', '-v', '--exclude', 'test_compile', '--exclude',
-        'cmfd', '--mpi-np', '3', '--mpi-exec', mpiexec])
+        'cmfd', '--mpi-np', '3', '--mpi-exec', mpiexec],
+        addplugins=[NoseMPI()])
     os.rename(pwd + '/../src/openmc', pwd + '/../src/openmc-gfortran-phdf5')
     if not result_gfortran_phdf5:
         sys.stdout.write('\nDid not pass gfortran-phdf5 tests\n')
@@ -193,7 +196,7 @@ if run_gfortran_petsc:
     os.rename(pwd + '/../src/openmc-gfortran-petsc', pwd + '/../src/openmc')
     result_gfortran_petsc = nose.run(argv=[
         'run_tests.py', '-v', '--exclude', 'test_compile',
-        '--mpi-np', '3', '--mpi-exec', mpiexec])
+        '--mpi-np', '3', '--mpi-exec', mpiexec], addplugins=[NoseMPI()])
     os.rename(pwd + '/../src/openmc', pwd + '/../src/openmc-gfortran-petsc')
     if not result_gfortran_petsc:
         sys.stdout.write('\nDid not pass gfortran-petsc tests\n')
@@ -208,7 +211,7 @@ if run_gfortran_phdf5_petsc:
               pwd + '/../src/openmc')
     result_gfortran_phdf5_petsc = nose.run(argv=[
         'run_tests.py', '-v', '--exclude', 'test_compile',
-        '--mpi-np', '3', '--mpi-exec', mpiexec])
+        '--mpi-np', '3', '--mpi-exec', mpiexec], addplugins=[NoseMPI()])
     os.rename(pwd + '/../src/openmc',
               pwd + '/../src/openmc-gfortran-phdf5-petsc')
     if not result_gfortran_phdf5_petsc:
@@ -224,7 +227,7 @@ if run_gfortran_phdf5_petsc_opt:
               pwd + '/../src/openmc')
     result_gfortran_phdf5_petsc_opt = nose.run(argv=[
         'run_tests.py', '-v', '--exclude', 'test_compile',
-        '--mpi-np', '3', '--mpi-exec', mpiexec])
+        '--mpi-np', '3', '--mpi-exec', mpiexec], addplugins=[NoseMPI()])
     os.rename(pwd + '/../src/openmc',
               pwd + '/../src/openmc-gfortran-phdf5-petsc-opt')
     if not result_gfortran_phdf5_petsc_opt:

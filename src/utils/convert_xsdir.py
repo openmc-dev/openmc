503412142413d1f147d6b16f988904257cb61c2f
itayhorin <itayhorin@gmail.com>
2013-12-19 11:48:47 -0500

    Fix bug in convert_xsdir.py.

--- src/utils/convert_xsdir.py
+++ src/utils/convert_xsdir.py
@@ -71,7 +71,7 @@ class Xsdir(object):
             # Handle continuation lines
             while words[-1] == '+':
                 extraWords = self.f.readline().split()
-                words = words + extraWords
+                words = words[:-1] + extraWords
             assert len(words) >= 7
 
             # Create XsdirTable object and add to line

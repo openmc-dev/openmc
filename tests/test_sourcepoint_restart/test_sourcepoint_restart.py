411aea322dddb42ca66975e600d7a7de6490e971
Bryan Herman <bherman@mit.edu>
2014-02-21 14:14:56 -0500

    restructured sourcepoint restart test

--- tests/test_sourcepoint_restart/test_sourcepoint_restart.py
+++ tests/test_sourcepoint_restart/test_sourcepoint_restart.py
@@ -24,49 +24,49 @@ def test_run():
     assert returncode == 0
 
 def test_created_statepoint():
-    statepoint = glob.glob(pwd + '/statepoint.7.*')
-    assert len(statepoint) == 1
+    statepoint = glob.glob(pwd + '/statepoint.*')
+    assert len(statepoint) == 2
     assert statepoint[0].endswith('binary') or statepoint[0].endswith('h5')
     sourcepoint = glob.glob(pwd + '/source.7.*')
     assert len(sourcepoint) == 1
     assert sourcepoint[0].endswith('binary') or sourcepoint[0].endswith('h5')
 
 def test_results():
-    statepoint = glob.glob(pwd + '/statepoint.7.*')
+    statepoint = glob.glob(pwd + '/statepoint.10.*')
     call(['python', 'results.py', statepoint[0]])
     compare = filecmp.cmp('results_test.dat', 'results_true.dat')
     if not compare:
       os.rename('results_test.dat', 'results_error.dat')
     assert compare
+    os.remove(statepoint[0])
 
 def test_restart_form1():
     statepoint = glob.glob(pwd + '/statepoint.7.*')
+    sourcepoint = glob.glob(pwd + '/source.7.*')
     openmc_path = pwd + '/../../src/openmc'
     if int(NoseMPI.mpi_np) > 0:
         proc = Popen([NoseMPI.mpi_exec, '-np', NoseMPI.mpi_np, openmc_path, 
-                      '-r', statepoint[0]],
+                      '-r', statepoint[0], sourcepoint[0]],
                stderr=STDOUT, stdout=PIPE)
     else:
-        proc = Popen([openmc_path, '-r', statepoint[0]], stderr=STDOUT, stdout=PIPE)
+        proc = Popen([openmc_path, '-r', statepoint[0], sourcepoint[0]], stderr=STDOUT, stdout=PIPE)
     print(proc.communicate()[0])
     returncode = proc.returncode
     assert returncode == 0
 
 def test_created_statepoint_form1():
-    statepoint = glob.glob(pwd + '/statepoint.7.*')
+    statepoint = glob.glob(pwd + '/statepoint.10.*')
     assert len(statepoint) == 1
     assert statepoint[0].endswith('binary') or statepoint[0].endswith('h5')
-    sourcepoint = glob.glob(pwd + '/source.7.*')
-    assert len(sourcepoint) == 1
-    assert sourcepoint[0].endswith('binary') or sourcepoint[0].endswith('h5')
 
 def test_results_form1():
-    statepoint = glob.glob(pwd + '/statepoint.7.*')
+    statepoint = glob.glob(pwd + '/statepoint.10.*')
     call(['python', 'results.py', statepoint[0]])
     compare = filecmp.cmp('results_test.dat', 'results_true.dat')
     if not compare:
       os.rename('results_test.dat', 'results_error.dat')
     assert compare
+    os.remove(statepoint[0])
 
 def test_restart_form2():
     statepoint = glob.glob(pwd + '/statepoint.7.*')
@@ -74,50 +74,46 @@ def test_restart_form2():
     openmc_path = pwd + '/../../src/openmc'
     if int(NoseMPI.mpi_np) > 0:
         proc = Popen([NoseMPI.mpi_exec, '-np', NoseMPI.mpi_np, openmc_path, 
-                      '--restart', statepoint[0], source[0]],
+                      '--restart', statepoint[0], sourcepoint[0]],
                stderr=STDOUT, stdout=PIPE)
     else:
-        proc = Popen([openmc_path, '--restart', statepoint[0]], 
+        proc = Popen([openmc_path, '--restart', statepoint[0], sourcepoint[0]],
                      stderr=STDOUT, stdout=PIPE)
     print(proc.communicate()[0])
     returncode = proc.returncode
     assert returncode == 0
 
 def test_created_statepoint_form2():
-    statepoint = glob.glob(pwd + '/statepoint.7.*')
+    statepoint = glob.glob(pwd + '/statepoint.10.*')
     assert len(statepoint) == 1
     assert statepoint[0].endswith('binary') or statepoint[0].endswith('h5')
-    sourcepoint = glob.glob(pwd + '/source.7.*')
-    assert len(sourcepoint) == 1
-    assert sourcepoint[0].endswith('binary') or sourcepoint[0].endswith('h5')
 
 def test_results_form2():
-    statepoint = glob.glob(pwd + '/statepoint.7.*')
+    statepoint = glob.glob(pwd + '/statepoint.10.*')
     call(['python', 'results.py', statepoint[0]])
     compare = filecmp.cmp('results_test.dat', 'results_true.dat')
     if not compare:
       os.rename('results_test.dat', 'results_error.dat')
     assert compare
+    os.remove(statepoint[0])
 
 def test_restart_serial():
     statepoint = glob.glob(pwd + '/statepoint.7.*')
+    sourcepoint = glob.glob(pwd + '/source.7.*')
     openmc_path = pwd + '/../../src/openmc'
-    proc = Popen([openmc_path, '--restart', statepoint[0]], 
+    proc = Popen([openmc_path, '--restart', statepoint[0], sourcepoint[0]], 
                  stderr=STDOUT, stdout=PIPE)
     print(proc.communicate()[0])
     returncode = proc.returncode
     assert returncode == 0
 
 def test_created_statepoint_serial():
-    statepoint = glob.glob(pwd + '/statepoint.7.*')
+    statepoint = glob.glob(pwd + '/statepoint.10.*')
     assert len(statepoint) == 1
     assert statepoint[0].endswith('binary') or statepoint[0].endswith('h5')
-    sourcepoint = glob.glob(pwd + '/source.7.*')
-    assert len(sourcepoint) == 1
-    assert sourcepoint[0].endswith('binary') or sourcepoint[0].endswith('h5')
 
 def test_results_serial():
-    statepoint = glob.glob(pwd + '/statepoint.7.*')
+    statepoint = glob.glob(pwd + '/statepoint.10.*')
     call(['python', 'results.py', statepoint[0]])
     compare = filecmp.cmp('results_test.dat', 'results_true.dat')
     if not compare:
@@ -125,8 +121,8 @@ def test_results_serial():
     assert compare
 
 def teardown():
-    output = glob.glob(pwd + '/statepoint.7.*')
-    output += glob.glob(pwd + '/source.7.*')
+    output = glob.glob(pwd + '/statepoint.*')
+    output += glob.glob(pwd + '/source.*')
     output.append(pwd + '/results_test.dat')
     for f in output:
         if os.path.exists(f):

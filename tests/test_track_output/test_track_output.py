8f139c626fa8f61b2396cf5e90818393f30f3084
itayhorin <itayhorin@gmail.com>
2014-04-08 17:12:23 -0400

    changed how test_track_output works

--- tests/test_track_output/test_track_output.py
+++ tests/test_track_output/test_track_output.py
@@ -32,22 +32,10 @@ def test_created_outputs():
         'Track files not a binary or hdf5 file'
 
 def test_outputs():
-    # If vtk python module is not available, we can't run track.py so skip this
-    # test
-    try:
-        import vtk
-    except ImportError:
-        print('----------------Skipping test-------------')
-        return
-
-    call(['../../src/utils/track.py', '-o', 'poly'] +
-         glob.glob(''.join((cwd, '/track*'))))
-    poly = ''.join((cwd, '/poly.pvtp'))
-    assert os.path.isfile(poly), 'poly.pvtp file not found.'
-    metric = ''.join((cwd, '/true_poly.pvtp'))
-    compare = filecmp.cmp(poly, metric)
+    call(['python', 'results.py'])
+    compare = filecmp.cmp('results_test.dat', 'results_true.dat')
     if not compare:
-        os.rename('poly.pvtp', 'error_poly.pvtp')
+        os.rename('results_test.dat', 'results_error.dat')
     assert compare, 'Results to not agree'
 
 def teardown():

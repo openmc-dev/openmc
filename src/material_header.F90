fc37454f94f5ece7842410650c8b3c09af5a93b7
Derek Lax <dereklax@mit.edu>
2014-05-27 11:48:38 -0400

    Added Composition and Material types.

--- src/material_header.F90
+++ src/material_header.F90
@@ -7,11 +7,11 @@ module material_header
 !===============================================================================
 
   type Material
-    integer              :: id              ! unique identifier
-    integer              :: n_nuclides      ! number of nuclides
-    integer, allocatable :: nuclide(:)      ! index in nuclides array
-    real(8)              :: density         ! total atom density in atom/b-cm
-    real(8), allocatable :: atom_density(:) ! nuclide atom density in atom/b-cm
+    integer                        :: id         ! unique identifier
+    integer                        :: n_nuclides ! number of nuclides
+    integer, allocatable           :: nuclide(:) ! index in nuclides array
+    type(Density)                  :: density    ! atom densities in atom/b-cm
+    type(Composition), allocatable :: comp(:)    ! atom fractions via compositions
 
     ! S(a,b) data references
     integer              :: n_sab = 0         ! number of S(a,b) tables
@@ -21,6 +21,43 @@ module material_header
     ! Temporary names read during initialization
     character(12), allocatable :: names(:)     ! isotope names
     character(12), allocatable :: sab_names(:) ! name of S(a,b) table
+
+  contains
+    procedure :: density => get_density
   end type Material
 
+
+!===============================================================================
+! COMPOSITION describes a material by its constituent nuclide atom fractions
+!===============================================================================
+
+  type Composition
+    real(8),allocatable  :: atom_densities(:)
+
+  end type Composition
+
+
+!===============================================================================
+! DENSITY describes a material by its constituent nuclide densities
+!===============================================================================
+
+  type Density
+    real(8), allocatable :: density(:)       ! nuclide densities in X/Y
+    integer              :: num              ! size of density
+  end type Density
+
+contains
+
+
+  function get_density(this, i, j) result(density)
+
+    class(Material)     :: this
+    integer, intent(in) :: i                ! i_th nuclide in the composition
+    integer, intent(in) :: j                ! j_th composition
+    real(8)             :: density          ! density to be returned
+
+    density = this & comp(j) % atom_densities(i)
+
+  end function get_density
+    
 end module material_header

c8f3fdf415b0e0a59c35c185ef982049894c7182
itayhorin <itayhorin@gmail.com>
2014-09-02 23:51:29 -0400

    Don't read tallies.xml when OpenMC is run in plotting mode. Closes #310 on github.

--- src/input_xml.F90
+++ src/input_xml.F90
@@ -33,10 +33,10 @@ contains
   subroutine read_input_xml()
 
     call read_settings_xml()
-    if ((run_mode /= MODE_PLOTTING)) call read_cross_sections_xml()
+    if (run_mode /= MODE_PLOTTING) call read_cross_sections_xml()
     call read_geometry_xml()
     call read_materials_xml()
-    call read_tallies_xml()
+    if (run_mode /= MODE_PLOTTING) call read_tallies_xml()
     if (cmfd_run) call configure_cmfd()
 
   end subroutine read_input_xml

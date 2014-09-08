d69b87d43d59d14c08467d56c2161feb0b260cc8
shenqicang <shenqicangst@gmail.com>
2014-09-08 22:43:47 +0800

    revise tests

---
+++ tests/build/xml/fox/sax/CMakeFiles/fox_sax.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "Fortran"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_Fortran
+  "/home/stsqc/openmc/src/xml/fox/sax/FoX_sax.f90" "/home/stsqc/openmc/tests/build/xml/fox/sax/CMakeFiles/fox_sax.dir/FoX_sax.f90.o"
+  "/home/stsqc/openmc/src/xml/fox/sax/m_sax_operate.F90" "/home/stsqc/openmc/tests/build/xml/fox/sax/CMakeFiles/fox_sax.dir/m_sax_operate.F90.o"
+  "/home/stsqc/openmc/src/xml/fox/sax/m_sax_parser.F90" "/home/stsqc/openmc/tests/build/xml/fox/sax/CMakeFiles/fox_sax.dir/m_sax_parser.F90.o"
+  "/home/stsqc/openmc/src/xml/fox/sax/m_sax_reader.F90" "/home/stsqc/openmc/tests/build/xml/fox/sax/CMakeFiles/fox_sax.dir/m_sax_reader.F90.o"
+  "/home/stsqc/openmc/src/xml/fox/sax/m_sax_tokenizer.F90" "/home/stsqc/openmc/tests/build/xml/fox/sax/CMakeFiles/fox_sax.dir/m_sax_tokenizer.F90.o"
+  "/home/stsqc/openmc/src/xml/fox/sax/m_sax_types.F90" "/home/stsqc/openmc/tests/build/xml/fox/sax/CMakeFiles/fox_sax.dir/m_sax_types.F90.o"
+  "/home/stsqc/openmc/src/xml/fox/sax/m_sax_xml_source.F90" "/home/stsqc/openmc/tests/build/xml/fox/sax/CMakeFiles/fox_sax.dir/m_sax_xml_source.F90.o"
+  )
+SET(CMAKE_Fortran_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "GIT_SHA1=\"0863666974b38270b73806b0bebf1be7aa5ca372\""
+  "UNIX"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/stsqc/openmc/tests/build/xml/fox/common/CMakeFiles/fox_common.dir/DependInfo.cmake"
+  "/home/stsqc/openmc/tests/build/xml/fox/utils/CMakeFiles/fox_utils.dir/DependInfo.cmake"
+  "/home/stsqc/openmc/tests/build/xml/fox/fsys/CMakeFiles/fox_fsys.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+SET(CMAKE_Fortran_TARGET_MODULE_DIR "/home/stsqc/openmc/tests/build/include")
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "xml/fox/common"
+  "xml/fox/utils"
+  "xml/fox/fsys"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})

310e4538c80f6938c182fc31d94aeb4b13d846de
itayhorin <itayhorin@gmail.com>
2014-10-15 09:48:24 -0400

    Moved statepoint.py into src/utils/openmc directory, and updated test suite to reflect this.

--- src/utils/setup.py
+++ src/utils/setup.py
@@ -2,14 +2,6 @@
 
 from distutils.core import setup
 
-setup(name='statepoint',
-      version='0.6.0',
-      description='OpenMC StatePoint',
-      author='Paul Romano',
-      author_email='paul.k.romano@gmail.com',
-      url='https://github.com/mit-crpg/openmc',
-      py_modules=['statepoint'])
-
 setup(name='openmc',
       version='0.6.0',
       description='OpenMC Python API',

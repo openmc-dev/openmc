1a6a86a262a48a8234eb5c2c632749aedbddce83
Nick Horelik <nicholas.horelik@gmail.com>
2014-11-03 12:49:26 -0500

    Finished otf tally allocation unit test

--- src/unittests/test_dd_otf_tally_allocation.F90
+++ src/unittests/test_dd_otf_tally_allocation.F90
@@ -4,7 +4,8 @@ module test_dd_otf_tally_allocation
   use dd_header,        only: dd_type, deallocate_dd
   use dd_testing_setup, only: check_procs, dd_simple_four_domains, &
                               dd_simple_four_domain_tallies
-  use global,           only: master, rank, message, free_memory, tallies
+  use global,           only: master, rank, message, free_memory, tallies, &
+                              domain_decomp, dd_run
   use error,            only: warning
   use output,           only: write_message
   use particle_header,  only: Particle
@@ -30,7 +31,6 @@ module test_dd_otf_tally_allocation
 
   type(test), public :: dd_otf_tally_allocation_test
 
-  type(dd_type)  :: dd
   type(Particle) :: p
 
 contains
@@ -62,10 +62,11 @@ contains
     end if
 
     ! Get generic DD setup with 4 domains for 5 MPI ranks
-    call dd_simple_four_domains(dd)
+    call dd_simple_four_domains(domain_decomp)
 
     ! Initialize dd_type
-    call initialize_domain_decomp(dd)
+    call initialize_domain_decomp(domain_decomp)
+    dd_run = .true.
 
     ! Setup tallies and particle for scoring to them
     call dd_simple_four_domain_tallies(p)
@@ -80,14 +81,61 @@ contains
 
     ! Simulate scoring to tally bins
     select case(rank)
-      case(0, 1)
-      case(2)
-      case(3)
-      case(4)
+      case(0)
+        p % coord % cell = 1
+        call score_analog_tally(p)
+        p % coord % cell = 2
+        call score_analog_tally(p)
+        call score_analog_tally(p)
+        call score_analog_tally(p)
+        p % coord % cell = 4
+        call score_analog_tally(p)
+        call score_analog_tally(p)
+        call score_analog_tally(p)
+        call score_analog_tally(p)
+      case(1)
+        p % coord % cell = 4
+        call score_analog_tally(p)
+        p % coord % cell = 2
+        call score_analog_tally(p)
+        call score_analog_tally(p)
+        call score_analog_tally(p)
         p % coord % cell = 1
         call score_analog_tally(p)
         call score_analog_tally(p)
+        call score_analog_tally(p)
+        call score_analog_tally(p)
+      case(2)
+        p % coord % cell = 3
+        call score_analog_tally(p)
+        p % coord % cell = 4
+        call score_analog_tally(p)
+        p % coord % cell = 3
+        call score_analog_tally(p)
+        call score_analog_tally(p)
+        p % coord % cell = 4
+        call score_analog_tally(p)
+        call score_analog_tally(p)
+        call score_analog_tally(p)
+      case(3)
         p % coord % cell = 2
+        call score_analog_tally(p)
+        call score_analog_tally(p)
+        p % coord % cell = 4
+        call score_analog_tally(p)
+      case(4)
+        p % coord % cell = 2
+        call score_analog_tally(p)
+        p % coord % cell = 4
+        call score_analog_tally(p)
+        p % coord % cell = 3
+        call score_analog_tally(p)
+        call score_analog_tally(p)
+        p % coord % cell = 2
+        call score_analog_tally(p)
+        call score_analog_tally(p)
+        p % coord % cell = 4
+        call score_analog_tally(p)
         call score_analog_tally(p)
     end select    
     
@@ -101,26 +149,62 @@ contains
 
     class(TestSuiteClass), intent(inout) :: suite
 
+    logical :: failure = .false.
+#ifdef MPI
+    integer :: mpi_err
+    logical :: any_fail
+#endif
+
     ! Check that the results arrays are the right size and have the right values
     select case(rank)
       case(0, 1)
-        print *, rank, 'results', tallies(1) % results
+        if (size(tallies(1) % results, 2) /= 3) then
+          failure = .true.
+        else
+          if (.not. int(tallies(1) % results(1,1) % value) == 1 .and. &
+              .not. int(tallies(1) % results(1,2) % value) == 3 .and. &
+              .not. int(tallies(1) % results(1,3) % value) == 4) failure = .true.
+        end if
       case(2)
-        print *, rank, 'results', tallies(1) % results
+        if (size(tallies(1) % results, 2) /= 2) then
+          failure = .true.
+        else
+          if (.not. int(tallies(1) % results(1,1) % value) == 3 .and. &
+              .not. int(tallies(1) % results(1,2) % value) == 4) failure = .true.
+        end if
       case(3)
-        print *, rank, 'results', tallies(1) % results
+        if (size(tallies(1) % results, 2) /= 2) then
+          failure = .true.
+        else
+          if (.not. int(tallies(1) % results(1,1) % value) == 2 .and. &
+              .not. int(tallies(1) % results(1,2) % value) == 1) failure = .true.
+        end if
       case(4)
-        print *, rank, 'results', tallies(1) % results
+        if (size(tallies(1) % results, 2) /= 3) then
+          failure = .true.
+        else
+          if (.not. int(tallies(1) % results(1,1) % value) == 3 .and. &
+              .not. int(tallies(1) % results(1,2) % value) == 3 .and. &
+              .not. int(tallies(1) % results(1,3) % value) == 2) failure = .true.
+        end if
     end select    
     
-
-    if (.true.) then
+    if (failure) then
       message = "FAILURE: Tally results arrays improperly allocated with " // &
                 "OTF on rank " // trim(to_str(rank))
       call write_message()
     end if
-    call suite % fail()
-    
+
+#ifdef MPI
+    call MPI_ALLREDUCE(failure, any_fail, 1, MPI_LOGICAL, MPI_LOR, &
+        MPI_COMM_WORLD, mpi_err)
+    if (.not. any_fail) then
+      call suite % pass()
+    else
+      call suite % fail()
+    end if
+#endif
+
   end subroutine test_check
 
 !===============================================================================
@@ -130,7 +214,7 @@ contains
   subroutine test_teardown()
 
     call p % clear()
-    call deallocate_dd(dd)
+    call deallocate_dd(domain_decomp)
     call free_memory()
     
   end subroutine test_teardown

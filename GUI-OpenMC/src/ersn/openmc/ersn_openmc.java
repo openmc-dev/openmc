74de6f6b7087d83f5cff89dd111f9c815d0eb354
Hp <elbakkali@ubuntu.(none)>
2014-11-05 22:09:19 +0100

    Adding a directory for GUI-OpenMC

---
+++ GUI-OpenMC/src/ersn/openmc/ersn_openmc.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ersn.openmc;
+
+/**
+ *
+ * @author elbakkali
+ */
+public class ersn_openmc {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+    java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new main().setVisible(true);
+            }
+        });
+    }
+    
+}

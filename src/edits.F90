a616d9dad9fd41757b25f76a38b68570b81f0031
walshjon <walshjon@mit.edu>
2015-04-18 10:54:50 -0700

    add module for writing quick edit routines, e.g. wigner distribution sampler

---
+++ src/edits.F90
@@ -0,0 +1,36 @@
+module edits
+
+  use output,     only: write_coords
+  use unresolved, only: wigner_dist
+
+  implicit none
+
+contains
+
+!$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+!
+! SAMPLEWIGNERDIST samples the Wigner distribution for level spacings using
+! a constant mean spacing in order to create a curve for comparison with the
+! analytical result
+!
+!$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+  subroutine sampleWignerDist(n)
+
+    integer :: n ! number of samples
+    integer :: i ! iteration index
+    real(8) :: D_mean    ! average level spacing in eV
+    real(8) :: D_vals(n) ! sampled level spacings
+
+    D_mean = 20.0_8
+
+    do i = 1, n
+      D_vals(i) = wigner_dist(D_mean)
+    end do
+
+    call write_coords(99, 'wigner-dist-samples.dat', n, n, dble([(i,i=1,n)]),&
+      D_vals(:)/D_mean)
+
+  end subroutine sampleWignerDist
+
+end module edits

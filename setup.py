9ba8f5b0ba9babc53aa674758d3f1d0504cd3b80
itayhorin <itayhorin@gmail.com>
2015-05-23 11:16:12 +0700

    Expand setup.py to include required and optional dependencies

--- setup.py
+++ setup.py
@@ -1,13 +1,35 @@
 #!/usr/bin/env python
 
+import glob
 import os
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='openmc',
       version='0.6.2',
-      description='OpenMC Python API',
+      packages=find_packages(),
+      scripts=glob.glob('scripts/openmc-*'),
+
+      # Required dependencies
+      install_requires=['numpy', 'scipy', 'h5py', 'matplotlib'],
+
+      # Optional dependencies
+      extras_require={
+          'vtk': ['vtk', 'silomesh'],
+          'validate': ['lxml'],
+      },
+
+      # Metadata
       author='Will Boyd',
       author_email='wbinventor@gmail.com',
+      description='OpenMC Python API',
       url='https://github.com/mit-crpg/openmc',
-      packages=['openmc'],
-      scripts=[os.path.join('scripts', f) for f in os.listdir('scripts')])
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Intended Audience :: End Users/Desktop',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: MIT License',
+          'Natural Language :: English',
+          'Programming Language :: Python',
+          'Topic :: Scientific/Engineering'
+      ]
+)

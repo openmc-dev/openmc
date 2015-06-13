ec2ecfd0e9327a1fe1ceab01c7b81a8f7d497677
walshjon <walshjon@mit.edu>
2015-06-12 22:36:58 -0700

    system test suite for OTF and pointwise runs

---
+++ testing/regression/system/system-suite.sh
@@ -0,0 +1,129 @@
+#!/bin/sh
+
+###########################################
+#
+# script for running jobs on Kilkenny head
+#
+###########################################
+
+export TEST_DIR=/home/walshjon/scratch/shutmc/testing/regression/system
+cd $TEST_DIR
+
+export LD_LIBRARY_PATH=/opt/gcc/4.9.0/lib64:/opt/mpich/3.1.2-gnu/lib
+
+export SHUTXS=/home/walshjon/scratch/shutmc/src/build/bin/openmc
+
+cd $TEST_DIR/case-1
+$SHUTXS
+cd $TEST_DIR/case-2
+$SHUTXS
+cd $TEST_DIR/case-3
+$SHUTXS
+cd $TEST_DIR/case-4
+$SHUTXS
+cd $TEST_DIR/case-5
+$SHUTXS
+cd $TEST_DIR/case-6
+$SHUTXS
+cd $TEST_DIR/case-7
+$SHUTXS
+cd $TEST_DIR/case-8
+$SHUTXS
+cd $TEST_DIR/case-9
+$SHUTXS
+cd $TEST_DIR/case-10
+$SHUTXS
+cd $TEST_DIR/case-11
+$SHUTXS
+cd $TEST_DIR/case-12
+$SHUTXS
+cd $TEST_DIR/case-13
+$SHUTXS
+cd $TEST_DIR/case-14
+$SHUTXS
+cd $TEST_DIR/case-15
+$SHUTXS
+cd $TEST_DIR/case-16
+$SHUTXS
+cd $TEST_DIR/case-17
+$SHUTXS
+cd $TEST_DIR/case-18
+$SHUTXS
+cd $TEST_DIR/case-19
+$SHUTXS
+cd $TEST_DIR/case-20
+$SHUTXS
+cd $TEST_DIR/case-21
+$SHUTXS
+cd $TEST_DIR/case-22
+$SHUTXS
+cd $TEST_DIR/case-23
+$SHUTXS
+cd $TEST_DIR/case-24
+$SHUTXS
+cd $TEST_DIR/case-25
+$SHUTXS
+cd $TEST_DIR/case-26
+$SHUTXS
+cd $TEST_DIR/case-27
+$SHUTXS
+cd $TEST_DIR/case-28
+$SHUTXS
+cd $TEST_DIR/case-29
+$SHUTXS
+cd $TEST_DIR/case-30
+$SHUTXS
+cd $TEST_DIR/case-31
+$SHUTXS
+cd $TEST_DIR/case-32
+$SHUTXS
+cd $TEST_DIR/case-33
+$SHUTXS
+cd $TEST_DIR/case-34
+$SHUTXS
+cd $TEST_DIR/case-35
+$SHUTXS
+cd $TEST_DIR/case-36
+$SHUTXS
+cd $TEST_DIR/case-37
+$SHUTXS
+cd $TEST_DIR/case-38
+$SHUTXS
+cd $TEST_DIR/case-39
+$SHUTXS
+cd $TEST_DIR/case-40
+$SHUTXS
+cd $TEST_DIR/case-41
+$SHUTXS
+cd $TEST_DIR/case-42
+$SHUTXS
+cd $TEST_DIR/case-43
+$SHUTXS
+cd $TEST_DIR/case-44
+$SHUTXS
+cd $TEST_DIR/case-45
+$SHUTXS
+cd $TEST_DIR/case-46
+$SHUTXS
+cd $TEST_DIR/case-47
+$SHUTXS
+cd $TEST_DIR/case-48
+$SHUTXS
+cd $TEST_DIR/case-49
+$SHUTXS
+cd $TEST_DIR/case-50
+$SHUTXS
+cd $TEST_DIR/case-51
+$SHUTXS
+cd $TEST_DIR/case-52
+$SHUTXS
+cd $TEST_DIR/case-53
+$SHUTXS
+cd $TEST_DIR/case-54
+$SHUTXS
+cd $TEST_DIR/case-55
+$SHUTXS
+cd $TEST_DIR/case-56
+$SHUTXS
+cd $TEST_DIR/case-57
+$SHUTXS

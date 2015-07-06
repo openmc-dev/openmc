c321b135ca149db3c46042c92563e278981254bd
Paul Romano <paul.k.romano@gmail.com>
2015-07-06 07:45:21 +0700

    Set maximum number of coordinates from CMakeLists.txt

--- src/constants.F90
+++ src/constants.F90
@@ -133,9 +133,6 @@ module constants
   ! Maximum number of lost particles
   integer, parameter :: MAX_LOST_PARTICLES = 10
 
-  ! Maximum levels of nested universes
-  integer, parameter :: MAX_COORD = 10
-
   ! ============================================================================
   ! CROSS SECTION RELATED CONSTANTS
 

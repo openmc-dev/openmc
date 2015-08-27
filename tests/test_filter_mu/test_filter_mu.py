012ffdfbb54bcd8cd127abc562db91b84b83e168
itayhorin <itayhorin@gmail.com>
2015-08-26 20:32:00 -0400

    Added test for mu filter

---
+++ tests/test_filter_mu/test_filter_mu.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import sys
+sys.path.insert(0, '..')
+from testing_harness import TestHarness
+
+
+if __name__ == '__main__':
+    harness = TestHarness('statepoint.10.*', True)
+    harness.main()

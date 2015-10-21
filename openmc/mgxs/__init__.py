8141ed3e327779470426023bea00a66859ec5a82
itayhorin <itayhorin@gmail.com>
2015-10-21 09:29:01 -0400

    Revised __init__.py for openmc.mgxs to be Python 3 compatible

--- openmc/mgxs/__init__.py
+++ openmc/mgxs/__init__.py
@@ -1,3 +1,3 @@
-from groups import EnergyGroups
-from library import Library
-from mgxs import *
+from openmc.mgxs.groups import EnergyGroups
+from openmc.mgxs.library import Library
+from openmc.mgxs.mgxs import *

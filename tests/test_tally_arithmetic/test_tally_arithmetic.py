5622dc2c6ebc1aa05e04952676e7ac865e5f006d
itayhorin <itayhorin@gmail.com>
2015-12-08 13:27:45 -0500

    added tallies.xml file to tally arithmetic test

--- tests/test_tally_arithmetic/test_tally_arithmetic.py
+++ tests/test_tally_arithmetic/test_tally_arithmetic.py
@@ -10,38 +10,6 @@ import openmc
 
 
 class TallyArithmeticTestHarness(TestHarness):
-    def _build_inputs(self):
-
-        u235 = openmc.Nuclide('U-235')
-        u238 = openmc.Nuclide('U-238')
-        pu239 = openmc.Nuclide('Pu-239')
-
-        # Instantiate energy filter
-        energy_filter = openmc.Filter(type='energy', bins=[0., 20.])
-
-        # Create tallies
-        tally_1 = openmc.Tally(name='tally 1')
-        tally_1.add_filter(openmc.Filter(type='cell', bins=[1]))
-        tally_1.add_filter(energy_filter)
-        tally_1.add_score('fission')
-        tally_1.add_score('nu-fission')
-        tally_1.add_nuclide(u235)
-        tally_1.add_nuclide(u238)
-
-        tally_2 = openmc.Tally(name='tally 2')
-        tally_2.add_filter(openmc.Filter(type='material', bins=[1]))
-        tally_2.add_filter(energy_filter)
-        tally_2.add_score('fission')
-        tally_2.add_score('absorption')
-        tally_2.add_nuclide(u235)
-        tally_2.add_nuclide(pu239)
-
-        # Export tallies to file
-        tallies_file = openmc.TalliesFile()
-        tallies_file.add_tally(tally_1)
-        tallies_file.add_tally(tally_2)
-        tallies_file.export_to_xml()
-
     def _get_results(self, hash_output=False):
         """Digest info in the statepoint and return as a string."""
 

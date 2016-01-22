bfa3c9b8ea8dd7b75dbd2bcddbd5e6531e6f61ce
itayhorin <itayhorin@gmail.com>
2016-01-22 13:29:59 -0600

    Modify test_energy_laws to include Ta-181
    Ta-181 is among a handful of nuclides that uses Kalbach-Mann systematics with
    linear-linear interpolation.

--- tests/test_energy_laws/test_energy_laws.py
+++ tests/test_energy_laws/test_energy_laws.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python
 
-"""The purpose of this test is to provide coverage of Watt, N-body phase space,
-and evaporation energy distributions. The only nuclide that uses a Watt fission
-spectrum in ENDF/B-VII.1 is U-233. The only nuclide that has a reaction using
-the N-body phase space distribution is H-2(n.2n). Several nuclides have
-reactions with evaporation spectra. In this test, the material is composed of
-U-233, H-2, and Na-23 (which has several reactions with evaporation spectra.
+"""The purpose of this test is to provide coverage of energy distributions that
+are not covered in other tests. It has a single material with the following
+nuclides:
+
+U-233: Only nuclide that has a Watt fission spectrum
+
+H-2: Only nuclide that has an N-body phase space distribution, in this case for
+(n,2n)
+
+Na-23: Has an evaporation spectrum and also has reactions that have multiple
+angle-energy distributions, so it provides coverage for both of those
+situations.
+
+Ta-181: One of a few nuclides that has reactions with Kalbach-Mann distributions
+that use linear-linear interpolation.
 
 """
 

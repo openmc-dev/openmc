f4e0525a053741fda2e507aa2ca7954e7d1d73d9
Sterling Harper <smharper@mit.edu>
2016-08-12 14:16:46 -0500

    Remove six dependency

--- openmc/data/function.py
+++ openmc/data/function.py
@@ -1,7 +1,6 @@
 from abc import ABCMeta, abstractmethod
 from collections import Iterable, Callable
 from numbers import Real, Integral
-from six import with_metaclass
 
 import numpy as np
 
@@ -11,8 +10,10 @@ INTERPOLATION_SCHEME = {1: 'histogram', 2: 'linear-linear', 3: 'linear-log',
                         4: 'log-linear', 5: 'log-log'}
 
 
-class Function1D(with_metaclass(ABCMeta, object)):
+class Function1D(object):
     """A function of one independent variable with HDF5 support."""
+
+    __metaclass__ = ABCMeta
 
     @abstractmethod
     def __call__(self): pass

Fixed distribcell MGXS subdomain averaging to preserve global reactivity

openmc/mgxs/mgxs.py
@@ -189,6 +189,8 @@ class MGXS(object):
             clone._rxn_rate_tally = copy.deepcopy(self._rxn_rate_tally, memo)
             clone._xs_tally = copy.deepcopy(self._xs_tally, memo)
             clone._sparse = self.sparse
+            clone._loaded_sp = self._loaded_sp
+            clone._hdf5_key = self._hdf5_key
             clone._derived = self.derived
 
             clone._tallies = OrderedDict()
@@ -949,28 +951,24 @@ class MGXS(object):
         # Construct a collection of the subdomain filter bins to average across
         if not isinstance(subdomains, basestring):
             cv.check_iterable_type('subdomains', subdomains, Integral)
+            subdomains = [(subdomain,) for subdomain in subdomains]
         elif self.domain_type == 'distribcell':
-            subdomains = np.arange(self.num_subdomains)
+            subdomains = [(i,) for i in range(self.num_subdomains)]
         else:
             subdomains = None
 
         # Clone this MGXS to initialize the subdomain-averaged version
         avg_xs = copy.deepcopy(self)
+        avg_xs._rxn_rate_tally = None
+        avg_xs._xs_tally = None
 
-        if self.derived:
-            avg_xs._rxn_rate_tally = avg_xs.rxn_rate_tally.average(
-                filter_type=self.domain_type, filter_bins=subdomains)
-        else:
-            avg_xs._rxn_rate_tally = None
-            avg_xs._xs_tally = None
+        # Average each of the tallies across subdomains
+        for tally_type, tally in avg_xs.tallies.items():
+            tally_avg = tally.summation(filter_type=self.domain_type,
+                                        filter_bins=subdomains)
+            avg_xs.tallies[tally_type] = tally_avg
 
-            # Average each of the tallies across subdomains
-            for tally_type, tally in avg_xs.tallies.items():
-                tally_avg = tally.average(filter_type=self.domain_type,
-                                          filter_bins=subdomains)
-                avg_xs.tallies[tally_type] = tally_avg
-
-        avg_xs._domain_type = 'avg({0})'.format(self.domain_type)
+        avg_xs._domain_type = 'sum({0})'.format(self.domain_type)
         avg_xs.sparse = self.sparse
         return avg_xs
 
@@ -1293,8 +1291,8 @@ class MGXS(object):
             cv.check_iterable_type('subdomains', subdomains, Integral)
         elif self.domain_type == 'distribcell':
             subdomains = np.arange(self.num_subdomains, dtype=np.int)
-        elif self.domain_type == 'avg(distribcell)':
-            domain_filter = self.xs_tally.find_filter('avg(distribcell)')
+        elif self.domain_type == 'sum(distribcell)':
+            domain_filter = self.xs_tally.find_filter('sum(distribcell)')
             subdomains = domain_filter.bins
         elif self.domain_type == 'mesh':
             xyz = [range(1, x+1) for x in self.domain.dimension]
@@ -3613,7 +3611,7 @@ class ScatterMatrixXS(MatrixMGXS):
         # Construct list of energy group bounds tuples for all requested groups
         if not isinstance(in_groups, basestring):
             cv.check_iterable_type('groups', in_groups, Integral)
-            filter.append('energy')
+            filters.append('energy')
             filter_bins.append([])
             for group in in_groups:
                 filter_bins[-1].append((self.energy_groups.get_group_bounds(group),))

openmc/tallies.py
@@ -285,8 +285,8 @@ class Tally(object):
             # Extract Tally data from the file
             data = f['tallies/tally {0}/results'.format(
                 self.id)].value
-            sum = data['sum']
-            sum_sq = data['sum_sq']
+            sum = data['sum'].astype(np.float32)
+            sum_sq = data['sum_sq'].astype(np.float32)
 
             # Reshape the results arrays
             sum = np.reshape(sum, self.shape)
@@ -2229,12 +2229,12 @@ class Tally(object):
         # Construct lists of tuples for the bins in each of the two filters
         filters = [filter1.type, filter2.type]
         if filter1.type == 'distribcell':
-            filter1_bins = np.arange(filter1.num_bins)
+            filter1_bins = [(i,) for i in range(filter1.num_bins)]
         else:
             filter1_bins = [(filter1.get_bin(i)) for i in range(filter1.num_bins)]
 
         if filter2.type == 'distribcell':
-            filter2_bins = np.arange(filter2.num_bins)
+            filter2_bins = [(i,) for i in range(filter2.num_bins)]
         else:
             filter2_bins = [filter2.get_bin(i) for i in range(filter2.num_bins)]
 
@@ -3349,16 +3349,16 @@ class Tally(object):
 
         # Inject this Tally's data along the diagonal of the diagonalized Tally
         if not self.derived and self.sum is not None:
-            new_tally._sum = np.zeros(new_tally.shape, dtype=np.float64)
+            new_tally._sum = np.zeros(new_tally.shape, dtype=np.float32)
             new_tally._sum[diag_indices, :, :] = self.sum
         if not self.derived and self.sum_sq is not None:
-            new_tally._sum_sq = np.zeros(new_tally.shape, dtype=np.float64)
+            new_tally._sum_sq = np.zeros(new_tally.shape, dtype=np.float32)
             new_tally._sum_sq[diag_indices, :, :] = self.sum_sq
         if self.mean is not None:
-            new_tally._mean = np.zeros(new_tally.shape, dtype=np.float64)
+            new_tally._mean = np.zeros(new_tally.shape, dtype=np.float32)
             new_tally._mean[diag_indices, :, :] = self.mean
         if self.std_dev is not None:
-            new_tally._std_dev = np.zeros(new_tally.shape, dtype=np.float64)
+            new_tally._std_dev = np.zeros(new_tally.shape, dtype=np.float32)
             new_tally._std_dev[diag_indices, :, :] = self.std_dev
 
         # Update the new tally's filter strides
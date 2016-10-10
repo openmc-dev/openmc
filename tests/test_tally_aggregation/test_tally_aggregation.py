4f3454edead1eacb3aed4b53e3aa9ff79724b316
Adam Nelson <nelsonag@umich.edu>
2016-10-10 09:49:01 -0400

    Significantly reduced the number of nuclides in the standard (HM problem I believe) input set, reducing the total test time by a good amount

--- tests/test_tally_aggregation/test_tally_aggregation.py
+++ tests/test_tally_aggregation/test_tally_aggregation.py
@@ -16,9 +16,9 @@ class TallyAggregationTestHarness(PyAPITestHarness):
         self._input_set.settings.output = {'summary': True}
 
         # Initialize the nuclides
+        u234 = openmc.Nuclide('U234')
         u235 = openmc.Nuclide('U235')
         u238 = openmc.Nuclide('U238')
-        pu239 = openmc.Nuclide('Pu239')
 
         # Initialize the filters
         energy_filter = openmc.EnergyFilter([0.0, 0.253e-6, 1.0e-3, 1.0, 20.0])
@@ -28,7 +28,7 @@ class TallyAggregationTestHarness(PyAPITestHarness):
         tally = openmc.Tally(name='distribcell tally')
         tally.filters = [energy_filter, distrib_filter]
         tally.scores = ['nu-fission', 'total']
-        tally.nuclides = [u235, u238, pu239]
+        tally.nuclides = [u234, u235, u238]
         tallies_file = openmc.Tallies([tally])
 
         # Export tallies to file
@@ -59,7 +59,7 @@ class TallyAggregationTestHarness(PyAPITestHarness):
         outstr += ', '.join(map(str, tally_sum.std_dev))
 
         # Sum across all nuclides
-        tally_sum = tally.summation(nuclides=['U235', 'U238', 'Pu239'])
+        tally_sum = tally.summation(nuclides=['U234', 'U235', 'U238'])
         outstr += ', '.join(map(str, tally_sum.mean))
         outstr += ', '.join(map(str, tally_sum.std_dev))
 

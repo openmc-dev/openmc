bee4a5795cb3af8cb625bfcf3f55b4913501078e
itayhorin <itayhorin@gmail.com>
2016-10-15 15:15:50 -0400

    Increased MG mode test coverage from 63% to 74%.

--- tests/test_mg_tallies/test_mg_tallies.py
+++ tests/test_mg_tallies/test_mg_tallies.py
@@ -9,32 +9,66 @@ import openmc
 
 class MGTalliesTestHarness(PyAPITestHarness):
     def _build_inputs(self):
+        """Write input XML files."""
+        self._input_set.build_default_materials_and_geometry(as_macro=True)
+        self._input_set.build_default_settings()
+
         # Instantiate a tally mesh
         mesh = openmc.Mesh(mesh_id=1)
         mesh.type = 'regular'
-        mesh.dimension = [17, 17, 1]
+        mesh.dimension = [1, 1, 10]
         mesh.lower_left = [0.0, 0.0, 0.0]
-        mesh.upper_right = [21.42, 21.42, 100.0]
+        mesh.upper_right = [10, 10, 5]
 
         # Instantiate some tally filters
         energy_filter = openmc.EnergyFilter([0.0, 20.0])
         energyout_filter = openmc.EnergyoutFilter([0.0, 20.0])
         mesh_filter = openmc.MeshFilter(mesh)
 
-        mat_filter = openmc.MaterialFilter([1,2,3])
+        mat_ids = [mat.id for mat in self._input_set.materials]
+        mat_filter = openmc.MaterialFilter(mat_ids)
 
-        tally1 = openmc.Tally(tally_id=1)
-        tally1.filters = [mesh_filter]
-        tally1.scores = ['total', 'absorption', 'flux',
-                         'fission', 'nu-fission']
+        tallies = []
 
-        tally2 = openmc.Tally(tally_id=2)
-        tally2.filters = [mat_filter, energy_filter, energyout_filter]
-        tally2.scores = ['scatter', 'nu-scatter']
+        tallies.append(openmc.Tally())
+        tallies[-1].filters = [mesh_filter]
+        tallies[-1].estimator = 'analog'
+        tallies[-1].scores = ['total', 'absorption', 'flux',
+                              'fission', 'nu-fission']
 
-        self._input_set.tallies = openmc.Tallies([tally1, tally2])
+        tallies.append(openmc.Tally())
+        tallies[-1].filters = [mesh_filter]
+        tallies[-1].estimator = 'tracklength'
+        tallies[-1].scores = ['total', 'absorption', 'flux',
+                              'fission', 'nu-fission']
 
-        super(MGTalliesTestHarness, self)._build_inputs()
+        tallies.append(openmc.Tally())
+        tallies[-1].filters = [mat_filter, energy_filter]
+        tallies[-1].estimator = 'analog'
+        tallies[-1].scores = ['total', 'absorption', 'flux',
+                              'fission', 'nu-fission', 'scatter',
+                              'nu-scatter']
+
+        tallies.append(openmc.Tally())
+        tallies[-1].filters = [mat_filter, energy_filter]
+        tallies[-1].estimator = 'collision'
+        tallies[-1].scores = ['total', 'absorption', 'flux',
+                              'fission', 'nu-fission']
+
+        tallies.append(openmc.Tally())
+        tallies[-1].filters = [mat_filter, energy_filter]
+        tallies[-1].estimator = 'tracklength'
+        tallies[-1].scores = ['total', 'absorption', 'flux',
+                              'fission', 'nu-fission', 'scatter']
+
+        tallies.append(openmc.Tally())
+        tallies[-1].filters = [mat_filter, energy_filter,
+                               energyout_filter]
+        tallies[-1].scores = ['scatter', 'nu-scatter', 'nu-fission']
+
+        self._input_set.tallies = openmc.Tallies(tallies)
+
+        self._input_set.export()
 
     def _cleanup(self):
         super(MGTalliesTestHarness, self)._cleanup()

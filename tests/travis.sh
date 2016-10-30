48678c92b9a46fddb88370f7ba1d3cf741e8e42c
Sterling Harper <smharper@mit.edu>
2016-10-30 16:32:54 -0400

    Show me your secrets, Travis

--- tests/travis.sh
+++ tests/travis.sh
@@ -5,7 +5,7 @@ set -ev
 # Run all debug tests
 ./check_source.py
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-  ./run_tests.py -C "^hdf5-debug$|^omp-hdf5-debug|^mpi-hdf5-debug|^phdf5-debug$|^phdf5-omp-debug$" -j 2 -s
+  ./run_tests.py -C "^hdf5-debug$|^omp-hdf5-debug|^mpi-hdf5-debug|^phdf5-debug$|^phdf5-omp-debug$" -j 2 -s -R "diff" -v
 else
   ./run_tests.py -C "^hdf5-debug$" -j 2
 fi

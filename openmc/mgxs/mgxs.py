Fixed MGXS.get_subdomain_avg_xs(...) to use track density-weighted averaging

openmc/mgxs/mgxs.py
@@ -192,7 +192,9 @@ class MGXS(object):
             clone._rxn_rate_tally = copy.deepcopy(self._rxn_rate_tally, memo)
             clone._xs_tally = copy.deepcopy(self._xs_tally, memo)
             clone._sparse = self.sparse
+            clone._loaded_sp = self._loaded_sp
             clone._derived = self.derived
+            clone._hdf5_key = self._hdf5_key
 
             clone._tallies = OrderedDict()
             for tally_type, tally in self.tallies.items():
@@ -325,7 +327,7 @@ class MGXS(object):
 
     @property
     def num_subdomains(self):
-        if self.domain_type.startswith('avg('):
+        if self.domain_type.startswith('sum('):
             domain_type = self.domain_type[4:-1]
         else:
             domain_type = self.domain_type
@@ -789,16 +791,22 @@ class MGXS(object):
         if not isinstance(subdomains, string_types):
             cv.check_iterable_type('subdomains', subdomains, Integral,
                                    max_depth=3)
+
+            filters.append(_DOMAIN_TO_FILTER[self.domain_type])
+            subdomain_bins = []
             for subdomain in subdomains:
-                filters.append(_DOMAIN_TO_FILTER[self.domain_type])
-                filter_bins.append((subdomain,))
+                subdomain_bins.append(subdomain)
+            filter_bins.append(tuple(subdomain_bins))
 
         # Construct list of energy group bounds tuples for all requested groups
         if not isinstance(groups, string_types):
             cv.check_iterable_type('groups', groups, Integral)
+            filters.append(openmc.EnergyFilter)
+            energy_bins = []
             for group in groups:
-                filters.append(openmc.EnergyFilter)
-                filter_bins.append((self.energy_groups.get_group_bounds(group),))
+                energy_bins.append(
+                    (self.energy_groups.get_group_bounds(group),))
+            filter_bins.append(tuple(energy_bins))
 
         # Construct a collection of the nuclides to retrieve from the xs tally
         if self.by_nuclide:
@@ -958,30 +966,27 @@ class MGXS(object):
         # Construct a collection of the subdomain filter bins to average across
         if not isinstance(subdomains, string_types):
             cv.check_iterable_type('subdomains', subdomains, Integral)
+            subdomains = [(subdomain,) for subdomain in subdomains]
+            subdomains = [tuple(subdomains)]
         elif self.domain_type == 'distribcell':
-            subdomains = np.arange(self.num_subdomains)
+            subdomains = [i for i in range(self.num_subdomains)]
+            subdomains = [tuple(subdomains)]
         else:
             subdomains = None
 
         # Clone this MGXS to initialize the subdomain-averaged version
         avg_xs = copy.deepcopy(self)
+        avg_xs._rxn_rate_tally = None
+        avg_xs._xs_tally = None
 
-        if self.derived:
-            avg_xs._rxn_rate_tally = avg_xs.rxn_rate_tally.average(
-                filter_type=_DOMAIN_TO_FILTER[self.domain_type],
-                filter_bins=subdomains)
-        else:
-            avg_xs._rxn_rate_tally = None
-            avg_xs._xs_tally = None
+        # Average each of the tallies across subdomains
+        for tally_type, tally in avg_xs.tallies.items():
+            filt_type = _DOMAIN_TO_FILTER[self.domain_type]
+            tally_avg = tally.summation(filter_type=filt_type,
+                                        filter_bins=subdomains)
+            avg_xs.tallies[tally_type] = tally_avg
 
-            # Average each of the tallies across subdomains
-            for tally_type, tally in avg_xs.tallies.items():
-                filt_type = _DOMAIN_TO_FILTER[self.domain_type]
-                tally_avg = tally.average(filter_type=filt_type,
-                                          filter_bins=subdomains)
-                avg_xs.tallies[tally_type] = tally_avg
-
-        avg_xs._domain_type = 'avg({0})'.format(self.domain_type)
+        avg_xs._domain_type = 'sum({0})'.format(self.domain_type)
         avg_xs.sparse = self.sparse
         return avg_xs
 
@@ -1304,8 +1309,8 @@ class MGXS(object):
             cv.check_iterable_type('subdomains', subdomains, Integral)
         elif self.domain_type == 'distribcell':
             subdomains = np.arange(self.num_subdomains, dtype=np.int)
-        elif self.domain_type == 'avg(distribcell)':
-            domain_filter = self.xs_tally.find_filter('avg(distribcell)')
+        elif self.domain_type == 'sum(distribcell)':
+            domain_filter = self.xs_tally.find_filter('sum(distribcell)')
             subdomains = domain_filter.bins
         elif self.domain_type == 'mesh':
             xyz = [range(1, x+1) for x in self.domain.dimension]
@@ -1784,17 +1789,19 @@ class MatrixMGXS(MGXS):
         if not isinstance(subdomains, string_types):
             cv.check_iterable_type('subdomains', subdomains, Integral,
                                    max_depth=3)
+            filters.append(_DOMAIN_TO_FILTER[self.domain_type])
+            subdomain_bins = []
             for subdomain in subdomains:
-                filters.append(_DOMAIN_TO_FILTER[self.domain_type])
-                filter_bins.append((subdomain,))
+                subdomain_bins.append(subdomain)
+            filter_bins.append(tuple(subdomain_bins))
 
         # Construct list of energy group bounds tuples for all requested groups
         if not isinstance(in_groups, string_types):
             cv.check_iterable_type('groups', in_groups, Integral)
+            filters.append(openmc.EnergyFilter)
             for group in in_groups:
-                filters.append(openmc.EnergyFilter)
-                filter_bins.append((
-                    self.energy_groups.get_group_bounds(group),))
+                energy_bins.append((self.energy_groups.get_group_bounds(group),))
+            filter_bins.append(tuple(energy_bins))
 
         # Construct list of energy group bounds tuples for all requested groups
         if not isinstance(out_groups, string_types):
@@ -3618,16 +3625,21 @@ class ScatterMatrixXS(MatrixMGXS):
         # Construct a collection of the domain filter bins
         if not isinstance(subdomains, string_types):
             cv.check_iterable_type('subdomains', subdomains, Integral, max_depth=3)
+            filters.append(_DOMAIN_TO_FILTER[self.domain_type])
+            subdomain_bins = []
             for subdomain in subdomains:
-                filters.append(_DOMAIN_TO_FILTER[self.domain_type])
-                filter_bins.append((subdomain,))
+                subdomain_bins.append(subdomain)
+            filter_bins.append(tuple(subdomain_bins))
 
         # Construct list of energy group bounds tuples for all requested groups
         if not isinstance(in_groups, string_types):
             cv.check_iterable_type('groups', in_groups, Integral)
+            filters.append(openmc.EnergyFilter)
+            energy_bins = []
             for group in in_groups:
-                filters.append(openmc.EnergyFilter)
-                filter_bins.append((self.energy_groups.get_group_bounds(group),))
+                energy_bins.append(
+                    (self.energy_groups.get_group_bounds(group),))
+            filter_bins.append(tuple(energy_bins))
 
         # Construct list of energy group bounds tuples for all requested groups
         if not isinstance(out_groups, string_types):
@@ -4606,16 +4618,21 @@ class Chi(MGXS):
         # Construct a collection of the domain filter bins
         if not isinstance(subdomains, string_types):
             cv.check_iterable_type('subdomains', subdomains, Integral, max_depth=3)
+            filters.append(_DOMAIN_TO_FILTER[self.domain_type])
+            subdomain_bins = []
             for subdomain in subdomains:
-                filters.append(_DOMAIN_TO_FILTER[self.domain_type])
-                filter_bins.append((subdomain,))
+                subdomain_bins.append(subdomain)
+            filter_bins.append(tuple(subdomain_bins))
 
         # Construct list of energy group bounds tuples for all requested groups
         if not isinstance(groups, string_types):
             cv.check_iterable_type('groups', groups, Integral)
+            filters.append(openmc.EnergyoutFilter)
+            energy_bins = []
             for group in groups:
-                filters.append(openmc.EnergyoutFilter)
-                filter_bins.append((self.energy_groups.get_group_bounds(group),))
+                energy_bins.append(
+                    (self.energy_groups.get_group_bounds(group),))
+            filter_bins.append(tuple(energy_bins))
 
         # If chi was computed for each nuclide in the domain
         if self.by_nuclide:

openmc/tallies.py
@@ -1327,7 +1327,7 @@ class Tally(object):
 
                     # Create list of cell instance IDs for distribcell Filters
                     elif isinstance(self_filter, openmc.DistribcellFilter):
-                        bins = [i for i in range(self_filter.num_bins)]
+                        bins = [b for b in range(self_filter.num_bins)]
 
                     # Create list of IDs for bins for all other filter types
                     else:
@@ -2258,12 +2258,12 @@ class Tally(object):
         # Construct lists of tuples for the bins in each of the two filters
         filters = [type(filter1), type(filter2)]
         if isinstance(filter1, openmc.DistribcellFilter):
-            filter1_bins = [i for i in range(filter1.num_bins)]
+            filter1_bins = [b for b in range(filter1.num_bins)]
         else:
             filter1_bins = [filter1.get_bin(i) for i in range(filter1.num_bins)]
 
         if isinstance(filter2, openmc.DistribcellFilter):
-            filter2_bins = [i for i in range(filter2.num_bins)]
+            filter2_bins = [b for b in range(filter2.num_bins)]
         else:
             filter2_bins = [filter2.get_bin(i) for i in range(filter2.num_bins)]
 
@@ -3108,8 +3108,16 @@ class Tally(object):
             # Sum across the bins in the user-specified filter
             for i, self_filter in enumerate(self.filters):
                 if isinstance(self_filter, filter_type):
+                    shape = mean.shape
                     mean = np.take(mean, indices=bin_indices, axis=i)
                     std_dev = np.take(std_dev, indices=bin_indices, axis=i)
+
+                    # NumPy take introduces a new dimension in output array
+                    # for some special cases that must be removed
+                    if len(mean.shape) > len(shape):
+                        mean = np.squeeze(mean, axis=i)
+                        std_dev = np.squeeze(std_dev, axis=i)
+
                     mean = np.sum(mean, axis=i, keepdims=True)
                     std_dev = np.sum(std_dev**2, axis=i, keepdims=True)
                     std_dev = np.sqrt(std_dev)
@@ -3255,8 +3263,16 @@ class Tally(object):
             # Average across the bins in the user-specified filter
             for i, self_filter in enumerate(self.filters):
                 if isinstance(self_filter, filter_type):
+                    shape = mean.shape
                     mean = np.take(mean, indices=bin_indices, axis=i)
                     std_dev = np.take(std_dev, indices=bin_indices, axis=i)
+
+                    # NumPy take introduces a new dimension in output array
+                    # for some special cases that must be removed
+                    if len(mean.shape) > len(shape):
+                        mean = np.squeeze(mean, axis=i)
+                        std_dev = np.squeeze(std_dev, axis=i)
+
                     mean = np.nanmean(mean, axis=i, keepdims=True)
                     std_dev = np.nanmean(std_dev**2, axis=i, keepdims=True)
                     std_dev /= len(bin_indices)
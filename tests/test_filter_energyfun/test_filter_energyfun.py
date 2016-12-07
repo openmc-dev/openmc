7b7840a5fcd3990323b86897c176e03d0b3b29a4
Sterling Harper <smharper@mit.edu>
2016-12-07 15:38:18 -0500

    EnergyFunctionFilter.from_tabulated1d constructor

--- tests/test_filter_energyfun/test_filter_energyfun.py
+++ tests/test_filter_energyfun/test_filter_energyfun.py
@@ -18,18 +18,25 @@ class FilterEnergyFunHarness(PyAPITestHarness):
         # Add Am241 to the fuel.
         self._input_set.materials[1].add_nuclide('Am241', 1e-7)
 
-        # Make an EnergyFunctionFilter for the Am242m / Am242 branching ratio
-        # (from ENDF/B-VII.1 data).
+        # Define Am242m / Am242 branching ratio from ENDF/B-VII.1 data.
         x = [1e-5, 3.69e-1, 1e3, 1e5, 6e5, 1e6, 2e6, 4e6, 3e7]
         y = [0.1, 0.1, 0.1333, 0.158, 0.18467, 0.25618, 0.4297, 0.48, 0.48]
-        filt = openmc.EnergyFunctionFilter(x, y)
+
+        # Make an EnergyFunctionFilter directly from the x and y lists.
+        filt1 = openmc.EnergyFunctionFilter(x, y)
+
+        # Also make a filter with the .from_tabulated1d constructor.  Make sure
+        # the filters are identical.
+        tab1d = openmc.data.Tabulated1D(x, y)
+        filt2 = openmc.EnergyFunctionFilter.from_tabulated1d(tab1d)
+        assert filt1 == filt2, 'Error with the .from_tabulated1d constructor'
 
         # Make tallies.
         tallies = [openmc.Tally(), openmc.Tally()]
         for t in tallies:
             t.scores = ['(n,gamma)']
             t.nuclides = ['Am241']
-        tallies[1].filters = [filt]
+        tallies[1].filters = [filt1]
         self._input_set.tallies = openmc.Tallies(tallies)
 
         # Export inputs to xml.

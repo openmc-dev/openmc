f971313db21152f10eecb7f58796dacdfa1c2e6f
itayhorin <itayhorin@gmail.com>
2017-06-09 14:22:39 -0500

    Finalize MPI communicator from main()

--- src/finalize.F90
+++ src/finalize.F90
@@ -33,9 +33,6 @@ contains
 #ifdef MPI
     ! Free all MPI types
     call MPI_TYPE_FREE(MPI_BANK, mpi_err)
-
-    ! If MPI is in use and enabled, terminate it
-    call MPI_FINALIZE(mpi_err)
 #endif
 
   end subroutine openmc_finalize

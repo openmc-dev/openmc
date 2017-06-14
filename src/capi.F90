93af8871d5df716877f3bf22641485bc61239749
Paul Romano <paul.k.romano@gmail.com>
2017-06-14 15:25:56 -0500

    Add openmc_find function, move openmc_tally_results

--- src/capi.F90
+++ src/capi.F90
@@ -2,13 +2,27 @@ module openmc_capi
 
   use, intrinsic :: ISO_C_BINDING
 
-  use constants, only: K_BOLTZMANN
-  use eigenvalue, only: k_sum
+  use constants,       only: K_BOLTZMANN
+  use eigenvalue,      only: k_sum
+  use finalize,        only: openmc_finalize
+  use geometry,        only: find_cell
   use global
+  use initialize,      only: openmc_init
+  use particle_header, only: Particle
+  use plot,            only: openmc_plot_geometry
+  use simulation,      only: openmc_run
+  use volume_calc,     only: openmc_calculate_volumes
 
   private
+  public :: openmc_calculate_volumes
   public :: openmc_cell_set_temperature
+  public :: openmc_finalize
+  public :: openmc_find
+  public :: openmc_init
+  public :: openmc_plot_geometry
   public :: openmc_reset
+  public :: openmc_run
+  public :: openmc_tally_results
 
 contains
 
@@ -38,6 +52,37 @@ contains
   end function openmc_cell_set_temperature
 
 !===============================================================================
+! OPENMC_FIND determines the ID or a cell or material at a given point in space
+!===============================================================================
+
+  function openmc_find(xyz, rtype) result(id) bind(C)
+    real(C_DOUBLE), intent(in)        :: xyz(3) ! Cartesian point
+    integer(C_INT), intent(in), value :: rtype  ! 1 for cell, 2 for material
+    integer(C_INT) :: id
+
+    logical :: found
+    type(Particle) :: p
+
+    call p % initialize()
+    p % coord(1) % xyz(:) = xyz
+    p % coord(1) % uvw(:) = [ZERO, ZERO, ONE]
+    call find_cell(p, found)
+
+    id = -1
+    if (found) then
+      if (rtype == 1) then
+        id = cells(p % coord(p % n_coord) % cell) % id
+      elseif (rtype == 2) then
+        if (p % material == MATERIAL_VOID) then
+          id = 0
+        else
+          id = materials(p % material) % id
+        end if
+      end if
+    end if
+  end function openmc_find
+
+!===============================================================================
 ! OPENMC_RESET resets all tallies
 !===============================================================================
 
@@ -61,5 +106,27 @@ contains
     k_sum(:) = ZERO
 
   end subroutine openmc_reset
+
+!===============================================================================
+! OPENMC_TALLY_RESULTS returns a pointer to a tally results array along with its
+! shape. This allows a user to obtain in-memory tally results from Python
+! directly.
+!===============================================================================
+
+  subroutine openmc_tally_results(i, ptr, shape_) bind(C)
+    integer(C_INT), intent(in), value :: i
+    type(C_PTR),    intent(out) :: ptr
+    integer(C_INT), intent(out) :: shape_(3)
+
+    ptr = C_NULL_PTR
+    if (allocated(tallies)) then
+      if (i >= 1 .and. i <= size(tallies)) then
+        if (allocated(tallies(i) % results)) then
+          ptr = C_LOC(tallies(i) % results(1,1,1))
+          shape_(:) = shape(tallies(i) % results)
+        end if
+      end if
+    end if
+  end subroutine openmc_tally_results
 
 end module openmc_capi

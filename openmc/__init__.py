e2ee8137fce4c95a6320ff636153641040fe7281
itayhorin <itayhorin@gmail.com>
2017-07-17 07:58:35 -0500

    Add context manager for shared library calls

--- openmc/__init__.py
+++ openmc/__init__.py
@@ -27,6 +27,6 @@ from openmc.particle_restart import *
 from openmc.mixin import *
 from openmc.plotter import *
 from openmc.search import *
-from openmc.capi import lib, OpenMCLibrary
+from openmc.capi import *
 
 __version__ = '0.9.0'

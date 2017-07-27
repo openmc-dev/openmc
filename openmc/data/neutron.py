0fc12b5ff9eedaa6a269c7777cd8dbdee8f9cc9a
itayhorin <itayhorin@gmail.com>
2017-07-27 11:32:15 -0600

    Make use of openmc.data.neutron.get_metadata()
    Changed _get_metadata() to a public function and
    used it in openmc-update-inputs

--- openmc/data/neutron.py
+++ openmc/data/neutron.py
@@ -34,7 +34,7 @@ from openmc.mixin import EqualityMixin
 _RESONANCE_ENERGY_GRID = np.logspace(-3, 3, 61)
 
 
-def _get_metadata(zaid, metastable_scheme='nndc'):
+def get_metadata(zaid, metastable_scheme='nndc'):
     """Return basic identifying data for a nuclide with a given ZAID.
 
     Parameters
@@ -677,7 +677,7 @@ class IncidentNeutron(EqualityMixin):
         # If mass number hasn't been specified, make an educated guess
         zaid, xs = ace.name.split('.')
         name, element, Z, mass_number, metastable = \
-            _get_metadata(int(zaid), metastable_scheme)
+            get_metadata(int(zaid), metastable_scheme)
 
         # Assign temperature to the running list
         kTs = [ace.temperature*EV_PER_MEV]

c46d0a76814636c5e1d320a53b4ad86d5fe5b4cf
Paul Romano <paul.k.romano@gmail.com>
2017-09-29 09:26:14 -0500

    Remove __all__ from openmc.stats modules

--- openmc/stats/univariate.py
+++ openmc/stats/univariate.py
@@ -11,10 +11,6 @@ import openmc.checkvalue as cv
 from openmc.mixin import EqualityMixin
 
 
-__all__ = ['Univariate', 'Discrete', 'Uniform', 'Maxwell', 'Watt', 'Tabular',
-           'Legendre', 'Mixture']
-
-
 _INTERPOLATION_SCHEMES = ['histogram', 'linear-linear', 'linear-log',
                           'log-linear', 'log-log']
 

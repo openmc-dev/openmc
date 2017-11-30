87cc70034604c4f6332499883c48a449c44e04ae
itayhorin <itayhorin@gmail.com>
2017-11-29 22:23:47 -0600

    Make Nuclide, Element, and Macroscopic subclasses of str

--- openmc/nuclide.py
+++ openmc/nuclide.py
@@ -5,7 +5,7 @@ from six import string_types
 import openmc.checkvalue as cv
 
 
-class Nuclide(object):
+class Nuclide(str):
     """A nuclide that can be used in a material.
 
     Parameters
@@ -20,54 +20,22 @@ class Nuclide(object):
 
     """
 
-    def __init__(self, name=''):
+    def __new__(cls, name):
         # Initialize class attributes
-        self._name = ''
+        orig_name = name
 
-        # Set the Material class attributes
-        self.name = name
+        if '-' in name:
+            name = name.replace('-', '')
+            name = name.replace('Nat', '0')
+            if name.endswith('m'):
+                name = name[:-1] + '_m1'
 
-    def __eq__(self, other):
-        if isinstance(other, Nuclide):
-            if self.name != other.name:
-                return False
-            else:
-                return True
-        elif isinstance(other, string_types) and other == self.name:
-            return True
-        else:
-            return False
+            msg = 'OpenMC nuclides follow the GND naming convention. Nuclide ' \
+                  '"{}" is being renamed as "{}".'.format(orig_name, name)
+            warnings.warn(msg)
 
-    def __ne__(self, other):
-        return not self == other
-
-    def __gt__(self, other):
-        return repr(self) > repr(other)
-
-    def __lt__(self, other):
-        return not self > other
-
-    def __hash__(self):
-        return hash(repr(self))
-
-    def __repr__(self):
-        return 'Nuclide    -    {0}\n'.format(self._name)
+        return super(Nuclide, cls).__new__(cls, name)
 
     @property
     def name(self):
-        return self._name
-
-    @name.setter
-    def name(self, name):
-        cv.check_type('name', name, string_types)
-        self._name = name
-
-        if '-' in name:
-            self._name = name.replace('-', '')
-            self._name = self._name.replace('Nat', '0')
-            if self._name.endswith('m'):
-                self._name = self._name[:-1] + '_m1'
-
-            msg = 'OpenMC nuclides follow the GND naming convention. Nuclide ' \
-                  '"{}" is being renamed as "{}".'.format(name, self._name)
-            warnings.warn(msg)
+        return self

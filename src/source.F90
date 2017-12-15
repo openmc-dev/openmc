8e6b61711644effe17af82d58ddb196e748be798
Brody Bassett <brbass@umich.edu>
2017-12-15 02:19:50 -0500

    Added a check for minimum energy in the source sampling. Set multigroup min and max energies and removed check for these after samping the source.

--- src/source.F90
+++ src/source.F90
@@ -129,15 +129,9 @@ contains
 
     ! If running in MG, convert site % E to group
     if (.not. run_CE) then
-      if (site % E <= rev_energy_bins(1)) then
-        site % E = real(num_energy_groups, 8)
-      else if (site % E > rev_energy_bins(num_energy_groups + 1)) then
-        site % E = real(1, 8)
-      else
-        site % E = real(binary_search(rev_energy_bins, num_energy_groups + 1, &
-             site % E), 8)
-        site % E = num_energy_groups + 1 - site % E
-      end if
+      site % E = real(binary_search(rev_energy_bins, num_energy_groups + 1, &
+           site % E), 8)
+      site % E = num_energy_groups + 1 - site % E
     end if
 
     ! Set the random number generator back to the tracking stream.

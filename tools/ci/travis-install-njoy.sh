dcde66e330de3350624f746100e6de2f8261b69e
itayhorin <itayhorin@gmail.com>
2017-12-18 20:58:31 +0700

    Build NJOY without shared library

--- tools/ci/travis-install-njoy.sh
+++ tools/ci/travis-install-njoy.sh
@@ -5,4 +5,4 @@ git clone https://github.com/njoy/NJOY2016
 cd NJOY2016
 sed -i -e 's/5\.1/4.8/' CMakeLists.txt
 mkdir build && cd build
-cmake .. && make 2>/dev/null && sudo make install
+cmake -Dstatic=on .. && make 2>/dev/null && sudo make install

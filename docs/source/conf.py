551230b057bf6fdf6de0617b5ef5b1e721edd96f
itayhorin <itayhorin@gmail.com>
2018-01-01 15:19:51 -0600

    Happy new year 2018!

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -70,7 +70,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'OpenMC'
-copyright = u'2011-2017, Massachusetts Institute of Technology'
+copyright = u'2011-2018, Massachusetts Institute of Technology'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the

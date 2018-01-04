17cc1fdfe8c5e9fc0a066658f87d346e3ba9f498
itayhorin <itayhorin@gmail.com>
2018-01-04 15:28:15 -0600

    Implement spatial Legendre filter

---
+++ src/tallies/tally_filter_sptl_legendre.F90
@@ -0,0 +1,91 @@
+module tally_filter_sptl_legendre
+
+  use, intrinsic :: ISO_C_BINDING
+
+  use hdf5, only: HID_T
+
+  use constants
+  use error
+  use hdf5_interface
+  use math,                only: calc_pn
+  use particle_header,     only: Particle
+  use string,              only: to_str
+  use tally_filter_header
+  use xml_interface
+
+  implicit none
+  private
+
+!===============================================================================
+! SpatialLEGENDREFILTER gives Legendre moments
+!===============================================================================
+
+  type, public, extends(TallyFilter) :: SpatialLegendreFilter
+    integer :: order
+  contains
+    procedure :: from_xml
+    procedure :: get_all_bins
+    procedure :: to_statepoint
+    procedure :: text_label
+  end type SpatialLegendreFilter
+
+contains
+
+!===============================================================================
+! SpatialLegendreFilter methods
+!===============================================================================
+
+  subroutine from_xml(this, node)
+    class(SpatialLegendreFilter), intent(inout) :: this
+    type(XMLNode), intent(in) :: node
+
+    ! Get specified order
+    call get_node_value(node, "order", this % order)
+    this % n_bins = this % order + 1
+  end subroutine from_xml
+
+  subroutine get_all_bins(this, p, estimator, match)
+    class(SpatialLegendreFilter), intent(in)  :: this
+    type(Particle),      intent(in)  :: p
+    integer,             intent(in)  :: estimator
+    type(TallyFilterMatch),   intent(inout) :: match
+
+    integer :: i
+    real(8) :: wgt
+    real(8) :: leg_x
+    real(8) :: xmin
+    real(8) :: xmax
+
+    ! TODO: Use recursive formula to calculate higher orders
+    do i = 0, this % order
+    !i is the order, second var is the value normalized between 0 and 1 given by
+    ! norm_x = 2*(leg_x-xmin)/(xmax-xmin)-1
+      wgt = 2.0*(leg_x - xmin)/(xmax - xmin) - 1
+      call match % bins % push_back(i + 1)
+      call match % weights % push_back(wgt)
+    end do
+  end subroutine get_all_bins
+
+  subroutine to_statepoint(this, filter_group)
+    class(SpatialLegendreFilter), intent(in) :: this
+    integer(HID_T),      intent(in) :: filter_group
+
+    call write_dataset(filter_group, "type", "legendre")
+    call write_dataset(filter_group, "n_bins", this % n_bins)
+    call write_dataset(filter_group, "order", this % order)
+  end subroutine to_statepoint
+
+  function text_label(this, bin) result(label)
+    class(SpatialLegendreFilter), intent(in) :: this
+    integer,             intent(in) :: bin
+    character(MAX_LINE_LEN)         :: label
+
+    label = "Legendre expansion, P" // trim(to_str(bin - 1))
+  end function text_label
+
+!===============================================================================
+!                               C API FUNCTIONS
+!===============================================================================
+
+
+end module tally_filter_sptl_legendre

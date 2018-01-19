27ee2bc038d6865d4a9b78be1fa6c49e713efd30
itayhorin <itayhorin@gmail.com>
2018-01-18 21:16:50 -0500

    Move brackets to match proposed C++ styleguide

--- src/surface_header.C
+++ src/surface_header.C
@@ -23,8 +23,7 @@ Surface **surfaces_c;
 // construct closed volumes (cells)
 //==============================================================================
 
-class Surface
-{
+class Surface {
   int id;                    // Unique ID
   int neighbor_pos[],        // List of cells on positive side
       neighbor_neg[];        // List of cells on negative side
@@ -42,34 +41,28 @@ public:
 };
 
 bool
-Surface::sense(double xyz[3], double uvw[3])
-{
+Surface::sense(double xyz[3], double uvw[3]) {
   // Evaluate the surface equation at the particle's coordinates to determine
   // which side the particle is on.
   const double f = evaluate(xyz);
 
   // Check which side of surface the point is on.
   bool s;
-  if (fabs(f) < FP_COINCIDENT)
-  {
+  if (fabs(f) < FP_COINCIDENT) {
     // Particle may be coincident with this surface. To determine the sense, we
     // look at the direction of the particle relative to the surface normal (by
     // default in the positive direction) via their dot product.
     double norm[3];
     normal(xyz, norm);
     s = (uvw[0] * norm[0] + uvw[1] * norm[1] + uvw[2] * norm[2] > 0.0);
-  }
-  else
-  {
+  } else {
     s = (f > 0.0);
   }
   return s;
 }
 
 void
-Surface::reflect(double xyz[3], double uvw[3])
-{
-
+Surface::reflect(double xyz[3], double uvw[3]) {
   // Determine projection of direction onto normal and squared magnitude of
   // normal.
   double norm[3];
@@ -85,8 +78,7 @@ Surface::reflect(double xyz[3], double uvw[3])
 
 //==============================================================================
 
-class SurfaceZPlane : public Surface
-{
+class SurfaceZPlane : public Surface {
   double z0;
 public:
   SurfaceZPlane(pugi::xml_node surf_node);
@@ -95,26 +87,22 @@ public:
   void normal(double xyz[3], double uvw[3]);
 };
 
-SurfaceZPlane::SurfaceZPlane(pugi::xml_node surf_node)
-{
+SurfaceZPlane::SurfaceZPlane(pugi::xml_node surf_node) {
   const char *coeffs = surf_node.attribute("coeffs").value();
   int stat = sscanf(coeffs, "%lf", &z0);
-  if (stat != 1)
-  {
+  if (stat != 1) {
     std::cout << "Something went wrong reading surface coeffs!" << std::endl;
   }
 }
 
 double
-SurfaceZPlane::evaluate(double xyz[3])
-{
+SurfaceZPlane::evaluate(double xyz[3]) {
   double f = xyz[2] - z0;
   return f;
 }
 
 double
-SurfaceZPlane::distance(double xyz[3], double uvw[3], bool coincident)
-{
+SurfaceZPlane::distance(double xyz[3], double uvw[3], bool coincident) {
   double f = z0 - xyz[2];
   double d;
   if (coincident or fabs(f) < FP_COINCIDENT or uvw[2] == 0.0)
@@ -130,8 +118,7 @@ SurfaceZPlane::distance(double xyz[3], double uvw[3], bool coincident)
 }
 
 void
-SurfaceZPlane::normal(double xyz[3], double uvw[3])
-{
+SurfaceZPlane::normal(double xyz[3], double uvw[3]) {
   uvw[0] = 0.0;
   uvw[1] = 0.0;
   uvw[2] = 1.0;
@@ -139,8 +126,7 @@ SurfaceZPlane::normal(double xyz[3], double uvw[3])
 
 //==============================================================================
 
-class SurfaceZCylinder : public Surface
-{
+class SurfaceZCylinder : public Surface {
   double x0, y0, r;
 public:
   SurfaceZCylinder(pugi::xml_node surf_node);
@@ -149,19 +135,16 @@ public:
   void normal(double xyz[3], double uvw[3]);
 };
 
-SurfaceZCylinder::SurfaceZCylinder(pugi::xml_node surf_node)
-{
+SurfaceZCylinder::SurfaceZCylinder(pugi::xml_node surf_node) {
   const char *coeffs = surf_node.attribute("coeffs").value();
   int stat = sscanf(coeffs, "%lf %lf %lf", &x0, &y0, &r);
-  if (stat != 3)
-  {
+  if (stat != 3) {
     std::cout << "Something went wrong reading surface coeffs!" << std::endl;
   }
 }
 
 double
-SurfaceZCylinder::evaluate(double xyz[3])
-{
+SurfaceZCylinder::evaluate(double xyz[3]) {
   const double x = xyz[0] - x0;
   const double y = xyz[1] - y0;
   double f = x*x + y*y - r*r;
@@ -169,8 +152,7 @@ SurfaceZCylinder::evaluate(double xyz[3])
 }
 
 double
-SurfaceZCylinder::distance(double xyz[3], double uvw[3], bool coincident)
-{
+SurfaceZCylinder::distance(double xyz[3], double uvw[3], bool coincident) {
   double a = 1.0 - uvw[2]*uvw[2];  // u^2 + v^2
 
   if (a == 0.0) return INFTY;
@@ -181,32 +163,27 @@ SurfaceZCylinder::distance(double xyz[3], double uvw[3], bool coincident)
   double c = x*x + y*y - r*r;
   double quad = k*k - a*c;
 
-  if (quad < 0.0)
-  {
+  if (quad < 0.0) {
     // No intersection with cylinder.
     return INFTY;
-  }
-  else if (coincident or fabs(c) < FP_COINCIDENT)
-  {
+
+  } else if (coincident or fabs(c) < FP_COINCIDENT) {
     // Particle is on the cylinder, thus one distance is positive/negative
     // and the other is zero. The sign of k determines if we are facing in or
     // out.
     if (k >= 0.0) return INFTY;
     else return (-k + sqrt(quad)) / a;
-  }
-  else if (c < 0.0)
-  {
+
+  } else if (c < 0.0) {
     // Particle is inside the cylinder, thus one distance must be negative
     // and one must be positive. The positive distance will be the one with
     // negative sign on sqrt(quad)
     return (-k + sqrt(quad)) / a;
-  }
-  else
-  {
+
+  } else {
     // Particle is outside the cylinder, thus both distances are either
     // positive or negative. If positive, the smaller distance is the one
     // with positive sign on sqrt(quad)
-
     double d = (-k - sqrt(quad)) / a;
     if (d < 0.0) return INFTY;
     return d;
@@ -214,8 +191,7 @@ SurfaceZCylinder::distance(double xyz[3], double uvw[3], bool coincident)
 }
 
 void
-SurfaceZCylinder::normal(double xyz[3], double uvw[3])
-{
+SurfaceZCylinder::normal(double xyz[3], double uvw[3]) {
   uvw[0] = 2.0 * (xyz[0] - x0);
   uvw[1] = 2.0 * (xyz[1] - y0);
   uvw[2] = 0.0;
@@ -224,13 +200,11 @@ SurfaceZCylinder::normal(double xyz[3], double uvw[3])
 //==============================================================================
 
 extern "C" void
-read_surfaces(pugi::xml_node *node)
-{
+read_surfaces(pugi::xml_node *node) {
   // Count the number of surfaces.
   int n_surfaces = 0;
   for (pugi::xml_node surf_node = node->child("surface"); surf_node;
-       surf_node = surf_node.next_sibling("surface"))
-  {
+       surf_node = surf_node.next_sibling("surface")) {
     n_surfaces += 1;
   }
 
@@ -242,10 +216,8 @@ read_surfaces(pugi::xml_node *node)
     pugi::xml_node surf_node;
     int i_surf;
     for (surf_node = node->child("surface"), i_surf = 0; surf_node;
-         surf_node = surf_node.next_sibling("surface"), i_surf++)
-    {
-      if (surf_node.attribute("type"))
-      {
+         surf_node = surf_node.next_sibling("surface"), i_surf++) {
+      if (surf_node.attribute("type")) {
         const pugi::char_t *surf_type = surf_node.attribute("type").value();
         if (strcmp(surf_type, "z-cylinder") == 0)
         {
@@ -268,13 +240,11 @@ read_surfaces(pugi::xml_node *node)
 //==============================================================================
 
 extern "C" bool
-surface_sense(int surf_ind, double xyz[3], double uvw[3])
-{
+surface_sense(int surf_ind, double xyz[3], double uvw[3]) {
   return surfaces_c[surf_ind]->sense(xyz, uvw);
 }
 
 extern "C" double
-surface_distance(int surf_ind, double xyz[3], double uvw[3], bool coincident)
-{
+surface_distance(int surf_ind, double xyz[3], double uvw[3], bool coincident) {
   return surfaces_c[surf_ind]->distance(xyz, uvw, coincident);
 }

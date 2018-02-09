4298c50c3fdf2796b0d6020b00889b8c1db691d9
Paul Romano <paul.k.romano@gmail.com>
2018-02-09 09:28:09 -0600

    Use numpy 1.13 when testing since float formatting changed in 1.14

--- tools/ci/travis-install.sh
+++ tools/ci/travis-install.sh
@@ -4,8 +4,11 @@ set -ex
 # Install NJOY 2016
 ./tools/ci/travis-install-njoy.sh
 
-# Running OpenMC's setup.py requires numpy/cython already
-pip install numpy cython
+# Running OpenMC's setup.py requires numpy/cython already. NumPy float
+# formatting changed in version 1.14, so stick with a lower version until we can
+# handle it in our test suite
+pip install 'numpy<1.14'
+pip install cython
 
 # pytest installed by default -- make sure we get latest
 pip install --upgrade pytest

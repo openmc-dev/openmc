592fae536f31521d09bd68a80d465550e6b34978
Paul Romano <paul.k.romano@gmail.com>
2018-02-09 16:17:12 -0600

    Fix file locations for depletion tests. Convert regression ones to pytest

--- tests/regression_tests/test_deplete_full.py
+++ tests/regression_tests/test_deplete_full.py
@@ -1,121 +1,105 @@
 """ Full system test suite. """
 
+from math import floor
 import shutil
 import unittest
-from os.path import join, dirname
+from pathlib import Path
 
 import numpy as np
 import openmc.deplete
 from openmc.deplete import results
 from openmc.deplete import utilities
 
-from . import example_geometry
+from .example_geometry import generate_problem
 
 
-class TestFull(unittest.TestCase):
-    """ Full system test suite.
+def test_full(run_in_tmpdir):
+    """Full system test suite.
 
     Runs an entire OpenMC simulation with depletion coupling and verifies
     that the outputs match a reference file.  Sensitive to changes in
     OpenMC.
+
+    This test runs a complete OpenMC simulation and tests the outputs.
+    It will take a while.
     """
 
-    def test_full(self):
-        """
-        This test runs a complete OpenMC simulation and tests the outputs.
-        It will take a while.
-        """
+    n_rings = 2
+    n_wedges = 4
 
-        n_rings = 2
-        n_wedges = 4
+    # Load geometry from example
+    geometry, lower_left, upper_right = generate_problem(n_rings, n_wedges)
 
-        # Load geometry from example
-        geometry, lower_left, upper_right = \
-            example_geometry.generate_problem(n_rings=n_rings, n_wedges=n_wedges)
+    # Create dt vector for 3 steps with 15 day timesteps
+    dt1 = 15.*24*60*60  # 15 days
+    dt2 = 1.5*30*24*60*60  # 1.5 months
+    N = floor(dt2/dt1)
+    dt = np.full(N, dt1)
 
-        # Create dt vector for 3 steps with 15 day timesteps
-        dt1 = 15*24*60*60  # 15 days
-        dt2 = 1.5*30*24*60*60  # 1.5 months
-        N = np.floor(dt2/dt1)
+    # Create settings variable
+    settings = openmc.deplete.OpenMCSettings()
 
-        dt = np.repeat([dt1], N)
 
-        # Create settings variable
-        settings = openmc.deplete.OpenMCSettings()
+    chain_file = str(Path(__file__).parents[2] / 'chains' / 'chain_simple.xml')
+    settings.chain_file = chain_file
+    settings.openmc_call = "openmc"
+    settings.openmc_npernode = 2
+    settings.particles = 100
+    settings.batches = 100
+    settings.inactive = 40
+    settings.lower_left = lower_left
+    settings.upper_right = upper_right
+    settings.entropy_dimension = [10, 10, 1]
 
-        settings.chain_file = "chains/chain_simple.xml"
-        settings.openmc_call = "openmc"
-        settings.openmc_npernode = 2
-        settings.particles = 100
-        settings.batches = 100
-        settings.inactive = 40
-        settings.lower_left = lower_left
-        settings.upper_right = upper_right
-        settings.entropy_dimension = [10, 10, 1]
+    settings.round_number = True
+    settings.constant_seed = 1
 
-        settings.round_number = True
-        settings.constant_seed = 1
+    joule_per_mev = 1.6021766208e-13
+    settings.power = 2.337e15*4*joule_per_mev  # MeV/second cm from CASMO
+    settings.dt_vec = dt
+    settings.output_dir = "test_full"
 
-        joule_per_mev = 1.6021766208e-13
-        settings.power = 2.337e15*4*joule_per_mev  # MeV/second cm from CASMO
-        settings.dt_vec = dt
-        settings.output_dir = "test_full"
+    op = openmc.deplete.OpenMCOperator(geometry, settings)
 
-        op = openmc.deplete.OpenMCOperator(geometry, settings)
+    # Perform simulation using the predictor algorithm
+    openmc.deplete.integrator.predictor(op)
 
-        # Perform simulation using the predictor algorithm
-        openmc.deplete.integrator.predictor(op)
+    # Load the files
+    res_test = results.read_results(settings.output_dir + "/results.h5")
 
-        # Load the files
-        res_test = results.read_results(settings.output_dir + "/results.h5")
+    # Load the reference
+    filename = str(Path(__file__).with_name('test_reference.h5'))
+    res_old = results.read_results(filename)
 
-        # Load the reference
-        filename = join(dirname(__file__), 'test_reference.h5')
-        res_old = results.read_results(filename)
+    # Assert same mats
+    for mat in res_old[0].mat_to_ind:
+        assert mat in res_test[0].mat_to_ind, \
+            "Material {} not in new results.".format(mat)
+    for nuc in res_old[0].nuc_to_ind:
+        assert nuc in res_test[0].nuc_to_ind, \
+            "Nuclide {} not in new results.".format(nuc)
 
-        # Assert same mats
-        for mat in res_old[0].mat_to_ind:
-            self.assertIn(mat, res_test[0].mat_to_ind,
-                          msg="Cell " + mat + " not in new results.")
-        for nuc in res_old[0].nuc_to_ind:
-            self.assertIn(nuc, res_test[0].nuc_to_ind,
-                          msg="Nuclide " + nuc + " not in new results.")
+    for mat in res_test[0].mat_to_ind:
+        assert mat in res_old[0].mat_to_ind, \
+            "Material {} not in old results.".format(mat)
+    for nuc in res_test[0].nuc_to_ind:
+        assert nuc in res_old[0].nuc_to_ind, \
+            "Nuclide {} not in old results.".format(nuc)
 
-        for mat in res_test[0].mat_to_ind:
-            self.assertIn(mat, res_old[0].mat_to_ind,
-                          msg="Cell " + mat + " not in old results.")
+    tol = 1.0e-6
+    for mat in res_test[0].mat_to_ind:
         for nuc in res_test[0].nuc_to_ind:
-            self.assertIn(nuc, res_old[0].nuc_to_ind,
-                          msg="Nuclide " + nuc + " not in old results.")
+            _, y_test = utilities.evaluate_single_nuclide(res_test, mat, nuc)
+            _, y_old = utilities.evaluate_single_nuclide(res_old, mat, nuc)
 
-        for mat in res_test[0].mat_to_ind:
-            for nuc in res_test[0].nuc_to_ind:
-                _, y_test = utilities.evaluate_single_nuclide(res_test, mat, nuc)
-                _, y_old = utilities.evaluate_single_nuclide(res_old, mat, nuc)
+            # Test each point
+            correct = True
+            for i, ref in enumerate(y_old):
+                if ref != y_test[i]:
+                    if ref != 0.0:
+                        correct = np.abs(y_test[i] - ref) / ref <= tol
+                    else:
+                        correct = False
 
-                # Test each point
-
-                tol = 1.0e-6
-
-                correct = True
-                for i, ref in enumerate(y_old):
-                    if ref != y_test[i]:
-                        if ref != 0.0:
-                            if np.abs(y_test[i] - ref) / ref > tol:
-                                correct = False
-                        else:
-                            correct = False
-
-                self.assertTrue(correct,
-                                msg="Discrepancy in mat " + mat + " and nuc " + nuc
-                                + "\n" + str(y_old) + "\n" + str(y_test))
-
-    def tearDown(self):
-        """ Clean up files"""
-        openmc.deplete.comm.barrier()
-        if openmc.deplete.comm.rank == 0:
-            shutil.rmtree("test_full", ignore_errors=True)
-
-
-if __name__ == '__main__':
-    unittest.main()
+            assert correct, "Discrepancy in mat {} and nuc {}\n{}\n{}".format(
+                mat, nuc, y_old, y_test)
--- tests/regression_tests/test_deplete_utilities.py
+++ tests/regression_tests/test_deplete_utilities.py
@@ -1,71 +1,54 @@
-""" Full system test suite. """
+""" Tests the utilities classes.
 
-import unittest
-from os.path import join, dirname
+This also tests the results read/write code.
+"""
+
+from pathlib import Path
 
 import numpy as np
+import pytest
 from openmc.deplete import results
 from openmc.deplete import utilities
 
 
-class TestUtilities(unittest.TestCase):
-    """ Tests the utilities classes.
-
-    This also tests the results read/write code.
-    """
-
-    def test_evaluate_single_nuclide(self):
-        """ Tests evaluating single nuclide utility code.
-        """
-
-        # Load the reference
-        filename = join(dirname(__file__), 'test_reference.h5')
-        res = results.read_results(filename)
-
-        x, y = utilities.evaluate_single_nuclide(res, "1", "Xe135")
-
-        x_ref = [0.0, 1296000.0, 2592000.0, 3888000.0]
-        y_ref = [6.6747328233649218e+08, 3.5519299354458244e+14,
-                 3.4599104054580338e+14, 3.3821165110278112e+14]
-
-        np.testing.assert_array_equal(x, x_ref)
-        np.testing.assert_array_equal(y, y_ref)
-
-    def test_evaluate_reaction_rate(self):
-        """ Tests evaluating reaction rate utility code.
-        """
-
-        # Load the reference
-        filename = join(dirname(__file__), 'test_reference.h5')
-        res = results.read_results(filename)
-
-        x, y = utilities.evaluate_reaction_rate(res, "1", "Xe135", "(n,gamma)")
-
-        x_ref = [0.0, 1296000.0, 2592000.0, 3888000.0]
-        xe_ref = np.array([6.6747328233649218e+08, 3.5519299354458244e+14,
-                           3.4599104054580338e+14, 3.3821165110278112e+14])
-        r_ref = np.array([4.0643598574337784e-05, 4.1457730544386974e-05,
-                          3.4121248544056681e-05, 3.9204686657643301e-05])
-
-        np.testing.assert_array_equal(x, x_ref)
-        np.testing.assert_array_equal(y, xe_ref * r_ref)
-
-    def test_evaluate_eigenvalue(self):
-        """ Tests evaluating eigenvalue
-        """
-
-        # Load the reference
-        filename = join(dirname(__file__), 'test_reference.h5')
-        res = results.read_results(filename)
-
-        x, y = utilities.evaluate_eigenvalue(res)
-
-        x_ref = [0.0, 1296000.0, 2592000.0, 3888000.0]
-        y_ref = [1.1921986054449838, 1.1712785643938586, 1.1927099024502694, 1.2269183590698847]
-
-        np.testing.assert_array_equal(x, x_ref)
-        np.testing.assert_array_equal(y, y_ref)
+@pytest.fixture
+def res():
+    """Load the reference results"""
+    filename = str(Path(__file__).with_name('test_reference.h5'))
+    return results.read_results(filename)
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_evaluate_single_nuclide(res):
+    """Tests evaluating single nuclide utility code."""
+    x, y = utilities.evaluate_single_nuclide(res, "1", "Xe135")
+
+    x_ref = [0.0, 1296000.0, 2592000.0, 3888000.0]
+    y_ref = [6.6747328233649218e+08, 3.5519299354458244e+14,
+             3.4599104054580338e+14, 3.3821165110278112e+14]
+
+    np.testing.assert_array_equal(x, x_ref)
+    np.testing.assert_array_equal(y, y_ref)
+
+def test_evaluate_reaction_rate(res):
+    """Tests evaluating reaction rate utility code."""
+    x, y = utilities.evaluate_reaction_rate(res, "1", "Xe135", "(n,gamma)")
+
+    x_ref = [0.0, 1296000.0, 2592000.0, 3888000.0]
+    xe_ref = np.array([6.6747328233649218e+08, 3.5519299354458244e+14,
+                       3.4599104054580338e+14, 3.3821165110278112e+14])
+    r_ref = np.array([4.0643598574337784e-05, 4.1457730544386974e-05,
+                      3.4121248544056681e-05, 3.9204686657643301e-05])
+
+    np.testing.assert_array_equal(x, x_ref)
+    np.testing.assert_array_equal(y, xe_ref * r_ref)
+
+
+def test_evaluate_eigenvalue(res):
+    """Tests evaluating eigenvalue."""
+    x, y = utilities.evaluate_eigenvalue(res)
+
+    x_ref = [0.0, 1296000.0, 2592000.0, 3888000.0]
+    y_ref = [1.1921986054449838, 1.1712785643938586, 1.1927099024502694, 1.2269183590698847]
+
+    np.testing.assert_array_equal(x, x_ref)
+    np.testing.assert_array_equal(y, y_ref)

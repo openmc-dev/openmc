11336f875f67e8fca8c0ded359d7b66d2316cb22
Sterling Harper <smharper@mit.edu>
2018-02-14 17:48:24 -0500

    Add C++ RectLattice distance_to_boundary

--- src/lattice.h
+++ src/lattice.h
@@ -3,6 +3,7 @@
 
 #include <array>
 #include <cstdint>
+#include <limits>  // For numeric_limits
 #include <map>
 #include <string>
 #include <vector>
@@ -12,6 +13,14 @@
 
 
 namespace openmc {
+
+//==============================================================================
+// Constants that should eventually be moved out of this file
+//==============================================================================
+
+extern "C" double FP_PRECISION;
+constexpr double INFTY{std::numeric_limits<double>::max()};
+constexpr int C_NONE {-1};
 
 //==============================================================================
 // Constants
@@ -51,6 +60,9 @@ public:
 
   //virtual bool are_valid_indices(const int i_xyz[3]) const = 0;
 
+  virtual std::pair<double, std::array<int, 3>>
+  distance(const double xyz[3], const double uvw[3]) const = 0;
+
   //virtual void get_indices(const double global_xyz[3], int i_xyz[3]) const = 0;
 
   //virtual void get_local_xyz(const double global_xyz[3], const int i_xyz[3],
@@ -74,10 +86,13 @@ public:
 
   virtual ~RectLattice() {}
 
+  std::pair<double, std::array<int, 3>>
+  distance(const double xyz[3], const double uvw[3]) const;
+
 protected:
-  std::array<int, 3> n_cells;     //! Number of cells along each axis
-  std::array<int, 3> lower_left;  //! Global lower-left corner of the lattice
-  std::array<int, 3> pitch;       //! Lattice tile width along each axis
+  std::array<int, 3> n_cells;        //! Number of cells along each axis
+  std::array<double, 3> lower_left;  //! Global lower-left corner of the lattice
+  std::array<double, 3> pitch;       //! Lattice tile width along each axis
 };
 
 class HexLattice : public Lattice
@@ -86,6 +101,9 @@ public:
   explicit HexLattice(pugi::xml_node lat_node);
 
   virtual ~HexLattice() {}
+
+  std::pair<double, std::array<int, 3>>
+  distance(const double xyz[3], const double uvw[3]) const;
 };
 
 } //  namespace openmc

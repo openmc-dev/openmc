939d47cffa59fd8570a60fbb255cb4a4a8164dff
Paul Romano <paul.k.romano@gmail.com>
2018-02-14 12:46:23 -0600

    Simplify OpenMCSettings class (can properly delegate to openmc.Settings)

--- scripts/example_run.py
+++ scripts/example_run.py
@@ -1,6 +1,8 @@
 """An example file showing how to run a simulation."""
 
 import numpy as np
+import openmc
+from openmc.data import JOULE_PER_EV
 import openmc.deplete
 
 import example_geometry
@@ -15,20 +17,18 @@ N = np.floor(dt2/dt1)
 
 dt = np.repeat([dt1], N)
 
-# Create settings variable
+# Depletion settings
 settings = openmc.deplete.OpenMCSettings()
+settings.power = 2.337e15*4*JOULE_PER_EV*1e6  # MeV/second cm from CASMO
+settings.dt_vec = dt
+settings.output_dir = 'test'
 
+# OpenMC-delegated settings
 settings.particles = 1000
 settings.batches = 100
 settings.inactive = 40
-settings.lower_left = lower_left
-settings.upper_right = upper_right
-settings.entropy_dimension = [10, 10, 1]
-
-joule_per_mev = 1.6021766208e-13
-settings.power = 2.337e15*4*joule_per_mev  # MeV/second cm from CASMO
-settings.dt_vec = dt
-settings.output_dir = 'test'
+settings.source = openmc.Source(space=openmc.stats.Box(lower_left, upper_right))
+settings.verbosity = 3
 
 op = openmc.deplete.OpenMCOperator(geometry, settings)
 

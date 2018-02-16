94ef8cee81ce6acd33781a9b86814a0869376f69
itayhorin <itayhorin@gmail.com>
2018-02-16 17:49:42 -0500

    Move lattice get_indices and are_valid to C++

--- src/geometry_header.F90
+++ src/geometry_header.F90
@@ -95,6 +95,15 @@ module geometry_header
       integer(C_INT32_T)             :: id
     end function lattice_id_c
 
+    function lattice_are_valid_indices_c(lat_ptr, i_xyz) &
+         bind(C, name='lattice_are_valid_indices') result (is_valid)
+      use ISO_C_BINDING
+      implicit none
+      type(C_PTR),    intent(in), value :: lat_ptr
+      integer(C_INT), intent(in)        :: i_xyz(3)
+      logical(C_BOOL)                   :: is_valid
+    end function lattice_are_valid_indices_c
+
     subroutine lattice_distance_c(lat_ptr, xyz, uvw, i_xyz, d, lattice_trans) &
          bind(C, name='lattice_distance')
       use ISO_C_BINDING
@@ -106,6 +115,15 @@ module geometry_header
       real(C_DOUBLE), intent(out)       :: d
       integer(C_INT), intent(out)       :: lattice_trans(3)
     end subroutine lattice_distance_c
+
+    subroutine lattice_get_indices_c(lat_ptr, xyz, i_xyz) &
+         bind(C, name='lattice_get_indices')
+      use ISO_C_BINDING
+      implicit none
+      type(C_PTR),    intent(in), value :: lat_ptr
+      real(C_DOUBLE), intent(in)        :: xyz(3)
+      integer(C_INT), intent(out)       :: i_xyz(3)
+    end subroutine lattice_get_indices_c
 
     subroutine lattice_to_hdf5_c(lat_ptr, group) bind(C, name='lattice_to_hdf5')
       use ISO_C_BINDING
@@ -146,36 +164,15 @@ module geometry_header
   contains
 
     procedure :: id => lattice_id
+    procedure :: are_valid_indices => lattice_are_valid_indices
     procedure :: distance => lattice_distance
+    procedure :: get_indices => lattice_get_indices
     procedure :: to_hdf5 => lattice_to_hdf5
 
-    procedure(lattice_are_valid_indices_), deferred :: are_valid_indices
-    procedure(lattice_get_indices_),       deferred :: get_indices
     procedure(lattice_get_local_xyz_),     deferred :: get_local_xyz
   end type Lattice
 
   abstract interface
-
-!===============================================================================
-! ARE_VALID_INDICES returns .true. if the given lattice indices fit within the
-! bounds of the lattice.  Returns false otherwise.
-
-    function lattice_are_valid_indices_(this, i_xyz) result(is_valid)
-      import Lattice
-      class(Lattice), intent(in) :: this
-      integer,        intent(in) :: i_xyz(3)
-      logical                    :: is_valid
-    end function lattice_are_valid_indices_
-
-!===============================================================================
-! GET_INDICES returns the indices in a lattice for the given global xyz.
-
-    function lattice_get_indices_(this, global_xyz) result(i_xyz)
-      import Lattice
-      class(Lattice), intent(in) :: this
-      real(8),        intent(in) :: global_xyz(3)
-      integer                    :: i_xyz(3)
-    end function lattice_get_indices_
 
 !===============================================================================
 ! GET_LOCAL_XYZ returns the translated local version of the given global xyz.
@@ -199,8 +196,6 @@ module geometry_header
 
     contains
 
-    procedure :: are_valid_indices => valid_inds_rect
-    procedure :: get_indices => get_inds_rect
     procedure :: get_local_xyz => get_local_rect
   end type RectLattice
 
@@ -215,8 +210,6 @@ module geometry_header
 
     contains
 
-    procedure :: are_valid_indices => valid_inds_hex
-    procedure :: get_indices => get_inds_hex
     procedure :: get_local_xyz => get_local_hex
   end type HexLattice
 
@@ -294,6 +287,13 @@ contains
     id = lattice_id_c(this % ptr)
   end function lattice_id
 
+  function lattice_are_valid_indices(this, i_xyz) result (is_valid)
+    class(Lattice), intent(in) :: this
+    integer(C_INT), intent(in) :: i_xyz(3)
+    logical(C_BOOL)            :: is_valid
+    is_valid = lattice_are_valid_indices_c(this % ptr, i_xyz)
+  end function lattice_are_valid_indices
+
   subroutine lattice_distance(this, xyz, uvw, i_xyz, d, lattice_trans)
     class(Lattice), intent(in)  :: this
     real(C_DOUBLE), intent(in)  :: xyz(3)
@@ -304,127 +304,18 @@ contains
     call lattice_distance_c(this % ptr, xyz, uvw, i_xyz, d, lattice_trans)
   end subroutine lattice_distance
 
+  function lattice_get_indices(this, xyz) result(i_xyz)
+    class(Lattice), intent(in)  :: this
+    real(C_DOUBLE), intent(in)  :: xyz(3)
+    integer(C_INT)              :: i_xyz(3)
+    call lattice_get_indices_c(this % ptr, xyz, i_xyz)
+  end function lattice_get_indices
+
   subroutine lattice_to_hdf5(this, group)
     class(Lattice), intent(in) :: this
     integer(HID_T), intent(in) :: group
     call lattice_to_hdf5_c(this % ptr, group)
   end subroutine lattice_to_hdf5
-
-!===============================================================================
-
-  function valid_inds_rect(this, i_xyz) result(is_valid)
-    class(RectLattice), intent(in) :: this
-    integer,            intent(in) :: i_xyz(3)
-    logical                        :: is_valid
-
-    is_valid = all(i_xyz > 0 .and. i_xyz <= this % n_cells)
-  end function valid_inds_rect
-
-!===============================================================================
-
-  function valid_inds_hex(this, i_xyz) result(is_valid)
-    class(HexLattice), intent(in) :: this
-    integer,           intent(in) :: i_xyz(3)
-    logical                       :: is_valid
-
-    is_valid = (all(i_xyz > 0) .and. &
-               &i_xyz(1) < 2*this % n_rings .and. &
-               &i_xyz(2) < 2*this % n_rings .and. &
-               &i_xyz(1) + i_xyz(2) > this % n_rings .and. &
-               &i_xyz(1) + i_xyz(2) < 3*this % n_rings .and. &
-               &i_xyz(3) <= this % n_axial)
-  end function valid_inds_hex
-
-!===============================================================================
-
-  function get_inds_rect(this, global_xyz) result(i_xyz)
-    class(RectLattice), intent(in) :: this
-    real(8),            intent(in) :: global_xyz(3)
-    integer                        :: i_xyz(3)
-
-    real(8) :: xyz(3)  ! global_xyz alias
-
-    xyz = global_xyz
-
-    i_xyz(1) = ceiling((xyz(1) - this % lower_left(1))/this % pitch(1))
-    i_xyz(2) = ceiling((xyz(2) - this % lower_left(2))/this % pitch(2))
-    if (this % is_3d) then
-      i_xyz(3) = ceiling((xyz(3) - this % lower_left(3))/this % pitch(3))
-    else
-      i_xyz(3) = 1
-    end if
-  end function get_inds_rect
-
-!===============================================================================
-
-  function get_inds_hex(this, global_xyz) result(i_xyz)
-    class(HexLattice), intent(in) :: this
-    real(8),           intent(in) :: global_xyz(3)
-    integer                       :: i_xyz(3)
-
-    real(8) :: xyz(3)    ! global xyz relative to the center
-    real(8) :: alpha     ! Skewed coord axis
-    real(8) :: xyz_t(3)  ! Local xyz
-    real(8) :: d, d_min  ! Squared distance from cell centers
-    integer :: i, j, k   ! Iterators
-    integer :: k_min     ! Minimum distance index
-
-    xyz(1) = global_xyz(1) - this % center(1)
-    xyz(2) = global_xyz(2) - this % center(2)
-
-    ! Index z direction.
-    if (this % is_3d) then
-      xyz(3) = global_xyz(3) - this % center(3)
-      i_xyz(3) = ceiling(xyz(3)/this % pitch(2) + HALF*this % n_axial)
-    else
-      xyz(3) = global_xyz(3)
-      i_xyz(3) = 1
-    end if
-
-    ! Convert coordinates into skewed bases.  The (x, alpha) basis is used to
-    ! find the index of the global coordinates to within 4 cells.
-    alpha = xyz(2) - xyz(1) / sqrt(THREE)
-    i_xyz(1) = floor(xyz(1) / (sqrt(THREE) / TWO * this % pitch(1)))
-    i_xyz(2) = floor(alpha / this % pitch(1))
-
-    ! Add offset to indices (the center cell is (i_x, i_alpha) = (0, 0) but
-    ! the array is offset so that the indices never go below 1).
-    i_xyz(1) = i_xyz(1) + this % n_rings
-    i_xyz(2) = i_xyz(2) + this % n_rings
-
-    ! Calculate the (squared) distance between the particle and the centers of
-    ! the four possible cells.  Regular hexagonal tiles form a centroidal
-    ! Voronoi tessellation so the global xyz should be in the hexagonal cell
-    ! that it is closest to the center of.  This method is used over a
-    ! method that uses the remainders of the floor divisions above because it
-    ! provides better finite precision performance.  Squared distances are
-    ! used becasue they are more computationally efficient than normal
-    ! distances.
-    k = 1
-    d_min = INFINITY
-    do i = 0, 1
-      do j = 0, 1
-        xyz_t = this % get_local_xyz(global_xyz, i_xyz + [j, i, 0])
-        d = xyz_t(1)**2 + xyz_t(2)**2
-        if (d < d_min) then
-          d_min = d
-          k_min = k
-        end if
-        k = k + 1
-      end do
-    end do
-
-    ! Select the minimum squared distance which corresponds to the cell the
-    ! coordinates are in.
-    if (k_min == 2) then
-      i_xyz(1) = i_xyz(1) + 1
-    else if (k_min == 3) then
-      i_xyz(2) = i_xyz(2) + 1
-    else if (k_min == 4) then
-      i_xyz(1) = i_xyz(1) + 1
-      i_xyz(2) = i_xyz(2) + 1
-    end if
-  end function get_inds_hex
 
 !===============================================================================
 

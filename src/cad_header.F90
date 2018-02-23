e5c9fb6e73e78d2c0021eb5ee5102dd7a74172f7
itayhorin <itayhorin@gmail.com>
2018-02-23 11:22:12 -0600

    Adding a deallocation method for CAD pointer.

--- src/cad_header.F90
+++ src/cad_header.F90
@@ -8,12 +8,20 @@ module cad_header
   interface
      subroutine load_cad_geometry_c() bind(C)
      end subroutine load_cad_geometry_c
+
+     subroutine dealloc_cad_c() bind(C)
+     end subroutine dealloc_cad_c
+
   end interface
-     
+
 contains
 
   subroutine load_cad_geometry()
     call load_cad_geometry_c()
   end subroutine load_cad_geometry
-  
+
+  subroutine dealloc_cad()
+    call dealloc_cad_c()
+  end subroutine dealloc_cad
+
 end module cad_header

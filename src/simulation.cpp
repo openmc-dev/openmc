38634d16eef6c12c65f30301f6a418e06e21e87c
itayhorin <itayhorin@gmail.com>
2018-04-12 07:45:47 -0500

    Move openmc_run to C++ side

---
+++ src/simulation.cpp
@@ -0,0 +1,18 @@
+#include "openmc.h"
+
+// OPENMC_RUN encompasses all the main logic where iterations are performed
+// over the batches, generations, and histories in a fixed source or k-eigenvalue
+// calculation.
+
+int openmc_run() {
+  openmc_simulation_init();
+
+  int err = 0;
+  int status = 0;
+  while (status == 0 && err == 0) {
+    err = openmc_next_batch(&status);
+  }
+
+  openmc_simulation_finalize();
+  return err;
+}

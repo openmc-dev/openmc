cf614b1b0a229dba5dc2a25f9f225617ce44babc
itayhorin <itayhorin@gmail.com>
2018-08-09 13:10:49 -0500

    Update results for numpy 1.14+. Closes #1026.

--- tests/unit_tests/test_deplete_resultslist.py
+++ tests/unit_tests/test_deplete_resultslist.py
@@ -19,21 +19,22 @@ def test_get_atoms(res):
     t, n = res.get_atoms("1", "Xe135")
 
     t_ref = [0.0, 1296000.0, 2592000.0, 3888000.0]
-    n_ref = [6.6747328233649218e+08, 3.5421791038348462e+14,
-             3.6208592242443462e+14, 3.3799758969347038e+14]
+    n_ref = [6.6747328233649218e+08, 3.4589992012016338e+14,
+             3.5635060369969225e+14, 3.5195113630100188e+14]
 
     np.testing.assert_allclose(t, t_ref)
     np.testing.assert_allclose(n, n_ref)
+
 
 def test_get_reaction_rate(res):
     """Tests evaluating reaction rate."""
     t, r = res.get_reaction_rate("1", "Xe135", "(n,gamma)")
 
     t_ref = [0.0, 1296000.0, 2592000.0, 3888000.0]
-    n_ref = np.array([6.6747328233649218e+08, 3.5421791038348462e+14,
-                       3.6208592242443462e+14, 3.3799758969347038e+14])
-    xs_ref = np.array([4.0594392323131994e-05, 3.9249546927524987e-05,
-                       3.8394587728581798e-05, 4.1521845978371697e-05])
+    n_ref = np.array([6.6747328233649218e+08, 3.4589992012016338e+14,
+                      3.5635060369969225e+14, 3.5195113630100188e+14])
+    xs_ref = np.array([4.1340608491478010e-05, 4.1120938620476115e-05,
+                       4.3341529708654921e-05, 3.8716623651147821e-05])
 
     np.testing.assert_allclose(t, t_ref)
     np.testing.assert_allclose(r, n_ref * xs_ref)
@@ -44,8 +45,8 @@ def test_get_eigenvalue(res):
     t, k = res.get_eigenvalue()
 
     t_ref = [0.0, 1296000.0, 2592000.0, 3888000.0]
-    k_ref = [1.181281798790367, 1.1798750921988739, 1.1965943696058159,
-             1.2207119847790813]
+    k_ref = [1.1798617938070866, 1.1745713141097096, 1.1732427763487678,
+             1.213699703239334]
 
     np.testing.assert_allclose(t, t_ref)
     np.testing.assert_allclose(k, k_ref)

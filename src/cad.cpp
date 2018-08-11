f2089707790decd743cf7bd01a0561898e616dd4
itayhorin <itayhorin@gmail.com>
2018-08-11 15:47:21 -0500

    Set material by property.

--- src/cad.cpp
+++ src/cad.cpp
@@ -11,26 +11,47 @@ void load_cad_geometry_c()
     DAGMC = new moab::DagMC();
   }
 
+  int32_t cad_univ_id = 0; // universe is always 0 for CAD
+  
   moab::ErrorCode rval = DAGMC->load_file("dagmc.h5m");
   MB_CHK_ERR_CONT(rval);
 
   rval = DAGMC->init_OBBTree();
   MB_CHK_ERR_CONT(rval);
 
-  int32_t cad_univ_id = 0; // universe is always 0 for CAD
+  std::vector< std::string > prop_keywords;
+  prop_keywords.push_back("mat");
+
+  std::map<std::string, std::string> ph;
+  DAGMC->parse_properties(prop_keywords, ph, ":");
+  MB_CHK_ERR_CONT(rval);
   
   // initialize cell objects
   openmc::n_cells = DAGMC->num_entities(3);
   for(int i = 0; i < openmc::n_cells; i++)
     {
+      moab::EntityHandle vol_handle = DAGMC->entity_by_index(3, i+1);      
       // set cell ids using global IDs
       openmc::CADCell* c = new openmc::CADCell();
       c->id_ = DAGMC->id_by_index(3, i+1);
       c->dagmc_ptr = DAGMC;
       c->universe_ = cad_univ_id; // set to zero for now
-      c->material_.push_back(40); // TEMPORARY
+
+      if(DAGMC->has_prop(vol_handle, "mat")){
+	std::string mat_value;
+	
+	rval = DAGMC->prop_value(vol_handle, "mat", mat_value);
+	MB_CHK_ERR_CONT(rval);	
+	int mat_id = std::stoi(mat_value);
+	c->material_.push_back(mat_id);
+      }
+      else {
+	c->material_.push_back(40); // TO-DO: add void material here
+      }
+      
+      c->fill_ = openmc::C_NONE;
       openmc::global_cells.push_back(c);
-      openmc::cell_map[c->id_] = c->id;
+      openmc::cell_map[c->id_] = c->id_;
 
       // Populate the Universe vector and dict
       auto it = openmc::universe_map.find(cad_univ_id);

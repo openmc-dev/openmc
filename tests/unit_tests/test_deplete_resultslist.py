516a17b04ccf172206553c29902d5c31d3804124
Sterling Harper <smharper@mit.edu>
2018-08-16 15:41:18 -0400

    Shorten the depletion regression test

--- tests/unit_tests/test_deplete_resultslist.py
+++ tests/unit_tests/test_deplete_resultslist.py
@@ -10,7 +10,8 @@ import openmc.deplete
 @pytest.fixture
 def res():
     """Load the reference results"""
-    filename = Path(__file__).parents[1] / 'regression_tests' / 'test_reference.h5'
+    filename = (Path(__file__).parents[1] / 'regression_tests' / 'deplete'
+                / 'test_reference.h5')
     return openmc.deplete.ResultsList(filename)
 
 
@@ -19,8 +20,8 @@ def test_get_atoms(res):
     t, n = res.get_atoms("1", "Xe135")
 
     t_ref = [0.0, 1296000.0, 2592000.0, 3888000.0]
-    n_ref = [6.6747328233649218e+08, 3.4589992012016338e+14,
-             3.5635060369969225e+14, 3.5195113630100188e+14]
+    n_ref = [6.674732823364922e+08, 4.022377919003244e+14,
+             3.469325382312551e+14, 3.639463751297369e+14]
 
     np.testing.assert_allclose(t, t_ref)
     np.testing.assert_allclose(n, n_ref)
@@ -31,10 +32,10 @@ def test_get_reaction_rate(res):
     t, r = res.get_reaction_rate("1", "Xe135", "(n,gamma)")
 
     t_ref = [0.0, 1296000.0, 2592000.0, 3888000.0]
-    n_ref = np.array([6.6747328233649218e+08, 3.4589992012016338e+14,
-                      3.5635060369969225e+14, 3.5195113630100188e+14])
-    xs_ref = np.array([4.1340608491478010e-05, 4.1120938620476115e-05,
-                       4.3341529708654921e-05, 3.8716623651147821e-05])
+    n_ref = [6.674732823364922e+08, 4.022377919003244e+14,
+             3.469325382312551e+14, 3.639463751297369e+14]
+    xs_ref = np.array([3.272630849911638e-05, 2.663333206501429e-05,
+                       3.378999816491878e-05, 3.277013860196171e-05])
 
     np.testing.assert_allclose(t, t_ref)
     np.testing.assert_allclose(r, n_ref * xs_ref)
@@ -45,8 +46,8 @@ def test_get_eigenvalue(res):
     t, k = res.get_eigenvalue()
 
     t_ref = [0.0, 1296000.0, 2592000.0, 3888000.0]
-    k_ref = [1.1798617938070866, 1.1745713141097096, 1.1732427763487678,
-             1.213699703239334]
+    k_ref = [1.2162584685809341, 1.1021840248590875, 1.223040353281542,
+             1.2360979537312164]
 
     np.testing.assert_allclose(t, t_ref)
     np.testing.assert_allclose(k, k_ref)

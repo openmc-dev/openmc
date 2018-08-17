0d0d3f390d422295ce47800cee7edc91e228b093
itayhorin <itayhorin@gmail.com>
2018-08-16 20:12:04 -0400

    Set cmfd tally type and estimator through C API

--- examples/cmfd_testing/basic/capi/run_openmc_cmfd.py
+++ examples/cmfd_testing/basic/capi/run_openmc_cmfd.py
@@ -7,15 +7,14 @@ cmfd_mesh.lower_left = [-10, -1, -1]
 cmfd_mesh.upper_right = [10, 1, 1]
 cmfd_mesh.dimension = [10, 1, 1]
 cmfd_mesh.albedo = [0., 0., 1., 1., 1., 1.]
-cmfd_mesh.energy = [0.001, 0.01, 0.1]
 
 # Initialize CMFDRun object
 cmfd_run = openmc.CMFDRun()
 
 # Set all runtime parameters (cmfd_mesh, tolerances, tally_resets, etc)
-
 # All error checking done under the hood when setter function called
 cmfd_run.cmfd_mesh = cmfd_mesh
+cmfd_run.cmfd_reset = [5,10]
 
 # Run CMFD
 cmfd_run.run()

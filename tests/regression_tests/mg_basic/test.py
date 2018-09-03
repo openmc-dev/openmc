c1652bbdec5a2aaa2aa9fc7f0c7cd19f2d94b666
Adam G Nelson <nelsonag@umich.edu>
2018-09-03 06:48:02 -0400

    Replacing old slab_mg example model with new one that uses a library that follows the library standard.  Also had to update tests accordingly.

--- tests/regression_tests/mg_basic/test.py
+++
@@ -1,9 +0,0 @@
-from openmc.examples import slab_mg
-
-from tests.testing_harness import PyAPITestHarness
-
-
-def test_mg_basic():
-    model = slab_mg()
-    harness = PyAPITestHarness('statepoint.10.h5', model)
-    harness.main()

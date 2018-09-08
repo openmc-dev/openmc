1befe1c059c23c0d5a45fa063474f6df21cf68f3
shriwise <pshriwise@gmail.com>
2018-09-08 14:51:44 -0500

    More updates to CAD test. Attempt at skip if DAGMC is not enabled. Attempting to add local directory to path.

--- tests/unit_tests/cad/test_cad.py
+++ tests/unit_tests/cad/test_cad.py
@@ -1,6 +1,20 @@
 
 from subprocess import call
 
+import os
+import sys
+
+
+import pytest
+pytestmark = pytest.mark.skipif(
+    os.environ.get('CAD') != 'y',
+    reason="CAD build is not enabled.")
+
 def test_cad():
-    assert not call(['openmc -s 1'])
+    # make sure cwd is in the python system path
+    d = os.path.abspath(os.path.dirname(__file__))
+    print(d)
+    sys.path.append(d)
+    
+    assert not call(['openmc','-s','1'])
     

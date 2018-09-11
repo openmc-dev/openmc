e1dde40f0ad838389706bd62d598a77a3f74b2aa
shriwise <pshriwise@gmail.com>
2018-09-11 08:19:23 -0500

    Using test harness to run openmc instead

--- tests/unit_tests/cad/test_cad.py
+++ tests/unit_tests/cad/test_cad.py
@@ -1,9 +1,4 @@
-
-from subprocess import call
-
-import os
-import sys
-
+from tests.testing_harness import TestHarness
 
 import pytest
 pytestmark = pytest.mark.skipif(
@@ -11,10 +6,5 @@ pytestmark = pytest.mark.skipif(
     reason="CAD build is not enabled.")
 
 def test_cad():
-    # make sure cwd is in the python system path
-    d = os.path.abspath(os.path.dirname(__file__))
-    # move here
-    os.chdir(d)
-    # run test
-    assert not call(['openmc','-s','1'])
-    
+    harness = TestHarness('statepoint.15.h5')
+    harness.main()    

26a13efab560493ae60dd9356008ae6792cfcede
itayhorin <itayhorin@gmail.com>
2018-09-25 23:00:50 -0500

    Moving CAD test to regression tests.

--- tests/unit_tests/cad/__init__.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pytest
-
-
-def assert_unbounded(obj):
-    """Assert that a region/cell is unbounded."""
-    ll, ur = obj.bounding_box
-    assert ll == pytest.approx((-np.inf, -np.inf, -np.inf))
-    assert ur == pytest.approx((np.inf, np.inf, np.inf))

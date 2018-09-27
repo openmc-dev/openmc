aabdb9aabc0e0f32b616bca38eb0735c4ce02507
Will Boyd <wbinventor@gmail.com>
2018-09-26 23:24:45 -0400

    Added docker download script

---
+++ docker/docker-download-xs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -ex
+
+# Download NNDC HDF5 data
+if [[ ! -e $HOME/nndc_hdf5/cross_sections.xml ]]; then
+    wget https://anl.box.com/shared/static/a0eflty17atnpd0pp7460exagr3nuhm7.xz -O - | tar -C $HOME -xvJ
+fi
+
+# Download ENDF/B-VII.1 distribution
+ENDF=$HOME/endf-b-vii.1/
+if [[ ! -d $ENDF/neutrons || ! -d $ENDF/photoat || ! -d $ENDF/atomic_relax ]]; then
+    wget https://anl.box.com/shared/static/4kd2gxnf4gtk4w1c8eua5fsua22kvgjb.xz -O - | tar -C $HOME -xvJ
+fi
+
+# Download multipole library
+if [[ ! -e $HOME/WMP_Library/092235.h5 ]]; then
+    wget https://github.com/mit-crpg/WMP_Library/releases/download/v1.0/WMP_Library_v1.0.tar.gz
+    tar -C $HOME -xzvf WMP_Library_v1.0.tar.gz
+fi

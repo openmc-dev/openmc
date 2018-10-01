82f68c441192a5212e4dc18fb75f5a898b154456
pshriwise <pshriwise@gmail.com>
2018-10-01 20:09:00 +0000

    Putting dagmc setting in an ifdef.

--- src/settings.cpp
+++ src/settings.cpp
@@ -408,10 +408,12 @@ void read_settings_xml()
 #endif
   }
 
+#ifdef _OPENMP
   if (dagmc) {
     warning("Forcing number of threads to 1 for DAGMC simulation.");
     omp_set_num_threads(1);
   }
+#endif
   
   // ==========================================================================
   // EXTERNAL SOURCE

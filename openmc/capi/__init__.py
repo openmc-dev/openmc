7eefb76cd14e96e037b1309c8569c3b732397069
itayhorin <itayhorin@gmail.com>
2018-10-02 21:26:24 -0500

    Updating CAPI dagmc_enabled exposure.

--- openmc/capi/__init__.py
+++ openmc/capi/__init__.py
@@ -12,7 +12,7 @@ objects in the :mod:`openmc.capi` subpackage, for example:
 
 """
 
-from ctypes import CDLL
+from ctypes import CDLL, c_bool
 import os
 import sys
 
@@ -38,7 +38,7 @@ else:
     from unittest.mock import Mock
     _dll = Mock()
 
-__dagmc_enabled__ = bool(_dll.dagmc_enabled())
+dagmc_enabled = bool(c_bool.in_dll(_dll, "dagmc_enabled"))
     
 from .error import *
 from .core import *

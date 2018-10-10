9edc55fc741e598bdae7ce8d7891f7d04ff9108d
itayhorin <itayhorin@gmail.com>
2018-10-09 22:42:21 -0500

    Make remaining variables in simulation_header.F90 C interoperable

--- src/simulation_header.F90
+++ src/simulation_header.F90
@@ -15,7 +15,7 @@ module simulation_header
   ! Number of lost particles
   integer(C_INT), bind(C) :: n_lost_particles = 0
 
-  real(8) :: log_spacing ! spacing on logarithmic grid
+  real(C_DOUBLE), bind(C) :: log_spacing ! spacing on logarithmic grid
 
   ! ============================================================================
   ! SIMULATION VARIABLES
@@ -24,12 +24,12 @@ module simulation_header
   integer(C_INT), bind(C) :: current_gen       ! current generation within a batch
   integer(C_INT), bind(C) :: total_gen     = 0 ! total number of generations simulated
   logical(C_BOOL), bind(C) :: simulation_initialized = .false.
-  logical :: need_depletion_rx ! need to calculate depletion reaction rx?
+  logical(C_BOOL), bind(C) :: need_depletion_rx ! need to calculate depletion reaction rx?
 
   ! ============================================================================
   ! TALLY PRECISION TRIGGER VARIABLES
 
-  logical :: satisfy_triggers = .false.       ! whether triggers are satisfied
+  logical(C_BOOL), bind(C) :: satisfy_triggers = .false.       ! whether triggers are satisfied
 
   integer(C_INT64_T), bind(C) :: work         ! number of particles per processor
   integer(C_INT64_T), allocatable :: work_index(:) ! starting index in source bank for each process
@@ -42,22 +42,22 @@ module simulation_header
   type(VectorReal) :: k_generation ! single-generation estimates of k
   real(C_DOUBLE), bind(C, name='openmc_keff')     :: keff = ONE  ! average k over active batches
   real(C_DOUBLE), bind(C, name='openmc_keff_std') :: keff_std    ! standard deviation of average k
-  real(8) :: k_col_abs = ZERO ! sum over batches of k_collision * k_absorption
-  real(8) :: k_col_tra = ZERO ! sum over batches of k_collision * k_tracklength
-  real(8) :: k_abs_tra = ZERO ! sum over batches of k_absorption * k_tracklength
+  real(C_DOUBLE), bind(C) :: k_col_abs = ZERO ! sum over batches of k_collision * k_absorption
+  real(C_DOUBLE), bind(C) :: k_col_tra = ZERO ! sum over batches of k_collision * k_tracklength
+  real(C_DOUBLE), bind(C) :: k_abs_tra = ZERO ! sum over batches of k_absorption * k_tracklength
 
   ! ============================================================================
   ! PARALLEL PROCESSING VARIABLES
 
 #ifdef _OPENMP
   integer(C_INT), bind(C) :: n_threads = NONE      ! number of OpenMP threads
-  integer :: thread_id             ! ID of a given thread
+  integer(C_INT), bind(C) :: thread_id             ! ID of a given thread
 #endif
 
   ! ============================================================================
   ! MISCELLANEOUS VARIABLES
 
-  integer :: restart_batch
+  integer(C_INT), bind(C) :: restart_batch
 
   logical(C_BOOL), bind(C) :: trace
 

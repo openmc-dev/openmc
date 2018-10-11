40b9537f6a0788bff9bdfb86b43773cce2956102
Shikhar Kumar <shikhark@mit.edu>
2018-10-11 02:57:54 -0400

    Add test cases for 2d beavrs

---
+++ examples/cmfd_testing/2d-beavrs/python-vec/run_openmc_cmfd.py
@@ -0,0 +1,46 @@
+import openmc
+import numpy as np
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+
+# Initialize CMFD Mesh
+cmfd_mesh = openmc.CMFDMesh()
+cmfd_mesh.lower_left = [-182.78094, -182.78094, 220.0]
+cmfd_mesh.upper_right = [182.78094, 182.78094, 240.0]
+cmfd_mesh.dimension = [17, 17, 1]
+cmfd_mesh.albedo = [0.0, 0.0, 0.0, 0.0, 1.0, 1.0]
+cmfd_mesh.map = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0,
+                 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0,
+                 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0,
+                 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0,
+                 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+                 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+                 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+                 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+                 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+                 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+                 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+                 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0,
+                 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0,
+                 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0,
+                 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0,
+                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+cmfd_mesh.energy = [0.0, 0.625, 20000000]
+
+# Initialize CMFDRun object
+cmfd_run = openmc.CMFDRun()
+
+# Set all runtime parameters (cmfd_mesh, tolerances, tally_resets, etc)
+# All error checking done under the hood when setter function called
+cmfd_run.cmfd_mesh = cmfd_mesh
+cmfd_run.cmfd_begin = 151
+cmfd_run.cmfd_display = 'balance'
+cmfd_run.cmfd_feedback = False
+cmfd_run.cmfd_downscatter = True
+cmfd_run.norm = 193
+cmfd_run.tally_reset = [101]
+
+# Run CMFD
+cmfd_run.run(vectorized=True, omp_num_threads=1)
--- examples/cmfd_testing/README.md
+++ examples/cmfd_testing/README.md
@@ -1,1 +1,10 @@
-run `python generate_all_tests.py -r` to generate and run all test cases
+Run `python generate_all_tests.py -r` to generate and run all test cases
+
+To run CMFD on 2D BEAVRS input files using Fortran, run `openmc` in `./2d-beavrs/fortran/`
+
+To run CMFD on 2D BEAVRS input files using C API with vectorized numpy functions,
+run `python run_openmc_cmfd.py` in `./2d-beavrs/python-vec/`
+
+To run CMFD on 2D BEAVRS input files using C API with non-vectorized functions,
+run `python run_openmc_cmfd.py` in `./2d-beavrs/python-unvec/`
+

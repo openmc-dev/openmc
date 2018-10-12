9eef0884af2f188fa7fcbe95f8f5c5301d376d5e
Patrick Shriwise <pshriwise@gmail.com>
2018-10-12 00:39:25 -0500

    Fortran style updates.

--- src/plot.F90
+++ src/plot.F90
@@ -17,7 +17,7 @@ module plot
       import C_INT
       integer(C_INT) :: err
     end function openmc_plot_geometry_c
-     
+
     subroutine create_ppm(pl) bind(C)
       import ObjectPlot
       type(ObjectPlot), intent(in) :: pl
@@ -40,24 +40,6 @@ contains
 
     err = openmc_plot_geometry_c()
     return
-
-    do i = 1, n_plots
-      associate (pl => plots(i))
-        ! Display output message
-        call write_message("Processing plot " // trim(to_str(pl % id)) &
-             // ": " // trim(pl % path_plot) // " ...", 5)
-
-        if (pl % type == PLOT_TYPE_SLICE) then
-          ! create 2d image
-          call create_ppm(pl)
-        else if (pl % type == PLOT_TYPE_VOXEL) then
-          ! create dump for 3D silomesh utility script
-          call create_voxel(pl)
-        end if
-      end associate
-    end do
-
-    err = 0
   end function openmc_plot_geometry
 
 end module plot

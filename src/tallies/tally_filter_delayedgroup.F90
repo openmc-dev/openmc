69c65ddf23a51abf86882597eb3281531e49f053
Sterling Harper <smharper@mit.edu>
2018-10-28 15:52:01 -0400

    Move the DelayedGroupFilter to C++

--- src/tallies/tally_filter_delayedgroup.F90
+++ src/tallies/tally_filter_delayedgroup.F90
@@ -2,13 +2,7 @@ module tally_filter_delayedgroup
 
   use, intrinsic :: ISO_C_BINDING
 
-  use constants,          only: ONE, MAX_LINE_LEN, MAX_DELAYED_GROUPS
-  use error,              only: fatal_error
-  use hdf5_interface
-  use particle_header,    only: Particle
-  use string,             only: to_str
-  use tally_filter_header
-  use xml_interface
+  use tally_filter_cpp
 
   implicit none
   private
@@ -19,68 +13,26 @@ module tally_filter_delayedgroup
 ! iterated over in the scoring subroutines.
 !===============================================================================
 
-  type, public, extends(TallyFilter) :: DelayedGroupFilter
-    integer, allocatable :: groups(:)
+  type, public, extends(CppTallyFilter) :: DelayedGroupFilter
   contains
-    procedure :: from_xml
-    procedure :: get_all_bins => get_all_bins_dg
-    procedure :: to_statepoint => to_statepoint_dg
-    procedure :: text_label => text_label_dg
+    procedure :: groups
   end type DelayedGroupFilter
 
 contains
 
-  subroutine from_xml(this, node)
-    class(DelayedGroupFilter), intent(inout) :: this
-    type(XMLNode), intent(in) :: node
-
-    integer :: i
-    integer :: n
-
-    n = node_word_count(node, "bins")
-
-    ! Allocate and store bins
-    this % n_bins = n
-    allocate(this % groups(n))
-    call get_node_array(node, "bins", this % groups)
-
-    ! Check that bins are all are between 1 and MAX_DELAYED_GROUPS
-    do i = 1, n
-      if (this % groups(i) < 1 .or. &
-           this % groups(i) > MAX_DELAYED_GROUPS) then
-        call fatal_error("Encountered delayedgroup bin with index " &
-             // trim(to_str(this % groups(i))) // " that is outside &
-             &the range of 1 to MAX_DELAYED_GROUPS ( " &
-             // trim(to_str(MAX_DELAYED_GROUPS)) // ")")
-      end if
-    end do
-  end subroutine from_xml
-
-  subroutine get_all_bins_dg(this, p, estimator, match)
-    class(DelayedGroupFilter), intent(in)  :: this
-    type(Particle),            intent(in)  :: p
-    integer,                   intent(in)  :: estimator
-    type(TallyFilterMatch),         intent(inout) :: match
-
-    call match % bins_push_back(1)
-    call match % weights_push_back(ONE)
-  end subroutine get_all_bins_dg
-
-  subroutine to_statepoint_dg(this, filter_group)
+  function groups(this, i) result(group)
     class(DelayedGroupFilter), intent(in) :: this
-    integer(HID_T),            intent(in) :: filter_group
-
-    call write_dataset(filter_group, "type", "delayedgroup")
-    call write_dataset(filter_group, "n_bins", this % n_bins)
-    call write_dataset(filter_group, "bins", this % groups)
-  end subroutine to_statepoint_dg
-
-  function text_label_dg(this, bin) result(label)
-    class(DelayedGroupFilter), intent(in) :: this
-    integer,                   intent(in) :: bin
-    character(MAX_LINE_LEN)               :: label
-
-    label = "Delayed Group " // to_str(this % groups(bin))
-  end function text_label_dg
+    integer,                   intent(in) :: i
+    integer                               :: group
+    interface
+      function delayedgroup_filter_groups(filt, i) result(group) bind(C)
+        import C_PTR, C_INT
+        type(C_PTR),    value :: filt
+        integer(C_INT), value :: i
+        integer(C_INT)        :: group
+      end function
+    end interface
+    group = delayedgroup_filter_groups(this % ptr, i)
+  end function groups
 
 end module tally_filter_delayedgroup

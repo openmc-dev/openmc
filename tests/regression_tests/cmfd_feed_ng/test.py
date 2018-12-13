d290b56d6a35ee5b124733b0ac709462b0e777ae
itayhorin <itayhorin@gmail.com>
2018-12-13 17:17:59 -0500

    Fix test suite, remove cmfd from openmc/__init.py

--- tests/regression_tests/cmfd_feed_ng/test.py
+++ tests/regression_tests/cmfd_feed_ng/test.py
@@ -1,11 +1,11 @@
 from tests.testing_harness import CMFDTestHarness
-import openmc
+from openmc import cmfd
 import numpy as np
 
 def test_cmfd_feed_ng():
     """ Test n group CMFD solver with CMFD feedback"""
     # Initialize and set CMFD mesh
-    cmfd_mesh = openmc.CMFDMesh()
+    cmfd_mesh = cmfd.CMFDMesh()
     cmfd_mesh.lower_left = -1.25984, -1.25984, -1.0
     cmfd_mesh.upper_right = 1.25984, 1.25984, 1.0
     cmfd_mesh.dimension = 2, 2, 1
@@ -13,7 +13,7 @@ def test_cmfd_feed_ng():
     cmfd_mesh.albedo = 1.0, 1.0, 1.0, 1.0, 1.0, 1.0
 
     # Initialize and run CMFDRun object
-    cmfd_run = openmc.CMFDRun()
+    cmfd_run = cmfd.CMFDRun()
     cmfd_run.mesh = cmfd_mesh
     cmfd_run.reset = [5]
     cmfd_run.begin = 10

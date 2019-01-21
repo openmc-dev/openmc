df4c2d9c32e134a23500742b66cdd0e3d97d7147
itayhorin <itayhorin@gmail.com>
2019-01-21 13:57:11 -0600

    Fixing off-by-one error in material/temp indexing when crossing surfaces in DAGMC.

--- src/tracking.F90
+++ src/tracking.F90
@@ -498,8 +498,8 @@ contains
       ! set new cell value
       p % coord(1) % cell = i_cell-1 ! decrement for C++ indexing
       p % cell_instance = 1
-      p % material = cells(i_cell) % material(1)
-      p % sqrtKT = cells(i_cell) % sqrtKT(1)
+      p % material = cells(i_cell) % material(0)
+      p % sqrtKT = cells(i_cell) % sqrtKT(0)
       return
     end if
 #endif

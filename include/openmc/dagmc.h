3c743cb5e24632669b247895ded81e87cc08fa80
Patrick Shriwise <pshriwise@gmail.com>
2019-01-22 12:50:13 -0600

    Moving dagmc_enabled outside of main DAGMC ifdef.

--- include/openmc/dagmc.h
+++ include/openmc/dagmc.h
@@ -2,6 +2,10 @@
 #ifndef OPENMC_DAGMC_H
 #define OPENMC_DAGMC_H
 
+namespace openmc {
+extern "C" const bool dagmc_enabled;
+}
+
 #ifdef DAGMC
 
 #include "DagMC.hpp"
@@ -9,12 +13,6 @@
 #include "openmc/surface.h"
 
 namespace openmc {
-
-//==============================================================================
-// Global variables
-//==============================================================================
-
-extern "C" const bool dagmc_enabled;
 
 namespace model {
 
@@ -30,6 +28,7 @@ extern "C" void load_dagmc_geometry();
 extern "C" void free_memory_dagmc();
 extern "C" pugi::xml_document* read_uwuw_materials();
 bool get_uwuw_materials_xml(std::string& s);
+
 } // namespace openmc
 
 #endif // DAGMC

5b8806c839bb080e4b0a944bd07f832094feaa3a
Sterling Harper <smharper@mit.edu>
2019-02-01 13:03:00 -0500

    Move check_triggers to C++

--- src/tallies/trigger.cpp
+++ src/tallies/trigger.cpp
@@ -1,12 +1,15 @@
 #include "openmc/tallies/trigger.h"
 
 #include <cmath>
+#include <sstream>
 #include <utility> // for std::pair
 
 #include "openmc/capi.h"
 #include "openmc/constants.h"
-#include "openmc/message_passing.h"
+#include "openmc/error.h"
+#include "openmc/reaction.h"
 #include "openmc/settings.h"
+#include "openmc/simulation.h"
 #include "openmc/tallies/tally.h"
 
 namespace openmc {
@@ -40,10 +43,10 @@ extern "C" Trigger* get_tally_trigger(int i_tally, int i_trig);
 //! param[out] tally_id The ID number of the most limiting tally
 //! param[out] score The most limiting tally score bin
 
-extern "C" void
-check_tally_triggers(double* ratio, int* tally_id, int* score)
+void
+check_tally_triggers(double& ratio, int& tally_id, int& score)
 {
-  *ratio = 0.;
+  ratio = 0.;
   //TODO: off-by-one
   for (auto i_tally = 1; i_tally < model::tallies.size()+1; ++i_tally) {
     const Tally& t {*model::tallies[i_tally-1]};
@@ -94,17 +97,17 @@ check_tally_triggers(double* ratio, int* tally_id, int* score)
 
           double this_ratio = uncertainty / trigger.threshold;
           if (trigger.type == VARIANCE) {
-            this_ratio = std::sqrt(*ratio);
+            this_ratio = std::sqrt(ratio);
           }
 
-          if (this_ratio > *ratio) {
-            *ratio = this_ratio;
+          if (this_ratio > ratio) {
+            ratio = this_ratio;
             int* scores;
             int junk;
             err = openmc_tally_get_scores(i_tally, &scores, &junk);
             //TODO: off-by-one
-            *score = scores[trigger.score_index-1];
-            err = openmc_tally_get_id(i_tally, tally_id);
+            score = scores[trigger.score_index-1];
+            err = openmc_tally_get_id(i_tally, &tally_id);
           }
         }
       }
@@ -114,7 +117,7 @@ check_tally_triggers(double* ratio, int* tally_id, int* score)
 
 //! Computes the uncertainty/threshold ratio for the eigenvalue trigger.
 
-extern "C" double
+double
 check_keff_trigger()
 {
   if (settings::run_mode != RUN_MODE_EIGENVALUE) return 0.;
@@ -139,6 +142,67 @@ check_keff_trigger()
   if (settings::keff_trigger.type == VARIANCE)
     ratio = std::sqrt(ratio);
   return ratio;
+}
+
+//! See if tally and eigenvalue uncertainties are under trigger thresholds.
+
+void
+check_triggers()
+{
+  // Make some aliases.
+  const auto current_batch {simulation::current_batch};
+  const auto n_batches {settings::n_batches};
+  const auto interval {settings::trigger_batch_interval};
+
+  // See if the current batch is one for which the triggers must be checked.
+  if (!settings::trigger_on) return;
+  if (current_batch < n_batches) return;
+  if (((current_batch - n_batches) % interval) != 0) return;
+
+  // Check the eigenvalue and tally triggers.
+  double keff_ratio = check_keff_trigger();
+  double tally_ratio;
+  int tally_id, score;
+  check_tally_triggers(tally_ratio, tally_id, score);
+
+  // If all the triggers are satisfied, alert the user and return.
+  if (std::max(keff_ratio, tally_ratio) <= 1.) {
+    simulation::satisfy_triggers = true;
+    write_message("Triggers satisfied for batch "
+      + std::to_string(current_batch), 7);
+    return;
+  }
+
+  // At least one trigger is unsatisfied.  Let the user know which one.
+  simulation::satisfy_triggers = false;
+  std::stringstream msg;
+  msg << "Triggers unsatisfied, max unc./thresh. is ";
+  if (keff_ratio >= tally_ratio) {
+    msg << keff_ratio << " for eigenvalue";
+  } else {
+    msg << tally_ratio << " for " << reaction_name(score) << " in tally "
+        << tally_id;
+  }
+  write_message(msg, 7);
+
+  // Estimate batches til triggers are satisfied.
+  if (settings::trigger_predict) {
+    // This calculation assumes tally variance is proportional to 1/N where N is
+    // the number of batches.
+    auto max_ratio = std::max(keff_ratio, tally_ratio);
+    auto n_active = current_batch - settings::n_inactive;
+    auto n_pred_batches = static_cast<int>(n_active * max_ratio * max_ratio)
+      + settings::n_inactive + 1;
+
+    std::stringstream msg;
+    msg << "The estimated number of batches is " << n_pred_batches;
+    if (n_pred_batches > settings::n_max_batches) {
+      msg << " --- greater than max batches";
+      warning(msg);
+    } else {
+      write_message(msg, 7);
+    }
+  }
 }
 
 } // namespace openmc

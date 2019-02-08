f8c8e4e9725e053140575f00321eac858f887277
Jingang Liang <liangjg2008@gmail.com>
2019-02-08 12:03:50 -0800

    updated depletion test to perform loading of new nuclides from the chain

--- tests/unit_tests/test_deplete_resultslist.py
+++ tests/unit_tests/test_deplete_resultslist.py
@@ -20,8 +20,7 @@ def test_get_atoms(res):
     t, n = res.get_atoms("1", "Xe135")
 
     t_ref = [0.0, 1296000.0, 2592000.0, 3888000.0]
-    n_ref = [6.674732823364922e+08, 4.022377919003244e+14,
-             3.469325382312551e+14, 3.639463751297369e+14]
+    n_ref = [6.67473282e+08, 3.76986925e+14, 3.68587383e+14, 3.91338675e+14]
 
     np.testing.assert_allclose(t, t_ref)
     np.testing.assert_allclose(n, n_ref)
@@ -32,13 +31,11 @@ def test_get_reaction_rate(res):
     t, r = res.get_reaction_rate("1", "Xe135", "(n,gamma)")
 
     t_ref = [0.0, 1296000.0, 2592000.0, 3888000.0]
-    n_ref = [6.674732823364922e+08, 4.022377919003244e+14,
-             3.469325382312551e+14, 3.639463751297369e+14]
-    xs_ref = np.array([3.272630849911638e-05, 2.663333206501429e-05,
-                       3.378999816491878e-05, 3.277013860196171e-05])
+    n_ref = [6.67473282e+08, 3.76986925e+14, 3.68587383e+14, 3.91338675e+14]
+    xs_ref = [3.32282266e-05, 2.76207120e-05, 4.10986677e-05, 3.72453665e-05]
 
     np.testing.assert_allclose(t, t_ref)
-    np.testing.assert_allclose(r, n_ref * xs_ref)
+    np.testing.assert_allclose(r, np.array(n_ref) * xs_ref)
 
 
 def test_get_eigenvalue(res):
@@ -46,8 +43,7 @@ def test_get_eigenvalue(res):
     t, k = res.get_eigenvalue()
 
     t_ref = [0.0, 1296000.0, 2592000.0, 3888000.0]
-    k_ref = [1.2162584685809341, 1.1021840248590875, 1.223040353281542,
-             1.2360979537312164]
+    k_ref = [1.16984322, 1.19097427, 1.03012572, 1.20045627]
 
     np.testing.assert_allclose(t, t_ref)
     np.testing.assert_allclose(k, k_ref)

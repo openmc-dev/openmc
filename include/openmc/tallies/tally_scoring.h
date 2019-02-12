49daad3f949819867279169618cd7cbca30980d7
Sterling Harper <smharper@mit.edu>
2019-02-11 20:22:48 -0500

    Cleanup C++ tallies work

--- include/openmc/tallies/tally_scoring.h
+++ include/openmc/tallies/tally_scoring.h
@@ -12,16 +12,21 @@ namespace openmc {
 //! This iterator handles two distinct tasks.  First, it maps the N-dimensional
 //! space created by the indices of N filters onto a 1D sequence.  In other
 //! words, it provides a single number that uniquely identifies a combination of
-//! bins for many filters.  Second, it handles the task of finding each all
-//! valid combinations of filter bins given that each filter can have 1 or 2 or
-//! many bins that are valid for the current tally event.
+//! bins for many filters.  Second, it handles the task of finding each valid
+//! combination of filter bins given that each filter can have 1 or 2 or many
+//! bins that are valid for the current tally event.
 //==============================================================================
 
 class FilterBinIter
 {
 public:
+
+  //! Construct an iterator over bins that match a given particle's state.
   FilterBinIter(const Tally& tally, Particle* p);
 
+  //! Construct an iterator over all filter bin combinations.
+  //
+  //! \param end if true, the returned iterator indicates the end of a loop.
   FilterBinIter(const Tally& tally, bool end);
 
   bool operator==(const FilterBinIter& other) const

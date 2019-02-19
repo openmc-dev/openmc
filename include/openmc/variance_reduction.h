4548d6f55a57277bd522a81efa6b38010a6e2b94
Andrew Davis <andrew.davis@ukaea.uk>
2019-02-19 11:02:49 +0000

    Updated and rebased

--- include/openmc/variance_reduction.h
+++ include/openmc/variance_reduction.h
@@ -1,15 +1,20 @@
 #ifndef VARIANCE_REDUCTION_H
 #define VARIANCE_REDUCTION_H 1
 
-#include "pugixml/pugixml.hpp"
+#include <vector>
 #include "hdf5.h"
 
+#include "openmc/xml_interface.h"
+#include "openmc/error.h"
+
 namespace openmc {
+namespace variance_reduction {
 
   void read_importances(pugi::xml_node *node);
 
   void read_weight_windows(pugi::xml_node *node);
 
+} // end variance_reduction namespace
 } // end openmc namespace
 
 #endif

1ccb41d2a0d6c7fc3dbeba829aa04fdf2bc622e3
itayhorin <itayhorin@gmail.com>
2019-04-01 21:02:15 -0500

    Updating test name.

--- tests/regression_tests/lattice_hex_coincident/test.py
+++ tests/regression_tests/lattice_hex_coincident/test.py
@@ -1,6 +1,6 @@
 from tests.testing_harness import TestHarness
 
 
-def test_lattice_hex():
+def test_lattice_hex_coincident_surf():
     harness = TestHarness('statepoint.10.h5')
     harness.main()

0890d08317a84cac7d0cf94cce92cd886d2a22fd
Patrick Shriwise <pshriwise@gmail.com>
2019-05-12 11:44:56 -0500

    adding compute barycentric data call.

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -1584,6 +1584,7 @@ UnstructuredMesh::UnstructuredMesh(pugi::xml_node node) : Mesh(node) {
     warning("Non-tetrahedral elements found in unstructured mesh: " + filename_);
   }
 
+  compute_barycentric_data(all_tets);
   build_tree(all_tets);
 }
 

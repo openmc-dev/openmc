e5e79fcb09bbbc0b36bacb2552322f92c3f22280
itayhorin <itayhorin@gmail.com>
2019-06-05 11:26:39 -0400

    fixed compiling errors with visual studio, mostly VLAs

--- src/finalize.cpp
+++ src/finalize.cpp
@@ -117,7 +117,7 @@ int openmc_finalize()
   data::energy_max = {INFTY, INFTY};
   data::energy_min = {0.0, 0.0};
   model::root_universe = -1;
-  openmc_set_seed(DEFAULT_SEED);
+  openmc::openmc_set_seed(DEFAULT_SEED);
 
   // Deallocate arrays
   free_memory();
@@ -159,6 +159,6 @@ int openmc_hard_reset()
   simulation::total_gen = 0;
 
   // Reset the random number generator state
-  openmc_set_seed(DEFAULT_SEED);
+  openmc::openmc_set_seed(DEFAULT_SEED);
   return 0;
 }

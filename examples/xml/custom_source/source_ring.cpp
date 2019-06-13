294fc6b4622d2f31abf567381564c239a702badb
itayhorin <itayhorin@gmail.com>
2019-06-13 17:49:29 +0100

    Added example on how to use xml based custom source

---
+++ examples/xml/custom_source/source_ring.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "openmc/random_lcg.h"
+#include "openmc/source.h"
+#include "openmc/particle.h"
+
+// you must have external C linkage here otherwise 
+// dlopen will not find the file
+extern "C" openmc::Particle::Bank sample_source() {
+    openmc::Particle::Bank particle;
+    // wgt
+    particle.particle = openmc::Particle::Type::neutron;
+    particle.wgt = 1.0;
+    // position
+    
+    double angle = 2.*M_PI*openmc::prn();
+    double radius = 3.0;
+    particle.r.x = radius*std::cos(angle);
+    particle.r.y = radius*std::sin(angle);
+    particle.r.z = 0.;
+    // angle
+    particle.u = {1.,0,0};
+    particle.E = 14.08e6;
+    particle.delayed_group = 0;
+    return particle;    
+}

6583834bd4a8460d708be6492025ca65500e9323
Andrew Johnson <drewej@protonmail.com>
2019-06-21 13:06:54 -0500

    Store depletion time for leqi, si_celi, si_leqi routines

--- tests/unit_tests/test_deplete_si_celi.py
+++ tests/unit_tests/test_deplete_si_celi.py
@@ -35,3 +35,15 @@ def test_si_celi(run_in_tmpdir):
 
     assert y1[2] == approx(s2[0])
     assert y2[2] == approx(s2[1])
+
+    # Test structure of depletion time dataset
+
+    dep_time = res.get_depletion_time()
+    assert dep_time.shape == (len(dt), 1)
+    assert all(dep_time > 0)
+
+    # Test structure of depletion time dataset
+
+    dep_time = res.get_depletion_time()
+    assert dep_time.shape == (len(dt), 1)
+    assert all(dep_time > 0)

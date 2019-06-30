1475e25bf344ac397358c872c1d2e5d19d98dccc
Andrew Johnson <drewej@protonmail.com>
2019-06-30 13:26:43 -0500

    Add Universe subclass openmc.model.Pin
    Designed to facilitate building universe that represent
    pins, or concentric cylinders of materials. Can be built
    by passing surfaces and materials, or radii and materials.
    Places last material spanning region out to infinity from
    the last surface.
    
    Supports subdividing rings into equal volume slices of
    unique [cloned] materials, useful for depletion.
    
    Unit tests added for failure modes as well as basic operation

---
+++ tests/unit_tests/test_pin.py
@@ -0,0 +1,86 @@
+"""
+Tests for constructing Pin universes
+"""
+
+import numpy
+import pytest
+
+import openmc
+from openmc.model import Pin
+
+
+@pytest.fixture
+def pin_mats():
+    fuel = openmc.Material(name="UO2")
+    clad = openmc.Material(name="zirc")
+    water = openmc.Material(name="water")
+    return fuel, clad, water
+
+
+@pytest.fixture
+def good_radii():
+    return (0.4, 0.42)
+
+
+def test_failure(pin_mats, good_radii):
+    """Check for various failure modes"""
+    # Bad material type
+    with pytest.raises(TypeError):
+        Pin.from_radii(good_radii, [mat.name for mat in pin_mats])
+
+    # Incorrect lengths
+    with pytest.raises(ValueError) as exec_info:
+        Pin.from_radii(good_radii[: len(pin_mats) - 2], pin_mats)
+    assert "length" in str(exec_info)
+
+    # Non-positive radii
+    rad = (-0.1,) + good_radii[1:]
+    with pytest.raises(ValueError) as exec_info:
+        Pin.from_radii(rad, pin_mats)
+    assert "index 0" in str(exec_info)
+
+    # Non-increasing radii
+    rad = tuple(reversed(good_radii))
+    with pytest.raises(ValueError) as exec_info:
+        Pin.from_radii(rad, pin_mats)
+    assert "index 1" in str(exec_info)
+
+    # Bad orientation
+    with pytest.raises(ValueError) as exec_info:
+        Pin.from_radii(good_radii, pin_mats, orientation="fail")
+    assert "Orientation" in str(exec_info)
+
+
+def test_from_radii(pin_mats, good_radii):
+    name = "test pin"
+    p = Pin.from_radii(good_radii, pin_mats, name=name)
+    assert len(p.cells) == len(pin_mats)
+    assert p.name == name
+    assert p.radii == good_radii
+
+def test_subdivide(pin_mats, good_radii):
+    surfs = [openmc.ZCylinder(r=r) for r in good_radii]
+    pin = Pin(surfs, pin_mats)
+    assert pin.radii == good_radii
+    assert len(pin.cells) == len(pin_mats)
+
+    # subdivide inner region
+    N = 5
+    pin.subdivide_ring(0, N)
+    assert len(pin.radii) == len(good_radii) + N - 1
+    assert len(pin.cells) == len(pin_mats) + N - 1
+    # check volumes of new rings
+    bounds = (0,) + pin.radii[:N]
+    sqrs = numpy.square(bounds)
+    assert sqrs[1:] - sqrs[:-1] == pytest.approx(good_radii[0] ** 2 / N)
+
+    # subdivide non-inner most region
+    new_pin = Pin.from_radii(good_radii, pin_mats)
+    new_pin.subdivide_ring(1, N)
+    assert len(new_pin.radii) == len(good_radii) + N - 1
+    assert len(new_pin.cells) == len(pin_mats) + N - 1
+    # check volumes of new rings
+    bounds = new_pin.radii[:N + 1]
+    sqrs = numpy.square(bounds)
+    assert sqrs[1:] - sqrs[:-1] == pytest.approx((good_radii[1] ** 2 - good_radii[0] ** 2) / N)
+

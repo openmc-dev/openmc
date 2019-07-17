8b787b2bb0e7cdfc504db9179ddd6fc7dec71faf
itayhorin <itayhorin@gmail.com>
2019-07-17 17:09:13 -0500

    Remove cf4 function for deplete.CF4Integrator
    The function openmc.deplete.cf4 has been removed in favor
    of openmc.deplete.CF4Integrator. The CF4 integration scheme
    can be peformed with
    
    >>> from openmc.deplete import CF4Integrator
    >>> CF4Integrator(operator, time, power).integrate()

--- tests/unit_tests/test_deplete_cf4.py
+++ tests/unit_tests/test_deplete_cf4.py
@@ -4,7 +4,7 @@ These tests integrate a simple test problem described in dummy_geometry.py.
 """
 
 from pytest import approx
-import openmc.deplete
+from openmc.deplete import CF4Integrator, ResultsList
 
 from tests import dummy_operator
 
@@ -18,10 +18,10 @@ def test_cf4(run_in_tmpdir):
     # Perform simulation using the cf4 algorithm
     dt = [0.75, 0.75]
     power = 1.0
-    openmc.deplete.cf4(op, dt, power, print_out=False)
+    CF4Integrator(op, dt, power).integrate()
 
     # Load the files
-    res = openmc.deplete.ResultsList(op.output_dir / "depletion_results.h5")
+    res = ResultsList(op.output_dir / "depletion_results.h5")
 
     _, y1 = res.get_atoms("1", "1")
     _, y2 = res.get_atoms("1", "2")

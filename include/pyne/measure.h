1c0a999baf17d89a7b845b86088c95299898b2f9
zxkjack123 <zxkjack123@163.com>
2019-07-29 13:20:30 -0500

    change two header files from link to file

--- include/pyne/measure.h
+++ include/pyne/measure.h
@@ -1,1 +1,14 @@
-/root/opt/pyne/src/measure.h
+#ifndef MEASURE_HPP
+
+#include "moab/CN.hpp"
+
+double edge_length( const double* start_vtx_coords,
+                    const double* end_vtx_coords );
+
+double measure( moab::EntityType type,
+                int num_vertices,
+                const double* vertex_coordinatee );
+
+#endif
+
+                

46461ca3c9ff9c9e81668bf7fd50d612bc2cb0d9
Xiaokang Zhang <zhangxk@ipp.ac.cn>
2019-08-29 11:21:30 -0500

    Update include/openmc/source.h
    Co-Authored-By: Paul Romano <paul.k.romano@gmail.com>

--- include/openmc/source.h
+++ include/openmc/source.h
@@ -81,7 +81,7 @@ Particle::Bank convert_pyne_source_particle(const pyne::SourceParticle&);
 
 //! Check the source particle converted from pyne source
 //! \return Bool
-bool check_pyne_source_particle(Particle::Bank);
+bool check_pyne_source_particle(const Particle::Bank&);
 #endif
 
 //! Fill source bank at end of generation for fixed source simulations

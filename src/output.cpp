44892387cd4bd0d3b4e3c19ed9f8d749cf1ef202
itayhorin <itayhorin@gmail.com>
2019-09-03 12:40:36 -0500

    Make sure output is always verbosity-limited

--- src/output.cpp
+++ src/output.cpp
@@ -211,6 +211,7 @@ extern "C" void print_particle(Particle* p)
 void print_plot()
 {
   header("PLOTTING SUMMARY", 5);
+  if (settings::verbosity < 5) return;
 
   for (auto pl : model::plots) {
     // Plot id
@@ -453,6 +454,7 @@ void print_runtime()
 
   // display header block
   header("Timing Statistics", 6);
+  if (settings::verbosity < 6) return;
 
   // Save state of cout
   auto f {std::cout.flags()};
@@ -537,6 +539,7 @@ void print_results()
 
   // display header block for results
   header("Results", 4);
+  if (settings::verbosity < 4) return;
 
   // Calculate t-value for confidence intervals
   int n = simulation::n_realizations;

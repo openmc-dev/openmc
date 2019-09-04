09fa8822938290bca1252bd8093699f1dd7f17b5
itayhorin <itayhorin@gmail.com>
2019-09-03 21:49:32 -0500

    Make sure MPI_Type_free is not called if MPI hasn't been initialized

--- src/message_passing.cpp
+++ src/message_passing.cpp
@@ -8,8 +8,8 @@ int n_procs {1};
 bool master {true};
 
 #ifdef OPENMC_MPI
-MPI_Comm intracomm;
-MPI_Datatype bank;
+MPI_Comm intracomm {MPI_COMM_NULL};
+MPI_Datatype bank {MPI_DATATYPE_NULL};
 #endif
 
 extern "C" bool openmc_master() { return mpi::master; }

4fc6319f03299d8192471f2bfc677909919b570f
Patrick Shriwise <pshriwise@gmail.com>
2019-10-10 10:06:33 -0500

    Updating legacy test as requested in PR by @paulromano.

--- tests/regression_tests/dagmc/legacy/test.py
+++ tests/regression_tests/dagmc/legacy/test.py
@@ -8,48 +8,48 @@ pytestmark = pytest.mark.skipif(
     not openmc.lib._dagmc_enabled(),
     reason="DAGMC CAD geometry is not enabled.")
 
-class DAGMCLegacyTest(PyAPITestHarness):
+@pytest.fixture
+def model():
 
-    def _build_inputs(self):
-        model = openmc.model.Model()
+    model = openmc.model.Model()
 
-        # settings
-        model.settings.batches = 5
-        model.settings.inactive = 0
-        model.settings.particles = 100
+    # settings
+    model.settings.batches = 5
+    model.settings.inactive = 0
+    model.settings.particles = 100
 
-        source_box = openmc.stats.Box([-4, -4, -4],
-                                      [ 4,  4,  4])
-        source = openmc.Source(space=source_box)
+    source_box = openmc.stats.Box([-4, -4, -4],
+                                  [ 4,  4,  4])
+    source = openmc.Source(space=source_box)
 
-        model.settings.source = source
+    model.settings.source = source
 
-        model.settings.dagmc = True
+    model.settings.dagmc = True
 
-        # tally
-        tally = openmc.Tally()
-        tally.scores = ['total']
-        tally.filters = [openmc.CellFilter(1)]
-        model.tallies = [tally]
+    # tally
+    tally = openmc.Tally()
+    tally.scores = ['total']
+    tally.filters = [openmc.CellFilter(1)]
+    model.tallies = [tally]
 
-        # materials
-        u235 = openmc.Material(name="no-void fuel")
-        u235.add_nuclide('U235', 1.0, 'ao')
-        u235.set_density('g/cc', 11)
-        u235.id = 40
+    # materials
+    u235 = openmc.Material(name="no-void fuel")
+    u235.add_nuclide('U235', 1.0, 'ao')
+    u235.set_density('g/cc', 11)
+    u235.id = 40
 
-        water = openmc.Material(name="water")
-        water.add_nuclide('H1', 2.0, 'ao')
-        water.add_nuclide('O16', 1.0, 'ao')
-        water.set_density('g/cc', 1.0)
-        water.add_s_alpha_beta('c_H_in_H2O')
-        water.id = 41
+    water = openmc.Material(name="water")
+    water.add_nuclide('H1', 2.0, 'ao')
+    water.add_nuclide('O16', 1.0, 'ao')
+    water.set_density('g/cc', 1.0)
+    water.add_s_alpha_beta('c_H_in_H2O')
+    water.id = 41
 
-        mats = openmc.Materials([u235, water])
-        model.materials = mats
+    mats = openmc.Materials([u235, water])
+    model.materials = mats
 
-        model.export_to_xml()
+    return model
 
-def test_dagmc():
-    harness = DAGMCLegacyTest('statepoint.5.h5')
+def test_dagmc(model):
+    harness = PyAPITestHarness('statepoint.5.h5', model)
     harness.main()

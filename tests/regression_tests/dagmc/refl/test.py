9fcf76cc898555a9f3c52ac0b7167009a1f382cb
John Tramm <john.tramm@gmail.com>
2019-12-17 19:32:06 +0000

    set DAGMC tests to skip

--- tests/regression_tests/dagmc/refl/test.py
+++ tests/regression_tests/dagmc/refl/test.py
@@ -35,6 +35,7 @@ class UWUWTest(PyAPITestHarness):
 
         model.tallies.export_to_xml()
 
+@pytest.mark.skip(reason="DAGMC not planned for GPU support.")
 def test_refl():
     harness = UWUWTest('statepoint.5.h5')
     harness.main()

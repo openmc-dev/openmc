dcc33f1a609ecaf00bdff8b5c09695861593c4ba
itayhorin <itayhorin@gmail.com>
2019-12-31 12:11:49 -0500

    Adding CylindricalIndependent source type
    ammended docstrings to mention local reference frame

--- src/distribution_spatial.cpp
+++ src/distribution_spatial.cpp
@@ -52,6 +52,72 @@ Position CartesianIndependent::sample(uint64_t* seed) const
 }
 
 //==============================================================================
+// CylindricalIndependent implementation
+//==============================================================================
+
+CylindricalIndependent::CylindricalIndependent(pugi::xml_node node)
+{
+  // Read distribution for r-coordinate
+  if (check_for_node(node, "r")) {
+    pugi::xml_node node_dist = node.child("r");
+    r_ = distribution_from_xml(node_dist);
+  } else {
+    // If no distribution was specified, default to a single point at r=0
+    double x[] {0.0};
+    double p[] {1.0};
+    r_ = std::make_unique<Discrete>(x, p, 1);
+  }
+
+  // Read distribution for phi-coordinate
+  if (check_for_node(node, "phi")) {
+    pugi::xml_node node_dist = node.child("phi");
+    phi_ = distribution_from_xml(node_dist);
+  } else {
+    // If no distribution was specified, default to a single point at phi=0
+    double x[] {0.0};
+    double p[] {1.0};
+    phi_ = std::make_unique<Discrete>(x, p, 1);
+  }
+
+  // Read distribution for z-coordinate
+  if (check_for_node(node, "z")) {
+    pugi::xml_node node_dist = node.child("z");
+    z_ = distribution_from_xml(node_dist);
+  } else {
+    // If no distribution was specified, default to a single point at z=0
+    double x[] {0.0};
+    double p[] {1.0};
+    z_ = std::make_unique<Discrete>(x, p, 1);
+  }
+
+  // Read cylinder center coordinates
+  if (check_for_node(node, "origin")) {
+    auto origin = get_node_array<double>(node, "origin");
+    if (origin.size() == 3) {
+      origin_ = origin;
+    } else {
+      std::stringstream err_msg;
+      err_msg << "Origin for cylindrical source distribution must be length 3";
+      fatal_error(err_msg);
+    }
+  } else {
+    // If no coordinates were specified, default to (0, 0, 0)
+    origin_ = {0.0, 0.0, 0.0};
+  }
+
+}
+
+Position CylindricalIndependent::sample(uint64_t* seed) const
+{
+  double r = r_->sample(seed);
+  double phi = phi_->sample(seed);
+  double x = r*cos(phi) + origin_.x;
+  double y =  r*sin(phi) + origin_.y;
+  double z = z_->sample(seed) + origin_.z;
+  return {x, y, z};
+}
+
+//==============================================================================
 // SphericalIndependent implementation
 //==============================================================================
 

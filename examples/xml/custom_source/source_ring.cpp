ac6f5124329b67d09bdedf1339157cad7e603cd2
itayhorin <itayhorin@gmail.com>
2020-02-12 13:11:53 +0000

    updated the example source according to style

--- examples/xml/custom_source/source_ring.cpp
+++ examples/xml/custom_source/source_ring.cpp
@@ -5,21 +5,22 @@
 
 // you must have external C linkage here otherwise 
 // dlopen will not find the file
-extern "C" openmc::Particle::Bank sample_source() {
-    openmc::Particle::Bank particle;
-    // wgt
-    particle.particle = openmc::Particle::Type::neutron;
-    particle.wgt = 1.0;
-    // position
+extern "C" openmc::Particle::Bank sample_source() 
+{
+  openmc::Particle::Bank particle;
+  // wgt
+  particle.particle = openmc::Particle::Type::neutron;
+  particle.wgt = 1.0; 
+  // position
     
-    double angle = 2. * M_PI * openmc::prn();
-    double radius = 3.0;
-    particle.r.x = radius * std::cos(angle);
-    particle.r.y = radius * std::sin(angle);
-    particle.r.z = 0.0;
-    // angle
-    particle.u = {1.0, 0.0, 0.0};
-    particle.E = 14.08e6;
-    particle.delayed_group = 0;
-    return particle;    
+  double angle = 2. * M_PI * openmc::prn();
+  double radius = 3.0;
+  particle.r.x = radius * std::cos(angle);
+  particle.r.y = radius * std::sin(angle);
+  particle.r.z = 0.0;
+  // angle
+  particle.u = {1.0, 0.0, 0.0};
+  particle.E = 14.08e6;
+  particle.delayed_group = 0;
+  return particle;    
 }

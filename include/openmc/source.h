eac442f31c28fa490b1c3ec99b31425a4b510105
davidjohnlong <david.long2@woodplc.com>
2020-03-12 09:34:38 +0000

    Move function description to header file

--- include/openmc/source.h
+++ include/openmc/source.h
@@ -68,7 +68,10 @@ Particle::Bank sample_external_source(uint64_t* seed);
 //! Sample a site from custom source library
 Particle::Bank sample_custom_source_library(uint64_t* seed);
 
+//Load custom source library
 void load_custom_source_library();
+
+//Release custom source library
 void close_custom_source_library();
 
 //! Fill source bank at the end of a generation for dlopen based source simulation

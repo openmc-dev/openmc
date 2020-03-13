23201a58d61d7f7a2a71549c32e5eae53dc482ff
itayhorin <itayhorin@gmail.com>
2020-03-13 16:13:53 -0500

    Add Python version of custom_source

---
+++ examples/python/custom_source/show_flux.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import openmc
+
+# Get the flux from the statepoint
+with openmc.StatePoint('statepoint.10.h5') as sp:
+    flux = sp.tallies[1].mean
+    flux.shape = (50, 50)
+
+# Plot the flux
+fig, ax = plt.subplots()
+ax.imshow(flux, origin='lower', extent=(-5.0, 5.0, -5.0, 5.0))
+ax.set_xlabel('x [cm]')
+ax.set_ylabel('y [cm]')
+plt.show()
+
+# If all worked well, you should see a ring "imprint" as well as a higher flux
+# to the right side (since the custom source has all particles moving in the
+# positive x direction))

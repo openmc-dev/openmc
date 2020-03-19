c01c67cfee0ffce5c1e4c0660e46d44b5a4a8d8d
itayhorin <itayhorin@gmail.com>
2020-03-19 13:18:02 -0500

    Some added documentation/cleanup of Python API files.

--- openmc/filter.py
+++ openmc/filter.py
@@ -750,7 +750,7 @@ class MeshFilter(Filter):
         cv.check_type('filter mesh', mesh, openmc.MeshBase)
         self._mesh = mesh
         if isinstance(mesh, openmc.UnstructuredMesh):
-            self.bins = [(n, 1, 1) for n in range(1, len(mesh.volumes) + 1)]
+            self.bins = list(range(len(mesh.volumes)))
         else:
             self.bins = list(mesh.indices)
 

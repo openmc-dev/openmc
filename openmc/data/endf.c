0af155590e1d0750831a88702672c1c463780bdc
itayhorin <itayhorin@gmail.com>
2020-04-15 08:05:02 -0500

    Changes in data/endf.c from PullRequest Inc. review

--- openmc/data/endf.c
+++ openmc/data/endf.c
@@ -1,14 +1,31 @@
 #include <stdlib.h>
 
+//! Convert string representation of a floating point number into a double
+//
+//! This function handles converting floating point numbers from an ENDF 11
+//! character field into a double, covering all the corner cases. Floating point
+//! numbers are allowed to contain whitespace (which is ignored). Also, in
+//! exponential notation, it allows the 'e' to be omitted. A field containing
+//! only whitespace is to be interpreted as a zero.
+//
+//! \param buffer character input from an ENDF file
+//! \param n Length of character input
+//! \return Floating point number
+
 double cfloat_endf(const char* buffer, int n)
 {
   char arr[12]; // 11 characters plus a null terminator
   int j = 0; // current position in arr
   int found_significand = 0;
   int found_exponent = 0;
+
+  // limit n to 11 characters
+  n = n > 11 ? 11 : n;
+
   for (int i = 0; i < n; ++i) {
-    // Skip whitespace characters
     char c = buffer[i];
+
+    // Skip whitespace characters
     if (c == ' ') continue;
 
     if (found_significand) {

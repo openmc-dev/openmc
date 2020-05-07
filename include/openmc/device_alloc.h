f53c81aa716aa6d65d213f6a77edda7efd41e3e6
John Tramm <john.tramm@gmail.com>
2020-05-07 19:38:45 +0000

    actually added the device_alloc files.

---
+++ include/openmc/device_alloc.h
@@ -0,0 +1,18 @@
+#ifndef OPENMC_DEVICE_ALLOC_H
+#define OPENMC_DEVICE_ALLOC_H
+
+#include <omp.h>
+#include <assert.h>
+#include <stdlib.h>
+
+#define USE_DEVICE
+
+namespace openmc {
+
+void * device_alloc( size_t sz, int device_id );
+
+void device_memcpy( void * dst_ptr, void * src_ptr, size_t sz, int dst_id, int src_id);
+
+} // namespace openmc
+
+#endif // OPENMC_DEVICE_ALLOC_H
---
+++ src/device_alloc.cpp
@@ -0,0 +1,29 @@
+#include "openmc/device_alloc.h"
+
+namespace openmc {
+
+void * device_alloc( size_t sz, int device_id )
+{
+  void * ptr;
+
+  #ifdef USE_DEVICE
+  ptr = (void *) omp_target_alloc(sz, device_id);
+  #else
+  ptr = (void *) malloc(sz);
+  #endif
+
+  assert(ptr != NULL);
+
+  return ptr;
+}
+
+void device_memcpy( void * dst_ptr, void * src_ptr, size_t sz, int dst_id, int src_id)
+{
+  #ifdef USE_DEVICE
+  omp_target_memcpy(dst_ptr, src_ptr, sz, 0, 0, dst_id, src_id);
+  #else
+  memcpy(dst_ptr, src_ptr, sz);
+  #endif
+}
+
+} // namespace openmc

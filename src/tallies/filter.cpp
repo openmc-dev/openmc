8265907c3f54fb65a6436a3d17a10ab1a0e32156
Patrick Shriwise <pshriwise@gmail.com>
2020-05-12 17:14:18 -0500

    Aesthetic changes to the new create method.

--- src/tallies/filter.cpp
+++ src/tallies/filter.cpp
@@ -73,13 +73,10 @@ template<typename T>
 T* Filter::create(int32_t id) {
   static_assert(std::is_base_of<Filter, T>::value,
                 "Type specified is not derived from openmc::Filter");
-
+  // Create filter and add to filters vector
   auto filter = std::make_unique<T>();
-
   auto ptr_out = filter.get();
-
   model::tally_filters.emplace_back(std::move(filter));
-
   // Assign ID
   model::tally_filters.back()->set_id(id);
 

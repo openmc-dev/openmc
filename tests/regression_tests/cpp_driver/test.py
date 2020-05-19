21ae7184f8f62dc6c4e584df31dfd4b8ae59f141
itayhorin <itayhorin@gmail.com>
2020-05-19 13:52:31 -0500

    Updating the cpp driver test to include testing of the new set_temperature behavior. Includes a fix to the dynamic source test to remove the CMake file as well.

--- tests/regression_tests/cpp_driver/test.py
+++ tests/regression_tests/cpp_driver/test.py
@@ -4,6 +4,7 @@ import shutil
 import subprocess
 import textwrap
 
+from numpy.testing import assert_allclose
 import openmc
 import pytest
 
@@ -46,6 +47,7 @@ def cpp_driver(request):
     finally:
         # Remove local build directory when test is complete
         shutil.rmtree('build')
+        os.remove('CMakeLists.txt')
 
 
 @pytest.fixture
@@ -57,22 +59,39 @@ def model():
     u235.add_nuclide('U235', 1.0, 'ao')
     u235.set_density('g/cc', 11)
 
+    zirc = openmc.Material(name='cladding')
+    zirc.add_nuclide('Zr90', 1.0)
+    zirc.set_density('g/cc', 6.44)
+
     water = openmc.Material(name="water")
     water.add_nuclide('H1', 2.0, 'ao')
     water.add_nuclide('O16', 1.0, 'ao')
     water.set_density('g/cc', 1.0)
 
-    mats = openmc.Materials([u235, water])
+    mats = openmc.Materials([u235, zirc, water])
     model.materials = mats
 
     # geometry
     fuel_or = openmc.ZCylinder(r=1.5)
-    coolant_or = openmc.ZCylinder(r=3.0, boundary_type='reflective')
+    cladding_or = openmc.ZCylinder(r=1.7)
 
     fuel = openmc.Cell(fill=u235, region=-fuel_or)
-    coolant = openmc.Cell(fill=water, region=+fuel_or & -coolant_or)
+    cladding = openmc.Cell(fill=zirc, region=+fuel_or & -cladding_or)
+    moderator = openmc.Cell(fill=water, region=+cladding_or)
 
-    model.geometry = openmc.Geometry([fuel, coolant])
+    pincell_univ = openmc.Universe(cells=[fuel, cladding, moderator])
+
+    # lattice
+    lattice = openmc.RectLattice()
+    lattice.pitch = (4.0, 4.0)
+    lattice.lower_left = (-4.0, -4.0)
+    lattice.universes = [[pincell_univ, pincell_univ], [pincell_univ, pincell_univ]]
+    lattice_region = openmc.model.rectangular_prism(8.0,
+                                                    8.0,
+                                                    boundary_type='reflective')
+    lattice_cell = openmc.Cell(fill=lattice, region=lattice_region)
+
+    model.geometry = openmc.Geometry([lattice_cell])
 
     model.settings.particles = 100
     model.settings.batches = 10
@@ -96,6 +115,22 @@ class ExternalDriverTestHarness(PyAPITestHarness):
         else:
             openmc.run(openmc_exec=self.executable,
                        event_based=config['event'])
+
+    def _compare_results(self):
+        super()._compare_results()
+
+        # load the summary file
+        summary = openmc.Summary('summary.h5')
+
+        # get the summary cells
+        cells = summary.geometry.get_all_cells()
+
+        # for the 2 by 2 lattice, each cell should have 4
+        # temperature values set to 300 K
+        for cell in cells.values():
+            if isinstance(cell.fill, openmc.Material):
+                assert len(cell.temperature) == 4
+                assert_allclose(cell.temperature, 300.0)
 
 
 def test_cpp_driver(cpp_driver, model):

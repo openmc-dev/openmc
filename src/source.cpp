7d6c6dae67e165c4a27962f2b90733ad97ae020a
YuanHu-PKU-KIT <59049521+YuanHu-PKU-KIT@users.noreply.github.com>
2020-07-02 15:04:21 +0200

    Update source.cpp

--- src/source.cpp
+++ src/source.cpp
@@ -248,12 +248,10 @@ Particle::Bank SourceDistribution::sample(uint64_t* seed) const
       // Resample if energy falls outside minimum or maximum particle energy
       if (site.E < data::energy_max[p] && site.E > data::energy_min[p]) break;
     }
-    // origin code
   }   // add by Yuan
   
   // Set delayed group
   site.delayed_group = 0;
-  
   return site;
 }
 

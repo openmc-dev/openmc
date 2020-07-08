e39e21ec1303d7256080adfb345a5cf8f11e4089
YuanHu-PKU-KIT <59049521+YuanHu-PKU-KIT@users.noreply.github.com>
2020-07-08 10:34:52 +0200

    Update particle.cpp

--- src/particle.cpp
+++ src/particle.cpp
@@ -84,34 +84,19 @@ Particle::clear()
 }
 
 void
-Particle::create_secondary(Direction u, double E, Type type)
+Particle::create_secondary(double wgt, Direction u, double E, Type type)
 {
   secondary_bank_.emplace_back();
 
   auto& bank {secondary_bank_.back()};
   bank.particle = type;
-  bank.wgt = wgt_;
+  bank.wgt = wgt;
   bank.r = this->r();
   bank.u = u;
   bank.E = settings::run_CE ? E : g_;
 
   n_bank_second_ += 1;
 }
-  
-void              // add by Yuan
-Particle::create_secondary(Direction u, double E, Type type, double weight)    
-{
-  secondary_bank_.emplace_back();
-
-  auto& bank {secondary_bank_.back()};
-  bank.particle = type;
-  bank.wgt = weight;
-  bank.r = this->r();
-  bank.u = u;
-  bank.E = settings::run_CE ? E : g_;
-
-  n_bank_second_ += 1;
-}       // add by Yuan
 
 void
 Particle::from_source(const Bank* src)

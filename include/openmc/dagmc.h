b04406f65612109a7913e0d043d2be12d728ee74
itayhorin <itayhorin@gmail.com>
2020-07-08 21:32:56 -0500

    Returning universe index from the original dagmc function now.

--- include/openmc/dagmc.h
+++ include/openmc/dagmc.h
@@ -23,7 +23,7 @@ namespace model {
 //==============================================================================
 
 void load_dagmc_geometry();
-void create_dagmc_universe(const std::string& filename);
+int32_t create_dagmc_universe(const std::string& filename);
 void read_geometry_dagmc();
 bool read_uwuw_materials(pugi::xml_document& doc);
 bool get_uwuw_materials_xml(std::string& s);

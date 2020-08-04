fbcd789f205630612ee3afb2320702dd7219bc4f
Paul Romano <paul.k.romano@gmail.com>
2020-08-04 16:11:11 -0500

    Use flux reaction_rate_mode in activation test

--- tests/unit_tests/test_deplete_activation.py
+++ tests/unit_tests/test_deplete_activation.py
@@ -1,5 +1,7 @@
-from math import pi, log
+from math import pi, log, log10
 from random import uniform, normalvariate
+
+import numpy as np
 
 import openmc.deplete
 import openmc
@@ -38,7 +40,8 @@ def model():
     return model
 
 
-def test_activation(run_in_tmpdir, model):
+@pytest.mark.parametrize("reaction_rate_mode", ["direct", "flux"])
+def test_activation(run_in_tmpdir, model, reaction_rate_mode):
     # Determine (n.gamma) reaction rate using initial run
     sp = model.run()
     with openmc.StatePoint(sp) as sp:
@@ -53,9 +56,12 @@ def test_activation(run_in_tmpdir, model):
     chain.export_to_xml('test_chain.xml')
 
     # Create transport operator
+    energies = np.logspace(log10(1e-5), log10(2e7), 100)
     op = openmc.deplete.Operator(
         model.geometry, model.settings, 'test_chain.xml',
-        normalization_mode="source-rate"
+        normalization_mode="source-rate",
+        reaction_rate_mode=reaction_rate_mode,
+        reaction_rate_energies=energies,
     )
 
     # To determine the source rate necessary to reduce W186 density in half, we
@@ -98,7 +104,7 @@ def test_activation(run_in_tmpdir, model):
     _, atoms = results.get_atoms(str(w.id), "W186")
 
     assert atoms[0] == pytest.approx(n0)
-    assert atoms[1] / atoms[0] == pytest.approx(0.5, rel=1e-3)
+    assert atoms[1] / atoms[0] == pytest.approx(0.5, rel=0.01)
 
 
 def test_decay(run_in_tmpdir):

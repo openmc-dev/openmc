ddfda7e6eddc4c23c4ea5806e04aaf22a100ee72
Jingang Liang <liangjg2008@gmail.com>
2020-09-10 07:31:46 +0800

    added unit test for wmp genetation from ENDF

---
+++ tools/ci/travis-install-vectfit.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -ex
+
+#pip3 install pybind11
+PYBIND_BRANCH='master'
+PYBIND_REPO='https://github.com/pybind/pybind11'
+
+XTL_BRANCH='0.6.9'
+XTL_REPO='https://github.com/xtensor-stack/xtl'
+
+XTENSOR_BRANCH='0.21.2'
+XTENSOR_REPO='https://github.com/xtensor-stack/xtensor'
+
+XTENSOR_PYTHON_BRANCH='0.24.1'
+XTENSOR_PYTHON_REPO='https://github.com/xtensor-stack/xtensor-python'
+
+XTENSOR_BLAS_BRANCH='0.17.1'
+XTENSOR_BLAS_REPO='https://github.com/xtensor-stack/xtensor-blas'
+
+cd $HOME
+git clone -b $PYBIND_BRANCH $PYBIND_REPO
+cd pybind11 && mkdir build && cd build && cmake .. && sudo make install
+
+cd $HOME
+git clone -b $XTL_BRANCH $XTL_REPO
+cd xtl && mkdir build && cd build && cmake .. && sudo make install
+
+cd $HOME
+git clone -b $XTENSOR_BRANCH $XTENSOR_REPO
+cd xtensor && mkdir build && cd build && cmake .. && sudo make install
+
+cd $HOME
+git clone -b $XTENSOR_PYTHON_BRANCH $XTENSOR_PYTHON_REPO
+cd xtensor-python && mkdir build && cd build && cmake .. && sudo make install
+
+cd $HOME
+git clone -b $XTENSOR_BLAS_BRANCH $XTENSOR_BLAS_REPO
+cd xtensor-blas && mkdir build && cd build && cmake .. && sudo make install
+
+# Install vectfit
+cd $HOME
+git clone https://github.com/liangjg/vectfit.git
+pip3 install ./vectfit

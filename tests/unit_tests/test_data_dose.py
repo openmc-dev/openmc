0c95b93d5c4590ee42bb2c96ab419a74d3beb084
Jingang Liang <liangjg2008@gmail.com>
2020-09-14 11:12:36 +0800

    fix energy unit conversion from Mev to eV

--- tests/unit_tests/test_data_dose.py
+++ tests/unit_tests/test_data_dose.py
@@ -5,21 +5,21 @@ from pytest import approx, raises
 def test_dose_coefficients():
     # Spot checks on values from ICRP tables
     energy, dose = dose_coefficients('photon', 'AP')
-    assert energy[0] == approx(0.01e-6)
+    assert energy[0] == approx(0.01e6)
     assert dose[0] == approx(0.0685)
-    assert energy[-1] == approx(10e-3)
+    assert energy[-1] == approx(10e9)
     assert dose[-1] == approx(90.4)  # updated in corrigendum
 
     energy, dose = dose_coefficients('neutron', 'LLAT')
-    assert energy[0] == approx(1e-15)
+    assert energy[0] == approx(1e-3)
     assert dose[0] == approx(1.04)
-    assert energy[-1] == approx(10e-3)
+    assert energy[-1] == approx(10e9)
     assert dose[-1] == approx(1.23e3)
 
     energy, dose = dose_coefficients('electron', 'ISO')
-    assert energy[0] == approx(0.01e-6)
+    assert energy[0] == approx(0.01e6)
     assert dose[0] == approx(0.0188)
-    assert energy[-1] == approx(10e-3)
+    assert energy[-1] == approx(10e9)
     assert dose[-1] == approx(699.0)
 
     # Invalid particle/geometry should raise an exception

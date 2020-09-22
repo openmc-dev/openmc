b75c8287336054b3c7baee207cbb2692c844ed3c
YuanHu-PKU-KIT <59049521+YuanHu-PKU-KIT@users.noreply.github.com>
2020-09-22 09:51:10 +0800

    Update physics.h

--- include/openmc/physics.h
+++ include/openmc/physics.h
@@ -90,6 +90,7 @@ void inelastic_scatter(const Nuclide& nuc, const Reaction& rx, Particle& p);
 void sample_secondary_photons(Particle& p, int i_nuclide);
   
 //！Split or Roulette particles based their weight and the lower weight window bound.
+//! \param[in] p, particle to be split or rouletted with the weight window.
 void split_particle(Particle& p);
 
 } // namespace openmc

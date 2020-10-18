58a0aa3297e6b722dd27325773eb0fa6ccfedeae
Sterling Harper <smharper@mit.edu>
2020-10-17 22:16:28 -0600

    Add surface indices to the PeriodicBC class

--- src/boundary_condition.cpp
+++ src/boundary_condition.cpp
@@ -30,10 +30,16 @@ WhiteBC::handle_particle(Particle& p, const Surface& surf) const
 void
 PeriodicBC::handle_particle(Particle& p, const Surface& surf) const
 {
-  // Get a pointer to the partner periodic surface
+  // Get a pointer of the first surface downcast to PeriodicSurface
   auto surf_p = dynamic_cast<const PeriodicSurface*>(&surf);
-  auto other = dynamic_cast<const PeriodicSurface*>(
-    model::surfaces[surf_p->i_periodic_].get());
+
+  // Get a pointer to the partner PeriodicSurface
+  auto other =
+    surf.id_ == model::surfaces[i_surf_]->id_ ?
+    dynamic_cast<const PeriodicSurface*>(model::surfaces[j_surf_].get()) :
+    dynamic_cast<const PeriodicSurface*>(model::surfaces[i_surf_].get());
+  //auto other = dynamic_cast<const PeriodicSurface*>(
+  //  model::surfaces[surf_p->i_periodic_].get());
 
   // Compute the new particle location and direction
   Position r {p.r()};

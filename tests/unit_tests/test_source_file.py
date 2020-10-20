73b68a196f51f2a51490cb206a4653a00239d294
itayhorin <itayhorin@gmail.com>
2020-10-20 14:07:45 -0500

    Make sure write_source_file creates filetype attribute

--- tests/unit_tests/test_source_file.py
+++ tests/unit_tests/test_source_file.py
@@ -3,9 +3,14 @@ from random import random
 import h5py
 import numpy as np
 import openmc
+import pytest
 
 
 def test_source_file(run_in_tmpdir):
+    # write_source_file shouldn't accept non-SourceParticle items
+    with pytest.raises(TypeError):
+        openmc.write_source_file([1, 2, 3], 'test_source.h5')
+
     # Create source particles
     source = []
     n = 1000
@@ -21,9 +26,11 @@ def test_source_file(run_in_tmpdir):
 
     # Get array of source particles from file
     with h5py.File('test_source.h5', 'r') as fh:
+        filetype = fh.attrs['filetype']
         arr = fh['source_bank'][...]
 
     # Ensure data is consistent
+    assert filetype == b'source'
     r = arr['r']
     assert np.all((r['x'] > 0.0) & (r['x'] < 1.0))
     assert np.all(r['y'] == np.arange(1000))

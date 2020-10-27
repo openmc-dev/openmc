08937e0eb57126b1bb41b51f4950670cb890c4d6
Patrick Shriwise <pshriwise@gmail.com>
2020-10-27 12:29:23 -0500

    Cloning specific version of libmesh.

--- tools/ci/travis-install-libmesh.sh
+++ tools/ci/travis-install-libmesh.sh
@@ -7,7 +7,7 @@ CURRENT_DIR=$(pwd)
 # LibMESH install
 cd $HOME
 mkdir LIBMESH && cd LIBMESH
-git clone https://github.com/libmesh/libmesh --recurse-submodules
+git clone https://github.com/libmesh/libmesh -b v1.6.0 --recurse-submodules
 mkdir build && cd build
 ../libmesh/configure --prefix=$HOME/LIBMESH --enable-exodus --disable-mpi
 make -j4 install

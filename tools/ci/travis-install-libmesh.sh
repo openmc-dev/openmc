3927985cdfa561a5d0d7fc7b64d2bdc33ca5aede
Patrick Shriwise <pshriwise@gmail.com>
2020-10-27 00:22:08 -0500

    Getting libmesh submodules as well.

--- tools/ci/travis-install-libmesh.sh
+++ tools/ci/travis-install-libmesh.sh
@@ -7,7 +7,7 @@ CURRENT_DIR=$(pwd)
 # LibMESH install
 cd $HOME
 mkdir LIBMESH && cd LIBMESH
-git clone https://github.com/libmesh/libmesh
+git clone https://github.com/libmesh/libmesh --recurse-submodules
 mkdir build && cd build
 ../libmesh/configure --prefix=$HOME/LIBMESH --enable-exodus --disable-mpi
 make -j4 install

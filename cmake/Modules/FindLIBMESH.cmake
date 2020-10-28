47e0a9ad88738093af4f83dbbda2a46e9e04a70f
itayhorin <itayhorin@gmail.com>
2020-10-27 22:54:33 -0500

    Cleanup of CMake files.

--- cmake/Modules/FindLIBMESH.cmake
+++ cmake/Modules/FindLIBMESH.cmake
@@ -1,15 +1,12 @@
 # Finds the libMesh installation using CMake's PkgConfig
 # module and creates a libmesh imported target
 
-find_path(LIBMESH_PC NAMES libmesh-opt.pc
-          HINTS ${LIBMESH_DIR} $ENV{LIBMESH_ROOT}
-          PATHS ENV LD_LIBRARY_PATH
-          PATH_SUFFIXES lib/pkgconfig pkgconfig
-          NO_DEFAULT_PATH)
-
 include(FindPkgConfig)
 set(ENV{PKG_CONFIG_PATH} "$ENV{PKG_CONFIG_PATH}:${LIBMESH_PC}")
 pkg_check_modules(LIBMESH REQUIRED libmesh IMPORTED_TARGET)
-pkg_check_modules(LIBMESH_DEVEL REQUIRED libmesh-devel IMPORTED_TARGET)
 
-message(STATUS "Found LIBMESH in ${LIBMESH_PC}")
+find_path(LIBMESH_PC NAMES libmesh.pc
+          HINTS ${LIBMESH_LIBDIR}
+          PATHS ENV LD_LIBRARY_PATH
+          PATH_SUFFIXES lib/pkgconfig pkgconfig
+          NO_DEFAULT_PATH)

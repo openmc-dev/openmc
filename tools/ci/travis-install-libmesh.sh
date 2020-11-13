fbbc421a0c7d07ec05e1511cb8d2fde2f75a8e8f
Patrick Shriwise <pshriwise@gmail.com>
2020-11-13 11:01:52 -0600

    Modifying path to libmesh pkgconfig.

--- tools/ci/travis-install-libmesh.sh
+++ tools/ci/travis-install-libmesh.sh
@@ -11,7 +11,7 @@ git clone https://github.com/libmesh/libmesh -b v1.6.0 --recurse-submodules
 mkdir build && cd build
 ../libmesh/configure --prefix=$HOME/LIBMESH --enable-exodus --disable-mpi
 make -j4 install
-export LIBMESH_PC=$HOME/LIBMESH
+export LIBMESH_PC=$HOME/LIBMESH/lib/pkgconfig/
 rm -rf $HOME/LIBMESH/build
 
 cd $CURRENT_DIR

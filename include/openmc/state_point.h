a8a837a23e6bca9dadeb7fbd9d8c9a6000e158af
itayhorin <itayhorin@gmail.com>
2021-01-12 12:05:52 -0600

    Surf source size function name change

--- include/openmc/state_point.h
+++ include/openmc/state_point.h
@@ -12,7 +12,7 @@
 namespace openmc {
 
 void load_state_point();
-std::vector<int64_t> query_surf_source_size();
+std::vector<int64_t> calculate_surf_source_size();
 void write_source_point(const char* filename, bool surf_source_bank = false);
 void write_source_bank(hid_t group_id, bool surf_source_bank);
 void read_source_bank(hid_t group_id, std::vector<Particle::Bank>& sites, bool distribute);

b662389da8bb5d9ba27430dabe10dec998bf4285
Patrick Shriwise <pshriwise@gmail.com>
2021-01-27 09:09:28 -0600

    Adding a serial build of libmesh to ci.

--- tools/ci/gha-install-libmesh.sh
+++ tools/ci/gha-install-libmesh.sh
@@ -10,7 +10,15 @@ mkdir LIBMESH && cd LIBMESH
 git clone https://github.com/libmesh/libmesh -b v1.6.0 --recurse-submodules
 mkdir build && cd build
 export METHODS="opt"
-../libmesh/configure --prefix=$HOME/LIBMESH CXX=mpicxx.mpich CC=mpicc.mpich FC=mpifort.mpich F77=mpif77.mpich --enable-exodus --disable-netcdf-4 --disable-eigen --disable-lapack
+
+
+if [[ $MPI == 'y']]; then
+../libmesh/configure --prefix=$HOME/LIBMESH CXX=mpicxx.mpich CC=mpicc.mpich FC=mpifort.mpich F77=mpif77.mpich \
+--enable-exodus --disable-netcdf-4 --disable-eigen --disable-lapack
+else
+../libmesh/configure --prefix=$HOME/LIBMESH CXX=mpicxx.mpich CC=mpicc.mpich FC=mpifort.mpich F77=mpif77.mpich \
+--enable-exodus --disable-netcdf-4 --disable-eigen --disable-lapack
+fi
 make -j4 install
 export LIBMESH_PC=$HOME/LIBMESH/lib/pkgconfig/
 rm -rf $HOME/LIBMESH/build

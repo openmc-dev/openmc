e6dd54bb9045736c032cea43af8b0e9c266b62c2
Gavin Ridley <gavin.keith.ridley@gmail.com>
2021-02-24 10:35:42 -0500

    slightly improve bulk pushback by reducing synthreads requirements

--- include/openmc/cuda/cross_surface.h
+++ include/openmc/cuda/cross_surface.h
@@ -43,36 +43,27 @@ __global__ void process_surface_crossing_events_device(EventQueueItem* queue,
 
   // Boilerplate for block-level scan and reduction
   typedef cub::BlockScan<unsigned, BLOCK_SIZE> BlockScanT;
-  __shared__ union TemporaryStorage {
-    typename BlockScanT::TempStorage scan;
-    unsigned start;
-  } shared;
+  __shared__ typename BlockScanT::TempStorage scan;
+  __shared__ unsigned start;
 
   // First put nonfuel indices back to their queue
-  __syncthreads();
-  BlockScanT(shared.scan)
-    .ExclusiveSum(goes_to_nonfuel_queue, goes_to_nonfuel_queue);
-  __syncthreads();
+  BlockScanT(scan).ExclusiveSum(goes_to_nonfuel_queue, goes_to_nonfuel_queue);
   constexpr unsigned max_thread_idx = BLOCK_SIZE - 1;
   if (threadIdx.x == max_thread_idx)
-    shared.start = atomicAdd(&managed_calculate_nonfuel_queue_index,
+    start = atomicAdd(&managed_calculate_nonfuel_queue_index,
       goes_to_nonfuel_queue + ((unsigned)nonfuel));
   __syncthreads();
   if (nonfuel)
-    calculate_nonfuel_xs_queue[shared.start + goes_to_nonfuel_queue] = {
-      *p, p_idx};
-  __syncthreads();
+    calculate_nonfuel_xs_queue[start + goes_to_nonfuel_queue] = {*p, p_idx};
 
   // Now put fuel indices back to their queue
-  BlockScanT(shared.scan).ExclusiveSum(goes_to_fuel_queue, goes_to_fuel_queue);
-  __syncthreads();
+  BlockScanT(scan).ExclusiveSum(goes_to_fuel_queue, goes_to_fuel_queue);
   if (threadIdx.x == max_thread_idx)
-    shared.start = atomicAdd(&managed_calculate_fuel_queue_index,
+    start = atomicAdd(&managed_calculate_fuel_queue_index,
       goes_to_fuel_queue + ((unsigned)fuel));
   __syncthreads();
   if (fuel)
-    calculate_fuel_xs_queue[shared.start + goes_to_fuel_queue] = {*p, p_idx};
-  __syncthreads();
+    calculate_fuel_xs_queue[start + goes_to_fuel_queue] = {*p, p_idx};
 }
 
 } // namespace gpu

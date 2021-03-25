87bc82ae1ee4f0be70a813326c030b9ec0c8b1d5
itayhorin <itayhorin@gmail.com>
2021-03-25 14:21:58 -0400

    addressed some of the requested changes from PR

--- src/tallies/filter_collision.cpp
+++ src/tallies/filter_collision.cpp
@@ -3,8 +3,6 @@
 #include <fmt/core.h>
 
 #include "openmc/capi.h"
-#include "openmc/constants.h" // For F90_NONE
-#include "openmc/mgxs_interface.h"
 #include "openmc/search.h"
 #include "openmc/settings.h"
 #include "openmc/xml_interface.h"
@@ -26,14 +24,12 @@ void CollisionFilter::set_bins(gsl::span<const int> bins)
   // Clear existing bins
   bins_.clear();
   bins_.reserve(bins.size());
+  map_.clear();
 
-  // Copy bins, ensuring they are valid
+  // Copy bins
   for (gsl::index i = 0; i < bins.size(); ++i) {
-    if (i > 0 && bins[i] <= bins[i - 1]) {
-      throw std::runtime_error {
-        "Number of Collisions bins must be monotonically increasing."};
-    }
     bins_.push_back(bins[i]);
+    map_[bins[i]] = i;
   }
 
   n_bins_ = bins_.size();
@@ -47,13 +43,10 @@ void CollisionFilter::get_all_bins(
 
   // Bin the collision number. Must fit exactly the desired collision number .
   if (n >= bins_.front() && n <= bins_.back()) {
-    auto it = find(bins_.begin(), bins_.end(), n);
-    if (it != bins_.end()) {
-      size_t bin = it - bins_.begin();
-      if (int(bins_[bin]) == n) {
-        match.bins_.push_back(bin);
-        match.weights_.push_back(1.0);
-      }
+    auto search = map_.find(n);
+    if (search != map_.end()){
+      match.bins_.push_back(search->second);
+      match.weights_.push_back(1.0);
     }
   }
 }
@@ -66,56 +59,7 @@ void CollisionFilter::to_statepoint(hid_t filter_group) const
 
 std::string CollisionFilter::text_label(int bin) const
 {
-  return fmt::format("Collision Number {}", int(bins_[bin]));
-}
-
-//==============================================================================
-// C-API functions
-//==============================================================================
-
-extern "C" int openmc_collision_filter_get_bins(
-  int32_t index, const int** energies, size_t* n)
-{
-  // Make sure this is a valid index to an allocated filter.
-  if (int err = verify_filter(index)) 
-    return err;
-
-  // Get a pointer to the filter and downcast.
-  const auto& filt_base = model::tally_filters[index].get();
-  auto* filt = dynamic_cast<CollisionFilter*>(filt_base);
-
-  // Check the filter type.
-  if (!filt) {
-    set_errmsg("Tried to get collision bins on a non-collision filter.");
-    return OPENMC_E_INVALID_TYPE;
-  }
-
-  // Output the bins.
-  *energies = filt->bins().data();
-  *n = filt->bins().size();
-  return 0;
-}
-
-extern "C" int openmc_collision_filter_set_bins(
-  int32_t index, size_t n, const int* energies)
-{
-  // Make sure this is a valid index to an allocated filter.
-  if (int err = verify_filter(index)) 
-    return err;
-
-  // Get a pointer to the filter and downcast.
-  const auto& filt_base = model::tally_filters[index].get();
-  auto* filt = dynamic_cast<CollisionFilter*>(filt_base);
-
-  // Check the filter type.
-  if (!filt) {
-    set_errmsg("Tried to set collision bins on a non-collision filter.");
-    return OPENMC_E_INVALID_TYPE;
-  }
-
-  // Update the filter.
-  filt->set_bins({energies, n});
-  return 0;
+  return fmt::format("Collision Number {}", bins_[bin]);
 }
 
 } // namespace openmc

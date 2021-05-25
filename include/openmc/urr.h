b7f31aa1d617b14f54a944dba84e685eecb54f15
John Tramm <john.tramm@gmail.com>
2021-05-25 15:25:06 +0000

    revert 1D offset in urr.

--- include/openmc/urr.h
+++ include/openmc/urr.h
@@ -33,9 +33,6 @@ public:
   explicit UrrData(hid_t group_id);
 
   void flatten_urr_data();
-  #pragma omp declare target
-  int get_offset(int i_energy, URRTableParam type) const;
-  #pragma omp end declare target
 };
 
 } // namespace openmc

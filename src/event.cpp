c094ba3ef7e6e0e4bce522e7840caa0e43f87e08
John Tramm <john.tramm@gmail.com>
2021-06-03 02:26:40 +0000

    offload of full calculate_xs. Required fiddling with OMP pragma to remove the teams -- may be a bug or just due to low particle count?

--- src/event.cpp
+++ src/event.cpp
@@ -119,28 +119,28 @@ void process_calculate_xs_events(SharedArray<EventQueueItem>& queue)
   int64_t offset = simulation::advance_particle_queue.size();;
   
   #pragma omp target update to(simulation::device_particles[:simulation::particles.size()])
-  //#pragma omp target update to(model::device_cells[:model::cells.size()])
 
+  queue.copy_host_to_device();
+
+  #ifdef USE_DEVICE
+  #pragma omp target distribute parallel for
+  #else
   #pragma omp parallel for schedule(runtime)
+  #endif
   for (int64_t i = 0; i < queue.size(); i++) {
     Particle& p = simulation::device_particles[queue[i].idx];
-    //#pragma omp target update to(p)
-    #pragma omp target
-    {
-      p.event_calculate_xs();
-    }
-    //#pragma omp target update from(p)
-
-    // After executing a calculate_xs event, particles will
-    // always require an advance event. Therefore, we don't need to use
-    // the protected enqueuing function.
-    simulation::advance_particle_queue[offset + i] = queue[i];
+    p.event_calculate_xs();
   }
   
   #pragma omp target update from(simulation::device_particles[:simulation::particles.size()])
-  //#pragma omp target update from(model::device_cells[:model::cells.size()])
-
+  
+  // After executing a calculate_xs event, particles will
+  // always require an advance event. Therefore, we don't need to use
+  // the protected enqueuing function.
   simulation::advance_particle_queue.resize(offset + queue.size());
+  for (int64_t i = 0; i < queue.size(); i++) {
+    simulation::advance_particle_queue[offset + i] = queue[i];
+  }
 
   queue.resize(0);
 

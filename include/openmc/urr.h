41c8cb4106047b7653cc93216c42c55aef1a7cc9
Gavin Ridley <gavin.keith.ridley@gmail.com>
2021-11-08 16:04:37 -0500

    improve URR lookup data layout and performance

--- include/openmc/urr.h
+++ include/openmc/urr.h
@@ -7,6 +7,7 @@
 
 #include "openmc/constants.h"
 #include "openmc/hdf5_interface.h"
+#include "openmc/vector.h"
 
 namespace openmc {
 
@@ -16,16 +17,46 @@ namespace openmc {
 
 class UrrData {
 public:
+  // Since we access all of these at once, we want
+  // them contiguous in memory.
+  struct XSSet {
+    double total;
+    double elastic;
+    double fission;
+    double n_gamma;
+    double heating;
+  };
+
   Interpolation interp_;          //!< interpolation type
   int inelastic_flag_;            //!< inelastic competition flag
   int absorption_flag_;           //!< other absorption flag
   bool multiply_smooth_;          //!< multiply by smooth cross section?
-  int n_energy_;                  //!< number of energy points
-  xt::xtensor<double, 1> energy_; //!< incident energies
-  xt::xtensor<double, 3> prob_;   //!< Actual probability tables
+
+  vector<double> energy_; //!< incident energies
+  auto n_energy() const { return energy_.size(); }
+
+  /* The row indexes correspond to the incident energy table, and column
+   * indices correspond to values of the CDF at that energy. For the CDF matrix
+   * below, obviously, values of the CDF are stored. For the xs_values
+   * variable, the columns line up with the index of cdf_values.
+   */
+  xt::xtensor<double, 2> cdf_values_; // Note: must be row major!
+  xt::xtensor<XSSet, 2> xs_values_;
+
+  // Number of points in the CDF
+  auto n_cdf() const { return cdf_values_.shape()[1]; }
 
   //! \brief Load the URR data from the provided HDF5 group
   explicit UrrData(hid_t group_id);
+
+  // Checks if any negative CDF or XS values are present
+  bool has_negative() const;
+
+  // Checks if the passed energy is within the bounds of the URR table
+  bool energy_in_bounds(double E) const
+  {
+    return energy_.front() < E && E < energy_.back();
+  }
 };
 
 } // namespace openmc
--- src/urr.cpp
+++ src/urr.cpp
@@ -1,5 +1,6 @@
 #include "openmc/urr.h"
 
+#include <algorithm> // any_of
 #include <iostream>
 
 namespace openmc {
@@ -21,11 +22,63 @@ UrrData::UrrData(hid_t group_id)
   // read the energies at which tables exist
   read_dataset(group_id, "energy", energy_);
 
-  // Set n_energy_
-  n_energy_ = energy_.shape()[0];
+  // Read URR tables. The HDF5 format is a little
+  // different from how we want it laid out in memory.
+  // This array used to be called "prob_".
+  xt::xtensor<double, 3> tmp_prob;
+  read_dataset(group_id, "table", tmp_prob);
+  auto shape = tmp_prob.shape();
 
-  // Read URR tables
-  read_dataset(group_id, "table", prob_);
+  // We separate out into two matrices (one with CDF values,
+  // the other with cross section sets) in order to improve
+  // contiguity of memory accesses.
+  const auto n_energy = shape[0];
+  const auto n_cdf_values = shape[2];
+  xt::xtensor<double, 2>::shape_type new_shape = {n_energy, n_cdf_values};
+  cdf_values_.resize(new_shape);
+  xs_values_.resize(new_shape);
+
+  // Now fill in the values. Using manual loops here since we might
+  // not have fancy xtensor slicing code written for GPU tensors.
+  // The below enum gives how URR tables are laid out in our HDF5 tables.
+  enum class URRTableParam {
+    CUM_PROB,
+    TOTAL,
+    ELASTIC,
+    FISSION,
+    N_GAMMA,
+    HEATING
+  };
+  for (int i_energy = 0; i_energy < n_energy; ++i_energy) {
+    for (int i_cdf = 0; i_cdf < n_cdf_values; ++i_cdf) {
+      cdf_values_(i_energy, i_cdf) =
+        tmp_prob(i_energy, URRTableParam::CUM_PROB, i_cdf);
+      xs_values_(i_energy, i_cdf).total =
+        tmp_prob(i_energy, URRTableParam::TOTAL, i_cdf);
+      xs_values_(i_energy, i_cdf).elastic =
+        tmp_prob(i_energy, URRTableParam::ELASTIC, i_cdf);
+      xs_values_(i_energy, i_cdf).fission =
+        tmp_prob(i_energy, URRTableParam::FISSION, i_cdf);
+      xs_values_(i_energy, i_cdf).n_gamma =
+        tmp_prob(i_energy, URRTableParam::N_GAMMA, i_cdf);
+      xs_values_(i_energy, i_cdf).heating =
+        tmp_prob(i_energy, URRTableParam::HEATING, i_cdf);
+    }
+  }
+}
+
+bool UrrData::has_negative() const
+{
+
+  // Lambda checks if any value in XSSset is negative
+  auto xs_set_negative = [](const XSSet& xs) {
+    return xs.total < 0.0 || xs.elastic < 0.0 || xs.fission < 0.0 ||
+           xs.n_gamma < 0.0 || xs.heating < 0.0;
+  };
+
+  return std::any_of(cdf_values_.begin(), cdf_values_.end(), [](double x) {
+    return x < 0.0;
+  }) || std::any_of(xs_values_.begin(), xs_values_.end(), xs_set_negative);
 }
 
 } // namespace openmc

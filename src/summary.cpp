e14de1600abd4ffb67f0a5390d826f1baefaa930
Jonathan Shimwell <drshimwell@gmail.com>
2022-03-10 21:08:46 +0000

    removing ,w from filename

--- src/summary.cpp
+++ src/summary.cpp
@@ -23,7 +23,7 @@ void write_summary()
   write_message("Writing summary.h5 file...", 5);
 
   // Set filename for summary file
-  filename = fmt::format("{}summary.h5", settings::path_output, w);
+  filename = fmt::format("{}summary.h5", settings::path_output);
 
   // Create a new file using default properties.
   hid_t file = file_open(filename, 'w');

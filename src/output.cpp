cfab5564ac22f8b93e1eb6a11fe743af9aa66b04
itayhorin <itayhorin@gmail.com>
2022-03-11 15:36:44 +0000

    Suggestions from review by @paulromano
    Co-authored-by: Paul Romano <paul.k.romano@gmail.com>

--- src/output.cpp
+++ src/output.cpp
@@ -577,8 +577,7 @@ void write_tallies()
     return;
 
   // Set filename for tallies_out
-  std::string filename;
-  filename = fmt::format("{}tallies.out", settings::path_output);
+  std::string filename = fmt::format("{}tallies.out", settings::path_output);
 
   // Open the tallies.out file.
   std::ofstream tallies_out;

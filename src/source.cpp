601d219c6de7031704557e498d3e755ae45daa87
Erik B Knudsen <erkn@fysik.dtu.dk>
2022-06-23 11:34:53 +0200

    include mcpl header

--- src/source.cpp
+++ src/source.cpp
@@ -31,6 +31,10 @@
 #include "openmc/state_point.h"
 #include "openmc/xml_interface.h"
 
+#ifdef HAS_MCPL
+#include <mcpl.h>
+#endif
+
 namespace openmc {
 
 //==============================================================================

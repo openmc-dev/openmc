362ea5bea435d0abdd6ed19b618a6bc350cc5484
myerspat <myerspat@umich.edu>
2022-07-06 08:38:35 -0500

    Testing different implementation for from_source

--- src/particle.cpp
+++ src/particle.cpp
@@ -94,6 +94,48 @@ void Particle::from_source(const SourceSite* src)
 
   // Check if the src has cell coordinates
   // and copy attributes or reset
+  if (src->coord.empty()) {
+    clear();
+    cell_born() = C_NONE;
+    material() = C_NONE;
+  } else {
+    // Get source coordinate bank size and low level cell
+    n_coord() = src->n_coord;
+    cell_born() = coord(n_coord() - 1).cell;
+
+    // Check lowest level cell to make sure it is a material
+    Cell& c {*model::cells[cell_born()]};
+    if (c.type_ == Fill::MATERIAL) {
+      coord() = src->coord;
+
+      // Find the distribcell instance number
+      cell_instance() = 0;
+      if (c.distribcell_index_ >= 0) {
+        cell_instance() = cell_instance_at_level(*this, n_coord() - 1);
+      }
+
+      // Set the material and temperature
+      material_last() = C_NONE;
+      if (c.material_.size() > 1) {
+        material() = c.material_[cell_instance()];
+      } else {
+        material() = c.material_[0];
+      }
+      sqrtkT_last() = C_NONE;
+      if (c.sqrtkT_.size() > 1) {
+        sqrtkT() = c.sqrtkT_[cell_instance()];
+      } else {
+        sqrtkT() = c.sqrtkT_[0];
+      }
+
+    } else {
+      clear();
+      cell_born() = C_NONE;
+      material() = C_NONE;
+    }
+  }
+
+  /*
   if (!src->coord.empty()) {
     n_coord() = src->n_coord;
     coord() = src->coord;
@@ -103,6 +145,7 @@ void Particle::from_source(const SourceSite* src)
     cell_born() = C_NONE;
     material() = C_NONE;
   }
+  */
 
   // Copy attributes from source bank site
   type() = src->particle;
@@ -361,6 +404,7 @@ void Particle::event_revive_from_secondary()
     secondary_bank().pop_back();
     n_event() = 0;
 
+    /*
     Cell& c {*model::cells[cell_born()]};
     // Find the distribcell instance number
     cell_instance() = 0;
@@ -375,7 +419,7 @@ void Particle::event_revive_from_secondary()
     } else {
       material() = c.material_[0];
     }
-    /*
+    
     sqrtkT_last() = sqrtkT();
     if (c.sqrtkT_.size() > 1) {
       sqrtkT() = c.sqrtkT_[cell_instance()];
@@ -383,6 +427,7 @@ void Particle::event_revive_from_secondary()
       sqrtkT() = c.sqrtkT_[0];
     }
     */
+    
 
     // Enter new particle in particle track file
     if (write_track())

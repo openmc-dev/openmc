35bac72e1809e93fbc176c253d000805e3ea6013
Paul Romano <paul.k.romano@gmail.com>
2022-07-18 22:44:10 -0500

    Define OperatorStack methods inline and remove .cpp, simplify implementation

--- include/openmc/operator_stack.h
+++ include/openmc/operator_stack.h
@@ -8,65 +8,66 @@
 namespace openmc {
 
 //==============================================================================
-// Constants
-//==============================================================================
-
-enum BitOperators : uint32_t {
-  BIT_OP_UNION = 0b01,
-  BIT_OP_INTERSECTION = 0b10,
-  BIT_OP_COMPLEMENT = 0b11
-};
-
-//==============================================================================
-//==============================================================================
 
 class OperatorStack {
 public:
-  //@{
-  //! Typedefs
+  //----------------------------------------------------------------------------
+  // Typedefs
   using value_type = int32_t;
   using size_type = uint32_t;
-  //}
+
+  //----------------------------------------------------------------------------
+  // Constructors, destructors
 
   //! Default constructor
   OperatorStack() {}
 
+  //----------------------------------------------------------------------------
+  // Accessors
+
   //! Greatest number of operators allowed on the stack, each operator takes two
   //! bits
-  static constexpr size_type max_stack_depth() { return sizeof(size_type) / 2; }
-
-  //// ACCESSORS ////
+  static constexpr size_type max_stack_depth() { return sizeof(size_type) * 4; }
 
   // Whether any elements exist
-  bool empty() const;
+  bool empty() const { return data_ == size_type(0); }
 
-  //// MUTATORS ////
+  //----------------------------------------------------------------------------
+  // Mutators
 
   // Push an operator to the stack
-  void push_back(value_type op);
+  void push_back(value_type op) { data_ = shl(data_) | twobit_operator(op); }
 
   // Pop a value off the stack
-  value_type pop();
+  value_type pop()
+  {
+    auto bit_op = top(data_);
+    data_ = shr(data_);
+    return signed_operator(bit_op);
+  }
 
 private:
-  //// DATA ////
+  //----------------------------------------------------------------------------
+  // Private methods
+  size_type twobit_operator(value_type op) const { return op - OP_UNION + 1; }
 
-  size_type data_ {0};
-
-  // Convert a operator to bit operator
-  size_type convert_to_bit_notation(value_type op) const;
-
-  // Convert a bit operator to operator
-  value_type convert_to_std_notation(size_type bit_op) const;
+  value_type signed_operator(size_type bit_op) const
+  {
+    return bit_op + OP_UNION - 1;
+  }
 
   //! Get the top two bits
-  static constexpr size_type top(size_type val) { return val & 0b11; }
+  static constexpr size_type top(size_type val) { return val & 0b11u; }
 
   //! Shift right by two
   static constexpr size_type shr(size_type val) { return val >> 2; }
 
   //! Shift left by two
   static constexpr size_type shl(size_type val) { return val << 2; }
+
+  //----------------------------------------------------------------------------
+  // Data
+  size_type data_ {0};
 };
 
 } // namespace openmc

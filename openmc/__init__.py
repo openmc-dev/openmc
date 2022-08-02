db22ed70f6bfa2fb9f47e5aa36f72c8e5df2b6e8
shimwell <mail@jshimwell.com>
2022-08-02 20:14:34 +0100

    update to use setup.cfg

--- openmc/__init__.py
+++ openmc/__init__.py
@@ -1,3 +1,18 @@
+try:
+    # this works for python 3.7 and lower
+    from importlib.metadata import version, PackageNotFoundError
+except (ModuleNotFoundError, ImportError):
+    # this works for python 3.8 and higher
+    from importlib_metadata import version, PackageNotFoundError
+try:
+    __version__ = version("openmc")
+except PackageNotFoundError:
+    from setuptools_scm import get_version
+
+    __version__ = get_version(root="..", relative_to=__file__)
+
+__all__ = ["__version__"]
+
 from openmc.arithmetic import *
 from openmc.cell import *
 from openmc.checkvalue import *

20e11f1dd9c95162666490e7677a57a1dfbb1fb3
Jonathan Shimwell <mail@jshimwell.com>
2022-08-05 12:20:38 +0100

    added material lib to setup package data

--- setup.py
+++ setup.py
@@ -33,7 +33,8 @@ kwargs = {
     'package_data': {
         'openmc.lib': ['libopenmc.{}'.format(suffix)],
         'openmc.data': ['mass16.txt', 'BREMX.DAT', 'half_life.json', '*.h5'],
-        'openmc.data.effective_dose': ['*.txt']
+        'openmc.data.effective_dose': ['*.txt'],
+        'openmc.data.material_libraries': ['*.json']
     },
 
     # Metadata

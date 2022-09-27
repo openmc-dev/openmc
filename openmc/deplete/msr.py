0797d501213b4ade5aaa8b1f66fcc578c54a1df6
church89 <l.chierici89@gmail.com>
2022-09-27 11:27:32 +0200

    add internal functions and add descriptions

--- openmc/deplete/msr.py
+++ openmc/deplete/msr.py
@@ -1,39 +1,65 @@
 from collections import OrderedDict
-
 from openmc import Materials, Material
 
 class MsrContinuous:
-    """Class defining a msr continuous object.
-
-    .. versionadded:: 0.13.2
+    """Class defining a msr continuous object for removal rates.
 
     Parameters
     ----------
     local_mats : openmc.Material
-        opoenmc.Material
+        openmc.Material
 
     Attributes
     ----------
-    local_mats : str or None
-        Name of nuclide.
-    index_burn : float or None
-        Half life of nuclide in [s].
-    ordr_burn : float
-        Energy deposited from decay in [eV].
-    removal_terms : int
-        Number of decay pathways.
+    local_mats : openmc.Material
+        All local material defined in the model.
+    index_burn : list of str
+        List of depletable material id
+    ordr_burn : OrderedDict of int and int
+        OrderedDict of depletable material id and enuemerated indeces
+    removal_terms : OrderedDict of str and OrderedDict
+        OrderedDict of depletable material id and OrderedDict to fill
     """
 
     def __init__(self, local_mats):
 
         if not isinstance(local_mats, Materials):
             raise ValueError(f'{local_mats} is not a valid openmc Material')
+        else:
+            self.local_mats = local_mats
 
-        self.index_burn = [mat.id for mat in local_mats if mat.depletable]
+        self.index_burn = self._get_index_burn()
         self.n_burn = len(self.index_burn)
-        self.ord_burn = OrderedDict((int(id), i) for i, id in enumerate(self.index_burn))
-        self.removal_terms = OrderedDict((id, OrderedDict()) for id in self.index_burn)
+        self.ord_burn = self._order_burn_mats()
+        self.removal_terms = self._initialize_removal_rates()
         self.index_msr = [(i, i) for i in range(self.n_burn)]
+
+    def _get_index_burn(self):
+        """Get burnable material id
+        Returns
+        ----------
+        list of str
+            List of depletable material id
+        """
+        return [mat.id for mat in self.local_mats if mat.depletable]
+
+    def _order_burn_mats(self):
+        """Order depletable material id
+        Returns
+        ----------
+        OrderedDict of int and int
+            OrderedDict of depletable material id and enuemrated indeces
+        """
+        return OrderedDict((int(id), i) for i, id in enumerate(self.index_burn))
+
+    def _initialize_removal_rates(self):
+        """Initialize removal rates container
+        Returns
+        ----------
+        OrderedDict of str and OrderedDict
+            OrderedDict of depletable material id and OrderedDict to fill
+        """
+        return OrderedDict((id, OrderedDict()) for id in self.index_burn)
 
     def _get_mat_index(self, mat):
         """Helper method for getting material index"""
@@ -42,6 +68,13 @@ class MsrContinuous:
         return self.index_burn[mat] if isinstance(mat, str) else mat
 
     def transfer_matrix_index(self):
+        """Get transfer matrices indeces
+        Returns
+        ----------
+        list of tuples :
+            List od tuples pairs to order the transfer matrices
+            when building the coupled matrix
+        """
         transfer_index = OrderedDict()
         for id, val in self.removal_terms.items():
             if val:
@@ -53,14 +86,48 @@ class MsrContinuous:
         return list(transfer_index.keys())
 
     def get_removal_rate(self, mat, element):
+        """Extract removal rates
+        Parameters
+        ----------
+        mat : Openmc,Material or int
+            Depletable material
+        element : str
+            Element to extract removal rate value
+        Returns:
+        ----------
+        float :
+            Removal rate value
+        """
         mat = self._get_mat_index(mat)
         return self.removal_terms[mat][element][0]
 
     def get_destination_mat(self, mat, element):
+        """Extract destination material
+        Parameters
+        ----------
+        mat : Openmc,Material or int
+            Depletable material
+        element : str
+            Element to extract removal rate value
+        Returns:
+        ----------
+        str :
+            Depletable material id to where elements get transferred
+        """
         mat = self._get_mat_index(mat)
         return self.removal_terms[mat][element][1]
 
     def get_elements(self, mat):
+        """Extract removing elements for a given material
+        Parameters
+        ----------
+        mat : Openmc,Material or int
+            Depletable material
+        Returns:
+        ----------
+        list :
+            list of elements
+        """
         mat = self._get_mat_index(mat)
         elements=[]
         for k, v in self.removal_terms.items():
@@ -69,7 +136,23 @@ class MsrContinuous:
                     elements.append(elm)
         return elements
 
-    def set_removal_rate(self, mat, elements, removal_rate, dest_mat=None, units='1/s'):
+    def set_removal_rate(self, mat, elements, removal_rate, dest_mat=None,
+                         units='1/s'):
+        """Set removal rates to depletable material
+        Parameters
+        ----------
+        mat : Openmc,Material or int
+            Depletable material to where add removal rates
+        elements : list[str]
+            List of strings of elements
+        removal_rate : float
+            Removal rate coefficient
+        dest_mat : Openmc,Material or int, Optional
+            Destination material if transfer or elements tracking is wanted
+        units: str, optional
+            Removal rates units (not set yet)
+            Default : '1/s'
+        """
         mat = self._get_mat_index(mat)
         if dest_mat is not None:
             dest_mat = self._get_mat_index(dest_mat)

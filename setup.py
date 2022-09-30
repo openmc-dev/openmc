7e305bb25093eb0e88e83a928cc16ab9219f24b8
Jonathan Shimwell <mail@jshimwell.com>
2022-09-30 13:39:16 +0100

    save

--- setup.py
+++ setup.py
@@ -1,11 +1,24 @@
 from skbuild import setup
 
 setup(
+    name="openmc",
+    version="0.14.4",
     include_package_data=True,
     description="OpenMC Monte Carlo Code",
     author='The OpenMC Development Team',
     license="MIT",
     packages=['openmc'],
     python_requires=">=3.8",
-    cmake_source_dir='src',
+    cmake_source_dir='.',
+    zip_safe=False,
+    install_requires=[
+        "matplotlib",
+        "numpy",
+        "scipy",
+        "ipython",
+        "matplotlib",
+        "uncertainties",
+        "lxml",
+        "pandas",
+        "h5py"],
 )

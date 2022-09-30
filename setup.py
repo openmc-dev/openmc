a26eb1904879b596817e08bca2ef180009d3924d
Jonathan Shimwell <mail@jshimwell.com>
2022-09-30 14:56:52 +0100

    added *.h.in files

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@ from skbuild import setup
 
 setup(
     name="openmc",
-    version="0.14.4",
+    version="0.14.0",
     include_package_data=True,
     description="OpenMC Monte Carlo Code",
     author='The OpenMC Development Team',
     license="MIT",
     packages=['openmc'],
     python_requires=">=3.8",
-    cmake_source_dir='.',
     zip_safe=False,
     install_requires=[
         "matplotlib",

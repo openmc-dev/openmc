45d702e33809030ccdd75856cf3e921a7ad0ca0e
Jonathan Shimwell <mail@jshimwell.com>
2022-10-03 12:34:56 +0100

    drop support for py3.6

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     version=version,
     packages=find_packages(exclude=['tests*']),
     include_package_data=True,
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     install_requires=[
         "matplotlib",
         "numpy",

90fe5cb79cd8dc8978db0754d34cd66e818e8f05
shimwell <mail@jshimwell.com>
2022-10-04 21:00:17 +0100

    added py3.9 and py3.10

--- setup.py
+++ setup.py
@@ -61,6 +61,7 @@ kwargs = {
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
 
     # Dependencies

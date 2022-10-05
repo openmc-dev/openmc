f61f514813a1fe476e13b8be78f368bebc721786
church89 <l.chierici89@gmail.com>
2022-10-05 15:02:31 +0200

    include abc into msr_batchwise parent class

--- openmc/deplete/msr.py
+++ openmc/deplete/msr.py
@@ -1,5 +1,7 @@
+from abc import ABC, abstractmethod
 from collections import OrderedDict
-from openmc import Materials, Material, search_for_keff
+from openmc import Materials, Material
+from openmc.search import search_for_keff
 from copy import deepcopy
 
 class MsrContinuous:
@@ -15,7 +17,7 @@ class MsrContinuous:
     ----------
     local_mats : openmc.Material
         All local material defined in the model.
-    index_burn : list of str
+    burn_mats : list of str
         List of depletable material id
     ordr_burn : OrderedDict of int and int
         OrderedDict of depletable material id and enuemerated indeces
@@ -30,7 +32,7 @@ class MsrContinuous:
         else:
             self.local_mats = local_mats
 
-        self.index_burn = [mat.id for mat in self.local_mats if mat.depletable]
+        self.burn_mats = [mat.id for mat in self.local_mats if mat.depletable]
         self.index_msr = [(i, i) for i in range(self.n_burn)]
 
         self.ord_burn = self._order_burn_mats()
@@ -38,13 +40,13 @@ class MsrContinuous:
 
     @property
     def n_burn(self):
-        return len(self.index_burn)
+        return len(self.burn_mats)
 
     def _get_mat_index(self, mat):
         """Helper method for getting material index"""
         if isinstance(mat, Material):
             mat = str(mat.id)
-        return self.index_burn[mat] if isinstance(mat, str) else mat
+        return self.burn_mats[mat] if isinstance(mat, str) else mat
 
     def _order_burn_mats(self):
         """Order depletable material id
@@ -53,7 +55,7 @@ class MsrContinuous:
         OrderedDict of int and int
             OrderedDict of depletable material id and enuemrated indeces
         """
-        return OrderedDict((int(id), i) for i, id in enumerate(self.index_burn))
+        return OrderedDict((int(id), i) for i, id in enumerate(self.burn_mats))
 
     def _initialize_removal_rates(self):
         """Initialize removal rates container
@@ -62,7 +64,7 @@ class MsrContinuous:
         OrderedDict of str and OrderedDict
             OrderedDict of depletable material id and OrderedDict to fill
         """
-        return OrderedDict((id, OrderedDict()) for id in self.index_burn)
+        return OrderedDict((id, OrderedDict()) for id in self.burn_mats)
 
     def transfer_matrix_index(self):
         """Get transfer matrices indeces
@@ -157,23 +159,20 @@ class MsrContinuous:
             self.removal_rates[mat][element] = [removal_rate, dest_mat]
 
 
-class MsrBatchwise:
+class MsrBatchwise(ABC):
     """
     """
-    def __init__(self, operator, model, range=None, keff_search_bracket_method='brentq',
-                 keff_search_tolerance=0.01, keff_search_target=1.0,
-                 atom_density_limit=None, refine_search=True, refuel=False,
-                 start_param=0.0):
+    def __init__(self, operator, model, range=None, bracketed_method='brentq',
+                 tol=0.01, target=1.0, atom_density_limit=None,
+                 refine_search=True, refuel=False, start_param=0.0):
 
-        #super().__init__()
         self.operator = operator
-        self.index_burn = operator.burnable_mats
+        self.burn_mats = operator.burnable_mats
 
         # Select nuclides with data that are also in the chain
-        self._burnable_nucs = [nuc.name for nuc in self.operator.chain.nuclides
-                               if nuc.name in self.operator.nuclides_with_data]
+        self.burn_nucs = [nuc.name for nuc in operator.chain.nuclides
+                               if nuc.name in operator.nuclides_with_data]
         self.model = model
-        self.settings = model.settings
         self.geometry = model.geometry
         self.materials = model.materials
 
@@ -182,227 +181,212 @@ class MsrBatchwise:
         else:
             self.range = range
 
-        self.keff_search_bracket_method = keff_search_bracket_method
-        self.keff_search_tolerance = keff_search_tolerance
-        self.keff_search_target = keff_search_target
+        self.bracketed_method = bracketed_method
+        self.tol = tol
+        self.target = target
         self.atom_density_limit = atom_density_limit
         self.refine_search = refine_search
         self.refuel = refuel
         self.start_param = start_param
 
-    def _order_burn_nuclides(self, x):
+    def _order_vector(self, x):
         """
+        Order concentrations vector into a list of nuclides dictionaries.
+        Parameters
+        ------------
         x : atoms vector
         """
-        self.burn_nucs = list()
-        for idx, burn_id in enumerate(self.index_burn):
+        ord_nucs = list()
+        for idx, burn_id in enumerate(self.burn_mats):
             nucs = OrderedDict()
             for id_nuc, nuc in enumerate(self.operator.number.burnable_nuclides):
                 nucs[nuc] = x[idx][id_nuc]
-            self.burn_nucs.append(nucs)
+            ord_nucs.append(nucs)
+        return ord_nucs
 
-    def _msr_criticality_search(self, parametric_model):
+    @abstractmethod
+    def msr_criticality_search(self, parametric_model):
+        pass
+
+    @abstractmethod
+    def _build_parametric_model(self, param):
+        pass
+
+    def finalize(self):
         """
         """
-        while res == None:
-            search = openmc.search_for_keff(parametric_model,
-                        bracket=[self.geom+lower_range,self.geom+upper_range],
-                        tol=self.keff_search_tolerance,
-                        bracketed_method= self.keff_search_bracket_method,
-                        target=self.keff_search_target,
-                        print_iterations=True,
-                        check_brackets=False)
-
-            if len(search) == 3:
-                res, _, _ = search
-            elif len(search) == 2:
-                print ("Invalid range")
-                guesses, k = search
-
-                if np.array(k).prod() < self.keff_search_target:
-
-                    if (self.keff_search_target - np.array(k).max()) < 0.002:
-                        index = [idx for idx,i in enumerate(k)
-                                 if i == np.array(k).max()][0]
-                        res =  guesses[index]
-                    else:
-                        lower_range = upper_range
-                        upper_range *= 5
-                else:
-                    if (np.array(k).min() -self.keff_search_target)  < 0.002:
-                        index = [idx for idx,i in enumerate(k)
-                                 if i == np.array(k).min()][0]
-                        res = guesses[index]
-                    else:
-                        upper_range = lower_range
-                        lower_range /= 5
-            else:
-                raise Exception(f'search_for_keff output not contemplated')
-        return res
-
-    def _get_parametric_geom_coeff(self, geom_id):
-        for surf in self.geometry.get_all_surfaces().items():
-            if surf[1].id == geom_id:
-                keys = list(surf[1].coefficients.keys())
-                if len(keys) == 1:
-                    self.coeff = getattr(surf[1], keys[0])
-                else:
-                    msg=(f'Surface coefficients {keys} are not one')
-                    raise Exception(msg)
-
-    def _update_geom(self, surf_id, res):
-        """
-        """
-        for surf in self.geometry.get_all_surfaces().items():
-            if surf[1].id == surf_id:
-                keys = list(surf[1].coefficients.keys())
-                if len(keys) == 1:
-                    setattr(surf[1], keys[0], res)
-                else:
-                    msg = (f'Surface coefficients {keys} are not one')
-                    raise Exception(msg)
+        pass
 
 class MsrBatchwiseGeom(MsrBatchwise):
     """
     """
-    def __init__(self, op, model, geom_id, type='surface', range=None,
-                    keff_search_bracket_method='brentq', keff_search_tolerance=0.01,
-                    keff_search_target=1.0, atom_density_limit=None,
-                    refine_search=True, refuel=False, start_param=0.0):
+    def __init__(self, operator, model, geom_id, type='surface', range=None,
+                    bracketed_method='brentq', tol=0.01, target=1.0,
+                    atom_density_limit=None, refine_search=True, refuel=False,
+                    start_param=0.0):
 
-        super().__init__(op, model, range, keff_search_bracket_method,
-                         keff_search_tolerance, keff_search_target, atom_density_limit,
-                         refine_search, refuel, start_param)
+        super().__init__(operator, model, range, bracketed_method, tol, target,
+                         atom_density_limit, refine_search, refuel, start_param)
+
         self.geom_id = geom_id
         if type == 'surface':
-            self._get_parametric_geom_coeff(self.geom_id)
+            self.coeff = self._extract_geom_coeff(self.geom_id)
+
+    def _extract_geom_coeff(self):
+        for surf in self.geometry.get_all_surfaces().items():
+            if surf[1].id == self.geom_id:
+                keys = list(surf[1].coefficients.keys())
+                if len(keys) == 1:
+                    coeff = getattr(surf[1], keys[0])
+                else:
+                    msg=(f'Surface coefficients {keys} are more than one')
+                    raise Exception(msg)
+        return coeff
+
+    def _set_geom_coeff(self, res):
+        """
+        """
+        for surf in self.geometry.get_all_surfaces().items():
+            if surf[1].id == self.geom_id:
+                keys = list(surf[1].coefficients.keys())
+                if len(keys) == 1:
+                    setattr(surf[1], keys[0], res)
+                else:
+                    msg = (f'Surface coefficients {keys} are more than one')
+                    raise Exception(msg)
 
     def _normalize_nuclides(self, x):
         """
+        Remove
         """
-        materials = deepcopy(self.model.materials)
-        self._order_burn_nuclides(x)
+        materials = deepcopy(self.materials)
+        nucs = super()._order_vector(x)
 
-        for idx, burn_id in enumerate(self.index_burn):
+        for idx, burn_mat in enumerate(self.burn_mats):
             atoms_gram_per_mol = 0
-            for nuc,val in self.burn_nucs[idx].items():
-                if val < density_limit or nuc not in self._burnable_nucs:
-                    materials[int(burn_id)-1].remove_nuclide(nuc)
+            for nuc, val in nucs[idx].items():
+                if val < self.atom_density_limit or nuc not in self.burn_nucs:
+                    materials[int(burn_mat)-1].remove_nuclide(nuc)
                 else:
-                    materials[int(burn_id)-1].remove_nuclide(nuc)
-                    materials[int(burn_id)-1].add_nuclide(nuc,val,'ao')
-                    atoms_gram_per_mol += val*openmc.data.atomic_mass(nuc)
+                    materials[int(burn_mat)-1].remove_nuclide(nuc)
+                    materials[int(burn_mat)-1].add_nuclide(nuc,val, 'ao')
+                    atoms_gram_per_mol += val * openmc.data.atomic_mass(nuc)
 
             #ensure constant density is set and assign new volume
-            density = materials[int(burn_id)-1].get_mass_density()
-            materials[int(burn_id)-1].set_density('g/cm3',density)
-            self.number.volume[idx] = atoms_gram_per_mol/openmc.data.AVOGADRO/density
+            density = materials[int(burn_mat)-1].get_mass_density()
+            materials[int(burn_mat)-1].set_density('g/cm3', density)
+            self.number.volume[idx] = atoms_gram_per_mol /\
+                                      openmc.data.AVOGADRO / density
         self.materials = materials
 
     def _build_parametric_model(self, param):
         """
         """
         model = deepcopy(self.model)
-        for surf in model.geometry.get_all_surfaces().items():
-            if surf[1].id == self.geom_id:
-                keys = list(surf[1].coefficients.keys())
-                if len(keys) == 1:
-                    setattr(surf[1],keys[0], self.coeff)
-                else:
-                    raise Exception(f'Surface coefficients {keys} are not one')
+        self._set_geom_coeff(param)
         model.export_to_xml()
         return model
 
-    def _update_geom_in_memory(self):
+    def _finalize(self):
         openmc.lib.init_geom()
 
-    def perform_bw_search_for_keff(self, x):
+    def msr_criticality_search(self, x):
         """
         """
         self._normalize_nuclides(x)
 
-        res = None
         lower_range = self.range[0]
         upper_range = self.range[1]
-        if -1.0 < self.geom < 1.0:
-            self.keff_search_tolerance /= 2
-        else:
-            self.keff_search_tolerance /= abs(self.geom)
 
+        if -1.0 < self.coeff < 1.0:
+            self.tol /= 2
+        else:
+            self.tol /= abs(self.coeff)
+
+        res = None
         while res == None:
-            if self.refine_search and self.geom >= abs(self.range[2])/2:
+
+            if self.refine_search and self.coeff >= abs(self.range[2])/2:
                 check_brackets = True
             else:
                 check_brackets = False
 
             search = search_for_keff(self._build_parametric_model,
-                        bracket=[self.geom + lower_range,self.geom+upper_range],
-                        tol=self.keff_search_tolerance,
-                        bracketed_method= self.keff_search_bracket_method,
-                        target=self.keff_search_target, print_iterations=True,
-                        check_brackets = check_brackets)
+                    bracket=[self.coeff + lower_range, self.coeff + upper_range],
+                    tol=self.tol, bracketed_method= self.bracketed_method,
+                    target=self.target, print_iterations=True) 
+                    #check_brackets=check_brackets)
 
             if len(search) == 3:
-                _res, guesses, k = search
+                _res, _, _ = search
                 # Further check, in case upper limit gets hit
                 if _res <= self.range[2]:
                     res = _res
                 else:
                     if self.refuel:
-                        print(f'Upper limit reached, level brought down to {init_param}cm and \
-                        start refueling...')
+                        msg = 'INFO: Hit upper limit {} cm. Update geom coeff \
+                                to {} and start refuel'.format(self.range[2],
+                                                               self.init_param)
+                        print(msg)
                         res = self.start_param
                         break
                     else:
-                        raise Exception(f'Upper limit reached, stopping depletion')
+                        msg = 'STOP: Hit upper limit and no further criteria \
+                                defined'
+                        raise Exception(msg)
 
             elif len(search) == 2:
                 guesses, k = search
 
                 if guesses[-1] > self.range[2]:
                     if self.refuel:
-                        print(f'Upper limit reached, level brought down to {init_param}cm and \
-                        start refueling...')
+                        msg = 'INFO: Hit upper limit {} cm. Update geom coeff \
+                                to {} and start refuel'.format(self.range[2],
+                                                               self.init_param)
+                        print(msg)
                         res = self.start_param
                         break
                     else:
-                        raise Exception(f'Upper limit reached, stopping depletion')
+                        msg = 'STOP: Hit upper limit and no further criteria \
+                                defined'
+                        raise Exception(msg)
 
-                if np.array(k).prod() < self.keff_search_target:
-                    print ("Invalid range, below target")
-                    if (self.keff_search_target - np.array(k).prod()) <= 0.02:
+                if np.array(k).prod() < self.target:
+                    print ('INFO: Function returned values BELOW target, \
+                                  adapting bracket range...')
+                    if (self.target - np.array(k).prod()) <= 0.02:
                         lower_range = upper_range - 3
-                    elif 0.02 < (self.keff_search_target - np.array(k).prod()) < 0.03:
+                    elif 0.02 < (self.target - np.array(k).prod()) < 0.03:
                         lower_range = upper_range - 1
                     else:
                         lower_range = upper_range - 0.5
                     upper_range += abs(self.range[1])/2
 
                 else:
-                    print ("Invalid range, above target")
+                    print ('INFO: Function returned values ABOVE target, \
+                                  adapting bracket range...')
                     upper_range = lower_range + 2
                     lower_range -= abs(self.range[0])
 
             else:
-                raise Exception(f'search_for_keff output not contemplated')
+                raise ValueError(f'ERROR: search_for_keff output not valid')
 
         #probably not needed
-        self._update_geom(self.geom_id, res)
-        self._update_geom_in_memory()
-        print(f'res: {res}, guess: {guess}')
-        diff = res - guess
+        self._set_geom_coeff(res)
+        self._finalize()
+        print(f'UPDATE: old coeff: {self.coeff}cm --> new coeff: {res}cm')
+        diff = res - self.coeff
         return res, diff
 
 class MsrBatchwiseMat(MsrBatchwise):
     """
     """
     def __init__(self, op, model, mat_id, refuel_vector, range=None,
-                 keff_search_bracket_method='brentq',
-                 keff_search_tolerance=0.01, keff_search_target=1.0,
+                 bracketed_method='brentq', tol=0.01, target=1.0,
                  atom_density_limit=None):
 
-        super().__init__(op, model, range, keff_search_bracket_method,
-                         keff_search_tolerance, keff_search_target, atom_density_limit)
+        super().__init__(op, model, range, bracketed_method, tol, target,
+                         atom_density_limit)
         if str(mat_id) not in self.burnable_mats:
                 raise ValueError(f'Mat_id: {mat_id} is not a valid depletable material id')
         else:

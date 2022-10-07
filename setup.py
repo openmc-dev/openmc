05b388fae0065ac2db84e0f0c9567dd760d4d42c
shimwell <mail@jshimwell.com>
2022-10-07 23:11:59 +0100

    using package dir

--- setup.py
+++ setup.py
@@ -24,16 +24,59 @@ with open('src/openmc/__init__.py', 'r') as f:
     version = f.readlines()[-1].split()[-1].strip("'")
 
 kwargs = {
+    'name': 'openmc',
     'version': version,
     'packages': find_packages(exclude=['tests*']),
+    'package_dir': {'openmc':'src'},
     'scripts': glob.glob('scripts/openmc-*'),
 
     # Data files and libraries
     'package_data': {
-        'openmc.lib': ['libopenmc.{suffix}'],
-        'openmc.data': ['*.{suffix}', 'mass16.txt', 'BREMX.DAT', 'half_life.json', '*.h5'],
+        'openmc.lib': ['libopenmc.{}'.format(suffix)],
+        'openmc.data': ['mass16.txt', 'BREMX.DAT', 'half_life.json', '*.h5'],
         'openmc.data.effective_dose': ['*.txt']
-    }
+    },
+
+    # Metadata
+    'author': 'The OpenMC Development Team',
+    'author_email': 'openmc@anl.gov',
+    'description': 'OpenMC',
+    'url': 'https://openmc.org',
+    'download_url': 'https://github.com/openmc-dev/openmc/releases',
+    'project_urls': {
+        'Issue Tracker': 'https://github.com/openmc-dev/openmc/issues',
+        'Documentation': 'https://docs.openmc.org',
+        'Source Code': 'https://github.com/openmc-dev/openmc',
+    },
+    'classifiers': [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Topic :: Scientific/Engineering'
+        'Programming Language :: C++',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+
+    # Dependencies
+    'python_requires': '>=3.7',
+    'install_requires': [
+        'numpy>=1.9', 'h5py', 'scipy', 'ipython', 'matplotlib',
+        'pandas', 'lxml', 'uncertainties'
+    ],
+    'extras_require': {
+        'depletion-mpi': ['mpi4py'],
+        'docs': ['sphinx', 'sphinxcontrib-katex', 'sphinx-numfig', 'jupyter',
+                 'sphinxcontrib-svg2pdfconverter', 'sphinx-rtd-theme'],
+        'test': ['pytest', 'pytest-cov', 'colorama'],
+        'vtk': ['vtk'],
+    },
 }
 
 # If Cython is present, add resonance reconstruction and fast float_endf

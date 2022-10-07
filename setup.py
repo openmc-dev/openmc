796c238f67a4d6a2a2bdb41467cc90531ab59d7f
Jonathan Shimwell <mail@jshimwell.com>
2022-10-07 16:09:58 +0100

    inital package layout

--- setup.py
+++ setup.py
@@ -4,12 +4,10 @@ import glob
 import sys
 import numpy as np
 
-from setuptools import setup, find_packages
-try:
-    from Cython.Build import cythonize
-    have_cython = True
-except ImportError:
-    have_cython = False
+from setuptools import find_packages
+
+from skbuild import setup
+from Cython.Build import cythonize
 
 
 # Determine shared library suffix
@@ -18,8 +16,8 @@ if sys.platform == 'darwin':
 else:
     suffix = 'so'
 
-# Get version information from __init__.py. This is ugly, but more reliable than
-# using an import.
+# Get version information from __init__.py. This is ugly, but more reliable
+# than using an import.
 with open('openmc/__init__.py', 'r') as f:
     version = f.readlines()[-1].split()[-1].strip("'")
 
@@ -32,57 +30,17 @@ kwargs = {
     # Data files and libraries
     'package_data': {
         'openmc.lib': ['libopenmc.{}'.format(suffix)],
-        'openmc.data': ['mass16.txt', 'BREMX.DAT', 'half_life.json', '*.h5'],
+        'openmc.data': ['*.pyx', '*.c', 'mass16.txt', 'BREMX.DAT', 'half_life.json', '*.h5'],
         'openmc.data.effective_dose': ['*.txt']
     },
-
-    # Metadata
-    'author': 'The OpenMC Development Team',
-    'author_email': 'openmc@anl.gov',
-    'description': 'OpenMC',
-    'url': 'https://openmc.org',
-    'download_url': 'https://github.com/openmc-dev/openmc/releases',
-    'project_urls': {
-        'Issue Tracker': 'https://github.com/openmc-dev/openmc/issues',
-        'Documentation': 'https://docs.openmc.org',
-        'Source Code': 'https://github.com/openmc-dev/openmc',
-    },
-    'classifiers': [
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Intended Audience :: End Users/Desktop',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Topic :: Scientific/Engineering'
-        'Programming Language :: C++',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-    ],
-
-    # Dependencies
     'python_requires': '>=3.7',
+    'include_package_data': True,
     'install_requires': [
         'numpy>=1.9', 'h5py', 'scipy', 'ipython', 'matplotlib',
-        'pandas', 'lxml', 'uncertainties'
+        'pandas', 'lxml', 'uncertainties', 'cython'
     ],
-    'extras_require': {
-        'depletion-mpi': ['mpi4py'],
-        'docs': ['sphinx', 'sphinxcontrib-katex', 'sphinx-numfig', 'jupyter',
-                 'sphinxcontrib-svg2pdfconverter', 'sphinx-rtd-theme'],
-        'test': ['pytest', 'pytest-cov', 'colorama'],
-        'vtk': ['vtk'],
-    },
+    'ext_modules': cythonize('openmc/data/*.pyx'),
+    'include_dirs': [np.get_include()]
 }
-
-# If Cython is present, add resonance reconstruction and fast float_endf
-if have_cython:
-    kwargs.update({
-        'ext_modules': cythonize('openmc/data/*.pyx'),
-        'include_dirs': [np.get_include()]
-    })
 
 setup(**kwargs)

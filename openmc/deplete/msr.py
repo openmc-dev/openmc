90ae929c0e1d9dd7644dffdf5d6d322f5b14b112
church89 <l.chierici89@gmail.com>
2022-10-10 12:15:33 +0200

    fix geometry re-initialization after geom critical control step

--- openmc/deplete/msr.py
+++ openmc/deplete/msr.py
@@ -181,10 +181,7 @@ class MsrBatchwise(ABC):
         self.geometry = model.geometry
         self.materials = model.materials
 
-        if len(range) != 3:
-            raise ValueError(f'{range} lenght must be 3')
-        else:
-            self.range = range
+        self.range = range
 
         self.bracketed_method = bracketed_method
         self.tol = tol
@@ -217,7 +214,7 @@ class MsrBatchwise(ABC):
     def _build_parametric_model(self, param):
         pass
 
-    def finalize(self):
+    def finalize(self, var):
         """
         """
         pass
@@ -227,7 +224,7 @@ class MsrBatchwiseGeom(MsrBatchwise):
     """
     def __init__(self, operator, model, geom_id, type='surface', range=None,
                     bracketed_method='brentq', tol=0.01, target=1.0,
-                    atom_density_limit=0.0, refine_search=True, refuel=False,
+                    atom_density_limit=0.0, refine_search=True, refuel=True,
                     start_param=0.0):
 
         super().__init__(operator, model, range, bracketed_method, tol, target,
@@ -248,14 +245,14 @@ class MsrBatchwiseGeom(MsrBatchwise):
                     raise Exception(msg)
         return coeff
 
-    def _set_geom_coeff(self, res):
+    def _set_geom_coeff(self, var, geometry):
         """
         """
-        for surf in self.model.geometry.get_all_surfaces().items():
+        for surf in geometry.get_all_surfaces().items():
             if surf[1].id == self.geom_id:
                 keys = list(surf[1].coefficients.keys())
                 if len(keys) == 1:
-                    setattr(surf[1], keys[0], res)
+                    setattr(surf[1], keys[0], var)
                 else:
                     msg = (f'Surface coefficients {keys} are more than one')
                     raise Exception(msg)
@@ -264,35 +261,41 @@ class MsrBatchwiseGeom(MsrBatchwise):
         """
         Remove
         """
-        #materials = deepcopy(self.materials)
+        _materials = deepcopy(self.model.materials)
         nucs = super()._order_vector(x)
 
         for idx, burn_mat in enumerate(self.burn_mats):
             atoms_gram_per_mol = 0
             for nuc, val in nucs[idx].items():
                 if val < self.atom_density_limit or nuc not in self.burn_nucs:
-                    self.model.materials[int(burn_mat)-1].remove_nuclide(nuc)
+                    _materials[int(burn_mat)-1].remove_nuclide(nuc)
                 else:
-                    self.model.materials[int(burn_mat)-1].remove_nuclide(nuc)
-                    self.model.materials[int(burn_mat)-1].add_nuclide(nuc, val)
+                    _materials[int(burn_mat)-1].remove_nuclide(nuc)
+                    _materials[int(burn_mat)-1].add_nuclide(nuc, val)
                     atoms_gram_per_mol += val * atomic_mass(nuc)
 
             #Calculate new volume to keep density constant
-            density = self.model.materials[int(burn_mat)-1].get_mass_density()
-            self.model.materials[int(burn_mat)-1].set_density('g/cm3', density)
+            density = _materials[int(burn_mat)-1].get_mass_density()
+            _materials[int(burn_mat)-1].set_density('g/cm3', density)
             self.operator.number.volume[idx] = atoms_gram_per_mol /\
                                                 AVOGADRO / density
-        #self.materials = materials
+        _materials.export_to_xml()
 
     def _build_parametric_model(self, param):
         """
         """
-        #model = deepcopy(self.model)
-        self._set_geom_coeff(param)
-        self.model.export_to_xml()
-        return self.model
+        _model = deepcopy(self.model)
+        self._set_geom_coeff(param, _model.geometry)
+        _model.geometry.export_to_xml()
+        return _model
 
-    def _finalize(self):
+    def _finalize(self, var):
+        """
+        The geometry needs to be exported so that inig_geom can be
+        initialized based on the updated coefficient.
+        """
+        self._set_geom_coeff(var, self.geometry)
+        self.geometry.export_to_xml()
         init_geom()
 
     def msr_criticality_search(self, x):
@@ -301,8 +304,8 @@ class MsrBatchwiseGeom(MsrBatchwise):
         self._normalize_nuclides(x)
         coeff = self._extract_geom_coeff()
 
-        lower_range = self.range[0]
-        upper_range = self.range[1]
+        low_range = self.range[0]
+        up_range = self.range[1]
 
         if -1.0 < coeff < 1.0:
             _tol = self.tol / 2
@@ -318,11 +321,10 @@ class MsrBatchwiseGeom(MsrBatchwise):
                 check_brackets = False
 
             search = search_for_keff(self._build_parametric_model,
-                    bracket=[coeff + lower_range, coeff + upper_range],
+                    bracket=[coeff + low_range, coeff + up_range],
                     tol=_tol, bracketed_method= self.bracketed_method,
                     target=self.target, print_iterations=True,
-                    run_args={'output':False})
-                    #check_brackets=check_brackets)
+                    run_args={'output':False}, check_brackets=check_brackets)
 
             if len(search) == 3:
                 _res, _, _ = search
@@ -333,7 +335,7 @@ class MsrBatchwiseGeom(MsrBatchwise):
                     if self.refuel:
                         msg = 'INFO: Hit upper limit {:.2f} cm. Update geom' \
                         'coeff to {:.2f} and start refuel'.format(self.range[2],
-                                                               self.init_param)
+                                                               self.start_param)
                         print(msg)
                         res = self.start_param
                         break
@@ -349,7 +351,7 @@ class MsrBatchwiseGeom(MsrBatchwise):
                     if self.refuel:
                         msg = 'INFO: Hit upper limit {:.2f} cm. Update geom' \
                         'coeff to {:.2f} and start refuel'.format(self.range[2],
-                                                               self.init_param)
+                                                               self.start_param)
                         print(msg)
                         res = self.start_param
                         break
@@ -362,25 +364,23 @@ class MsrBatchwiseGeom(MsrBatchwise):
                     print ('INFO: Function returned values BELOW target, ' \
                            'adapting bracket range...')
                     if (self.target - np.array(k).prod()) <= 0.02:
-                        lower_range = upper_range - 3
+                        low_range = up_range - 3
                     elif 0.02 < (self.target - np.array(k).prod()) < 0.03:
-                        lower_range = upper_range - 1
+                        low_range = up_range - 1
                     else:
-                        lower_range = upper_range - 0.5
-                    upper_range += abs(self.range[1])/2
+                        low_range = up_range - 0.5
+                    up_range += abs(self.range[1])/2
 
                 else:
                     print ('INFO: Function returned values ABOVE target, ' \
                            'adapting bracket range...')
-                    upper_range = lower_range + 2
-                    lower_range -= abs(self.range[0])
+                    up_range = low_range + 2
+                    low_range -= abs(self.range[0])
 
             else:
                 raise ValueError(f'ERROR: search_for_keff output not valid')
 
-        #probably not needed
-        self._set_geom_coeff(res)
-        self._finalize()
+        self._finalize(res)
         msg = 'UPDATE: old coeff: {:.2f} cm --> ' \
               'new coeff: {:.2f} cm'.format(coeff, res)
         print(msg)
@@ -390,67 +390,120 @@ class MsrBatchwiseGeom(MsrBatchwise):
 class MsrBatchwiseMat(MsrBatchwise):
     """
     """
-    def __init__(self, op, model, mat_id, refuel_vector, range=None,
+    def __init__(self, operator, model, mat_id, refuel_vector, range=None,
                  bracketed_method='brentq', tol=0.01, target=1.0,
                  atom_density_limit=None):
 
-        super().__init__(op, model, range, bracketed_method, tol, target,
+        super().__init__(operator, model, range, bracketed_method, tol, target,
                          atom_density_limit)
-        if str(mat_id) not in self.burnable_mats:
-                raise ValueError(f'Mat_id: {mat_id} is not a valid depletable material id')
+
+        if str(mat_id) not in self.burn_mats:
+                msg = 'Mat_id: {} is not a valid depletable material id'\
+                      .format(mat_id)
+                raise ValueError(msg)
         else:
             self.mat_id = mat_id
+
         self.refuel_vector = refuel_vector
 
-    def initialize_geometry(self, geom_id=None, res=None):
-        if res is not None:
-            self._set_geom_coeff(geom_id, res)
 
     def _build_parametric_model(self, param):
         """
+        param: mass of refuel vector
         """
-        _materials = deepcopy(self.materials)
-        nucs = super()._order_vector(x)
+        _model = deepcopy(self.model)
+
         for idx, burn_id in enumerate(self.burn_mats):
-            for nuc, val in nucs[idx].items():
+            for nuc, val in self.nucs[idx].items():
                 if nuc not in self.refuel_vector.keys():
                     if val < self.atom_density_limit or nuc not in self.burn_nucs:
-                        _materials[int(burn_id)-1].remove_nuclide(nuc)
+                        _model.materials[int(burn_id)-1].remove_nuclide(nuc)
                     else:
-                        _materials[int(burn_id)-1].remove_nuclide(nuc)
-                        _materials[int(burn_id)-1].add_nuclide(nuc,val)
+                        _model.materials[int(burn_id)-1].remove_nuclide(nuc)
+                        _model.materials[int(burn_id)-1].add_nuclide(nuc,val)
                 else:
-                    if _materials[int(burn_id)-1].id == int(self.mat_id):
-                        _materials[int(burn_id)-1].remove_nuclide(nuc)
+                    if _model.materials[int(burn_id)-1].id == int(self.mat_id):
+                        _model.materials[int(burn_id)-1].remove_nuclide(nuc)
                         # convert grams into atoms
                         atoms = param / atomic_mass(nuc) * AVOGADRO * \
                                 self.refuel_vector[nuc]
-                        _materials[int(burn_id)-1].add_nuclide(nuc, val+atoms)
+                        _model.materials[int(burn_id)-1].add_nuclide(nuc, val+atoms)
                     else:
-                        _materials[int(burn_id)-1].remove_nuclide(nuc)
-                        _materials[int(burn_id)-1].add_nuclide(nuc, val)
+                        _model.materials[int(burn_id)-1].remove_nuclide(nuc)
+                        _model.materials[int(burn_id)-1].add_nuclide(nuc, val)
             # ensure density is set
-            density = _materials[int(burn_id)-1].get_mass_density()
-            _materials[int(burn_id)-1].set_density('g/cm3',density)
-        _materials.export_to_xml()
-        return self.model
+            density = _model.materials[int(burn_id)-1].get_mass_density()
+            _model.materials[int(burn_id)-1].set_density('g/cm3',density)
+
+        _model.export_to_xml()
+        return _model
 
     def _update_x_vector_and_volumes(self, x, res):
+        """
+        """
         diff = dict()
-        for idx, burn_id in enumerate(self.burnable_mats):
+
+        for idx, burn_id in enumerate(self.burn_mats):
             #to calculate the new volume we need total atoms-gram per mol
             atoms_gram_per_mol = 0
-            for id_nuc, nuc in enumerate(self.number.burnable_nuclides):
+            for id_nuc, nuc in enumerate(self.operator.number.burnable_nuclides):
                 if nuc in self.refuel_vector.keys() and int(burn_id) == int(self.mat_id):
                     # Convert res grams into atoms
-                    res_atoms = res / atomic_mass(nuc) * AVOGADRO * self.refuel_vector[nuc]
+                    res_atoms = res / atomic_mass(nuc) * AVOGADRO *\
+                                self.refuel_vector[nuc]
                     diff[nuc] = x[idx][id_nuc] - res_atoms
                     x[idx][id_nuc] += res_atoms
-                atoms_gram_per_mol += x[idx][id_nuc]*atomic_mass(nuc)
+                atoms_gram_per_mol += x[idx][id_nuc] * atomic_mass(nuc)
+
             # Calculate new volume and assign it in memory
-            vol = atoms_gram_per_mol/AVOGADRO/self.materials[int(self.burnable_mats[idx])-1].get_mass_density()
-            self.number.volume[idx] = vol
+            vol = atoms_gram_per_mol / AVOGADRO /\
+                self.model.materials[int(self.burn_mats[idx])-1].get_mass_density()
+            self.operator.number.volume[idx] = vol
         return x, diff
 
     def msr_criticality_search(self, x):
-        pass
+        """
+        """
+        self.nucs = super()._order_vector(x)
+
+        low_range = self.range[0]
+        up_range = self.range[1]
+        res = None
+        while res == None:
+            search = search_for_keff(self._build_parametric_model,
+                    bracket=[low_range, up_range],
+                    tol=self.tol, bracketed_method= self.bracketed_method,
+                    target=self.target, print_iterations=True,
+                    run_args={'output':False})
+
+            if len(search) == 3:
+                res, _, _ = search
+
+            elif len(search) == 2:
+                guesses, k = search
+                if np.array(k).prod() < self.target:
+                    print ('INFO: Function returned values BELOW target, ' \
+                           'adapting bracket range...')
+                    if (self.target - np.array(k).max()) < 0.002:
+                        print ('INFO: Max value is close enough')
+                        res =  guesses[np.array(k).argmax()]
+                    else:
+                        low_range = up_range
+                        up_range *= 5
+                else:
+                    print ('INFO: Function returned values ABOVE target, ' \
+                           'adapting bracket range...')
+                    if (np.array(k).min() - self.target)  < 0.002:
+                        print ('INFO: Min value is close enough')
+                        res = guesses[np.array(k).argmin()]
+                    else:
+                        up_range = low_range
+                        low_range /= 5
+
+            else:
+                raise ValueError(f'ERROR: search_for_keff output not valid')
+
+        msg = 'UPDATE: Refueling: {:.2f} g --> ' \
+              .format(res)
+        print(msg)
+        return  self._update_x_vector_and_volumes(x, res)

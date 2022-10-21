b874d83634962c9e1fcae2d0ffc360437e11abf5
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-10-21 15:55:08 +0200

    only translate when origin is not 0, 0, 0

--- openmc/mesh.py
+++ openmc/mesh.py
@@ -1322,9 +1322,10 @@ class CylindricalMesh(StructuredMesh):
         pts_cartesian[:, 1] = r * np.sin(phi)
 
         # offset with origin
-        pts_cartesian[:, 0] = pts_cartesian[:, 0] + self.origin[0]
-        pts_cartesian[:, 1] = pts_cartesian[:, 1] + self.origin[1]
-        pts_cartesian[:, 2] = pts_cartesian[:, 2] + self.origin[2]
+        if self.origin != [0.0, 0.0, 0.0]:
+            pts_cartesian[:, 0] = pts_cartesian[:, 0] + self.origin[0]
+            pts_cartesian[:, 1] = pts_cartesian[:, 1] + self.origin[1]
+            pts_cartesian[:, 2] = pts_cartesian[:, 2] + self.origin[2]
 
         return super().write_data_to_vtk(
             points=pts_cartesian,
@@ -1576,9 +1577,10 @@ class SphericalMesh(StructuredMesh):
         pts_cartesian[:, 2] = r * np.cos(phi)
 
         # offset with origin
-        pts_cartesian[:, 0] = pts_cartesian[:, 0] + self.origin[0]
-        pts_cartesian[:, 1] = pts_cartesian[:, 1] + self.origin[1]
-        pts_cartesian[:, 2] = pts_cartesian[:, 2] + self.origin[2]
+        if self.origin != [0.0, 0.0, 0.0]:
+            pts_cartesian[:, 0] = pts_cartesian[:, 0] + self.origin[0]
+            pts_cartesian[:, 1] = pts_cartesian[:, 1] + self.origin[1]
+            pts_cartesian[:, 2] = pts_cartesian[:, 2] + self.origin[2]
 
         return super().write_data_to_vtk(
             points=pts_cartesian,

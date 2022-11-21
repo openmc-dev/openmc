f0721ac5351eec9425ecf4e33a90b5ac13eba1cc
Rémi Delaporte-Mathurin <40028739+RemDelaporteMathurin@users.noreply.github.com>
2022-11-21 14:30:36 -0500

    Apply suggestions from code review
    Co-authored-by: Patrick Shriwise <pshriwise@gmail.com>

--- openmc/mesh.py
+++ openmc/mesh.py
@@ -1052,7 +1052,7 @@ class CylindricalMesh(StructuredMesh):
         self._r_grid = None
         self._phi_grid = [0.0, 2*pi]
         self._z_grid = None
-        self._origin = (0., 0., 0.)
+        self.origin = (0., 0., 0.)
 
     @property
     def dimension(self):
@@ -1378,7 +1378,7 @@ class SphericalMesh(StructuredMesh):
         self._r_grid = None
         self._theta_grid = [0, pi]
         self._phi_grid = [0, 2*pi]
-        self._origin = (0., 0., 0.)
+        self.origin = (0., 0., 0.)
 
     @property
     def dimension(self):

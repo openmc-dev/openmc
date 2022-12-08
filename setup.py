69583358f1420d58eb78b17cae0be2353169f40e
Bob Urberger <burberger@gmail.com>
2022-12-08 13:25:01 -0800

    Fix regression test for newer version of numpy, update numpy version restriction and add 3.11 to the classifiers list

--- setup.py
+++ setup.py
@@ -54,10 +54,10 @@ kwargs = {
         'Programming Language :: C++',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
 
     # Dependencies

4d7797c25b3932aaa166b4a6b12c4ffebd48d925
Erik B Knudsen <erik.knudsen@copenhagenatomics.com>
2022-12-22 17:18:37 +0100

    mistake assigning the wrong variable

--- src/settings.cpp
+++ src/settings.cpp
@@ -656,7 +656,7 @@ void read_settings_xml()
       source_write = get_node_value_bool(node_sp, "write");
     }
     if (check_for_node(node_sp, "mcpl")) {
-      source_write = get_node_value_bool(node_sp, "mcpl");
+      source_mcpl_write = get_node_value_bool(node_sp, "mcpl");
     }
     if (check_for_node(node_sp, "overwrite_latest")) {
       source_latest = get_node_value_bool(node_sp, "overwrite_latest");

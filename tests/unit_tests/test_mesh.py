91ebcdb21e80174bba37a6284c73a8972be0e114
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2023-01-26 14:05:23 -0500

    better test

--- tests/unit_tests/test_mesh.py
+++ tests/unit_tests/test_mesh.py
@@ -1,9 +1,34 @@
 import openmc
 import pytest
 
-def test_raises_error_when_flat():
+@pytest.mark.parametrize("val_left,val_right", [(0, 0), (-1., -1.), (2.0, 2)])
+def test_raises_error_when_flat(val_left, val_right):
+    """Checks that an error is raised when a mesh is flat"""
+    mesh = openmc.RegularMesh()
+
+    # Same X
     with pytest.raises(ValueError):
-        mesh = openmc.RegularMesh()
-        mesh.dimension = [50, 50, 1]
-        mesh.lower_left = [-25, -25, 0]
-        mesh.upper_right = [25, 25, 0]
+        mesh.lower_left = [val_left, -25, -25]
+        mesh.upper_right = [val_right, 25, 25]
+
+    with pytest.raises(ValueError):
+        mesh.upper_right = [val_right, 25, 25]
+        mesh.lower_left = [val_left, -25, -25]
+
+    # Same Y
+    with pytest.raises(ValueError):
+        mesh.lower_left = [-25, val_left, -25]
+        mesh.upper_right = [25, val_right, 25]
+
+    with pytest.raises(ValueError):
+        mesh.upper_right = [25, val_right, 25]
+        mesh.lower_left = [-25, val_left, -25]
+
+    # Same Z
+    with pytest.raises(ValueError):
+        mesh.lower_left = [-25, -25, val_left]
+        mesh.upper_right = [25, 25, val_right]
+
+    with pytest.raises(ValueError):
+        mesh.upper_right = [25, 25, val_right]
+        mesh.lower_left = [-25, -25, val_left]

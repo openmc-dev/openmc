178abe33527ada45ecbb6371e170cd30ef6cb8a5
church89 <l.chierici89@gmail.com>
2023-02-01 10:39:10 +0100

    add unit test for deplete msr continuous

---
+++ tests/unit_tests/test_deplete_msr_continuous.py
@@ -0,0 +1,85 @@
+""" Tests for Msr continuos class """
+
+from pathlib import Path
+
+import pytest
+from openmc.deplete import CoupledOperator
+import openmc
+from openmc.deplete import MsrContinuous
+import numpy as np
+from math import exp
+
+CHAIN_PATH = Path(__file__).parents[1] / "chain_simple.xml"
+
+@pytest.fixture
+def model():
+    f = openmc.Material(name="f")
+    f.add_element("U", 1, percent_type="ao", enrichment=4.25)
+    f.add_element("O", 2)
+    f.set_density("g/cc", 10.4)
+
+    w = openmc.Material(name="w")
+    w.add_element("O", 1)
+    w.add_element("H", 2)
+    w.set_density("g/cc", 1.0)
+    w.depletable = True
+
+    radii = [0.42, 0.45]
+    f.volume = np.pi * radii[0] ** 2
+    w.volume = np.pi * (radii[1]**2 - radii[0]**2)
+
+    materials = openmc.Materials([f, w])
+
+    surf_f = openmc.Sphere(r=radii[0])
+    surf_w = openmc.Sphere(r=radii[1], boundary_type='vacuum')
+    cell_f = openmc.Cell(fill=f, region=-surf_f)
+    cell_w = openmc.Cell(fill=w, region=+surf_f & -surf_w)
+    geometry = openmc.Geometry([cell_f,cell_w])
+
+    settings = openmc.Settings()
+    settings.particles = 1000
+    settings.inactive = 10
+    settings.batches = 50
+
+    return openmc.Model(geometry, materials, settings)
+
+def test_get_set(model):
+    """Tests the get/set methods"""
+
+    # create removal rates for U and Xe
+    removal_rates = {'U': 0.01, 'Xe': 0.1}
+    op = CoupledOperator(model, CHAIN_PATH)
+    msr = MsrContinuous(op, model)
+
+    # Test by Openmc material, material name and material id
+    mat, dest_mat = [m for m in model.materials if m.depletable]
+    for i in [mat, mat.name, mat.id]:
+        for j in [dest_mat, dest_mat.name, dest_mat.id]:
+            for key, value in removal_rates.items():
+                msr.set_removal_rate(i, [key], value, dest_mat=j)
+                assert msr.get_removal_rate(i, key) == value
+                assert msr.get_destination_mat(i, key) == str(dest_mat.id)
+            assert msr.get_elements(i) == removal_rates.keys()
+
+def test_msr(run_in_tmpdir, model):
+
+    """Tests msr depletion class without neither reaction rates nor decay
+    but only removal rates"""
+
+    # create removal rates for U and Xe
+    element = ['U']
+    removal_rate = 1e-5
+    op = CoupledOperator(model, CHAIN_PATH)
+    msr = MsrContinuous(op, model)
+    msr.set_removal_rate('f', element, removal_rate)
+    integrator = openmc.deplete.PredictorIntegrator(
+        op, [1,1], 0.0, msr_continuous = msr, timestep_units = 'd')
+    integrator.integrate()
+
+    # Get number of U238 atoms from results
+    results = openmc.deplete.Results('depletion_results.h5')
+    _, atoms = results.get_atoms(model.materials[0], "U238")
+
+    # Ensure number of atoms equal removal decay
+    assert atoms[1] == pytest.approx(atoms[0]*exp(-removal_rate*3600*24))
+    assert atoms[2] == pytest.approx(atoms[1]*exp(-removal_rate*3600*24))

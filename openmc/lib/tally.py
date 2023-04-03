38b7f70b53cc57f507695d711b492a61cdc27cba
Christopher Fichtlscherer <fichtlscherer@mailbox.com>
2023-04-03 14:25:59 -0400

    type in score dic

--- openmc/lib/tally.py
+++ openmc/lib/tally.py
@@ -98,7 +98,7 @@ _SCORES = {
     -5: 'absorption', -6: 'fission', -7: 'nu-fission', -8: 'kappa-fission',
     -9: 'current', -10: 'events', -11: 'delayed-nu-fission',
     -12: 'prompt-nu-fission', -13: 'inverse-velocity', -14: 'fission-q-prompt',
-    -15: 'fission-q-recoverable', -16: 'decay-rate', -17 'pulse-height'
+    -15: 'fission-q-recoverable', -16: 'decay-rate', -17: 'pulse-height'
 }
 _ESTIMATORS = {
     0: 'analog', 1: 'tracklength', 2: 'collision', 3: 'pulse-height'

750a263d71d540115b44fc3f831887033147609b
John Tramm <john.tramm@gmail.com>
2023-04-13 08:51:45 -0500

    Removed "C" from new settings variables

--- include/openmc/settings.h
+++ include/openmc/settings.h
@@ -86,8 +86,8 @@ extern "C" int64_t n_particles;              //!< number of particles per genera
 
 extern int64_t max_particles_in_flight; //!< Max num. event-based particles in flight
 
-extern "C" bool sort_fissionable_xs_lookups; //!< Sort fissionable material XS lookups in event-based mode?
-extern "C" bool sort_non_fissionable_xs_lookups; //!< Sort non-fissionable material XS lookups in event-based mode?
+extern bool sort_fissionable_xs_lookups; //!< Sort fissionable material XS lookups in event-based mode?
+extern bool sort_non_fissionable_xs_lookups; //!< Sort non-fissionable material XS lookups in event-based mode?
 
 #pragma omp declare target
 extern ElectronTreatment electron_treatment;       //!< how to treat secondary electrons

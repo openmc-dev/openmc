c659331fb291fb3b7462c575e74837a5fbf627cf
Christopher Fichtlscherer <fichtlscherer@mailbox.com>
2023-05-12 22:35:26 -0400

    source id is read out the correct way for settings

--- src/settings.cpp
+++ src/settings.cpp
@@ -486,17 +486,10 @@ void read_settings_xml(pugi::xml_node root)
       std::vector<int> source_group;
 
       for (xml_node source_node : group_node.children("source")) {
-        int source_id = source_node.attribute("id").as_int();
+        int source_id = source_node.text().as_int();
         source_group.push_back(source_id);
       }
       coincident_sources.push_back(std::move(source_group));
-
-      for (const auto& group : coincident_sources) {
-        for (const auto& source_id : group) {
-          std::cout << source_id << " ";
-        }
-        std::cout << std::endl;
-      }
     }
   }
 

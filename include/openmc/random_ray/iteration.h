d76033a299a92f8c59045c3e8dd93385ae6b833b
John Tramm <john.tramm@gmail.com>
2023-06-12 17:18:33 +0000

    first commit. File layout, classes, source region init.

---
+++ include/openmc/random_ray/iteration.h
@@ -0,0 +1,10 @@
+#ifndef OPENMC_RANDOM_RAY_ITERATION_H
+#define OPENMC_RANDOM_RAY_ITERATION_H
+
+
+namespace openmc {
+
+} // namespace openmc
+
+
+#endif // OPENMC_RANDOM_RAY_ITERATION_H
---
+++ include/openmc/random_ray/source_region.h
@@ -0,0 +1,35 @@
+#ifndef OPENMC_RANDOM_RAY_SOURCE_REGION_H
+#define OPENMC_RANDOM_RAY_SOURCE_REGION_H
+
+#include <vector>
+
+#include "openmc/openmp_interface.h"
+#include "openmc/position.h"
+
+namespace openmc {
+
+  namespace random_ray {
+
+    // Scalars
+    extern int64_t n_source_elements;
+    extern int64_t n_source_regions; // number of source regions (a.k.a. cells)
+
+    // Cell-wise Data
+    std::vector<OpenMPMutex> lock;
+    extern std::vector<int> material;
+    extern std::vector<int> position_recorded;
+    extern std::vector<Position> position;
+    extern std::vector<float> scalar_flux_new;
+    extern std::vector<float> scalar_flux_old;
+    extern std::vector<float> source;
+    extern std::vector<double> volume;
+    extern std::vector<double> volume_t;
+    extern std::vector<int> was_hit;
+
+  } // namespace random_ray
+  
+  void initialize_source_regions();
+
+} // namespace openmc
+
+#endif // OPENMC_RANDOM_RAY_SOURCE_REGION_H

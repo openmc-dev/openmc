3d8d38f9991ee14ca3908d08817c5c75f0e8ef78
Saad Amin <58188800+saada2006@users.noreply.github.com>
2023-06-11 19:15:55 -0700

    reduce particles for faster debugging

--- examples/atr/simulation.py
+++ examples/atr/simulation.py
@@ -3,7 +3,7 @@ import openmc
 settings = openmc.Settings()
 settings.batches = 30
 settings.inactive = 10
-settings.particles = 40000
+settings.particles = 2000
 settings.export_to_xml()
 
 openmc.run()

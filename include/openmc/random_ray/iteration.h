cfab02a612828266f49d90d5a3ba8a6e9007d8a9
John Tramm <john.tramm@gmail.com>
2023-06-12 19:30:42 +0000

    basic iteration code is in.

--- include/openmc/random_ray/iteration.h
+++ include/openmc/random_ray/iteration.h
@@ -9,7 +9,7 @@ namespace openmc {
   void update_neutron_source(double k_eff);
   double compute_k_eff(double k_eff_old);
   void normalize_scalar_flux_and_volumes(double total_active_distance_per_iteration, int iter);
-  void add_source_to_scalar_flux(double total_active_distance_per_iteration, int iter);
+  int64_t add_source_to_scalar_flux(void);
   void tally_fission_rates(void);
   double calculate_miss_rate(void);
   int openmc_run_random_ray(void);

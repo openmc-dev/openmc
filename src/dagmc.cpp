3eb998fedbedec7d0d3df023389ee6f01f0f748e
Christopher <chris1218@gmx.de>
2023-06-21 14:35:25 +0200

    clang

--- src/dagmc.cpp
+++ src/dagmc.cpp
@@ -182,11 +182,10 @@ void DAGUniverse::init_geometry()
       model::cell_map[c->id_] = model::cells.size();
     } else {
       warning(fmt::format("DAGMC Cell IDs: {}", dagmc_ids_for_dim(3)));
-      fatal_error(fmt::format(
-        "DAGMC Universe {} contains a cell with ID {}, which "
-        "already exists elsewhere in the geometry. Setting auto_geom_ids "
-        "to True when initiating the DAGMC Universe may "
-        "resolve this issue",
+      fatal_error(fmt::format("DAGMC Universe {} contains a cell with ID {}, which "
+                              "already exists elsewhere in the geometry. Setting auto_geom_ids "
+                              "to True when initiating the DAGMC Universe may "
+                              "resolve this issue",
         this->id_, c->id_));
     }
 
@@ -610,8 +609,7 @@ std::pair<double, int32_t> DAGCell::distance(
     // into the implicit complement on the other side where no intersection will
     // be found. Treating this as a lost particle is problematic when plotting.
     // Instead, the infinite distance and invalid surface index are returned.
-    if (settings::run_mode == RunMode::PLOTTING)
-      return {INFTY, -1};
+    if (settings::run_mode == RunMode::PLOTTING) return {INFTY, -1};
 
     // the particle should be marked as lost immediately if an intersection
     // isn't found in a volume that is not the implicit complement. In the case
@@ -741,8 +739,7 @@ void check_dagmc_root_univ()
   }
 }
 
-int32_t next_cell(int32_t surf, int32_t curr_cell, int32_t univ)
-{
+int32_t next_cell(int32_t surf, int32_t curr_cell, int32_t univ) {
   auto surfp = dynamic_cast<DAGSurface*>(model::surfaces[surf - 1].get());
   auto cellp = dynamic_cast<DAGCell*>(model::cells[curr_cell].get());
   auto univp = static_cast<DAGUniverse*>(model::universes[univ].get());
@@ -753,12 +750,11 @@ int32_t next_cell(int32_t surf, int32_t curr_cell, int32_t univ)
     cellp->dagmc_ptr()->entity_by_index(3, cellp->dag_index());
 
   moab::EntityHandle new_vol;
-  moab::ErrorCode rval =
-    cellp->dagmc_ptr()->next_vol(surf_handle, curr_vol, new_vol);
-  if (rval != moab::MB_SUCCESS)
-    return -1;
+  moab::ErrorCode rval = cellp->dagmc_ptr()->next_vol(surf_handle, curr_vol, new_vol);
+  if (rval != moab::MB_SUCCESS) return -1;
 
-  return cellp->dagmc_ptr()->index_by_handle(new_vol) + univp->cell_idx_offset_;
+  return cellp->dagmc_ptr()->index_by_handle(new_vol) +
+         univp->cell_idx_offset_;
 }
 
 } // namespace openmc

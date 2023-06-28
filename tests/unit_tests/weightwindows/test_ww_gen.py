b6c621f9b4bac7badbcffb71fe2b8e45d8e7c798
Jack Fletcher <115663563+j-fletcher@users.noreply.github.com>
2023-06-28 18:57:44 -0400

    Change "update_params" to "update_parameters" in test suite: test_ww_gen.py (#2577)

--- tests/unit_tests/weightwindows/test_ww_gen.py
+++ tests/unit_tests/weightwindows/test_ww_gen.py
@@ -229,9 +229,9 @@ def test_ww_gen_roundtrip(run_in_tmpdir, model):
     particle_type = 'neutron'
 
     wwg = openmc.WeightWindowGenerator(mesh, energy_bounds, particle_type)
-    wwg.update_params = {'ratio' : 5.0,
-                         'threshold': 0.8,
-                         'value' : 'mean'}
+    wwg.update_parameters = {'ratio' : 5.0,
+                             'threshold': 0.8,
+                             'value' : 'mean'}
 
     model.settings.weight_window_generators = wwg
     model.export_to_xml()

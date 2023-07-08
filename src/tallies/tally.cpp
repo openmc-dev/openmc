ebee0168da2a0216756845c5b65a2faf1af17343
Christopher <chris1218@gmx.de>
2023-07-08 15:49:48 +0200

    type 2

--- src/tallies/tally.cpp
+++ src/tallies/tally.cpp
@@ -391,9 +391,9 @@ void Tally::add_filter(Filter* filter)
     return;
 
   // Keep track of indices for special filters
-  if (filt->type() == FilterType::ENERGY_OUT) {
+  if (filter->type() == FilterType::ENERGY_OUT) {
     energyout_filter_ = filters_.size();
-  } else if (filt->type() == FilterType::DELAYED_GROUP) {
+  } else if (filter->type() == FilterType::DELAYED_GROUP) {
     delayedgroup_filter_ = filters_.size();
   } else if (filter->type() == FilterType::CELL) {
     cell_filter_ = filters_.size();

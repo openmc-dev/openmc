eb37098bb313eac75656e342cac7c680e052587b
Saad Amin <58188800+saada2006@users.noreply.github.com>
2023-07-12 18:51:07 -0700

    Remove files

--- build.sh
+++
@@ -1,4 +0,0 @@
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j 16
-sudo make install # update our old build with our new build
--- debug.sh
+++
@@ -1,5 +0,0 @@
-./build.sh
-echo "OpenMC is now done building and updating."
-echo "Running ATR test..."
-cd examples/atr/
-python3 simulation.py

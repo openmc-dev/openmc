0481313e5bb37b7a9cc85edcf075d67232fddde2
Baptiste Mouginot <bam.git.no.reply@gmail.com>
2023-08-16 23:43:30 +0200

    using @pshriwise suggestion

--- src/volume_calc.cpp
+++ src/volume_calc.cpp
@@ -534,20 +534,17 @@ int openmc_calculate_volumes()
 
       // Display domain volumes
       for (int j = 0; j < vol_calc.domain_ids_.size(); j++) {
-        std::string cell_name = "";
-        if (domain_type == "  Cell ") {
-          int cell_pos = model::cell_map[vol_calc.domain_ids_[j]];
-          cell_name = model::cells[cell_pos]->name();
+        std::string cell_name {""};
+        if (vol_calc.domain_type_ == VolumeCalculation::TallyDomain::CELL) {
+          int cell_idx = model::cell_map[vol_calc.domain_ids_[j]];
+          cell_name = model::cells[cell_idx]->name();
+          if (cell_name.size())
+            cell_name.insert(0, " "); // prepend space for formatting
         }
-        if (cell_name != "") {
-          write_message(4, "{}{} {}: {} +/- {} cm^3", domain_type,
-            vol_calc.domain_ids_[j], cell_name, results[j].volume[0],
-            results[j].volume[1]);
-        } else {
-          write_message(4, "{}{}: {} +/- {} cm^3", domain_type,
-            vol_calc.domain_ids_[j], results[j].volume[0],
-            results[j].volume[1]);
-        }
+
+        write_message(4, "{}{}{}: {} +/- {} cm^3", domain_type,
+          vol_calc.domain_ids_[j], cell_name, results[j].volume[0],
+          results[j].volume[1]);
       }
 
       // Write volumes to HDF5 file

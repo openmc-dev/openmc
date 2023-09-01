a1c8ff401bcf25277ecbffa2e28d37edb53fa283
Patrick Shriwise <pshriwise@gmail.com>
2023-09-01 07:23:20 -0500

    Apply @paulromano suggestions from code review
    Co-authored-by: Paul Romano <paul.k.romano@gmail.com>

--- openmc/cell.py
+++ openmc/cell.py
@@ -618,10 +618,10 @@ class Cell(IDManagerMixin):
             Axes containing resulting image
 
         """
-        u = openmc.Universe(cells=[self])
-        ax = u.plot(*args, **kwargs)
-        openmc.release_ids([u.id], openmc.Universe)
-        return ax
+        # Create dummy universe but preserve used_ids
+        u = openmc.Universe(cells=[self], universe_id=openmc.Universe.next_id + 1)
+        openmc.Universe.used_ids.remove(u.id)
+        return u.plot(*args, **kwargs)
 
     def create_xml_subelement(self, xml_element, memo=None):
         """Add the cell's xml representation to an incoming xml element

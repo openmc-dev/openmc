9336ea6bda741bbea2b3ab5ec17d9caf9d9738cd
Baptiste Mouginot <bam.git.no.reply@gmail.com>
2023-09-09 17:06:44 +0200

    disable XS loading on plot mode

--- src/material.cpp
+++ src/material.cpp
@@ -224,11 +224,13 @@ Material::Material(pugi::xml_node node)
 
     // Check that this nuclide is listed in the nuclear data library
     // (cross_sections.xml for CE and the MGXS HDF5 for MG)
-    LibraryKey key {Library::Type::neutron, name};
-    if (data::library_map.find(key) == data::library_map.end()) {
-      fatal_error("Could not find nuclide " + name +
-                  " in the "
-                  "nuclear data library.");
+    if (settings::run_mode != RunMode::PLOTTING) {
+      LibraryKey key {Library::Type::neutron, name};
+      if (data::library_map.find(key) == data::library_map.end()) {
+        fatal_error("Could not find nuclide " + name +
+                    " in the "
+                    "nuclear data library.");
+      }
     }
 
     // If this nuclide hasn't been encountered yet, we need to add its name

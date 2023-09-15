64f43f975ea844700efd1674231a656242dc9bbb
Erik B Knudsen <erik.knudsen@copenhagenatomics.com>
2023-09-15 10:35:31 +0200

    make sure test allows for threads/mpi
    omp threads and/or mpi may create some additional
    files. protect test against that

--- tests/regression_tests/particle_limit_lost_output/test.py
+++ tests/regression_tests/particle_limit_lost_output/test.py
@@ -10,11 +10,22 @@ def _cleanup_wrapper(func):
     func()
   return _cleanup_particles
 
+try:
+  mpi_np=config['mpi_np']
+except:
+  mpi_np=1
+
+try:
+  omp_nt=os.environ['OMP_NUM_THREADS']
+except:
+  omp_nt=1
+  os.environ['OMP_NUM_THREADS']=str(omp_nt)
+
 class TestHarness_limit_particle_output(TestHarness):
     #redefine the check output method to do something else
     def _check_output_limit_lost(self):
       glb=glob.glob('particle_[0-9]_*.h5')
-      assert (5== approx(len(glb),abs=2))
+      assert (len(glb) <= 5 + numthreads-1)
 
     def _cleanup(self):
       super()._cleanup()

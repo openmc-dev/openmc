71a43edd4bfc8a51d096b10b7321ba2e4a5409d3
Patrick Shriwise <pshriwise@gmail.com>
2023-09-22 13:51:09 -0500

    Adding proper test check and explanation of test

--- tests/regression_tests/source_mesh/test.py
+++ tests/regression_tests/source_mesh/test.py
@@ -6,7 +6,6 @@ import openmc
 import openmc.model
 
 def test_source_mesh():
-
     # build a simple void model
     min, max = -10, 10
     box = openmc.model.RectangularParallelepiped(min, max, min, max, min, max, boundary_type='vacuum')
@@ -25,11 +24,17 @@ def test_source_mesh():
 
     energy = openmc.stats.Discrete([1.e6], [1.0])
 
-    angle = openmc.stats.Monodirectional((-1, -1, -1))
+    # create sources with only one non-zero strength for the source in the
+    # mesh voxel occupyting the lowest octant. Direct source particles straight
+    # out of the problem from there. This demonstrates that
+    # 1) particles are only being sourced within the intented mesh voxel based on source strength
+    # 2) particles are respecting the angle distributions assigned to each voxel
+    vec = np.ones((3,))
+    vec /= -np.linalg.norm(vec)
+    angle = openmc.stats.Monodirectional(vec)
 
     sources = [openmc.Source(energy=energy, angle=angle, strength=0.0) for _ in range(strengths.size)]
 
-    # create only one non-zero strength source in voxel (0, 0, 0)
     sources[0].strength = 1.0
 
     mesh_source = openmc.MeshSource(mesh, sources=sources)
@@ -46,5 +51,12 @@ def test_source_mesh():
 
     model.tallies = openmc.Tallies([tally])
 
-    model.run()
+    sp_file = model.run()
 
+    with openmc.StatePoint(sp_file) as sp:
+        tally_out = sp.get_tally(id=tally.id)
+
+    assert tally_out.mean[0] != 0
+    assert np.all(tally_out.mean[1:] == 0)
+
+    

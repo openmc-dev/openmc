c220a14ed43def1e1523c02f1c474751c0564cdb
yrrepy <100789850+yrrepy@users.noreply.github.com>
2023-09-28 16:55:20 -0700

    Revert
    Revert to original, now that cutoffs were multiplied on the fly

--- src/physics_common.cpp
+++ src/physics_common.cpp
@@ -11,15 +11,11 @@ namespace openmc {
 
 void russian_roulette(Particle& p, double weight_survive)
 {
-  // if survival normalization is applicable, use normalized weight survive
-  if((settings::source_file || settings::surf_source_read)&&(settings::survival_normalization && settings::survival_biasing)){
-    weight_survive = p.wgt_survive();
-  }
   if (weight_survive * prn(p.current_seed()) < p.wgt()) {
     p.wgt() = weight_survive;
   } else {
     p.wgt() = 0.;
-  } 
+  }
 }
 
 } // namespace openmc

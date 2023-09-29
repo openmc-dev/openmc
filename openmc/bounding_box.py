6fb16893fec9c922b81b90210b9337655448b9a1
Jonathan Shimwell <mail@jshimwell.com>
2023-09-29 16:30:46 +0100

    added padding arg to bounding_region (#2701)
    Co-authored-by: Paul Romano <paul.k.romano@gmail.com>

--- openmc/bounding_box.py
+++ openmc/bounding_box.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from typing import Iterable
 
 import numpy as np
@@ -87,3 +88,30 @@ class BoundingBox(tuple):
     @property
     def width(self):
         return self.upper_right - self.lower_left
+
+    def extend(self, padding_distance: float) -> BoundingBox:
+        """Returns an extended bounding box
+
+        Parameters
+        ----------
+        padding_distance : float
+            The distance to enlarge the bounding box by
+
+        Returns
+        -------
+        An enlarged bounding box
+
+        """
+        return BoundingBox(np.array(
+            [
+                self[0][0] - padding_distance,
+                self[0][1] - padding_distance,
+                self[0][2] - padding_distance
+            ]
+        ), np.array(
+            [
+                self[1][0] + padding_distance,
+                self[1][1] + padding_distance,
+                self[1][2] + padding_distance
+            ]
+        ))
--- tests/unit_tests/dagmc/test_bounds.py
+++ tests/unit_tests/dagmc/test_bounds.py
@@ -23,23 +23,38 @@ def test_bounding_region(request):
     assert isinstance(region, openmc.Region)
     assert len(region) == 6
     assert region[0].surface.type == "x-plane"
+    assert region[0].surface.x0 == -25.
     assert region[1].surface.type == "x-plane"
+    assert region[1].surface.x0 == 25.
     assert region[2].surface.type == "y-plane"
+    assert region[2].surface.y0 == -25.
     assert region[3].surface.type == "y-plane"
+    assert region[3].surface.y0 == 25.
     assert region[4].surface.type == "z-plane"
+    assert region[4].surface.z0 == -25.
     assert region[5].surface.type == "z-plane"
+    assert region[5].surface.z0 == 25.
     assert region[0].surface.boundary_type == "vacuum"
     assert region[1].surface.boundary_type == "vacuum"
     assert region[2].surface.boundary_type == "vacuum"
     assert region[3].surface.boundary_type == "vacuum"
     assert region[4].surface.boundary_type == "vacuum"
     assert region[5].surface.boundary_type == "vacuum"
+    region = u.bounding_region(padding_distance=5)
+    assert region[0].surface.x0 == -30.
+    assert region[1].surface.x0 == 30.
+    assert region[2].surface.y0 == -30.
+    assert region[3].surface.y0 == 30.
+    assert region[4].surface.z0 == -30.
+    assert region[5].surface.z0 == 30.
 
     region = u.bounding_region(bounded_type="sphere", boundary_type="reflective")
     assert isinstance(region, openmc.Region)
     assert isinstance(region, openmc.Halfspace)
     assert region.surface.type == "sphere"
     assert region.surface.boundary_type == "reflective"
+    larger_region = u.bounding_region(bounded_type="sphere", padding_distance=10)
+    assert larger_region.surface.r > region.surface.r
 
 
 def test_bounded_universe(request):

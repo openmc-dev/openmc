6c0b7db7f231c4f9a8fd2a2b1936d36a16793b28
yrrepy <100789850+yrrepy@users.noreply.github.com>
2023-09-29 21:22:08 -0700

    Reorganize Boolean flags
    Reorganize and reorder Boolean flags to account for changes that have occurred in the interim with the recent addition of weight_window_checkpoint_surface and weight_window_checkpoint_collision.
    
    Thus Resolving a conflict in the present PR.

--- include/openmc/settings.h
+++ include/openmc/settings.h
@@ -45,6 +45,7 @@ extern "C" bool reduce_tallies;    //!< reduce tallies at end of batch?
 extern bool res_scat_on;           //!< use resonance upscattering method?
 extern "C" bool restart_run;       //!< restart run?
 extern "C" bool run_CE;            //!< run with continuous-energy data?
+extern bool source_file;           //!< is there a source file?
 extern bool source_latest;         //!< write latest source at each batch?
 extern bool source_separate;       //!< write source to separate file?
 extern bool source_write;          //!< write source in HDF5 files?
@@ -53,16 +54,19 @@ extern bool surf_source_write;     //!< write surface source file?
 extern bool surf_mcpl_write;       //!< write surface mcpl file?
 extern bool surf_source_read;      //!< read surface source file?
 extern bool survival_biasing;      //!< use survival biasing?
+extern bool survival_normalization;//!< use survival normalization?
 extern bool temperature_multipole; //!< use multipole data?
 extern "C" bool trigger_on;        //!< tally triggers enabled?
 extern bool trigger_predict;       //!< predict batches for triggers?
 extern bool ufs_on;                //!< uniform fission site method on?
 extern bool urr_ptables_on;        //!< use unresolved resonance prob. tables?
 extern "C" bool weight_windows_on; //!< are weight windows are enabled?
-extern bool write_all_tracks;      //!< write track files for every particle?
-extern bool write_initial_source;  //!< write out initial source file?
-extern bool survival_normalization;//!< using survival normalization?
-extern bool source_file;           //!< is there a source file?
+extern bool weight_window_checkpoint_surface;   //!< enable weight window check
+                                                //!< upon surface crossing?
+extern bool weight_window_checkpoint_collision; //!< enable weight window check
+                                                //!< upon collision?
+extern bool write_all_tracks;     //!< write track files for every particle?
+extern bool write_initial_source; //!< write out initial source file?
 
 // Paths to various files
 extern std::string path_cross_sections; //!< path to cross_sections.xml

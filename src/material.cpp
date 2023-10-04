913e8d27a3d835a73567f79aaf4d0c223d7c119d
Baptiste Mouginot <bam.git.no.reply@gmail.com>
2023-10-04 20:11:09 +0200

    this should fix the conditions

--- src/material.cpp
+++ src/material.cpp
@@ -249,7 +249,8 @@ Material::Material(pugi::xml_node node)
     // transport will be used, we need to add its symbol to the element_dict
     if (settings::photon_transport and
         (settings::run_mode != RunMode::PLOTTING or
-          settings::run_mode != RunMode::VOLUME)) {
+        (settings::run_mode == RunMode::VOLUME and
+          settings::path_cross_sections != ""))) {
       std::string element = to_element(name);
 
       // Make sure photon cross section data is available
@@ -311,7 +312,8 @@ Material::Material(pugi::xml_node node)
   // =======================================================================
   // READ AND PARSE <sab> TAG FOR THERMAL SCATTERING DATA
   if (settings::run_CE and (settings::run_mode != RunMode::PLOTTING or
-                             settings::run_mode != RunMode::VOLUME)) {
+        (settings::run_mode == RunMode::VOLUME and
+          settings::path_cross_sections != ""))) {
     // Loop over <sab> elements
 
     vector<std::string> sab_names;

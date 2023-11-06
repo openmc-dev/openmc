06f7b3aa9ea51972ae57c7c1c8b37dea44f00fff
Patrick Shriwise <pshriwise@gmail.com>
2023-11-06 16:15:26 -0600

    Update openmc/stats/multivariate.py
    Co-authored-by: Jonathan Shimwell <drshimwell@gmail.com>

--- openmc/stats/multivariate.py
+++ openmc/stats/multivariate.py
@@ -714,7 +714,6 @@ class MeshSpatial(Spatial):
         else:
             element = ET.Element('space')
 
-
         element.set('type', 'mesh')
         element.set("mesh_id", str(self.mesh.id))
         element.set("volume_normalized", str(self.volume_normalized))

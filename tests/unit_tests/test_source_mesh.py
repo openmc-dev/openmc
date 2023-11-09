b4369f73d9130fd4b7b4ab473f9b1a7045afd1d5
Patrick Shriwise <pshriwise@gmail.com>
2023-11-09 14:16:57 -0600

    Adding a test for a file source

--- tests/unit_tests/test_source_mesh.py
+++ tests/unit_tests/test_source_mesh.py
@@ -3,6 +3,7 @@ import numpy as np
 import pytest
 
 import openmc
+import openmc.lib
 import openmc.model
 
 @pytest.mark.parametrize('mesh_type', ('rectangular', 'cylindrical'))
@@ -103,3 +104,61 @@ def test_source_mesh(run_in_tmpdir, mesh_type):
 
     mesh_source.normalize_source_strengths()
     assert mesh_source.strength == 1.0
+
+
+def test_file_source(run_in_tmpdir):
+    source_particle = openmc.SourceParticle(r=(0.0, 0.0, 0.0),
+                                            u=(0.0, 0.0, 1.0),
+                                            E=1e6,
+                                            time=10.0)
+
+    openmc.write_source_file([source_particle], 'source.h5')
+
+    file_source = openmc.FileSource('source.h5')
+
+    model = openmc.Model()
+
+    rect_prism = openmc.model.RectangularParallelepiped(-5.0, 5.0,
+                                                        -5.0, 5.0,
+                                                        -5.0, 5.0,
+                                                        boundary_type='vacuum')
+    mat = openmc.Material()
+    mat.add_nuclide('H1', 1.0)
+
+    model.geometry = openmc.Geometry([openmc.Cell(fill=mat, region=-rect_prism)])
+    model.settings.particles = 1000
+    model.settings.batches = 10
+    model.settings.inactive = 1
+    model.settings.run_mode = 'fixed source'
+
+    mesh = openmc.RegularMesh()
+    mesh.lower_left = (-1, -2, -3)
+    mesh.upper_right = (2, 3, 4)
+    mesh.dimension = (1, 1, 1)
+
+    mesh_source_arr = np.asarray([file_source]).reshape(mesh.dimension)
+    source = openmc.MeshSource(mesh, mesh_source_arr)
+
+    model.settings.source = source
+
+    model.export_to_model_xml()
+
+    openmc.lib.init()
+    openmc.lib.simulation_init()
+    sites = openmc.lib.sample_external_source(10)
+    openmc.lib.finalize()
+
+
+    # the mesh bounds do not contain the point of the lone
+    # source site in the file source, so it should not appear
+    # in the set of source sites produced from the mesh source
+
+    # additionally, the source should be located within the mesh
+    bbox = mesh.bounding_box
+    for site in sites:
+        assert site.r != (0, 0, 0)
+        assert site.E == source_particle.E
+        assert site.u == source_particle.u
+        assert site.time == source_particle.time
+        assert site.r in bbox
+

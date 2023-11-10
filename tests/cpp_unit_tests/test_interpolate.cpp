022fa01c7432d857dce32ae1838ae485821b017d
Patrick Shriwise <pshriwise@gmail.com>
2023-11-10 13:48:36 -0600

    Adding new test file for interpolation

---
+++ tests/cpp_unit_tests/test_interpolate.cpp
@@ -0,0 +1,11 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include "openmc/interpolate.h"
+
+using namespace openmc;
+
+TEST_CASE("Test Lagranian Interpolation")
+{
+
+    std::vector<double> vals, xs, ys;
+}

83dbbbb40bf126169112806213796cb3d8c988d6
zoeprieto <zoe.prieto@ib.edu.ar>
2023-11-09 21:23:42 -0300

    test fixed

--- tests/unit_tests/test_filter_musurface.py
+++ tests/unit_tests/test_filter_musurface.py
@@ -7,21 +7,27 @@ from uncertainties import unumpy
 import openmc
 
 
-def test_spherical_mesh_estimators(run_in_tmpdir):
+def test_musurface(run_in_tmpdir):
     #"""Test that collision/tracklength estimators agree for SphericalMesh"""
 
-    sphere = openmc.Sphere(r=10.0, boundary_type='vacuum')
+    sphere = openmc.Sphere(r=1.0, boundary_type='vacuum')
     cell = openmc.Cell(fill=None, region=-sphere)
     model = openmc.Model()
     model.geometry = openmc.Geometry([cell])
-    model.settings.particles = 1000
-    model.settings.inactive = 10
+    model.settings.particles = 10000
     model.settings.batches = 20
+    E = 1.0
+    source=openmc.IndependentSource()
+    source.space = openmc.stats.Point()
+    source.angle= openmc.stats.Isotropic()
+    source.energy = openmc.stats.Discrete([E,], [1.0,]) 
+    model.settings.source = source
+    model.settings.run_mode = "fixed source"
 
-    sph_mesh = openmc.MuSurfaceFilter(np.linspace(-1.0,1.0,201))
-
+    filter1 = openmc.MuSurfaceFilter(np.linspace(-1.0,1.0,201))
+    filter2 = openmc.SurfaceFilter(sphere)
     tally = openmc.Tally()
-    tally.filters = [openmc.MuSurfaceFilter(sphere)]
+    tally.filters = [filter1, filter2]
     tally.scores = ['current']
 
     model.tallies = openmc.Tallies([tally])
@@ -32,9 +38,9 @@ def test_spherical_mesh_estimators(run_in_tmpdir):
     # Get radial flux distribution
     with openmc.StatePoint(sp_filename) as sp:
         current_mu = sp.tallies[tally.id].mean.ravel()
-
-    assert (current_mu[-1] ==  current_mu.max())
+    
     assert (current_mu[-1] ==  1.0)
-    assert (current_mu[0:-2] == 0)
+    for element in current_mu[:-2]:
+        assert (element == 0)
 
 

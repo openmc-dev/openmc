f363645f5b1a0af00a53b0b49e5a69cea0c2b01c
John Tramm <john.tramm@gmail.com>
2023-11-21 19:03:25 +0000

    commented out unneeded init statements

--- src/random_ray/iteration.cpp
+++ src/random_ray/iteration.cpp
@@ -30,8 +30,8 @@ int openmc_run_random_ray(void)
   // Reset global variables -- this is done before loading state point (as that
   // will potentially populate k_generation and entropy)
   //simulation::current_batch = 0;
-  simulation::current_gen = 1;
-  simulation::n_realizations = 0;
+  //simulation::current_gen = 1;
+  //simulation::n_realizations = 0;
   //simulation::k_generation.clear();
   //simulation::entropy.clear();
   //openmc_reset();

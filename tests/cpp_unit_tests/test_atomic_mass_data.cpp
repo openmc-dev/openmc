610ec22e466484bd5e015918d6ea0de6b5b1685b
Baptiste Mouginot <bam.git.no.reply@gmail.com>
2023-12-31 13:34:36 +0100

    Adding test to Makefile for compilation, and implementing tests

--- tests/cpp_unit_tests/test_atomic_mass_data.cpp
+++ tests/cpp_unit_tests/test_atomic_mass_data.cpp
@@ -1,5 +1,4 @@
-#include <iostream>
-#include <map>
+#include <string>
 
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_floating_point.hpp>
@@ -8,4 +7,25 @@
 
 using namespace openmc;
 
-TEST_CASE("Test ") {}
+TEST_CASE("Test read and storing atomic data")
+{
+  AtomicData my_atomic_data();
+
+  std::string nucs[3] = {"Cl50", "Rh118", "Nd151"};
+  double expected_mass[3] = {50.008266, 117.930341116, 150.923839363};
+  double expected_binding[3] = {7651, 8322.8539, 8230.2741};
+  double expected_mass_excess[3] = {7700, -64886.840, -70943.183};
+
+  for (i < 0; i < 3; i++) {
+    double mass_out = my_atomic_data.get_atomic_mass[nucs[i]];
+    REQUIRE_THAT(mass_out, Catch::Matchers::WithinAbs(expected_mass[i], 1e-04));
+
+    double binding_out = my_atomic_data.get_atomic_binding_energy[nucs[i]];
+    REQUIRE_THAT(
+      binding_out, Catch::Matchers::WithinAbs(expected_binding[i], 1e-04));
+
+    double excess_out = my_atomic_data.get_atomic_mass_excess[nucs[i]];
+    REQUIRE_THAT(
+      excess_out, Catch::Matchers::WithinAbs(expected_mass_excess[i], 1e-04));
+  }
+}

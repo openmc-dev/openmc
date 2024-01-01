25d8e80b4acd651a4e09044bd79c5e468028e61c
Ilham Variansyah <variansyah.ilham@gmail.com>
2024-01-01 12:29:38 -0800

    add alpha_mode_tallies hdf5 group

--- src/eigenvalue.cpp
+++ src/eigenvalue.cpp
@@ -801,12 +801,50 @@ void write_eigenvalue_hdf5(hid_t group)
 
   // alpha-eigenvalue mode
   if (settings::alpha_mode) {
-    write_dataset(group, "alpha_generation", simulation::alpha_generation);
-    std::array<double, 2> alpha_final;
+    hid_t alpha_group = create_group(group, "alpha_mode_tallies");
+    write_dataset(alpha_group, "alpha_generation", simulation::alpha_generation);
+
     const int n = simulation::k_generation.size() - settings::n_inactive;
-    alpha_final[0] = simulation::alpha_sum[0]/n;
-    alpha_final[1] = simulation::alpha_eff_std;
-    write_dataset(group, "alpha_final", alpha_final);
+    double t_n1 = 1.0;
+    if (settings::confidence_intervals) {
+      double alpha = 1.0 - CONFIDENCE_LEVEL;
+      t_n1 = t_percentile(1.0 - alpha / 2.0, n - 1);
+    }
+
+    // Alpha eigenvalue
+    std::array<double, 2> alpha_eff;
+    alpha_eff[0] = simulation::alpha_sum[0]/n;
+    alpha_eff[1] =  t_n1 * std::sqrt((simulation::alpha_sum[1]/n 
+                    - std::pow(alpha_eff[0], 2)) / (n - 1));
+    write_dataset(alpha_group, "alpha_effective", alpha_eff);
+    
+    // Multiplication factor
+    std::array<double, 2> k_eff;
+    k_eff[0] = simulation::k_alpha_sum[0]/n;
+    k_eff[1] =  t_n1 * std::sqrt((simulation::k_alpha_sum[1]/n 
+                    - std::pow(k_eff[0], 2)) / (n - 1));
+    write_dataset(alpha_group, "k_effective", k_eff);
+    
+    // Reactivity
+    std::array<double, 2> rho;
+    rho[0] = simulation::rho_sum[0]/n;
+    rho[1] =  t_n1 * std::sqrt((simulation::rho_sum[1]/n 
+                    - std::pow(rho[0], 2)) / (n - 1));
+    write_dataset(alpha_group, "reactivity", rho);
+    
+    // Delayed fission fraction
+    std::array<double, 2> beta;
+    beta[0] = simulation::beta_sum[0]/n;
+    beta[1] =  t_n1 * std::sqrt((simulation::beta_sum[1]/n 
+                    - std::pow(beta[0], 2)) / (n - 1));
+    write_dataset(alpha_group, "delayed_fraction", beta);
+
+    // Generation time
+    std::array<double, 2> Lambda;
+    Lambda[0] = simulation::Lambda_sum[0]/n;
+    Lambda[1] =  t_n1 * std::sqrt((simulation::Lambda_sum[1]/n 
+                    - std::pow(Lambda[0], 2)) / (n - 1));
+    write_dataset(alpha_group, "generation_time", Lambda);
   }
 }
 

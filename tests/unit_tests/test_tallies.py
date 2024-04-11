ad765c5ec4a3680b3b4cc452cf70c56ec7db1b25
Patrick Shriwise <pshriwise@gmail.com>
2024-04-11 03:51:56 -0500

    Updates to tally equivalence and version numbers

--- tests/unit_tests/test_tallies.py
+++ tests/unit_tests/test_tallies.py
@@ -44,6 +44,54 @@ def test_xml_roundtrip(run_in_tmpdir):
     assert new_tally.triggers[0].scores == tally.triggers[0].scores
     assert new_tally.multiply_density == tally.multiply_density
 
+def test_tally_equivalence():
+    tally_a = openmc.Tally()
+    tally_b = openmc.Tally(tally_id=tally_a.id)
+
+    tally_a.name = 'new name'
+    assert tally_a != tally_b
+    tally_b.name = tally_a.name
+    assert tally_a == tally_b
+
+    assert tally_a == tally_b
+    ef_a = openmc.EnergyFilter([0.0, 0.1, 1.0, 10.0e6])
+    ef_b = openmc.EnergyFilter([0.0, 0.1, 1.0, 10.0e6])
+
+    tally_a.filters = [ef_a]
+    assert tally_a != tally_b
+    tally_b.filters = [ef_b]
+    assert tally_a == tally_b
+
+    tally_a.scores = ['flux', 'absorption', 'fission', 'scatter']
+    assert tally_a != tally_b
+    tally_b.scores = ['flux', 'absorption', 'fission', 'scatter']
+    assert tally_a == tally_b
+
+    tally_a.nuclides = []
+    tally_b.nuclides = []
+    assert tally_a == tally_b
+
+    tally_a.nuclides = ['total']
+    assert tally_a == tally_b
+
+    tally_a.estimator = 'collision'
+    assert tally_a != tally_b
+    tally_b.estimator = 'collision'
+    assert tally_a == tally_b
+
+    tally_a.multiply_density = False
+    assert tally_a != tally_b
+    tally_b.multiply_density = False
+    assert tally_a == tally_b
+
+    trigger_a = openmc.Trigger('rel_err', 0.025)
+    trigger_b = openmc.Trigger('rel_err', 0.025)
+
+    tally_a.triggers = [trigger_a]
+    assert tally_a != tally_b
+    tally_b.triggers = [trigger_b]
+    assert tally_a == tally_b
+
 def test_tally_application(run_in_tmpdir):
 
     model = openmc.examples.pwr_pin_cell()
@@ -51,12 +99,12 @@ def test_tally_application(run_in_tmpdir):
     # Create a tally with most possible gizmos
     tally = openmc.Tally()
     tally.name = 'test tally'
-    ef = openmc.EnergyFilter.from_group_structure('CASMO-40')
+    ef = openmc.EnergyFilter([0.0, 0.1, 1.0, 10.0e6])
     bbox = model.geometry.bounding_box
     mesh = openmc.RegularMesh()
     mesh.lower_left = bbox[0][:2]
     mesh.upper_right = bbox[1][:2]
-    mesh.dimension = (10, 10)
+    mesh.dimension = (2, 2)
     mf = openmc.MeshFilter(mesh)
     tally.filters = [ef, mf]
     tally.scores = ['flux', 'absorption', 'fission', 'scatter']
@@ -64,8 +112,8 @@ def test_tally_application(run_in_tmpdir):
 
     # run the simulation and apply retsults
     sp_file = model.run(apply_tally_results=True)
-
     with openmc.StatePoint(sp_file) as sp:
+        assert tally in sp.tallies.values()
         sp_tally = sp.tallies[tally.id]
 
     # at this point the tally information regarding results should be the same

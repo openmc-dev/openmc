a4c375956a7c4317e063dca9fe1bbfcec93d216a
Paul Romano <paul.k.romano@gmail.com>
2024-04-12 17:20:31 -0500

    Add wheel

--- tools/ci/gha-install.sh
+++ tools/ci/gha-install.sh
@@ -36,7 +36,7 @@ fi
 # correct version of MPI
 if [[ $MPI == 'y' ]]; then
     # setuptools 69.4.0 causes problems with mpi4py installation
-    pip install --upgrade "setuptools<69.4.0" build
+    pip install --upgrade "setuptools<69.4.0" build wheel
     pip install --no-build-isolation --no-binary=mpi4py mpi4py
 
     export CC=mpicc

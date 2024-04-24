f543c007a3662226c65a7dd71a6c5e93937c4468
Patrick Shriwise <pshriwise@gmail.com>
2024-04-24 17:09:56 -0500

    Statepoint file loading refactor and CAPI function (#2886)
    Co-authored-by: Paul Romano <paul.k.romano@gmail.com>

--- openmc/lib/core.py
+++ openmc/lib/core.py
@@ -95,6 +95,11 @@ _dll.openmc_simulation_finalize.errcheck = _error_handler
 _dll.openmc_statepoint_write.argtypes = [c_char_p, POINTER(c_bool)]
 _dll.openmc_statepoint_write.restype = c_int
 _dll.openmc_statepoint_write.errcheck = _error_handler
+_dll.openmc_statepoint_load.argtypes = [c_char_p]
+_dll.openmc_statepoint_load.restype = c_int
+_dll.openmc_statepoint_load.errcheck = _error_handler
+_dll.openmc_statepoint_write.restype = c_int
+_dll.openmc_statepoint_write.errcheck = _error_handler
 _dll.openmc_global_bounding_box.argtypes = [POINTER(c_double),
                                             POINTER(c_double)]
 _dll.openmc_global_bounding_box.restype = c_int
@@ -566,6 +571,19 @@ def statepoint_write(filename=None, write_source=True):
     if filename is not None:
         filename = c_char_p(filename.encode())
     _dll.openmc_statepoint_write(filename, c_bool(write_source))
+
+
+def statepoint_load(filename: PathLike):
+    """Load a statepoint file.
+
+    Parameters
+    ----------
+    filename : path-like
+        Path to the statepoint to load.
+
+    """
+    filename = c_char_p(str(filename).encode())
+    _dll.openmc_statepoint_load(filename)
 
 
 @contextmanager

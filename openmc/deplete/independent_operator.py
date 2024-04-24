ff50afb19ffe9cffb5707118a7796b1ba534f280
Gavin Ridley <gavin.keith.ridley@gmail.com>
2024-04-24 17:14:37 -0500

    Allow pure decay IndependentOperator (#2966)
    Co-authored-by: Paul Romano <paul.k.romano@gmail.com>

--- openmc/deplete/independent_operator.py
+++ openmc/deplete/independent_operator.py
@@ -241,7 +241,6 @@ class IndependentOperator(OpenMCOperator):
                    reduce_chain_level=reduce_chain_level,
                    fission_yield_opts=fission_yield_opts)
 
-
     @staticmethod
     def _consolidate_nuclides_to_material(nuclides, nuc_units, volume):
         """Puts nuclide list into an openmc.Materials object.
@@ -270,7 +269,6 @@ class IndependentOperator(OpenMCOperator):
         self.prev_res[-1].transfer_volumes(model)
         self.materials = model.materials
 
-
         # Store previous results in operator
         # Distribute reaction rates according to those tracked
         # on this process
@@ -281,7 +279,6 @@ class IndependentOperator(OpenMCOperator):
             for res_obj in prev_results:
                 new_res = res_obj.distribute(self.local_mats, mat_indexes)
                 self.prev_res.append(new_res)
-
 
     def _get_nuclides_with_data(self, cross_sections: List[MicroXS]) -> Set[str]:
         """Finds nuclides with cross section data"""
@@ -411,6 +408,12 @@ class IndependentOperator(OpenMCOperator):
         """
 
         self._update_materials_and_nuclides(vec)
+
+        # If the source rate is zero, return zero reaction rates
+        if source_rate == 0.0:
+            rates = self.reaction_rates.copy()
+            rates.fill(0.0)
+            return OperatorResult(ufloat(0.0, 0.0), rates)
 
         rates = self._calculate_reaction_rates(source_rate)
         keff = self._keff

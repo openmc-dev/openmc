4ca3996b26334e208e33a68953159d287636d2a9
Paul Romano <paul.k.romano@gmail.com>
2024-05-07 09:12:04 -0500

    Remove superfluous check in transport_history_based_single_particle

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -761,8 +761,6 @@ void transport_history_based_single_particle(Particle& p)
       p.event_collide();
     }
     p.event_revive_from_secondary();
-    if (!p.alive())
-      break;
   }
   p.event_death();
 }

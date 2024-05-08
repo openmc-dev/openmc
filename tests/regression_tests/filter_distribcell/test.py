27394adec0138083ca51963be2651919de7d21df
Patrick Shriwise <pshriwise@gmail.com>
2024-05-08 16:23:43 -0500

    Adding test for using outer universe of lattice in a distribcell tally

--- tests/regression_tests/filter_distribcell/test.py
+++ tests/regression_tests/filter_distribcell/test.py
@@ -1,60 +1,56 @@
 import glob
 import os
+from pathlib import Path
+
+import pytest
 
 from tests.testing_harness import *
 
 
 class DistribcellTestHarness(TestHarness):
-    def __init__(self):
-        super().__init__(None)
+    def __init__(self, statepoint_name, dir, tallies_out, hash_out):
+        self.dir = dir
+        self.tallies_out = tallies_out
+        self.hash_out = hash_out
+        super().__init__(statepoint_name)
 
     def execute_test(self):
         """Run OpenMC with the appropriate arguments and check the outputs."""
-        base_dir = os.getcwd()
         try:
             dirs = ('case-1', '../case-2', '../case-3', '../case-4')
             sps = ('statepoint.1.*', 'statepoint.1.*', 'statepoint.3.*',
                    'statepoint.1.*')
             tallies_out_present = (True, True, False, True)
             hash_out = (False, False, True, False)
-            for i in range(len(dirs)):
-                os.chdir(dirs[i])
-                self._sp_name = sps[i]
 
-                self._run_openmc()
-                self._test_output_created(tallies_out_present[i])
-                results = self._get_results(hash_out[i])
-                self._write_results(results)
-                self._compare_results()
+            os.chdir(self.dir)
+            self._run_openmc()
+            self._test_output_created(self.tallies_out)
+            results = self._get_results(self.hash_out)
+            self._write_results(results)
+            self._compare_results()
         finally:
-            os.chdir(base_dir)
-            for i in range(len(dirs)):
-                os.chdir(dirs[i])
-                self._cleanup()
+            os.chdir(self.dir)
+            self._cleanup()
 
     def update_results(self):
         """Update the results_true using the current version of OpenMC."""
-        base_dir = os.getcwd()
         try:
-            dirs = ('case-1', '../case-2', '../case-3', '../case-4')
+            dirs = ('case-1', '../case-2', '../case-3', '../case-4', '../case-5')
             sps = ('statepoint.1.h5', 'statepoint.1.h5', 'statepoint.3.h5',
-                   'statepoint.1.h5')
-            tallies_out_present = (True, True, False, True)
-            hash_out = (False, False, True, False)
-            for i in range(len(dirs)):
-                os.chdir(dirs[i])
-                self._sp_name = sps[i]
+                   'statepoint.1.h5', 'statepoint.1.h5')
+            tallies_out_present = (True, True, False, True, True)
+            hash_out = (False, False, True, False, False)
 
-                self._run_openmc()
-                self._test_output_created(tallies_out_present[i])
-                results = self._get_results(hash_out[i])
-                self._write_results(results)
-                self._overwrite_results()
+            os.chdir(self.dir)
+            self._run_openmc()
+            self._test_output_created(self.tallies_out)
+            results = self._get_results(self.hash_out)
+            self._write_results(results)
+            self._overwrite_results()
         finally:
-            os.chdir(base_dir)
-            for i in range(len(dirs)):
-                os.chdir(dirs[i])
-                self._cleanup()
+            os.chdir(self.dir)
+            self._cleanup()
 
     def _test_output_created(self, tallies_out_present):
         """Make sure statepoint.* and tallies.out have been created."""
@@ -68,6 +64,16 @@ class DistribcellTestHarness(TestHarness):
                  'Tally output file does not exist.'
 
 
-def test_filter_distribcell():
-    harness = DistribcellTestHarness()
+cases = [
+    ('case-1', 'statepoint.1.h5', True, False),
+    ('case-2', 'statepoint.1.h5', True, False),
+    ('case-3', 'statepoint.3.h5', False, True),
+    ('case-4', 'statepoint.1.h5', True, False),
+    ('case-5', 'statepoint.1.h5', True, False)
+]
+@pytest.mark.parametrize(','.join(['dir', 'statepoint', 'tallies_out', 'hash_out']), cases,
+                         ids=[cases[0] for cases in cases])
+def test_filter_distribcell(request, dir, statepoint, tallies_out, hash_out):
+    dir = request.fspath.dirname / Path(dir)
+    harness = DistribcellTestHarness(statepoint, dir.resolve(), tallies_out, hash_out)
     harness.main()

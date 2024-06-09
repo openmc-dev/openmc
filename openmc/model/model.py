4a8814378966894c4faef137530322a3b6922af4
Yuvraj Jain <20016119+nutcasev15@users.noreply.github.com>
2024-06-09 12:51:04 +0100

    [CAPI] Implement Accelerated Reactivity Coefficient and Keff Search Calculations

--- openmc/model/model.py
+++ openmc/model/model.py
@@ -716,6 +716,229 @@ class Model:
                     last_statepoint = sp
         return last_statepoint
 
+    def search_for_keff(self, model_updater, updater_static_args=None,
+                        initial_guess=None, target=1.0, bracket=None,
+                        tol=None, bracketed_method='bisect',
+                        print_iterations=False, init_args=None,
+                        started_initialized=False, run_args=None, **kwargs):
+        """Perform an accelerated keff search by modifying the model in
+        memory using the C API.
+
+        .. versionadded:: 0.14.1
+
+        Parameters
+        ----------
+        model_updater : collections.Callable
+            Callable function which updates an `openmc.Model` object passed
+            to it using a single numerical input variable. Cell temperature, rotation
+            and translation updates are allowed, as well as material density and
+            composition updates.
+        updater_static_args : dict, optional
+            Keyword-based arguments to pass to the `model_updater` method
+            which do not change during the reactivity calculation. Defaults
+            to no arguments.
+        initial_guess : Real, optional
+            Initial guess for the parameter to be searched in
+            `model_updater`. One of `guess` or `bracket` must be provided.
+        target : Real, optional
+            keff value to search for, defaults to 1.0.
+        bracket : None or Iterable of Real, optional
+            Bracketing interval to search for the solution; if not provided,
+            a generic non-bracketing method is used. If provided, the brackets
+            are used. Defaults to no brackets provided. One of `guess` or `bracket`
+            must be provided. If both are provided, the bracket will be
+            preferentially used.
+        tol : float
+            Tolerance to pass to the search method
+        bracketed_method : {'brentq', 'brenth', 'ridder', 'bisect'}, optional
+            Solution method to use; only applies if
+            `bracket` is set, otherwise the Secant method is used.
+            Defaults to 'bisect'.
+        print_iterations : bool
+            Whether or not to print the guess and the result during the iteration
+            process. Defaults to False.
+        init_args : dict, optional
+            Keyword arguments to pass to :meth:`openmc.Model.init_lib`. Defaults
+            to no arguments.
+        started_initialized : bool
+            Whether or not to initialize and finalize the C API interface. Defaults
+            to False, which assumes the C API interface was previously inactive.
+        run_args : dict, optional
+            Keyword arguments to pass to :meth:`openmc.Model.run`. Defaults to no
+            arguments.
+        **kwargs
+            All remaining keyword arguments are passed to the root-finding
+            method.
+
+        Returns
+        -------
+        zero_value : float
+            Estimated value of the variable parameter where keff is the
+            targeted value
+        guesses : List of Real
+            List of guesses attempted by the search
+        results : List of 2-tuple of Real
+            List of keffs and uncertainties corresponding to the guess attempted by
+            the search
+
+        """
+
+        if updater_static_args is None:
+            updater_static_args = {}
+        else:
+            check_type('updater_static_args', updater_static_args, dict)
+        if init_args is None:
+            init_args = {}
+        else:
+            check_type('init_args', init_args, dict)
+
+        # Initialize C API and export initial model xml file
+        if not started_initialized:
+            self.init_lib(**init_args)
+
+        # Define dummy function that updates the model and
+        # returns a copy. This function allows reuse of the
+        # openmc.search_for_keff method.
+        def wrapper(guess):
+            model_updater(self, guess, **updater_static_args)
+            return self
+
+        # Call openmc.search_for_keff with dummy function
+        # and pass all other parameters explicitly.
+        zero_value, guesses, results = openmc.search_for_keff(
+            model_builder=wrapper,
+            initial_guess=initial_guess, target=target,
+            bracket=bracket, model_args=None, tol=tol,
+            bracketed_method=bracketed_method,
+            print_iterations=print_iterations,
+            run_args=run_args, **kwargs)
+
+        # Deallocate resources
+        if not started_initialized:
+            self.finalize_lib()
+
+        return zero_value, guesses, results
+
+    def calculate_reactivity_coeffs(self, param_list,
+                                   model_updater, updater_static_args=None,
+                                   print_output=True, init_args=None,
+                                   started_initialized=False, **kwargs):
+        """Perform an accelerated reactivity calculation by modifying the
+        model in memory using the C API.
+
+        .. versionadded:: 0.14.1
+
+        Parameters
+        ----------
+        param_list : collections.Iterable
+            Iterable of floats to pass to the `model_updater` method
+            that serves as the indepedent variable during the reactivity
+            calculation.
+        model_updater : collections.Callable
+            Callable function which updates an `openmc.Model` object passed
+            to it using a single numerical input variable. Cell temperature,
+            rotation and translation updates are allowed,
+            as well as material density and composition updates.
+        updater_static_args : dict, optional
+            Keyword-based arguments to pass to the `model_updater` method
+            which do not change during the reactivity calculation. Defaults
+            to no arguments.
+        print_output : bool
+            Whether or not to output the results during the calculation
+            process. Defaults to True.
+        init_args : dict, optional
+            Keyword arguments to pass to :meth:`openmc.Model.init_lib`. Defaults
+            to no arguments.
+        started_initialized : bool
+            Whether or not to initialize and finalize the C API interface. Defaults
+            to False, which assumes the C API interface was previously inactive.
+        **kwargs
+            All remaining keyword arguments are passed to :meth:`openmc.Model.run`
+
+        Returns
+        -------
+        values : List of float
+            Numerical outputs of the `model_updater` method. Defaults to a
+            duplicate of the param_list input variable if the method returns
+            None.
+        coefficients : List of float
+            List of the calculated reactivity coefficients
+
+        """
+
+        check_iterable_type('param_list', param_list, float)
+
+        if updater_static_args is None:
+            updater_static_args = {}
+        else:
+            check_type('updater_static_args', updater_static_args, dict)
+        check_type('model_updater', model_updater, Callable)
+        check_type('model_updater',
+                   model_updater(self, param_list[0], **updater_static_args),
+                   float, none_ok=True)
+
+        if init_args is None:
+            init_args = {}
+        else:
+            check_type('init_args', init_args, dict)
+
+        check_type('print_output', print_output, bool)
+
+        # Check the return of model_updater method. If there is output,
+        # we need to save the output to return it to the user.
+        if model_updater(self, param_list[0], **updater_static_args) is None:
+            duplicate_input = True
+            values = param_list
+        else:
+            duplicate_input = False
+            values = []
+
+        # Define container for the calculated data
+        coefficients = []
+
+        # Initialize C API and export initial model xml file
+        if not started_initialized:
+            self.init_lib(**init_args)
+
+        # Update model and calculate raw keff for each input value
+        for param in param_list:
+            # Update model and record the input corresponding
+            # to the keff simulation.
+            if duplicate_input is True:
+                input = param
+                model_updater(self, input, **updater_static_args)
+            else:
+                input = model_updater(self, param, **updater_static_args)
+            values.append(input)
+
+            # Run using C API in memory and save raw simulation output
+            sp_filepath = self.run(**kwargs)
+            with openmc.StatePoint(sp_filepath) as sp:
+                raw_output = sp.keff
+                if print_output:
+                    text = 'Input Variable : {:.3f}; Model produced \
+                        a keff of {:1.5f} +/- {:1.5f}'
+                    print(text.format(input,  raw_output.n, raw_output.s))
+                coefficients.append(raw_output)
+
+        # Forward difference raw keff values and calculate reactivity
+        # coefficients.
+        for i in range(0, len(values) - 1):
+            coeff = coefficients[i + 1] - coefficients[i]
+            coeff /= coefficients[i + 1] * coefficients[i]
+            coeff /= values[i + 1] - values[i]
+            coefficients[i] = coeff
+
+        # Discard last data point due to forward differencing
+        del values[-1]
+        del coefficients[-1]
+
+        # Deallocate resources
+        if not started_initialized:
+            self.finalize_lib()
+
+        return values, coefficients
+
     def calculate_volumes(self, threads=None, output=True, cwd='.',
                           openmc_exec='openmc', mpi_args=None,
                           apply_volumes=True):

2ce6736b2779298bc87f35cc91027b7821ae5888
John Tramm <john.tramm@gmail.com>
2024-06-11 20:58:50 -0500

    removed comment

--- tests/regression_tests/random_ray_fixed_source_cell/test.py
+++ tests/regression_tests/random_ray_fixed_source_cell/test.py
@@ -257,7 +257,7 @@ def create_random_ray_model():
     settings.random_ray['ray_source']= openmc.IndependentSource(space=uniform_dist)
     
     # Create the neutron source in the bottom right of the moderator
-    strengths = [1.0] # Good - fast group appears largest (besides most thermal)
+    strengths = [1.0]
     midpoints = [100.0]
     energy_distribution = openmc.stats.Discrete(x=midpoints,p=strengths)
     source = openmc.IndependentSource(energy=energy_distribution, constraints={'domains':[source_lattice_cell]}, strength=1.0)

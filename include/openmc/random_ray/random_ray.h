ebf7971345def41b0fe4bd7677734275bbb71085
John Tramm <john.tramm@gmail.com>
2024-07-04 18:10:57 +0000

    ran llvm 15 format

--- include/openmc/random_ray/random_ray.h
+++ include/openmc/random_ray/random_ray.h
@@ -34,9 +34,9 @@ public:
 
   //----------------------------------------------------------------------------
   // Static data members
-  static double distance_inactive_;      // Inactive (dead zone) ray length
-  static double distance_active_;        // Active ray length
-  static unique_ptr<Source> ray_source_; // Starting source for ray sampling
+  static double distance_inactive_;          // Inactive (dead zone) ray length
+  static double distance_active_;            // Active ray length
+  static unique_ptr<Source> ray_source_;     // Starting source for ray sampling
   static RandomRaySourceShape source_shape_; // Flag for linear source
 
   //----------------------------------------------------------------------------

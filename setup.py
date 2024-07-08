8f6e77c2fa37056270a7a05ae84873748753a900
Jonathan Shimwell <drshimwell@gmail.com>
2024-07-08 07:07:55 +0000

    adding openmc.lib

--- setup.py
+++ setup.py
@@ -8,12 +8,6 @@ from setuptools import setup, find_packages
 from Cython.Build import cythonize
 
 
-# Determine shared library suffix
-if sys.platform == 'darwin':
-    suffix = 'dylib'
-else:
-    suffix = 'so'
-
 kwargs = {
     # Cython is used to add resonance reconstruction and fast float_endf
     'ext_modules': cythonize('openmc/data/*.pyx'),

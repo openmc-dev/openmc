1524cf74bbfb83f5ec8545db68a784ebf0ea1ef3
John Vincent Cauilan <64677361+johvincau@users.noreply.github.com>
2024-07-16 13:46:44 -0500

    Test NJOY install script refactor for specific install location

--- tools/ci/gha-install-njoy.sh
+++ tools/ci/gha-install-njoy.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 set -ex
+
+# NJOY variables
+NJOY_BRANCH='main'
+NJOY_REPO='https://github.com/njoy/NJOY2016'
+NJOY_INSTALL_DIR=$HOME/NJOY2016/
+
 cd $HOME
-git clone https://github.com/njoy/NJOY2016
-cd NJOY2016
+git clone -b $NJOY_BRANCH $NJOY_REPO
+cd $NJOY_INSTALL_DIR
 mkdir build && cd build
-cmake -Dstatic=on .. && make 2>/dev/null && sudo make install
+cmake -Dstatic=on -DCMAKE_INSTALL_PREFIX=$NJOY_INSTALL_DIR .. 
+make 2>/dev/null && sudo make install

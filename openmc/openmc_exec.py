61b7035681230cb0c94e8b819584ee104873faed
Jon Shimwell <jon@proximafusion.com>
2024-07-19 10:02:52 +0100

    linking libopenmc with openmc in cmakelists

--- openmc/openmc_exec.py
+++ openmc/openmc_exec.py
@@ -9,10 +9,6 @@ from pathlib import Path
 
 
 def main():
-    lib_path = sysconfig.get_path("platlib") + "openmc/lib/openmc"
-    os.environ["LD_LIBRARY_PATH"] = (
-        lib_path + ":" + os.environ.get("LD_LIBRARY_PATH", "")
-    )
     os.execv(
         Path(sysconfig.get_path("platlib")) / "openmc" / "bin" / "openmc", sys.argv
     )

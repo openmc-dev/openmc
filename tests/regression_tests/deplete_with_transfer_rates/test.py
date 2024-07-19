bd07bbbe499d6bad02863d64ffe780dea7170098
church89 <l.chierici89@gmail.com>
2024-07-19 13:57:55 +0200

    add external source rates tests to regression

--- tests/regression_tests/deplete_with_transfer_rates/test.py
+++ tests/regression_tests/deplete_with_transfer_rates/test.py
@@ -1,4 +1,4 @@
-""" TransferRates depletion test suite """
+""" ExternalRates depletion test suite """
 
 from pathlib import Path
 import shutil
@@ -43,6 +43,7 @@ def model():
     settings.particles = 100
     settings.inactive = 0
     settings.batches = 10
+    settings.seed = 1
 
     return openmc.Model(geometry, materials, settings)
 
@@ -67,6 +68,40 @@ def test_transfer_rates(run_in_tmpdir, model, rate, dest_mat, power, ref_result)
         op, [1], power, timestep_units = 'd')
     integrator.add_transfer_rate('f', transfer_elements, rate,
                                 destination_material=dest_mat)
+    integrator.integrate()
+
+    # Get path to test and reference results
+    path_test = op.output_dir / 'depletion_results.h5'
+    path_reference = Path(__file__).with_name(f'ref_{ref_result}.h5')
+
+    # If updating results, do so and return
+    if config['update']:
+        shutil.copyfile(str(path_test), str(path_reference))
+        return
+
+    # Load the reference/test results
+    res_ref = openmc.deplete.Results(path_reference)
+    res_test = openmc.deplete.Results(path_test)
+
+    assert_atoms_equal(res_ref, res_test, 1e-6)
+    assert_reaction_rates_equal(res_ref, res_test)
+
+@pytest.mark.parametrize("rate, power, ref_result", [
+    (1e-1, 0.0, 'no_depletion_with_ext_source'),
+    (1e-1, 174., 'depletion_with_ext_source'),
+    ])
+def test_external_source_rates(run_in_tmpdir, model, rate, power, ref_result):
+    """Tests external_rates depletion class with external source rates"""
+
+    chain_file = Path(__file__).parents[2] / 'chain_simple.xml'
+
+    external_source_vector = {'U':1}
+
+    op = CoupledOperator(model, chain_file)
+    op.round_number = True
+    integrator = openmc.deplete.PredictorIntegrator(
+        op, [1], power, timestep_units = 'd')
+    integrator.add_external_source_rate('f', external_source_vector, rate)
     integrator.integrate()
 
     # Get path to test and reference results

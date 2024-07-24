ac060420c42921fee19aaabcc73ecde575cfefb6
John Vincent Cauilan <64677361+johvincau@users.noreply.github.com>
2024-07-24 11:49:54 -0500

    Refactor mcpl install script

--- tools/ci/gha-install-mcpl.sh
+++ tools/ci/gha-install-mcpl.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -ex
+
+MCPL_BRANCH='main'
+MCPL_REPO='https://github.com/mctools/mcpl'
+MCPL_INSTALL_DIR=$HOME/mcpl/
+
 cd $HOME
-git clone https://github.com/mctools/mcpl
-cd mcpl
+git clone -b $MCPL_BRANCH $MCPL_REPO
+cd $MCPL_INSTALL_DIR
 mkdir build && cd build
-cmake .. && make 2>/dev/null && sudo make install
+cmake .. -DCMAKE_INSTALL_PREFIX=$MCPL_INSTALL_DIR
+make 2>/dev/null && sudo make install

d5f157559c8ebf411df32ff47034da3cf1da8345
Tomas P <tpaganin@gmail.com>
2024-07-24 16:11:42 -0500

    tbd_2

--- src/settings.cpp
+++ src/settings.cpp
@@ -134,7 +134,7 @@ int verbosity {7};
 double weight_cutoff {0.25};
 double weight_survive {1.0};
 // HARDCODED INPUTS - First Collided Flux
-bool FIRST_COLLIDED_FLUX {true};
+bool FIRST_COLLIDED_FLUX {false};
 int n_uncollided_rays {10000};
 int n_volume_estimator_rays {10000};
 bool first_collided_mode {false};

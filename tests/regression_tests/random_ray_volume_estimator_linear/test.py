4fb9296715a2c2c493088be9825b753bf8af7649
John Tramm <john.tramm@gmail.com>
2024-08-06 11:32:15 -0500

    added regression tests for linear source + alternative volume estimators

--- tests/regression_tests/random_ray_volume_estimator_linear/test.py
+++ tests/regression_tests/random_ray_volume_estimator_linear/test.py
@@ -1,6 +1,5 @@
 import os
 
-import numpy as np
 import openmc
 from openmc.utility_funcs import change_directory
 from openmc.examples import random_ray_three_region_cube
@@ -17,12 +16,16 @@ class MGXSTestHarness(TolerantPyAPITestHarness):
             os.remove(f)
 
 
-@pytest.mark.parametrize("shape", ["linear", "linear_xy"])
-def test_random_ray_fixed_source_linear(shape):
-    with change_directory(shape):
+@pytest.mark.parametrize("estimator", ["hybrid",
+                                       "simulation_averaged",
+                                       "naive"
+                                       ])
+def test_random_ray_volume_estimator_linear(estimator):
+    with change_directory(estimator):
         openmc.reset_auto_ids()
         model = random_ray_three_region_cube()
-        model.settings.random_ray['source_shape'] = shape
+        model.settings.random_ray['source_shape'] = 'linear'
+        model.settings.random_ray['volume_estimator'] = estimator
         model.settings.inactive = 20
         model.settings.batches = 40
         harness = MGXSTestHarness('statepoint.40.h5', model)

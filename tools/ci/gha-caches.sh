36d0dedd07a898bd71fe2f9b1f810176cf017528
John Vincent Cauilan <64677361+johvincau@users.noreply.github.com>
2024-08-14 10:20:25 -0500

    Improve gha-caches.sh hash commands

--- tools/ci/gha-caches.sh
+++ tools/ci/gha-caches.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 set -ex
 
-echo "NJOY_HASH=$(git ls-remote https://github.com/njoy/NJOY2016.git | cut -f 1 |\
+echo "NJOY_HASH=$(git ls-remote https://github.com/njoy/NJOY2016.git HEAD |\
     head -c 15)" >> $GITHUB_ENV
 
 if [[ $DAGMC = 'y' ]]; then
     echo "MOAB_HASH=$(git ls-remote https://bitbucket.org/fathomteam/moab.git \
-    --branch Version5.1.0 | cut -f 1 | head -c 15)" >> $GITHUB_ENV
+    --branch Version5.1.0 HEAD | head -c 15)" >> $GITHUB_ENV
 
-    echo "DAGMC_HASH=$(git ls-remote https://github.com/svalinn/dagmc.git |\
-    cut -f 1 | head -c 15)" >> $GITHUB_ENV
+    echo "DAGMC_HASH=$(git ls-remote https://github.com/svalinn/dagmc.git HEAD |\
+    head -c 15)" >> $GITHUB_ENV
 fi
 
 if [[ $NCRYSTAL = 'y' ]]; then
     echo "NC_HASH=$(git ls-remote https://github.com/mctools/ncrystal \
-    --branch develop | cut -f 1 | head -c 15)" >> $GITHUB_ENV
+    --branch develop HEAD | head -c 15)" >> $GITHUB_ENV
 fi
 
 if [[ $VECTFIT = 'y' ]]; then
     echo "PYBIND_HASH=$(git ls-remote https://github.com/pybind/pybind11 \
-    --branch master | cut -f 1 | head -c 15)" >> $GITHUB_ENV
+    --branch master HEAD | head -c 15)" >> $GITHUB_ENV
 
-    echo "VF_HASH=$(git ls-remote https://github.com/liangjg/vectfit.git |\
-    cut -f 1 | head -c 15)" >> $GITHUB_ENV
+    echo "VF_HASH=$(git ls-remote https://github.com/liangjg/vectfit.git HEAD |\
+    head -c 15)" >> $GITHUB_ENV
 fi
 
 if [[ $LIBMESH = 'y' ]]; then
     echo "NC_HASH=$(git ls-remote https://github.com/libmesh/libmesh \
-    --branch v1.7.1 | cut -f 1 | head -c 15)" >> $GITHUB_ENV
+    --branch v1.7.1 HEAD | head -c 15)" >> $GITHUB_ENV
 fi

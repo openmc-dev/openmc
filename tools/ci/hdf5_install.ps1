61b716483d0037afc07db82562f6de554b769a70
Hunter Belanger <hunter.belanger@gmail.com>
2024-08-18 18:31:16 -0400

    More annoying Windows fixes

--- tools/ci/hdf5_install.ps1
+++ tools/ci/hdf5_install.ps1
@@ -3,7 +3,7 @@ git clone https://github.com/microsoft/vcpkg.git "$Env:USERPROFILE\vcpkg"
 cd "$Env:USERPROFILE\vcpkg"
 .\bootstrap-vcpkg.bat
 
-$Env:VCPKG_ROOT = "$Env:USERPROFILE\vcpkg"
+[Environment]::SetEnvironmentVariable("VCPKG_ROOT", "$Env:USERPROFILE\vcpkg", [System.EnvironmentVariableTarget]::User)
 $Env:Path += ";$Env:VCPKG_ROOT"
 
 vcpkg install hdf5:x64-windows-static

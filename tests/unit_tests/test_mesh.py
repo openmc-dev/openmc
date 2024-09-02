0fcddbffaa5319ab38791d9c824201a81b8fbcaa
Paul Romano <paul.k.romano@gmail.com>
2024-09-02 12:48:11 -0500

    Add test for material_volumes

--- tests/unit_tests/test_mesh.py
+++ tests/unit_tests/test_mesh.py
@@ -402,7 +402,7 @@ def test_mesh_get_homogenized_materials():
     mesh.lower_left = (-1., -1., -1.)
     mesh.upper_right = (1., 1., 1.)
     mesh.dimension = (3, 1, 1)
-    m1, m2, m3 = mesh.get_homogenized_materials(model, n_samples=1_000_000)
+    m1, m2, m3 = mesh.get_homogenized_materials(model, n_samples=10_000)
 
     # Left mesh element should be only Fe56
     assert m1.get_mass_density('Fe56') == pytest.approx(5.0)
@@ -418,7 +418,7 @@ def test_mesh_get_homogenized_materials():
     mesh_void.lower_left = (0.5, 0.5, -1.)
     mesh_void.upper_right = (1.5, 1.5, 1.)
     mesh_void.dimension = (1, 1, 1)
-    m4, = mesh_void.get_homogenized_materials(model, n_samples=1_000_000)
+    m4, = mesh_void.get_homogenized_materials(model, n_samples=10_000)
 
     # Mesh element that overlaps void should have half density
     assert m4.get_mass_density('H1') == pytest.approx(0.5, rel=1e-2)
@@ -428,3 +428,51 @@ def test_mesh_get_homogenized_materials():
     m5, = mesh_void.get_homogenized_materials(
         model, n_samples=1000, include_void=False)
     assert m5.get_mass_density('H1') == pytest.approx(1.0)
+
+
+def test_mesh_material_volumes():
+    """Test the material_volumes method"""
+    # Model with three materials separated by planes x=0 and z=0
+    mats = []
+    for i in range(3):
+        mat = openmc.Material()
+        mat.add_nuclide('H1', 1.0)
+        mat.set_density('g/cm3', float(i + 1))
+        mats.append(mat)
+
+    sph = openmc.Sphere(r=25.0, boundary_type='vacuum')
+    x0 = openmc.XPlane(0.0)
+    z0 = openmc.ZPlane(0.0)
+    cell1 = openmc.Cell(fill=mats[0], region=-sph & +x0 & +z0)
+    cell2 = openmc.Cell(fill=mats[1], region=-sph & -x0 & +z0)
+    cell3 = openmc.Cell(fill=mats[2], region=-sph & -z0)
+    model = openmc.Model()
+    model.geometry = openmc.Geometry([cell1, cell2, cell3])
+
+    # Test material volumes on a regular mesh
+    mesh = openmc.RegularMesh()
+    mesh.lower_left = (-1., -1., -1.)
+    mesh.upper_right = (1., 1., 1.)
+    mesh.dimension = (2, 2, 2)
+    volumes = mesh.material_volumes(model)
+    np.testing.assert_almost_equal(volumes[mats[0].id], [0., 0., 0., 0., 0., 1., 0., 1.])
+    np.testing.assert_almost_equal(volumes[mats[1].id], [0., 0., 0., 0., 1., 0., 1., 0.])
+    np.testing.assert_almost_equal(volumes[mats[2].id], [1., 1., 1., 1., 0., 0., 0., 0.])
+    assert volumes.by_element(4) == [(mats[1].id, 1.)]
+    assert volumes.by_element(0) == [(mats[2].id, 1.)]
+
+    # Test material volumes on a cylindrical mesh
+    cyl_mesh = openmc.CylindricalMesh([0., 1.], [-1., 0., 1.,], [0.0, pi/4, 3*pi/4, 5*pi/4, 7*pi/4, 2*pi])
+    volumes = cyl_mesh.material_volumes(model)
+    np.testing.assert_almost_equal(volumes[mats[0].id], [
+        0., 0., 0., 0., 0.,
+        pi/8, pi/8, 0., pi/8, pi/8
+    ])
+    np.testing.assert_almost_equal(volumes[mats[1].id], [
+        0., 0., 0., 0., 0.,
+        0., pi/8, pi/4, pi/8, 0.
+    ])
+    np.testing.assert_almost_equal(volumes[mats[2].id], [
+        pi/8, pi/4, pi/4, pi/4, pi/8,
+        0., 0., 0., 0., 0.
+    ])

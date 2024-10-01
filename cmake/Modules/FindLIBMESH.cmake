2e1a06573c66901f56060baee766d8e0695d7b01
Ahnaf Tahmid Chowdhury <ahnaf-tahmid@outlook.com>
2024-10-01 16:17:04 +0600

    remove libmoab root

--- cmake/Modules/FindLIBMESH.cmake
+++ cmake/Modules/FindLIBMESH.cmake
@@ -15,7 +15,23 @@ if(DEFINED ENV{METHOD})
 endif()
 
 find_package(PkgConfig REQUIRED)
-set(ENV{PKG_CONFIG_PATH} "$ENV{PKG_CONFIG_PATH}:${LIBMESH_PC}")
-set(PKG_CONFIG_USE_CMAKE_PREFIX_PATH True)
+
+# Set hints to search for libmesh.pc in CMAKE_PREFIX_PATH
+find_path(PKGCONFIG_DIR
+    NAMES ${LIBMESH_PC_FILE}.pc
+    HINTS ${CMAKE_PREFIX_PATH}
+    PATH_SUFFIXES lib/pkgconfig lib64/pkgconfig lib/x86_64-linux-gnu/pkgconfig share/pkgconfig
+    NO_DEFAULT_PATH
+    DOC "The path to libMesh pkgconfig directory"
+)
+
+if(PKGCONFIG_DIR)
+    message(STATUS "Found ${LIBMESH_PC_FILE}.pc in: ${PKGCONFIG_DIR}")
+    set(ENV{PKG_CONFIG_PATH} "${PKGCONFIG_DIR}:$ENV{PKG_CONFIG_PATH}")
+else()
+    message(FATAL_ERROR "Could not find ${LIBMESH_PC_FILE}.pc in CMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}")
+endif()
+
+set(PKG_CONFIG_USE_CMAKE_PREFIX_PATH TRUE)
 pkg_check_modules(LIBMESH REQUIRED ${LIBMESH_PC_FILE}>=1.7.0 IMPORTED_TARGET)
 pkg_get_variable(LIBMESH_PREFIX ${LIBMESH_PC_FILE} prefix)

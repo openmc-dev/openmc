05a4fa0fc86769610d77332f64b276b887223bfe
Ahnaf Tahmid Chowdhury <ahnaf-tahmid@outlook.com>
2024-10-10 10:34:21 +0600

    remove find path and see if pkg_check_modules can handle itself

--- cmake/Modules/FindLIBMESH.cmake
+++ cmake/Modules/FindLIBMESH.cmake
@@ -16,22 +16,6 @@ endif()
 
 find_package(PkgConfig REQUIRED)
 
-# Set hints to search for libmesh.pc in CMAKE_PREFIX_PATH
-find_path(PKGCONFIG_DIR
-    NAMES ${LIBMESH_PC_FILE}.pc
-    HINTS ${CMAKE_PREFIX_PATH}
-    PATH_SUFFIXES lib/pkgconfig lib64/pkgconfig lib/x86_64-linux-gnu/pkgconfig share/pkgconfig
-    NO_DEFAULT_PATH
-    DOC "The path to libMesh pkgconfig directory"
-)
-
-if(PKGCONFIG_DIR)
-    message(STATUS "Found ${LIBMESH_PC_FILE}.pc in: ${PKGCONFIG_DIR}")
-    set(ENV{PKG_CONFIG_PATH} "${PKGCONFIG_DIR}:$ENV{PKG_CONFIG_PATH}")
-else()
-    message(FATAL_ERROR "Could not find ${LIBMESH_PC_FILE}.pc in CMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}")
-endif()
-
 set(PKG_CONFIG_USE_CMAKE_PREFIX_PATH TRUE)
 pkg_check_modules(LIBMESH REQUIRED ${LIBMESH_PC_FILE}>=1.7.0 IMPORTED_TARGET)
 pkg_get_variable(LIBMESH_PREFIX ${LIBMESH_PC_FILE} prefix)

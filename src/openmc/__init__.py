fbda53058e92c22ce7fd307cad98a30c7e5685e8
Ahnaf Tahmid Chowdhury <ahnaf-tahmid@outlook.com>
2024-10-18 00:50:00 +0600

    update lib paths

--- src/openmc/__init__.py
+++ src/openmc/__init__.py
@@ -1,3 +1,5 @@
+import os
+import glob
 import importlib.metadata
 from openmc.arithmetic import *
 from openmc.bounding_box import *
@@ -43,3 +45,31 @@ from . import examples
 
 
 __version__ = importlib.metadata.version("openmc")
+
+def get_path(subdir, pattern=""):
+    """Helper function to return paths that match a given pattern within a subdirectory."""
+    path = os.path.join(__path__[0], "core", subdir)
+    return glob.glob(os.path.join(path, pattern)) if os.path.exists(path) else []
+
+def get_include_path():
+    """Return the include directory path for OpenMC headers."""
+    return get_path("include")
+
+def get_core_libraries():
+    """Return library paths and library directory paths."""
+    lib_paths = [libs for lib in ["lib", "lib64"] for libs in get_path(lib)]
+    lib = [libs for lib in ["lib", "lib64"] for libs in get_path(lib, pattern="*openmc*")]
+    return lib, lib_paths
+
+def get_extra_libraries():
+    """List all the extra libraries of OpenMC."""
+    libs_path = os.path.join(__path__[0], ".dylibs") if sys.platform == "darwin" else os.path.join(__path__[0], "..", "pyne.libs")
+    return (glob.glob(os.path.join(libs_path, "*")), libs_path) if os.path.exists(libs_path) else ([], [])
+
+# Setup variables
+include_path = get_include_path()
+lib, lib_path = get_core_libraries()
+extra_lib, extra_lib_path = get_extra_libraries()
+
+# Export variables for easy access
+__all__ = ["include_path", "lib", "lib_path", "extra_lib", "extra_lib_path"]

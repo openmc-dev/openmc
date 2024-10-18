0d3f6f20f632214841c23111b39a317b761eb0ec
Ahnaf Tahmid Chowdhury <ahnaf-tahmid@outlook.com>
2024-10-18 20:53:02 +0600

    add include

--- src/openmc/__init__.py
+++ src/openmc/__init__.py
@@ -46,30 +46,32 @@ from . import examples
 
 __version__ = importlib.metadata.version("openmc")
 
-def get_path(subdir, pattern=""):
+def get_core_path(subdir, pattern="*"):
     """Helper function to return paths that match a given pattern within a subdirectory."""
     path = os.path.join(__path__[0], "core", subdir)
     return glob.glob(os.path.join(path, pattern)) if os.path.exists(path) else []
 
 def get_include_path():
     """Return the include directory path for OpenMC headers."""
-    return get_path("include")
+    include = get_core_path("include")
+    include_path = get_core_path("include", "")
+    return include, include_path
 
 def get_core_libraries():
     """Return library paths and library directory paths."""
-    lib_paths = [libs for lib in ["lib", "lib64"] for libs in get_path(lib)]
-    lib = [libs for lib in ["lib", "lib64"] for libs in get_path(lib, pattern="*openmc*")]
+    lib = [libs for lib in ["lib", "lib64"] for libs in get_core_path(lib)]
+    lib_paths = [libs for lib in ["lib", "lib64"] for libs in get_core_path(lib, "")]
     return lib, lib_paths
 
 def get_extra_libraries():
-    """List all the extra libraries of OpenMC."""
-    libs_path = os.path.join(__path__[0], ".dylibs") if sys.platform == "darwin" else os.path.join(__path__[0], "..", "pyne.libs")
+    """List all the extra libraries."""
+    libs_path = os.path.join(__path__[0], ".dylibs") if sys.platform == "darwin" else os.path.join(__path__[0], "..", "openmc.libs")
     return (glob.glob(os.path.join(libs_path, "*")), libs_path) if os.path.exists(libs_path) else ([], [])
 
 # Setup variables
-include_path = get_include_path()
+include, include_path = get_include_path()
 lib, lib_path = get_core_libraries()
 extra_lib, extra_lib_path = get_extra_libraries()
 
 # Export variables for easy access
-__all__ = ["include_path", "lib", "lib_path", "extra_lib", "extra_lib_path"]
+__all__ = ["include", "include_path", "lib", "lib_path", "extra_lib", "extra_lib_path"]

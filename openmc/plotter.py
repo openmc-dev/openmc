339d78c5fae8ba3a815ae10097238d0099a142c4
Paul Romano <paul.k.romano@gmail.com>
2024-11-06 05:59:25 -0600

    Fix plot_xs type hint (#3184)

--- openmc/plotter.py
+++ openmc/plotter.py
@@ -1,6 +1,3 @@
-# annotations package is required to enable postponed evaluation of type
-# hints in conjugation with the | operator. This avoids TypeError: 
-# unsupported operand type(s) for |: 'str' and 'NoneType' error
 from __future__ import annotations
 from itertools import chain
 from numbers import Integral, Real
@@ -126,7 +123,7 @@ def _get_title(reactions):
 
 
 def plot_xs(
-    reactions: Dict[str, openmc.Material | List[str]],
+    reactions: Dict[str | openmc.Material, List[str]],
     divisor_types: Iterable[str] | None = None,
     temperature: float = 294.0,
     axis: "plt.Axes" | None = None,
@@ -139,7 +136,7 @@ def plot_xs(
     divisor_orders: Iterable[int] | None = None,
     energy_axis_units: str = "eV",
     **kwargs,
-) -> "plt.Figure":
+) -> "plt.Figure" | None:
     """Creates a figure of continuous-energy cross sections for this item.
 
     Parameters
@@ -289,7 +286,6 @@ def plot_xs(
     ax.set_title(_get_title(reactions))
 
     return fig
-
 
 
 def calculate_cexs(this, types, temperature=294., sab_name=None,

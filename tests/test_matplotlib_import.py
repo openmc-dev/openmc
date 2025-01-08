1ec1772b4e7d7d1770a466f430d4f273767798db
Paul Romano <paul.k.romano@gmail.com>
2025-01-08 10:58:48 -0600

    Revert "Add test forfor deferred import of openmc.lib"
    This reverts commit 6e461e10e72f4e97dfbebb3588ee9a43acab3b98.

--- tests/test_matplotlib_import.py
+++ tests/test_matplotlib_import.py
@@ -1,12 +1,6 @@
 import sys
 import openmc
 
-
 def test_matplotlib_presence():
     """Checks that matplotlib remains a deferred import"""
     assert 'matplotlib' not in sys.modules
-
-
-def test_openmc_lib_presence():
-    """Checks that openmc.lib remains a deferred import"""
-    assert 'openmc.lib' not in sys.modules

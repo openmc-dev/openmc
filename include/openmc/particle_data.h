5f010591dd21c57f6142780df16bca5ec5397ae5
Patrick Shriwise <pshriwise@gmail.com>
2025-01-11 22:44:44 -0600

    Style again

--- include/openmc/particle_data.h
+++ include/openmc/particle_data.h
@@ -320,47 +320,20 @@ public:
 
 #ifdef DAGMC
   // DagMC state variables
-  moab::DagMC::RayHistory& history()
-  {
-    return history_;
-  }
-  Direction& last_dir()
-  {
-    return last_dir_;
-  }
+  moab::DagMC::RayHistory& history() { return history_; }
+  Direction& last_dir() { return last_dir_; }
 #endif
 
   // material of current and last cell
-  int& material()
-  {
-    return material_;
-  }
-  const int& material() const
-  {
-    return material_;
-  }
-  int& material_last()
-  {
-    return material_last_;
-  }
-  const int& material_last() const
-  {
-    return material_last_;
-  }
+  int& material() { return material_; }
+  const int& material() const { return material_; }
+  int& material_last() { return material_last_; }
+  const int& material_last() const { return material_last_; }
 
   // temperature of current and last cell
-  double& sqrtkT()
-  {
-    return sqrtkT_;
-  }
-  const double& sqrtkT() const
-  {
-    return sqrtkT_;
-  }
-  double& sqrtkT_last()
-  {
-    return sqrtkT_last_;
-  }
+  double& sqrtkT() { return sqrtkT_; }
+  const double& sqrtkT() const { return sqrtkT_; }
+  double& sqrtkT_last() { return sqrtkT_last_; }
 
 private:
   int64_t id_ {-1}; //!< Unique ID

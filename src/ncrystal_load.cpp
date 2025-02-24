262881384d529cd240ec94cd13563a9f428344cc
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2025-02-24 21:39:56 +0100

    Try to follow openmc coding style in ncrystal_load.h/.cpp files

--- src/ncrystal_load.cpp
+++ src/ncrystal_load.cpp
@@ -1,146 +1,144 @@
 #include "ncrystal_load.hh"
-#include <string>
 #include <mutex>
 #include <stdexcept>
 #include <stdio.h>
+#include <string>
 
-#if !defined(NCLOAD_WINDOWS) && ( defined (_WIN32) || defined (WIN32) )
-#  define NCLOAD_WINDOWS
+#if !defined(NCLOAD_WINDOWS) && (defined(_WIN32) || defined(WIN32))
+#define NCLOAD_WINDOWS
 #endif
 #ifdef NCLOAD_WINDOWS
-#  ifndef WIN32_LEAN_AND_MEAN
-#    define WIN32_LEAN_AND_MEAN
-#  endif
-#  include <windows.h>
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include <windows.h>
 #else
-#  include <dlfcn.h>
+#include <dlfcn.h>
 #endif
 
 namespace openmc {
-  namespace {
+namespace {
 
-    struct NCrystalConfig {
-      std::string shlibpath;
-      unsigned long intversion = 0;
-      std::string symbol_namespace;
-    };
+struct NCrystalConfig {
+  std::string shlibpath;
+  unsigned long intversion = 0;
+  std::string symbol_namespace;
+};
 
-    NCrystalConfig query_ncrystal_config()
-    {
-      char buffer[4096];
+NCrystalConfig query_ncrystal_config()
+{
+  char buffer[4096];
 #ifdef NCLOAD_WINDOWS
-      FILE* pipe = _popen("ncrystal-config --show "
-                          "intversion shlibpath namespace", "r");
+  FILE* pipe = _popen("ncrystal-config --show "
+                      "intversion shlibpath namespace",
+    "r");
 #else
-      FILE* pipe = popen("ncrystal-config --show "
-                         "intversion shlibpath namespace 2>/dev/null", "r");
+  FILE* pipe = popen("ncrystal-config --show "
+                     "intversion shlibpath namespace 2>/dev/null",
+    "r");
 #endif
-      if (!pipe)
-        return {};//failure
-      auto readLine = [pipe]( std::string& tgt ) -> bool
-      {
-        //Read line and discard trailing whitespace (including newline chars).
-        char buffer[4096];
-        if (fgets(buffer, sizeof(buffer), pipe) == NULL)
-          return false;
-        tgt = buffer;
-        while( !tgt.empty() && std::isspace( tgt.back() ) )
-          tgt.pop_back();
-        return true;
-      };
-      auto parseIntVersion = []( const std::string& s )
-      {
-        char * str_end = nullptr;
-        unsigned long v = std::strtoul( s.c_str(), &str_end, 10 );
-        return ( v >= 2002000
-                 && v < 999999999
-                 && str_end == s.c_str() + s.size() ) ? v : 0;
-      };
+  if (!pipe)
+    return {}; // failure
+  auto readLine = [pipe](std::string& tgt) -> bool {
+    // Read line and discard trailing whitespace (including newline chars).
+    char buffer[4096];
+    if (fgets(buffer, sizeof(buffer), pipe) == NULL)
+      return false;
+    tgt = buffer;
+    while (!tgt.empty() && std::isspace(tgt.back()))
+      tgt.pop_back();
+    return true;
+  };
+  auto parseIntVersion = [](const std::string& s) {
+    char* str_end = nullptr;
+    unsigned long v = std::strtoul(s.c_str(), &str_end, 10);
+    return (v >= 2002000 && v < 999999999 && str_end == s.c_str() + s.size())
+             ? v
+             : 0;
+  };
 
-      NCrystalConfig res;
-      bool all_ok(true);
-      if ( !readLine(res.shlibpath)
-           || !(res.intversion = parseIntVersion( res.shlibpath ))
-           || !readLine( res.shlibpath )
-           || res.shlibpath.empty()
-           || !readLine(res.symbol_namespace ) ) {
-        res.intversion = 0;//failure
-      }
-
-#ifdef NCLOAD_WINDOWS
-      auto returnCode = _pclose(pipe);
-#else
-      auto returnCode = pclose(pipe);
-#endif
-      if ( returnCode == 0 && res.intversion >= 2002000  )
-        return res;
-      return {};//failure
-    }
-
-    struct NCrystalAPIDB {
-      std::mutex mtx;
-      std::shared_ptr<const NCrystalAPI> api;
-      typedef void* (*FctSignature)(int);
-      FctSignature ncrystal_access_dynapi_fct = nullptr;
-    };
-
-    void* load_dynapi_raw( unsigned interface_id, NCrystalAPIDB& db )
-    {
-      if ( !db.ncrystal_access_dynapi_fct ) {
-        auto cfg = query_ncrystal_config();
-        if ( !cfg.intversion >= 4000003 )
-          throw std::runtime_error("Could not locate a functioning and"
-                                   " recent enough NCrystal installation.");
-#ifdef NCLOAD_WINDOWS
-        auto handle = LoadLibrary(cfg.shlibpath.c_str());
-#else
-        dlerror();//clear previous errors
-        void * handle = dlopen( cfg.shlibpath.c_str(), RTLD_LOCAL | RTLD_LAZY );
-#endif
-        if (!handle)
-          throw std::runtime_error("Loading of the NCrystal library failed");
-
-        std::string symbol("ncrystal");
-        symbol += cfg.symbol_namespace;
-        symbol += "_access_dynamic_api";
-
-#ifdef NCLOAD_WINDOWS
-        FARPROC fproc;
-        void * addr = (void *)(intptr_t)GetProcAddress(handle,
-                                                       symbol.c_str());
-        if (!addr)
-          throw std::runtime_error("GetProcAddress("
-                                   "ncrystal_access_dynamic_api) failed");
-#else
-        dlerror();//clear previous errors
-        void * addr = dlsym( handle, symbol.c_str());
-        if ( !addr )
-          throw std::runtime_error("dlsym(ncrystal_access_dynamic_api) failed");
-#endif
-        db.ncrystal_access_dynapi_fct
-          = reinterpret_cast<NCrystalAPIDB::FctSignature>(addr);
-      }
-
-      return (*db.ncrystal_access_dynapi_fct)( interface_id );
-    }
-
-    NCrystalAPIDB& get_ncrystal_api_db()
-    {
-      static NCrystalAPIDB db;
-      return db;
-    }
+  NCrystalConfig res;
+  bool all_ok(true);
+  if (!readLine(res.shlibpath) ||
+      !(res.intversion = parseIntVersion(res.shlibpath)) ||
+      !readLine(res.shlibpath) || res.shlibpath.empty() ||
+      !readLine(res.symbol_namespace)) {
+    res.intversion = 0; // failure
   }
 
-  std::shared_ptr<const NCrystalAPI> load_ncrystal_api()
-  {
-    auto& db = get_ncrystal_api_db();
-    std::lock_guard<std::mutex> lock(db.mtx);
-    if ( ! db.api ) {
-      void * raw_api = load_dynapi_raw(NCrystalAPI::interface_id,db);
-      if (!raw_api)
-        throw std::runtime_error("Failed to access required NCrystal interface.");
-      db.api = *reinterpret_cast<std::shared_ptr<const NCrystalAPI>*>(raw_api);
-    }
-    return db.api;
-  }
+#ifdef NCLOAD_WINDOWS
+  auto returnCode = _pclose(pipe);
+#else
+  auto returnCode = pclose(pipe);
+#endif
+  if (returnCode == 0 && res.intversion >= 2002000)
+    return res;
+  return {}; // failure
 }
+
+struct NCrystalAPIDB {
+  std::mutex mtx;
+  std::shared_ptr<const NCrystalAPI> api;
+  typedef void* (*FctSignature)(int);
+  FctSignature ncrystal_access_dynapi_fct = nullptr;
+};
+
+void* load_dynapi_raw(unsigned interface_id, NCrystalAPIDB& db)
+{
+  if (!db.ncrystal_access_dynapi_fct) {
+    auto cfg = query_ncrystal_config();
+    if (!cfg.intversion >= 4000003)
+      throw std::runtime_error("Could not locate a functioning and"
+                               " recent enough NCrystal installation.");
+#ifdef NCLOAD_WINDOWS
+    auto handle = LoadLibrary(cfg.shlibpath.c_str());
+#else
+    dlerror(); // clear previous errors
+    void* handle = dlopen(cfg.shlibpath.c_str(), RTLD_LOCAL | RTLD_LAZY);
+#endif
+    if (!handle)
+      throw std::runtime_error("Loading of the NCrystal library failed");
+
+    std::string symbol("ncrystal");
+    symbol += cfg.symbol_namespace;
+    symbol += "_access_dynamic_api";
+
+#ifdef NCLOAD_WINDOWS
+    FARPROC fproc;
+    void* addr = (void*)(intptr_t)GetProcAddress(handle, symbol.c_str());
+    if (!addr)
+      throw std::runtime_error("GetProcAddress("
+                               "ncrystal_access_dynamic_api) failed");
+#else
+    dlerror(); // clear previous errors
+    void* addr = dlsym(handle, symbol.c_str());
+    if (!addr)
+      throw std::runtime_error("dlsym(ncrystal_access_dynamic_api) failed");
+#endif
+    db.ncrystal_access_dynapi_fct =
+      reinterpret_cast<NCrystalAPIDB::FctSignature>(addr);
+  }
+
+  return (*db.ncrystal_access_dynapi_fct)(interface_id);
+}
+
+NCrystalAPIDB& get_ncrystal_api_db()
+{
+  static NCrystalAPIDB db;
+  return db;
+}
+} // namespace
+
+std::shared_ptr<const NCrystalAPI> load_ncrystal_api()
+{
+  auto& db = get_ncrystal_api_db();
+  std::lock_guard<std::mutex> lock(db.mtx);
+  if (!db.api) {
+    void* raw_api = load_dynapi_raw(NCrystalAPI::interface_id, db);
+    if (!raw_api)
+      throw std::runtime_error("Failed to access required NCrystal interface.");
+    db.api = *reinterpret_cast<std::shared_ptr<const NCrystalAPI>*>(raw_api);
+  }
+  return db.api;
+}
+} // namespace openmc
--- src/ncrystal_load.h
+++ src/ncrystal_load.h
@@ -6,97 +6,95 @@
 
 namespace NCrystalDynamicAPI {
 
-  class DynAPI_Type1_v1 {
-  public:
+// NOTICE: Do NOT make ANY changes in the NCrystalDynamicAPI::DynAPI_Type1_v1
+// class, it is required to stay exactly constant over time and compatible
+// with the same definition used to compile the NCrystal library! But changes
+// to white space, comments, and formatting is of course allowed.  This API
+// was introduced in NCrystal 4.1.0.
 
-    //NOTICE: Do NOT make ANY changes in the DynAPI_Type1_v1 class, it is
-    //required to stay exactly constant over time and compatible with the same
-    //definition used to compile the NCrystal library! But changes to white
-    //space, comments, and formatting is of course allowed.
-    //This API was introduced in NCrystal 4.1.0.
+class DynAPI_Type1_v1 {
+public:
+  static constexpr unsigned interface_id = 1001; // 1000*typenumber+version
 
-    static constexpr unsigned interface_id = 1001;//1000*typenumber+version
+  class ScatterProcess;
+  virtual const ScatterProcess* createScatter(const char* cfgstr) const = 0;
+  virtual void deallocateScatter(const ScatterProcess*) const = 0;
 
-    class ScatterProcess;
-    virtual const ScatterProcess * createScatter( const char * cfgstr ) const = 0;
-    virtual void deallocateScatter( const ScatterProcess * ) const = 0;
+  // NB: Cross section units returned are barn/atom:
+  virtual double crossSectionUncached(const ScatterProcess&,
+    double neutron_ekin_eV, double neutron_dir_ux, double neutron_dir_uy,
+    double neutron_dir_uz) const = 0;
+  virtual void sampleScatterUncached(const ScatterProcess&,
+    std::function<double()>& rng, double& neutron_ekin_eV,
+    double& neutron_dir_ux, double& neutron_dir_uy,
+    double& neutron_dir_uz) const = 0;
 
-    //NB: Cross section units returned are barn/atom:
-    virtual double crossSectionUncached( const ScatterProcess&,
-                                         double neutron_ekin_eV,
-                                         double neutron_dir_ux,
-                                         double neutron_dir_uy,
-                                         double neutron_dir_uz ) const = 0;
-    virtual void sampleScatterUncached( const ScatterProcess&,
-                                        std::function<double()>& rng,
-                                        double& neutron_ekin_eV,
-                                        double& neutron_dir_ux,
-                                        double& neutron_dir_uy,
-                                        double& neutron_dir_uz ) const = 0;
+  virtual ~DynAPI_Type1_v1() = default;
+  DynAPI_Type1_v1() = default;
+  DynAPI_Type1_v1(const DynAPI_Type1_v1&) = delete;
+  DynAPI_Type1_v1& operator=(const DynAPI_Type1_v1&) = delete;
+  DynAPI_Type1_v1(DynAPI_Type1_v1&&) = delete;
+  DynAPI_Type1_v1& operator=(DynAPI_Type1_v1&&) = delete;
+};
 
-    virtual ~DynAPI_Type1_v1() = default;
-    DynAPI_Type1_v1() = default;
-    DynAPI_Type1_v1( const DynAPI_Type1_v1& ) = delete;
-    DynAPI_Type1_v1& operator=( const DynAPI_Type1_v1& ) = delete;
-    DynAPI_Type1_v1( DynAPI_Type1_v1&& ) = delete;
-    DynAPI_Type1_v1& operator=( DynAPI_Type1_v1&& ) = delete;
-  };
-
-}
+} // namespace NCrystalDynamicAPI
 
 namespace openmc {
 
-  using NCrystalAPI = NCrystalDynamicAPI::DynAPI_Type1_v1;
+using NCrystalAPI = NCrystalDynamicAPI::DynAPI_Type1_v1;
 
-  std::shared_ptr<const NCrystalAPI> load_ncrystal_api();
+std::shared_ptr<const NCrystalAPI> load_ncrystal_api();
 
-  class NCrystalScatProc final {
-  public:
-  NCrystalScatProc( const char * cfgstr )
-    : m_api( load_ncrystal_api() ),
-      m_p( m_api->createScatter( cfgstr ) )
-      {
-      }
-    ~NCrystalScatProc()
-      {
-        if ( m_p )
-          m_api->deallocateScatter( m_p );
-      }
+class NCrystalScatProc final {
+public:
+  NCrystalScatProc(const char* cfgstr)
+    : api_(load_ncrystal_api()), p_(api_->createScatter(cfgstr))
+  {
+  }
 
-    struct NeutronState { double ekin, ux, uy, uz; };
+  struct NeutronState { double ekin, ux, uy, uz; };
 
-    double cross_section( const NeutronState& n ) const
-    {
-      return m_api->crossSectionUncached( *m_p, n.ekin, n.ux, n.uy, n.uz );
+  double cross_section(const NeutronState& n) const
+  {
+    return api_->crossSectionUncached(*p_, n.ekin, n.ux, n.uy, n.uz);
+  }
+
+  void scatter(std::function<double()>& rng, NeutronState& n) const
+  {
+    api_->sampleScatterUncached(*p_, rng, n.ekin, n.ux, n.uy, n.uz);
+  }
+
+  //Plumbing (move-only semantics):
+  NCrystalScatProc(const NCrystalScatProc&) = delete;
+  NCrystalScatProc& operator=(const NCrystalScatProc&) = delete;
+
+  NCrystalScatProc(NCrystalScatProc&& o) : api_(std::move(o.api_)), p_(nullptr)
+  {
+    std::swap(p_, o.p_);
+  }
+
+  NCrystalScatProc& operator=(NCrystalScatProc&& o)
+  {
+    if (p_) {
+      api_->deallocateScatter(p_);
+      p_ = nullptr;
     }
-    void scatter( std::function<double()>& rng, NeutronState& n ) const
-    {
-      m_api->sampleScatterUncached( *m_p, rng, n.ekin, n.ux, n.uy, n.uz );
-    }
+    std::swap(api_, o.api_);
+    std::swap(p_, o.p_);
+    return *this;
+  }
 
-    NCrystalScatProc( const NCrystalScatProc& ) = delete;
-    NCrystalScatProc& operator=( const NCrystalScatProc& ) = delete;
-  NCrystalScatProc( NCrystalScatProc&& o  )
-    : m_api(std::move(o.m_api)), m_p(nullptr)
-      {
-        std::swap( m_p, o.m_p );
-      }
-    NCrystalScatProc& operator=( NCrystalScatProc&& o )
-      {
-        if ( m_p ) {
-          m_api->deallocateScatter( m_p );
-          m_p = nullptr;
-        }
-        std::swap( m_api, o.m_api );
-        std::swap( m_p, o.m_p );
-        return *this;
-      }
+  ~NCrystalScatProc()
+  {
+    if (p_)
+      api_->deallocateScatter(p_);
+  }
 
-  private:
-    std::shared_ptr<const NCrystalAPI> m_api;
-    const NCrystalAPI::ScatterProcess * m_p;
-  };
+private:
+  std::shared_ptr<const NCrystalAPI> api_;
+  const NCrystalAPI::ScatterProcess* p_;
+};
 
-}
+} // namespace openmc
 
 #endif

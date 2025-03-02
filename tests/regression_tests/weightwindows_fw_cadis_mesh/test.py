fa6a22cfbe8bc14527b4ec17cd9c4b31bc86fab5
John Tramm <john.tramm@gmail.com>
2025-03-02 10:30:10 -0600

    updated FW cadis mesh test to reduce rounding errors again.

--- tests/regression_tests/weightwindows_fw_cadis_mesh/test.py
+++ tests/regression_tests/weightwindows_fw_cadis_mesh/test.py
@@ -25,7 +25,7 @@ def test_weight_windows_fw_cadis_mesh(shape):
 
         # The base model has a resolution of 12, so we overlay
         # something else for FW-CADIS
-        n = 15
+        n = 13
         width = 30.0
         ww_mesh = openmc.RegularMesh()
         ww_mesh.dimension = (n, n, n)
@@ -39,11 +39,11 @@ def test_weight_windows_fw_cadis_mesh(shape):
         root = model.geometry.root_universe
         model.settings.random_ray['source_region_meshes'] = [(ww_mesh, [root])]
 
-        model.settings.particles = 300
-        model.settings.batches = 20
-        model.settings.inactive = 15
+        model.settings.particles = 200
+        model.settings.batches = 16
+        model.settings.inactive = 13
 
         model.settings.random_ray['source_shape'] = shape
 
-        harness = MGXSTestHarness('statepoint.20.h5', model)
+        harness = MGXSTestHarness('statepoint.16.h5', model)
         harness.main()

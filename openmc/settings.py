a7d9abf789493a35c4c0ea68a31b42c073729b76
Gregoire Biot <grego01@mit.edu>
2025-03-28 14:33:15 -0400

    Update settings.py

--- openmc/settings.py
+++ openmc/settings.py
@@ -1644,6 +1644,7 @@ class Settings:
                 else:
                     subelement = ET.SubElement(element, key)
                     subelement.text = str(value)
+                    
     def _eigenvalue_from_xml_element(self, root):
         elem = root.find('eigenvalue')
         if elem is not None:

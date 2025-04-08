9ea8680259f9055f7569fff2a3ffc21cb734a347
Grego01 <grego01@pc-neutronic-06.psfc.mit.edu>
2025-04-08 15:49:34 -0400

    clang-format filter_weight.h

--- include/openmc/tallies/filter_weight.h
+++ include/openmc/tallies/filter_weight.h
@@ -1,9 +1,9 @@
 #ifndef OPENMC_TALLIES_FILTER_WEIGHTS_H
 #define OPENMC_TALLIES_FILTER_WEIGHTS_H
 
+#include "openmc/span.h"
 #include "openmc/tallies/filter.h"
 #include "openmc/vector.h"
-#include "openmc/span.h"
 
 namespace openmc {
 
@@ -11,38 +11,38 @@ namespace openmc {
 //! Bins the weights of the particles.
 //==============================================================================
 
-class WeightFilter : public Filter{
-public: 
-    //----------------------------------------------------------------------------
-    // Constructors, destructors
-    
-    ~WeightFilter() = default;
-    
-    //----------------------------------------------------------------------------
-    // Methods
-    
-    std::string type_str() const override { return "weight"; }
-    FilterType type() const override { return FilterType::WEIGHT; }
-    
-    void from_xml(pugi::xml_node node) override;
+class WeightFilter : public Filter {
+public:
+  //----------------------------------------------------------------------------
+  // Constructors, destructors
 
-    void get_all_bins(const Particle& p, TallyEstimator estimator,
-        FilterMatch& match) const override;
-    
-    void to_statepoint(hid_t filter_group) const override;
-    
-    std::string text_label(int bin) const override;
-    
-    //----------------------------------------------------------------------------
-    // Accessors
+  ~WeightFilter() = default;
 
-    const vector<double>& bins() const { return bins_; }
-    void set_bins(span<const double> bins);
+  //----------------------------------------------------------------------------
+  // Methods
+
+  std::string type_str() const override { return "weight"; }
+  FilterType type() const override { return FilterType::WEIGHT; }
+
+  void from_xml(pugi::xml_node node) override;
+
+  void get_all_bins(const Particle& p, TallyEstimator estimator,
+    FilterMatch& match) const override;
+
+  void to_statepoint(hid_t filter_group) const override;
+
+  std::string text_label(int bin) const override;
+
+  //----------------------------------------------------------------------------
+  // Accessors
+
+  const vector<double>& bins() const { return bins_; }
+  void set_bins(span<const double> bins);
 
 protected:
-    //----------------------------------------------------------------------------
-    // Data members
-    vector<double> bins_;
+  //----------------------------------------------------------------------------
+  // Data members
+  vector<double> bins_;
 };
 
 } // namespace openmc

c892eca7a06d36ee3f86d1c07f3fdd04fc663ac9
shimwell <mail@jshimwell.com>
2025-04-12 00:50:45 +0200

    added test for dag plot

---
+++ tests/unit_tests/dagmc/test_plot.py
@@ -0,0 +1,35 @@
+import openmc
+
+def test_ploting_dagmc_geometry():
+    """Test plotting a DAGMC geometry with OpenMC. This is different to CSG
+    geometry plotting as the path to the DAGMC file needs handling."""
+
+    dag_universe = openmc.DAGMCUniverse(filename='dagmc.h5m')
+    csg_with_dag_inside = dag_universe.bounded_universe()
+    my_geometry = openmc.Geometry(csg_with_dag_inside)
+
+    all_materials = []
+    for mat_name in dag_universe.material_names:
+        material = openmc.Material(name=mat_name)
+        material.add_nuclide("Fe56", 1, "ao")
+        material.set_density("g/cm3", 7)
+        all_materials.append(material)
+    my_materials = openmc.Materials(all_materials)
+
+    my_source = openmc.IndependentSource()
+    # putting the source at the center of the bounding box of the DAGMC
+    my_source.space = openmc.stats.Point(dag_universe.bounding_box.center)
+
+    my_settings = openmc.Settings(
+        batches=10,
+        particles=50,
+        source=my_source
+    )
+
+    my_model = openmc.Model(
+        geometry=my_geometry,
+        materials=my_materials,
+        settings=my_settings
+    )
+
+    my_model.plot()

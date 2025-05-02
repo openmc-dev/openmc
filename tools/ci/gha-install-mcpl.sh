b97900391e32a826eac554aba60a1753c31f9ec0
Paul Romano <paul.k.romano@gmail.com>
2025-05-02 15:04:47 -0500

    Install MCPL from PyPI in CI

--- tools/ci/gha-install-mcpl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -ex
-cd $HOME
-git clone https://github.com/mctools/mcpl
-cd mcpl
-mkdir build && cd build
-cmake -DCMAKE_BUILD_TYPE=Debug .. && make 2>/dev/null && sudo make install

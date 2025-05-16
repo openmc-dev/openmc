c6894ff2044566be43540bcd26782b5c9611127a
Paul Romano <paul.k.romano@gmail.com>
2025-05-16 06:59:29 -0500

    Fix test

--- tests/unit_tests/dagmc/test_plot.py
+++ tests/unit_tests/dagmc/test_plot.py
@@ -1,5 +1,6 @@
 import pytest
 import openmc
+import openmc.lib
 
 
 pytestmark = pytest.mark.skipif(
@@ -10,33 +11,22 @@ def test_plotting_dagmc_geometry(request):
     """Test plotting a DAGMC geometry with OpenMC. This is different to CSG
     geometry plotting as the path to the DAGMC file needs handling."""
 
-    with openmc.config.patch(request.path.parent / 'dagmc.h5m', True):
-        dag_universe = openmc.DAGMCUniverse(filename='dagmc.h5m')
+    dag_universe = openmc.DAGMCUniverse(request.path.parent / 'dagmc.h5m')
     csg_with_dag_inside = dag_universe.bounded_universe()
-    my_geometry = openmc.Geometry(csg_with_dag_inside)
+    model = openmc.Model()
+    model.geometry = openmc.Geometry(csg_with_dag_inside)
 
-    all_materials = []
     for mat_name in dag_universe.material_names:
         material = openmc.Material(name=mat_name)
-        material.add_nuclide("Fe56", 1, "ao")
-        material.set_density("g/cm3", 7)
-        all_materials.append(material)
-    my_materials = openmc.Materials(all_materials)
+        material.add_nuclide("Fe56", 1.0)
+        material.set_density("g/cm3", 7.0)
+        model.materials.append(material)
 
-    my_source = openmc.IndependentSource()
     # putting the source at the center of the bounding box of the DAGMC
-    my_source.space = openmc.stats.Point(dag_universe.bounding_box.center)
-
-    my_settings = openmc.Settings(
-        batches=10,
-        particles=50,
-        source=my_source
+    model.settings.source = openmc.IndependentSource(
+        space=openmc.stats.Point(dag_universe.bounding_box.center)
     )
+    model.settings.batches = 10
+    model.settings.particles = 50
 
-    my_model = openmc.Model(
-        geometry=my_geometry,
-        materials=my_materials,
-        settings=my_settings
-    )
-
-    my_model.plot()
+    model.plot()

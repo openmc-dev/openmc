879350c9a764486729e6f34f6f105fab2e6c6ae3
lewisgross1296 <ligross@wisc.edu>
2025-05-19 15:30:36 -0500

    add assertions to tests and add new test to ensure a killed continue run can restart with the proper timesteps

--- tests/unit_tests/test_deplete_continue.py
+++ tests/unit_tests/test_deplete_continue.py
@@ -4,7 +4,9 @@ These tests run in two steps: first a normal run and then a continue run using t
 """
 
 import pytest
+import numpy as np
 import openmc.deplete
+from pathlib import Path
 
 from tests import dummy_operator
 
@@ -26,6 +28,69 @@ def test_continue(run_in_tmpdir):
     bundle.solver(operator, [1.0, 2.0, 3.0, 4.0], [1.0, 2.0, 3.0, 4.0],
                   continue_timesteps=True).integrate()
 
+    final_res = openmc.deplete.Results(operator.output_dir / "depletion_results.h5")
+
+    assert np.array_equal(np.diff(final_res.get_times(time_units="s")),[1.0, 2.0, 3.0, 4.0])
+
+
+def test_continue_continue(run_in_tmpdir):
+    """Test to ensure that a continue run can be continued"""
+    # set up the problem
+    bundle = dummy_operator.SCHEMES['predictor']
+    operator = dummy_operator.DummyOperator()
+
+    # initial depletion
+    bundle.solver(operator, [1.0, 2.0], [1.0, 2.0]).integrate()
+
+    # set up continue run
+    prev_res = openmc.deplete.Results(operator.output_dir / "depletion_results.h5")
+    operator = dummy_operator.DummyOperator(prev_res)
+
+    # first continue run
+    bundle.solver(operator, [1.0, 2.0, 3.0, 4.0], [1.0, 2.0, 3.0, 4.0],
+                  continue_timesteps=True).integrate()
+
+    prev_res = openmc.deplete.Results(operator.output_dir / "depletion_results.h5")
+    # second continue run
+    bundle.solver(operator, [1.0, 2.0, 3.0, 4.0, 5.0, 6.0], [1.0, 2.0, 3.0, 4.0, 5.0, 6.0],
+                  continue_timesteps=True).integrate()
+    
+    final_res = openmc.deplete.Results(operator.output_dir / "depletion_results.h5")
+
+    assert np.array_equal(np.diff(final_res.get_times(time_units="s")),[1.0, 2.0, 3.0, 4.0, 5.0, 6.0])
+
+def test_killed_and_continue(run_in_tmpdir):
+    """
+    Attempt to continue from a simulation that was killed mid state.
+    The previous state is provided in the form of a few local files:
+
+    continue_model.xml contains the necessary XML information
+    simple_chain.xml contians a simplified version of the CASL chain
+
+    continue_depletion_results.h5 contains the results output by
+    an OpenMC (v0.15.2) depletion simulation that was killed in
+    the middle of the third step
+    """
+    base_path = Path(__file__).parents[0]
+    model = openmc.Model.from_model_xml(f"{base_path}/kill_continue/continue_model.xml")
+    power = 35000 # W
+
+    time_steps = [1.0,2.0,3.0,4.0] # days
+    prev_results = openmc.deplete.Results(f"{base_path}/kill_continue/continue_depletion_results.h5")
+    operator = openmc.deplete.CoupledOperator(
+        model, prev_results=prev_results, chain_file=f"{base_path}/kill_continue/simple_chain.xml"
+    )
+    integrator = openmc.deplete.CECMIntegrator(
+        operator,
+        time_steps,
+        power=power,
+        timestep_units="d",
+        continue_timesteps=True,
+    )
+    integrator.integrate(path=f"{base_path}/kill_continue/continue_depletion_results.h5")
+    final_res = openmc.deplete.Results(f"{base_path}/kill_continue/continue_depletion_results.h5")
+
+    assert np.array_equal(np.diff(final_res.get_times(time_units="d")),[1.0, 2.0, 3.0, 4.0])
 
 def test_mismatched_initial_times(run_in_tmpdir):
     """Test to ensure that a continue run with different initial steps is properly caught"""

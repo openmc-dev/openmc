b7986d004a41a70f53d05d1d5cb414fb31bb9e0a
Lewis Gross <43077972+lewisgross1296@users.noreply.github.com>
2025-05-21 14:01:29 -0500

    PPHW docstring improvements
    Co-authored-by: Paul Wilson <paul.wilson@wisc.edu>

--- openmc/deplete/abc.py
+++ openmc/deplete/abc.py
@@ -822,19 +822,20 @@ class Integrator(ABC):
         """
         This function fetches the starting state of a simulation in terms of the
         simulation physical time at which to start and the index at which the
-        depletion simulation should start at. When no previous results exist,
+        depletion simulation should start. When no previous results exist,
         the time and index are both zero. When previous results do exist, it 
-        returns the time corresponding to beginning the previous resutls last
+        returns the time corresponding to beginning the previous results last
+``
         timestep and the index as N-1 where N is the number of previous StepResults
-        found in the previous Results.
+        found in the previous Results. (as expected from 0-based indexing)
         
         Note that the openmc.deplete.Results.time object is a list of float with
         [t,t+dt] where t is the beginning of timestep time and t+dt is the end of
         timestep time. If the previous results correspond to a simulation that
         finished to completeion, it will contain a results in the form of [t,t],
         but if a simulation doesn't finish all the given timesteps, it is the t
-        that is the desired start time, not t+dt. Thus, it is always save to take 
-        dtime[0].
+        that is the desired start time, not t+dt. Thus, it is always safe to take 
+        time[0].
 
         Returns
         _______

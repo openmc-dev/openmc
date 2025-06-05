487c7f228338472750cc2fc81cb48a6f673c51da
Ahnaf Tahmid Chowdhury <tahmid@nukehub.org>
2025-06-05 21:16:23 +0600

    initialize the mcpl interface

--- include/openmc/mcpl_interface.h
+++ include/openmc/mcpl_interface.h
@@ -10,12 +10,6 @@
 namespace openmc {
 
 //==============================================================================
-// Constants
-//==============================================================================
-
-extern "C" const bool MCPL_ENABLED;
-
-//==============================================================================
 // Functions
 //==============================================================================
 
@@ -29,14 +23,18 @@ vector<SourceSite> mcpl_source_sites(std::string path);
 //
 //! \param[in] filename     Path to MCPL file
 //! \param[in] source_bank  Vector of SourceSites to write to file for this
-//!                         MPI rank. Note that this can't be const due to
-//!                         it being used as work space by MPI.
-//! \param[in] bank_indx    Pointer to vector of site index ranges over all
-//!                         MPI ranks. This can be computed by calling
-//!                         calculate_parallel_index_vector on
-//!                         source_bank.size().
+//!                         MPI rank.
+//! \param[in] bank_index   Pointer to vector of site index ranges over all
+//!                         MPI ranks.
 void write_mcpl_source_point(const char* filename, span<SourceSite> source_bank,
   const vector<int64_t>& bank_index);
+
+//! Check if MCPL functionality is available
+bool is_mcpl_interface_available();
+
+//! Initialize the MCPL interface
+void initialize_mcpl_interface_if_needed();
+
 } // namespace openmc
 
 #endif // OPENMC_MCPL_INTERFACE_H

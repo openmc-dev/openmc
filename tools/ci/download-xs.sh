057fc3df57829993a5bf1ea3e5d210023217fd78
GuySten <62616591+GuySten@users.noreply.github.com>
2025-06-12 23:44:03 +0300

    Update download-xs.sh

--- tools/ci/download-xs.sh
+++ tools/ci/download-xs.sh
@@ -9,5 +9,5 @@ fi
 # Download ENDF/B-VII.1 distribution
 ENDF=$HOME/endf-b-vii.1
 if [[ ! -d $ENDF/neutrons || ! -d $ENDF/photoat || ! -d $ENDF/atomic_relax || ! -d $ENDF/gammas ]]; then
-    wget -q -O - https://anl.box.com/shared/static/4kd2gxnf4gtk4w1c8eua5fsua22kvgjb.xz | tar -C $HOME -xJ
+    wget -q -O - https://github.com/GuySten/data/releases/download/v1.0.0/endf71.tar.xz | tar -C $HOME -xJ
 fi

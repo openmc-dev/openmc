df1158dc4ffe793c6f45a5fed679bda89f28afe1
Paul Romano <paul.k.romano@gmail.com>
2025-07-17 07:57:51 +0800

    Update use of <run_mode> in source_mcpl_file test

--- tests/regression_tests/source_mcpl_file/test.py
+++ tests/regression_tests/source_mcpl_file/test.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-import openmc.lib
 import pytest
 import glob
 import os
@@ -13,13 +12,12 @@ pytestmark = pytest.mark.skipif(
 
 settings1="""<?xml version="1.0"?>
 <settings>
+  <run_mode>eigenvalue</run_mode>
   <state_point batches="10" />
   <source_point mcpl="true" separate="true" />
-  <eigenvalue>
-    <batches>10</batches>
-    <inactive>5</inactive>
-    <particles>1000</particles>
-  </eigenvalue>
+  <batches>10</batches>
+  <inactive>5</inactive>
+  <particles>1000</particles>
   <source>
     <space type="box">
       <parameters>-4 -4 -4  4  4  4</parameters>
@@ -30,11 +28,10 @@ settings1="""<?xml version="1.0"?>
 
 settings2 = """<?xml version="1.0"?>
 <settings>
-  <eigenvalue>
-    <batches>10</batches>
-    <inactive>5</inactive>
-    <particles>1000</particles>
-  </eigenvalue>
+  <run_mode>eigenvalue</run_mode>
+  <batches>10</batches>
+  <inactive>5</inactive>
+  <particles>1000</particles>
   <source>
     <file>source.10.{}</file>
   </source>

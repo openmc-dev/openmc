ee1f2c5ffb28679c9fe20b7477055373e256da99
itay-space <126396074+itay-space@users.noreply.github.com>
2025-09-02 15:16:59 +0300

    Apply suggestion from @GuySten
    Co-authored-by: GuySten <62616591+GuySten@users.noreply.github.com>

--- include/openmc/thermal.h
+++ include/openmc/thermal.h
@@ -52,8 +52,7 @@ public:
   //! \param[inout] seed Pseudorandom seed pointer
   void sample(const NuclideMicroXS& micro_xs, double E_in, double* E_out,
     double* mu, uint64_t* seed);
-  double get_pdf(const NuclideMicroXS& micro_xs, double E, double& E_out,
-    double mu, uint64_t* seed);
+  double get_pdf(const NuclideMicroXS& micro_xs, double E_in, double mu, double& E_out, uint64_t* seed);
 
 private:
   struct Reaction {

ca7cd8501b1ffdfb04b20fb6f98f2622be8ff016
Your Name <you@example.com>
2025-09-03 19:45:33 +0000

    returning pdf in uncorrelated

--- src/secondary_uncorrelated.cpp
+++ src/secondary_uncorrelated.cpp
@@ -70,6 +70,14 @@ double UncorrelatedAngleEnergy::get_pdf(
 {
   // Sample outgoing energy
   E_out = energy_->sample(E_in, seed);
+  double pdf_cm;
+    if (!angle_.empty()) {
+      pdf_cm = angle_.get_pdf(E_in, mu, seed);
+      } else {
+       // no angle distribution given => assume isotropic for all energies
+        pdf_cm = 0.5;
+     }
+return pdf_cm;
 }
 
 } // namespace openmc

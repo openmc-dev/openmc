21781e21c3118528f7c7ad294dd15135580da1d7
skywalker_cn <2324714651@qq.com>
2025-09-11 08:13:58 +0000

    Fix the format error

--- include/openmc/universe.h
+++ include/openmc/universe.h
@@ -27,10 +27,10 @@ extern vector<unique_ptr<Universe>> universes;
 
 class Universe {
 public:
-  int32_t id_;            //!< Unique ID
-  vector<int32_t> cells_; //!< Cells within this universe
+  int32_t id_;                      //!< Unique ID
+  vector<int32_t> cells_;           //!< Cells within this universe
   int filled_with_triso_base_ = -1; //!< ID of cell filled with virtual lattice
-  int32_t n_instances_;   //!< Number of instances of this universe
+  int32_t n_instances_;             //!< Number of instances of this universe
 
   //! \brief Write universe information to an HDF5 group.
   //! \param group_id An HDF5 group id.

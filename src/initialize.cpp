31c39c3ca6b5738e6744c9f1a0c66289c21035db
Paul Romano <paul.k.romano@gmail.com>
2025-09-17 13:29:01 -0500

    Make sure finalize_cell_densities gets called for individual XML

--- src/initialize.cpp
+++ src/initialize.cpp
@@ -445,6 +445,11 @@ void read_separate_xml_files()
 
   // Finalize cross sections having assigned temperatures
   finalize_cross_sections();
+
+  // Compute cell density multipliers now that material densities
+  // have been finalized (from geometry_aux.h)
+  finalize_cell_densities();
+
   read_tallies_xml();
 
   // Initialize distribcell_filters

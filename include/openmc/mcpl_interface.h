e338fece3ffb31e7ae429a0f4a6adcb760fdc77f
Paul Romano <paul.k.romano@gmail.com>
2025-10-14 15:01:11 -0500

    Update Doxygen comment in mcpl_interface.h

--- include/openmc/mcpl_interface.h
+++ include/openmc/mcpl_interface.h
@@ -33,22 +33,30 @@ vector<SourceSite> mcpl_source_sites(std::string path);
 //! \param[in] filename     Path to MCPL file
 //! \param[in] source_bank  Vector of SourceSites to write to file for this
 //!                         MPI rank.
-//! \param[in] collision_track_bank  Vector of CollisionTrackSites to write to
-//! file
-//!                         for this MPI rank.
 //! \param[in] bank_index   Pointer to vector of site index ranges over all
 //!                         MPI ranks.
 void write_mcpl_source_point(const char* filename, span<SourceSite> source_bank,
   const vector<int64_t>& bank_index);
+
+//! Write an MCPL collision track file
+//!
+//! This function writes collision track data to an MCPL file. Additional
+//! collision-specific metadata (such as energy deposition, material info, etc.)
+//! is stored in the file header as blob data.
+//!
+//! \param[in] filename     Path to MCPL file
+//! \param[in] collision_track_bank  Vector of CollisionTrackSites to write to
+//!                         file for this MPI rank.
+//! \param[in] bank_index   Pointer to vector of site index ranges over all
+//!                         MPI ranks.
 void write_mcpl_collision_track(const char* filename,
   span<CollisionTrackSite> collision_track_bank,
   const vector<int64_t>& bank_index);
-//! Check if MCPL functionality is available
 
+  //! Check if MCPL functionality is available
 bool is_mcpl_interface_available();
 
 //! Initialize the MCPL interface
-
 void initialize_mcpl_interface_if_needed();
 
 } // namespace openmc

578501b17352656274c01b354fccc7fe2686a3b0
Paul Romano <paul.k.romano@gmail.com>
2025-10-15 15:53:31 -0500

    Update test

--- tests/regression_tests/electron_heating/test.py
+++ tests/regression_tests/electron_heating/test.py
@@ -1,63 +1,40 @@
-import os
-import glob
-
-import numpy as np
 import pytest
-
 import openmc
 
 from tests.testing_harness import PyAPITestHarness
 
 
-class ElectronHeatingTest(PyAPITestHarness):
+@pytest.fixture
+def water_model():
+    # Define materals and geometry
+    water = openmc.Material()
+    water.add_nuclide("H1", 2.0)
+    water.add_nuclide("O16", 1.0)
+    water.set_density("g/cc", 1.0)
+    sphere = openmc.Sphere(r=1.0, boundary_type="reflective")
+    sph = openmc.Cell(fill=water, region=-sphere)
+    geometry = openmc.Geometry([sph])
+    source = openmc.IndependentSource(
+        energy=openmc.stats.delta_function(10.0e6),
+        particle="electron"
+    )
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    # Define settings
+    settings = openmc.Settings()
+    settings.particles = 10000
+    settings.batches = 1
+    settings.cutoff = {"energy_photon": 1000.0}
+    settings.run_mode = "fixed source"
+    settings.source = source
 
-        # Define materials
-        water = openmc.Material(1)
-        water.add_nuclide("H1", 2.0)
-        water.add_nuclide("O16", 1.0)
-        water.set_density("g/cc", 1.0)
+    # Define tallies
+    tally = openmc.Tally()
+    tally.scores = ["heating"]
+    tallies = openmc.Tallies([tally])
 
-        materials = openmc.Materials((water,))
-        self._model.materials = materials
-
-        sphere = openmc.Sphere(surface_id=1, r=1.0, boundary_type="reflective")
-
-        # Define geometry
-        sph = openmc.Cell(1, fill=water, region=-sphere)
-        root = openmc.Universe(0, cells=(sph,))
-
-        self._model.geometry = openmc.Geometry(root)
-
-        # Define source
-
-        source = openmc.IndependentSource()
-        source.space = openmc.stats.Point((0, 0, 0))
-        source.angle = openmc.stats.Isotropic()
-        source.energy = openmc.stats.Discrete([10.0e6], [1.0])
-        source.particle = "electron"
-
-        # Define settings
-        settings = openmc.Settings()
-        settings.particles = 10000
-        settings.batches = 1
-        settings.photon_transport = True
-        settings.electron_treatment = "ttb"
-        settings.cutoff = {"energy_photon": 1000.0}
-        settings.run_mode = "fixed source"
-        settings.source = source
-        self._model.settings = settings
-
-        # Define tallies
-
-        tally = openmc.Tally()
-        tally.scores = ["heating"]
-        tallies = openmc.Tallies([tally])
-        self._model.tallies = tallies
+    return openmc.Model(geometry=geometry, settings=settings, tallies=tallies)
 
 
-def test_electron_heating_calc():
-    harness = ElectronHeatingTest("statepoint.1.h5", model=openmc.Model())
+def test_electron_heating_calc(water_model):
+    harness = PyAPITestHarness("statepoint.1.h5", water_model)
     harness.main()

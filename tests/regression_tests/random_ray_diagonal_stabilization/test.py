89f2f47180087479a1dac6430ae94167d91d281c
John Tramm <john.tramm@gmail.com>
2025-10-20 14:06:12 -0500

    regenerated test results with nndc data

--- tests/regression_tests/random_ray_diagonal_stabilization/test.py
+++ tests/regression_tests/random_ray_diagonal_stabilization/test.py
@@ -23,7 +23,7 @@ def test_random_ray_diagonal_stabilization():
     # MGXS data with some negatives on the diagonal, in order
     # to trigger diagonal correction.
     model.convert_to_multigroup(
-        method='material_wise', groups='CASMO-70', nparticles=30,
+        method='material_wise', groups='CASMO-70', nparticles=15,
         overwrite_mgxs_library=True, mgxs_path="mgxs.h5", correction='P0'
     )
 

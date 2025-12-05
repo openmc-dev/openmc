01ccfdc8fc11652561eb3b008c59ef41bf1b3958
John Tramm <john.tramm@gmail.com>
2025-12-05 16:52:51 -0600

    updated python SourceSite type

--- openmc/lib/core.py
+++ openmc/lib/core.py
@@ -31,7 +31,11 @@ class _SourceSite(Structure):
                 ('particle', c_int),
                 ('parent_nuclide', c_int),
                 ('parent_id', c_int64),
-                ('progeny_id', c_int64)]
+                ('progeny_id', c_int64),
+                ('wgt_born', c_double),
+                ('wgt_ww_born', c_double),
+                ('n_split', c_int64),
+                ('current_work', c_int64)]
 
 
 # Define input type for numpy arrays that will be passed into C++ functions

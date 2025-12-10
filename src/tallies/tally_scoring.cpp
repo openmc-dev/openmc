eabad048ceb1a4292c51bc578cadaa464935f7a0
GuySten <guyste@post.bgu.ac.il>
2025-12-10 18:22:37 +0200

    fix typo

--- src/tallies/tally_scoring.cpp
+++ src/tallies/tally_scoring.cpp
@@ -963,7 +963,7 @@ void score_general_ce_nonanalog(Particle& p, int i_tally, int start_index,
               simulation::ifp_source_delayed_group_bank[p.current_work() - 1];
             if (delayed_groups.size() == settings::ifp_n_generation) {
               if ((delayed_groups[0] > 0 && tally.nuclides_[0] == -1) ||
-                  (delayed_groups[1] > 0 && tally.nuclides_[0] > 0)) {
+                  (delayed_groups[1] > 0 && tally.nuclides_[0] > -1)) {
                 const auto& ancestor_event_nuclide = simulation::
                   ifp_source_ancestor_nuclide_bank[p.current_work() - 1];
                 if (tally.nuclides_[0] == -1 ||

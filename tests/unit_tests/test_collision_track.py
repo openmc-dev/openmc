2dcc74fcd492a87dd5f2b83843c4ab5299e267ce
GuySten <guyste@post.bgu.ac.il>
2025-12-18 15:11:12 +0200

    fix mcpl dependency in test

--- tests/unit_tests/test_collision_track.py
+++ tests/unit_tests/test_collision_track.py
@@ -109,6 +109,7 @@ def test_particle_location(run_in_tmpdir, model):
                 assert False
 
 
+@pytest.mark.skipif(shutil.which("mcpl-config") is None, reason="MCPL is not available.")
 def test_format_similarity(run_in_tmpdir, model):
     model.settings.collision_track = {"max_collisions": 200, "reactions": ['elastic'],
                                       "cell_ids": [1, 2], "mcpl": False}

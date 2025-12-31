89abd4404417474dd6c1864bdd89d3f3bb9a356f
yardasol <oyardas2@illinois.edu>
2025-12-30 23:22:24 -0600

    add random_ray_k_eff_kinetic regression test

---
+++ tests/regression_tests/random_ray_k_eff_kinetic/test.py
@@ -0,0 +1,30 @@
+import os
+
+import openmc
+from openmc.utility_funcs import change_directory
+from openmc.examples import random_ray_pin_cell
+import pytest
+
+from tests.testing_harness import KineticTolerantPyAPITestHarness
+
+
+class KineticMGXSTestHarness(KineticTolerantPyAPITestHarness):
+    def _cleanup(self):
+        super()._cleanup()
+        f = 'mgxs.h5'
+        if os.path.exists(f):
+            os.remove(f)
+
+
+@pytest.mark.parametrize("time_method", ["isotropic",
+                                         "propagation"])
+def test_random_ray_time_dependent(time_method):
+    with change_directory(time_method):
+        openmc.reset_auto_ids()
+        model = random_ray_pin_cell(kinetic=True)
+        model.settings.timestep_parameters['n_timesteps'] = 5
+        model.settings.random_ray['time_method'] = time_method
+        model.settings.batches = 400
+        model.settings.inactive = 200 
+        harness = KineticMGXSTestHarness(model, 6)# 21)
+        harness.main()

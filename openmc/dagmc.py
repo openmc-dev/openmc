13a89397bd1028bf35acfd0c2b647892f54f1e3a
GuySten <62616591+GuySten@users.noreply.github.com>
2026-01-05 18:58:58 +0200

    Refactor material name extraction logic

--- openmc/dagmc.py
+++ openmc/dagmc.py
@@ -223,20 +223,19 @@ class DAGMCUniverse(openmc.UniverseBase):
 
     @property
     def material_names(self):
+        material_tags_ascii = []
         with h5py.File(self.filename) as dagmc_file_contents:
             material_tags_hex = dagmc_file_contents['/tstt/tags/NAME'].get('values')
-        material_tags_ascii = []
-        for tag in material_tags_hex:
-            candidate_tag = tag.tobytes().decode().replace('\x00', '')
-            # tags might be for temperature or reflective surfaces
-            if candidate_tag.startswith('mat:'):
-                # if name ends with _comp remove it, it is not parsed
-                if candidate_tag.endswith('_comp'):
-                   candidate_tag = candidate_tag[:-5]
-                # removes first 4 characters as openmc.Material name should be
-                # set without the 'mat:' part of the tag
-                material_tags_ascii.append(candidate_tag[4:])
-
+            for tag in material_tags_hex:
+                candidate_tag = tag.tobytes().decode().replace('\x00', '')
+                # tags might be for temperature or reflective surfaces
+                if candidate_tag.startswith('mat:'):
+                    # if name ends with _comp remove it, it is not parsed
+                    if candidate_tag.endswith('_comp'):
+                       candidate_tag = candidate_tag[:-5]
+                    # removes first 4 characters as openmc.Material name should be
+                    # set without the 'mat:' part of the tag
+                    material_tags_ascii.append(candidate_tag[4:])
         return sorted(set(material_tags_ascii))
 
     def _n_geom_elements(self, geom_type):

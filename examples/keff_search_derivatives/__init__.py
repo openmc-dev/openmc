e2eafa5f54356cce5af301b7d942f8d8c5944998
pranav <pranav8iisc@gmail.com>
2026-01-08 07:40:35 +0000

    remove keff search example dir

--- examples/keff_search_derivatives/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-keff_search with derivative tallies example.
-
-This example demonstrates derivative-accelerated criticality searches using
-OpenMC's derivative tally capability. See README.md for detailed documentation.
-"""

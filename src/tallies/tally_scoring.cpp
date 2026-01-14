e23cbf091d10a0f2c76b98108454e1df168c2ca0
GuySten <62616591+GuySten@users.noreply.github.com>
2026-01-14 19:52:03 +0200

    Apply suggestions from code review

--- src/tallies/tally_scoring.cpp
+++ src/tallies/tally_scoring.cpp
@@ -2628,7 +2628,7 @@ void score_surface_tally(Particle& p, const vector<int>& tallies)
       // for a further scoring function.
       double score = current * filter_weight;
       for (auto score_index = 0; score_index < tally.scores_.size();
-        ++score_index) {
+          ++score_index) {
 #pragma omp atomic
         tally.results_(filter_index, score_index, TallyResult::VALUE) += score;
       }
@@ -2703,7 +2703,7 @@ void score_pulse_height_tally(Particle& p, const vector<int>& tallies)
 
             // Loop over scores.
             for (auto score_index = 0; score_index < tally.scores_.size();
-              ++score_index) {
+                ++score_index) {
 #pragma omp atomic
               tally.results_(filter_index, score_index, TallyResult::VALUE) +=
                 filter_weight;

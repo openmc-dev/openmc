8a0f8dbe546b1475f7d1209b315d299bbd54bd83
GuySten <guyste@post.bgu.ac.il>
2026-02-04 02:50:53 +0200

    fix typo

--- src/particle.cpp
+++ src/particle.cpp
@@ -47,7 +47,7 @@ double Particle::speed() const
 {
   if (settings::run_CE) {
     // Determine mass in eV/c^2
-    double mass = this->type().mass();
+    double mass = this->type().mass() * AMU_EV;
     // Equivalent to C * sqrt(1-(m/(m+E))^2) without problem at E<<m:
     return C_LIGHT * std::sqrt(this->E() * (this->E() + 2 * mass)) /
            (this->E() + mass);

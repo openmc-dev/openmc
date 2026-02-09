4f4930b63395a6ea6b83a52bdd807e3939bbd59e
Kevin Sawatzky <66632997+nuclearkevin@users.noreply.github.com>
2026-02-09 16:54:59 -0600

    Temperature feedback support in the random ray solver. (#3737)

--- src/random_ray/random_ray_simulation.cpp
+++ src/random_ray/random_ray_simulation.cpp
@@ -79,10 +79,6 @@ void validate_random_ray_inputs()
       fatal_error("Anisotropic MGXS detected. Only isotropic XS data sets "
                   "supported in random ray mode.");
     }
-    if (material.get_xsdata().size() > 1) {
-      warning("Non-isothermal MGXS detected. Only isothermal XS data sets "
-              "supported in random ray mode. Using lowest temperature.");
-    }
     for (int g = 0; g < data::mg.num_energy_groups_; g++) {
       if (material.exists_in_model) {
         // Temperature and angle indices, if using multiple temperature

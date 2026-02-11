3de2714afbf861c519c83412b6a8dfd5f508b953
Christopher <chris1218@gmx.de>
2026-02-11 10:59:10 +0100

    clang

--- include/openmc/settings.h
+++ include/openmc/settings.h
@@ -102,8 +102,8 @@ extern bool weight_window_checkpoint_surface;   //!< enable weight window check
                                                 //!< upon surface crossing?
 extern bool weight_window_checkpoint_collision; //!< enable weight window check
                                                 //!< upon collision?
-extern bool write_all_tracks;     //!< write track files for every particle?
-extern bool write_initial_source; //!< write out initial source file?
+extern bool write_all_tracks;      //!< write track files for every particle?
+extern bool write_initial_source;  //!< write out initial source file?
 extern bool activity_based_timing; //!< use source strengths as activity rates
                                    //!< for Poisson timestamp generation?
 

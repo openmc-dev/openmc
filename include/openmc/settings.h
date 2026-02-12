1fad4a96b9d29997c289ae08c6602c3f6d5afebd
Christopher <chris1218@gmx.de>
2026-02-12 16:53:19 +0100

    clang again

--- include/openmc/settings.h
+++ include/openmc/settings.h
@@ -102,8 +102,8 @@ extern bool weight_window_checkpoint_surface;   //!< enable weight window check
                                                 //!< upon surface crossing?
 extern bool weight_window_checkpoint_collision; //!< enable weight window check
                                                 //!< upon collision?
-extern bool write_all_tracks;      //!< write track files for every particle?
-extern bool write_initial_source;  //!< write out initial source file?
+extern bool write_all_tracks;     //!< write track files for every particle?
+extern bool write_initial_source; //!< write out initial source file?
 
 // Paths to various files
 extern std::string path_cross_sections; //!< path to cross_sections.xml

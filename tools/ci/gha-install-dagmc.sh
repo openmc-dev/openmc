a4f57df32d727eed979462488185fcc01774e373
GuySten <guyste@post.bgu.ac.il>
2026-02-12 09:03:30 +0200

    improve install time

--- tools/ci/gha-install-dagmc.sh
+++ tools/ci/gha-install-dagmc.sh
@@ -17,7 +17,7 @@ CURRENT_DIR=$(pwd)
 # MOAB Install
 cd $HOME
 mkdir MOAB && cd MOAB
-git clone -b $MOAB_BRANCH $MOAB_REPO
+git clone --depth 1 -b $MOAB_BRANCH $MOAB_REPO
 mkdir build && cd build
 cmake ../moab -DENABLE_HDF5=ON -DENABLE_NETCDF=ON -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=$MOAB_INSTALL_DIR
 make -j && make -j install
@@ -26,7 +26,7 @@ rm -rf $HOME/MOAB/moab $HOME/MOAB/build
 # DAGMC Install
 cd $HOME
 mkdir DAGMC && cd DAGMC
-git clone -b $DAGMC_BRANCH $DAGMC_REPO
+git clone --depth 1 -b $DAGMC_BRANCH $DAGMC_REPO
 mkdir build && cd build
 cmake ../dagmc -DBUILD_TALLY=ON -DCMAKE_INSTALL_PREFIX=$DAGMC_INSTALL_DIR -DBUILD_STATIC_LIBS=OFF -DMOAB_DIR=$MOAB_INSTALL_DIR
 make -j install
--- tools/ci/gha-install-njoy.sh
+++ tools/ci/gha-install-njoy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 cd $HOME
-git clone https://github.com/njoy/NJOY2016
+git clone --depth 1 https://github.com/njoy/NJOY2016
 cd NJOY2016
 mkdir build && cd build
-cmake -Dstatic=on .. && make 2>/dev/null && sudo make install
+cmake -Dstatic=on .. && make -j4 2>/dev/null && sudo make -j4 install

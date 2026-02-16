fa4e0b61d71af54e27aa2fadfa8f6ec25615e80f
Paul Romano <paul.k.romano@gmail.com>
2026-02-16 11:26:37 -0600

    Update regression test

--- tests/regression_tests/filter_reaction/test.py
+++ tests/regression_tests/filter_reaction/test.py
@@ -1,23 +1,31 @@
-from openmc.examples import pwr_pin_cell
-from tests.testing_harness import PyAPITestHarness
-
 import openmc
+
+from tests.testing_harness import PyAPITestHarness
 
 
 def test_filter_reaction():
-    model = pwr_pin_cell()
+    model = openmc.Model()
+
+    m = openmc.Material()
+    m.set_density('g/cm3', 10.0)
+    m.add_nuclide('U235', 1.0)
+    model.materials.append(m)
+
+    s = openmc.Sphere(r=100.0, boundary_type='vacuum')
+    c = openmc.Cell(fill=m, region=-s)
+    model.geometry = openmc.Geometry([c])
 
     # Create a tally with reaction filter
     tally = openmc.Tally()
     tally.filters = [openmc.ReactionFilter(
-        ['(n,elastic)', '(n,fission)', '(n,gamma)']
+        ['(n,elastic)', '(n,2n)', '(n,fission)', '(n,gamma)']
     )]
     tally.scores = ['flux']
     model.tallies = openmc.Tallies([tally])
 
     # Reduce particles for faster testing
     model.settings.particles = 1000
-    model.settings.batches = 10
+    model.settings.batches = 5
 
-    harness = PyAPITestHarness('statepoint.10.h5', model)
+    harness = PyAPITestHarness('statepoint.5.h5', model)
     harness.main()

977ade79a1c66db0ff8542b3d231cccf6d7d1200
John Tramm <john.tramm@gmail.com>
2026-02-17 09:50:38 -0600

    Replace xtensor with internal Tensor/View classes (#3805)
    Co-authored-by: John Tramm <jtramm@gmail.com>

--- src/tallies/filter_meshmaterial.cpp
+++ src/tallies/filter_meshmaterial.cpp
@@ -1,5 +1,6 @@
 #include "openmc/tallies/filter_meshmaterial.h"
 
+#include <cassert>
 #include <utility> // for move
 
 #include <fmt/core.h>
@@ -10,6 +11,7 @@
 #include "openmc/error.h"
 #include "openmc/material.h"
 #include "openmc/mesh.h"
+#include "openmc/tensor.h"
 #include "openmc/xml_interface.h"
 
 namespace openmc {
@@ -161,7 +163,7 @@ void MeshMaterialFilter::to_statepoint(hid_t filter_group) const
   write_dataset(filter_group, "mesh", model::meshes[mesh_]->id_);
 
   size_t n = bins_.size();
-  xt::xtensor<size_t, 2> data({n, 2});
+  tensor::Tensor<size_t> data({n, 2});
   for (int64_t i = 0; i < n; ++i) {
     const auto& x = bins_[i];
     data(i, 0) = x.index_element;

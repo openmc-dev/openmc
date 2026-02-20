6a862b6f5ca175da9df2f743ed8fe57c0cb812eb
Paul P.H. Wilson <paul.wilson@wisc.edu>
2026-02-19 20:50:01 -0500

    declare new XML reader

--- include/openmc/xml_interface.h
+++ include/openmc/xml_interface.h
@@ -21,6 +21,9 @@ inline bool check_for_node(pugi::xml_node node, const char* name)
 std::string get_node_value(pugi::xml_node node, const char* name,
   bool lowercase = false, bool strip = false);
 
+std::string get_node_path(pugi::xml_node node, const char* name, 
+  bool lowercase, bool strip)
+
 bool get_node_value_bool(pugi::xml_node node, const char* name);
 
 template<typename T>

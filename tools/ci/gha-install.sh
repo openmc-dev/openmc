1d9a8f542b2b4fc588f0961c59ea674e4c6ca5ca
Micah Gale <mgale@fastmail.com>
2026-02-26 18:18:04 -0600

    Add Python 3.14 to testing matrix and drop Python 3.11 (#3642)
    Co-authored-by: GuySten <guyste@post.bgu.ac.il>
    Co-authored-by: Paul Romano <paul.k.romano@gmail.com>

--- tools/ci/gha-install.sh
+++ tools/ci/gha-install.sh
@@ -35,7 +35,7 @@ if [[ $MPI == 'y' ]]; then
     export HDF5_MPI=ON
     export HDF5_DIR=/usr/lib/x86_64-linux-gnu/hdf5/mpich
     # Install h5py without build isolation to pick up already installed mpi4py
-    pip install Cython pkgconfig
+    pip install setuptools Cython pkgconfig
     pip install --no-build-isolation --no-binary=h5py h5py
 fi
 

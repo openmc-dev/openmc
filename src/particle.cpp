d4f7c34e58e96b22298d6880f1b35a8778d7a8bb
GuySten <guyste@post.bgu.ac.il>
2026-03-06 23:11:42 +0200

    Revert "another simplification"
    This reverts commit fe7e5a3148d3b0438d66975d59ea80e6f63ac7c0.

--- src/particle.cpp
+++ src/particle.cpp
@@ -247,7 +247,8 @@ void Particle::event_advance()
 
   double speed = this->speed();
   double time_cutoff = settings::time_cutoff[type().transport_index()];
-  double distance_cutoff = (time_cutoff - time()) * speed;
+  double distance_cutoff =
+    (time_cutoff < INFTY) ? (time_cutoff - time()) * speed : INFTY;
 
   // Select smaller of the three distances
   double distance =

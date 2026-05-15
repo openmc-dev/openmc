95bd57fc17f733c783cc68a7eff77b23c0bbff34
John Tramm <john.tramm@gmail.com>
2026-05-15 12:00:45 -0500

    removed dead code

--- src/particle.cpp
+++ src/particle.cpp
@@ -103,19 +103,6 @@ bool Particle::create_secondary(
   bank.wgt_ww_born = wgt_ww_born();
   bank.n_split = n_split();
 
-  // In shared secondary mode, subtract secondary photon energy from parent's
-  // pulse-height storage now, since the secondary will be transported as a
-  // separate Particle object and won't have access to the parent's pht_storage.
-  if (settings::use_shared_secondary_bank &&
-      !model::active_pulse_height_tallies.empty() && type.is_photon()) {
-    auto it = std::find(model::pulse_height_cells.begin(),
-      model::pulse_height_cells.end(), lowest_coord().cell());
-    if (it != model::pulse_height_cells.end()) {
-      int index = std::distance(model::pulse_height_cells.begin(), it);
-      pht_storage()[index] -= bank.E;
-    }
-  }
-
   local_secondary_bank().emplace_back(bank);
   return true;
 }

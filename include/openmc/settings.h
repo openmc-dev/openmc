b8e214871c0b0d4c038b8e91e3cd936d738e2639
Paul Romano <paul.k.romano@gmail.com>
2026-05-28 07:38:19 -0500

    Reorder settings

--- include/openmc/settings.h
+++ include/openmc/settings.h
@@ -69,9 +69,8 @@ extern bool
   delayed_photon_scaling;   //!< Scale fission photon yield to include delayed
 extern "C" bool entropy_on; //!< calculate Shannon entropy?
 extern "C" bool
-  event_based; //!< use event-based mode (instead of history-based)
-extern bool temperature_field_on; //!< Is there a temperature field defined?
-extern bool ifp_on;               //!< Use IFP for kinetics parameters?
+  event_based;      //!< use event-based mode (instead of history-based)
+extern bool ifp_on; //!< Use IFP for kinetics parameters?
 extern bool legendre_to_tabular; //!< convert Legendre distributions to tabular?
 extern bool material_cell_offsets;   //!< create material cells offsets?
 extern "C" bool output_summary;      //!< write summary.h5?
@@ -92,6 +91,7 @@ extern bool surf_mcpl_write;         //!< write surface mcpl file?
 extern bool surf_source_read;        //!< read surface source file?
 extern bool survival_biasing;        //!< use survival biasing?
 extern bool survival_normalization;  //!< use survival normalization?
+extern bool temperature_field_on;    //!< Is there a temperature field defined?
 extern bool temperature_multipole;   //!< use multipole data?
 extern "C" bool trigger_on;          //!< tally triggers enabled?
 extern bool trigger_predict;         //!< predict batches for triggers?

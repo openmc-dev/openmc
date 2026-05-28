9e81f7377a628a271ac7a1f1dcdc98cde7a8ed92
Joffrey Dorville <jdorville@anl.gov>
2026-05-28 16:09:47 -0500

    Add failing test

--- tests/cpp_unit_tests/test_mesh.cpp
+++ tests/cpp_unit_tests/test_mesh.cpp
@@ -255,3 +255,51 @@ TEST_CASE("Test multiple meshes HDF5 roundtrip - spherical")
   REQUIRE(regular_mesh_hdf5->lower_left() == regular_mesh_xml->lower_left());
   REQUIRE(regular_mesh_hdf5->upper_right() == regular_mesh_xml->upper_right());
 }
+
+TEST_CASE("Test get_index_in_direction - regular mesh")
+{
+  // The XML data as a string
+  std::string xml_string = R"(
+        <mesh id="1">
+            <dimension>3 4 5</dimension>
+            <lower_left>-2 -3 -5</lower_left>
+            <upper_right>2 3 5</upper_right>
+       </mesh>
+    )";
+
+  // Create a pugixml document object
+  pugi::xml_document doc;
+
+  // Load the XML from the string
+  pugi::xml_parse_result result = doc.load_string(xml_string.c_str());
+
+  pugi::xml_node root = doc.child("mesh");
+
+  auto mesh = RegularMesh(root);
+
+  REQUIRE(mesh.get_index_in_direction(-2.0, 0) == 1);
+}
+
+TEST_CASE("Test get_index_in_direction - rectilinear mesh")
+{
+  // The XML data as a string
+  std::string xml_string = R"(
+        <mesh id="1" type="rectilinear">
+            <x_grid>0.0 1.0 5.0 10.0</x_grid>
+            <y_grid>-10.0 -5.0 0.0</y_grid>
+            <z_grid>-100.0 0.0 100.0</z_grid>
+        </mesh>
+    )";
+
+  // Create a pugixml document object
+  pugi::xml_document doc;
+
+  // Load the XML from the string
+  pugi::xml_parse_result result = doc.load_string(xml_string.c_str());
+
+  pugi::xml_node root = doc.child("mesh");
+
+  auto mesh = RectilinearMesh(root);
+
+  REQUIRE(mesh.get_index_in_direction(0.0, 0) == 1);
+}

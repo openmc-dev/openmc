885343abd022bafcd096be9fcbec06fddf2d2ffd
nuclearkevin <66632997+nuclearkevin@users.noreply.github.com>
2026-06-10 13:05:09 -0500

    Shouldn't need this going into a boundary crossing.

--- src/particle.cpp
+++ src/particle.cpp
@@ -835,12 +835,11 @@ void Particle::cross_reflective_bc(const Surface& surf, Direction new_u)
   // the lower universes.
   // (unless we're using a dagmc model, which has exactly one universe)
   n_coord() = 1;
-  if (surf.geom_type() != GeometryType::DAG && !neighbor_list_find_cell(*this)) {
-    if (!exhaustive_find_cell(*this)) {
-      mark_as_lost("Couldn't find particle after reflecting from surface " +
-                   std::to_string(surf.id_) + ".");
-      return;
-    }
+  if (surf.geom_type() != GeometryType::DAG &&
+      !neighbor_list_find_cell(*this)) {
+    mark_as_lost("Couldn't find particle after reflecting from surface " +
+                 std::to_string(surf.id_) + ".");
+    return;
   }
 
   // Set previous coordinate going slightly past surface crossing

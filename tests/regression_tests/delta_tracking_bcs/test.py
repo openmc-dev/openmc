8575c0b199f384d8f3fa7ca06924270171589ca3
nuclearkevin <66632997+nuclearkevin@users.noreply.github.com>
2026-06-12 11:45:45 -0500

    Update tests to account for the removal of duplicate code & consolidation.

--- tests/regression_tests/delta_tracking_bcs/test.py
+++ tests/regression_tests/delta_tracking_bcs/test.py
@@ -1,76 +1,14 @@
 import openmc
 import pytest
 from openmc.utility_funcs import change_directory
+from openmc.examples import delta_tracking_lattice
 
 from tests.testing_harness import PyAPITestHarness
 
 
-def model(boundary_type):
-    openmc.reset_auto_ids()
-    model = openmc.Model()
-
-    uo2 = openmc.Material(name='UO2')
-    uo2.set_density('g/cm3', 10.0)
-    uo2.add_nuclide('U235', 1.0)
-    uo2.add_nuclide('O16', 2.0)
-    water = openmc.Material(name='light water')
-    water.add_nuclide('H1', 2.0)
-    water.add_nuclide('O16', 1.0)
-    water.set_density('g/cm3', 1.0)
-    water.add_s_alpha_beta('c_H_in_H2O')
-    model.materials.extend([uo2, water])
-
-    cyl = openmc.ZCylinder(r=0.4)
-    pin = openmc.model.pin([cyl], [uo2, water])
-    d = 1.0
-
-    lattice = openmc.RectLattice()
-    lattice.lower_left = (-d, -d)
-    lattice.pitch = (d, d)
-    lattice.universes = [[pin, pin],
-                         [pin, pin]]
-    box = openmc.model.RectangularPrism(
-        2.0 * d, 2.0 * d,
-        origin=(0.0, 0.0),
-        boundary_type=boundary_type
-    )
-
-    model.geometry = openmc.Geometry([openmc.Cell(fill=lattice, region=-box)])
-    model.geometry.merge_surfaces = True
-
-    msh = openmc.RegularMesh(mesh_id=0)
-    msh.lower_left = (-d, -d)
-    msh.upper_right = (d, d)
-    msh.dimension = (2, 2)
-    t = openmc.Tally()
-    t.filters = [openmc.ParticleFilter(bins='neutron'), openmc.MeshFilter(mesh=msh)]
-    t.scores = ['total']
-    t.estimator = 'collision'
-    model.tallies.append(t)
-
-    model.settings.batches = 10
-    model.settings.inactive = 5
-    model.settings.particles = 1000
-    model.settings.delta_tracking = True
-
-    return model
-
-
-@pytest.mark.parametrize("photon", [False, True])
 @pytest.mark.parametrize("boundary", ['vacuum', 'reflective', 'periodic', 'white'])
-def test_lattice(photon, boundary):
-    m = model(boundary)
-    with change_directory(boundary+str(photon)):
-        m.settings.photon_transport = photon
-        if photon:
-            msh = openmc.RegularMesh(mesh_id=1)
-            msh.lower_left = (-1.0, -1.0)
-            msh.upper_right = (1.0, 1.0)
-            msh.dimension = (2, 2)
-            t = openmc.Tally()
-            t.filters = [openmc.ParticleFilter(bins='photon'), openmc.MeshFilter(mesh=msh)]
-            t.scores = ['total']
-            t.estimator = 'collision'
-            m.tallies.append(t)
-        harness = PyAPITestHarness('statepoint.10.h5', m)
+def test_lattice(boundary):
+    with change_directory(boundary):
+        model = delta_tracking_lattice(False, boundary)
+        harness = PyAPITestHarness('statepoint.10.h5', model)
         harness.main()
--- tests/regression_tests/delta_tracking_distribrho/test.py
+++ tests/regression_tests/delta_tracking_distribrho/test.py
@@ -1,77 +1,14 @@
 import openmc
 import pytest
 from openmc.utility_funcs import change_directory
+from openmc.examples import delta_tracking_lattice
 
 from tests.testing_harness import PyAPITestHarness
 
 
-@pytest.fixture
-def model():
-    openmc.reset_auto_ids()
-    model = openmc.Model()
-
-    uo2 = openmc.Material(name='UO2')
-    uo2.set_density('g/cm3', 10.0)
-    uo2.add_nuclide('U235', 1.0)
-    uo2.add_nuclide('O16', 2.0)
-    water = openmc.Material(name='light water')
-    water.add_nuclide('H1', 2.0)
-    water.add_nuclide('O16', 1.0)
-    water.set_density('g/cm3', 1.0)
-    water.add_s_alpha_beta('c_H_in_H2O')
-    model.materials.extend([uo2, water])
-
-    cyl = openmc.ZCylinder(r=0.4)
-    pin = openmc.model.pin([cyl], [uo2, water])
-    d = 1.0
-
-    lattice = openmc.RectLattice()
-    lattice.lower_left = (-d, -d)
-    lattice.pitch = (d, d)
-    lattice.universes = [[pin, pin],
-                         [pin, pin]]
-    box = openmc.model.RectangularPrism(
-        2.0 * d, 2.0 * d,
-        origin=(0.0, 0.0),
-        boundary_type='reflective'
-    )
-
-    pin.cells[1].density = [10.0, 20.0, 10.0, 20.0]
-
-    model.geometry = openmc.Geometry([openmc.Cell(fill=lattice, region=-box)])
-    model.geometry.merge_surfaces = True
-
-    msh = openmc.RegularMesh(mesh_id=0)
-    msh.lower_left = (-d, -d)
-    msh.upper_right = (d, d)
-    msh.dimension = (2, 2)
-    t = openmc.Tally()
-    t.filters = [openmc.ParticleFilter(bins='neutron'), openmc.MeshFilter(mesh=msh)]
-    t.scores = ['total']
-    t.estimator = 'collision'
-    model.tallies.append(t)
-
-    model.settings.batches = 10
-    model.settings.inactive = 5
-    model.settings.particles = 1000
-    model.settings.delta_tracking = True
-
-    return model
-
-
 @pytest.mark.parametrize("photon", [False, True])
-def test_lattice_checkerboard(model, photon):
+def test_lattice_checkerboard(photon):
     with change_directory(str(photon)):
-        model.settings.photon_transport = photon
-        if photon:
-            msh = openmc.RegularMesh(mesh_id=1)
-            msh.lower_left = (-1.0, -1.0)
-            msh.upper_right = (1.0, 1.0)
-            msh.dimension = (2, 2)
-            t = openmc.Tally()
-            t.filters = [openmc.ParticleFilter(bins='photon'), openmc.MeshFilter(mesh=msh)]
-            t.scores = ['total']
-            t.estimator = 'collision'
-            model.tallies.append(t)
+        model = delta_tracking_lattice(photon, densities=[10.0, 20.0, 10.0, 20.0])
         harness = PyAPITestHarness('statepoint.10.h5', model)
         harness.main()

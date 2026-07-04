f4094ff8fc9a5ad16500f5482d105730492cdef1
John Vincent Cauilan <64677361+johvincau@users.noreply.github.com>
2026-07-04 16:03:28 -0500

    Specify pinned versions for NJOY, MOAB, and LIBMESH

--- tools/ci/gha-cache-keys.sh
+++ tools/ci/gha-cache-keys.sh
@@ -2,11 +2,11 @@
 set -ex
 
 echo "NJOY_HASH=$(git ls-remote https://github.com/njoy/NJOY2016.git HEAD \
-    --branch 2016.78 | head -c 15)" >> $GITHUB_ENV
+    --tags 2016.78 | head -c 15)" >> $GITHUB_ENV
 
 if [[ "$DAGMC" == 'y' ]]; then
     echo "MOAB_HASH=$(git ls-remote https://bitbucket.org/fathomteam/moab.git \
-    --branch Version5.1.0 HEAD | head -c 15)" >> $GITHUB_ENV
+    --tags Version5.1.0 | head -c 15)" >> $GITHUB_ENV
 
     echo "DAGMC_HASH=$(git ls-remote https://github.com/svalinn/dagmc.git HEAD |\
     head -c 15)" >> $GITHUB_ENV
@@ -14,5 +14,5 @@ fi
 
 if [[ "$LIBMESH" == 'y' ]]; then
     echo "LB_HASH=$(git ls-remote https://github.com/libmesh/libmesh \
-    --branch v1.7.1 HEAD | head -c 15)" >> $GITHUB_ENV
+    --tags v1.7.1 | head -c 15)" >> $GITHUB_ENV
 fi

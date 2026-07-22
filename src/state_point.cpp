25ace6e7806a171fb7132f90e7990cd330935221
GuySten <guyste@post.bgu.ac.il>
2026-07-22 07:47:29 +0300

    fix

--- src/state_point.cpp
+++ src/state_point.cpp
@@ -887,26 +887,18 @@ void write_global_tallies(hid_t file_id)
 {
   // Get global tallies
   auto& gt = simulation::global_tallies;
-  auto write_view = xt::view(gt,
-    xt::range(static_cast<int>(GlobalTally::K_COLLISION),
-      static_cast<int>(GlobalTally::LEAKAGE) + 1),
-    xt::range(static_cast<int>(TallyResult::SUM),
-      static_cast<int>(TallyResult::SUM_SQ) + 1));
-  auto gt_reduced = xt::empty_like(write_view);
-  gt_reduced = write_view;
-  write_dataset(file_id, "global_tallies", gt_reduced);
+  write_dataset(file_id, "global_tallies", 
+  gt.slice(range(static_cast<int>(GlobalTally::K_COLLISION), static_cast<int>(GlobalTally::LEAKAGE) + 1), 
+           range(static_cast<int>(TallyResult::SUM), static_cast<int>(TallyResult::SUM_SQ) + 1))));
 }
 
 void read_global_tallies(hid_t file_id)
 {
   // Get global tallies
   auto& gt = simulation::global_tallies;
-  auto gt_view = xt::view(gt,
-    xt::range(static_cast<int>(GlobalTally::K_COLLISION),
-      static_cast<int>(GlobalTally::LEAKAGE) + 1),
-    xt::range(static_cast<int>(TallyResult::SUM),
-      static_cast<int>(TallyResult::SUM_SQ) + 1));
-  auto gt_reduced = xt::empty_like(gt_view);
+  auto gt_view = gt.slice(range(static_cast<int>(GlobalTally::K_COLLISION), static_cast<int>(GlobalTally::LEAKAGE) + 1), 
+           range(static_cast<int>(TallyResult::SUM), static_cast<int>(TallyResult::SUM_SQ) + 1)));
+  auto gt_reduced = Tensor<double>(gt_view.shape());
   read_dataset_lowlevel(file_id, "global_tallies", H5T_NATIVE_DOUBLE, H5S_ALL,
     false, gt_reduced.data());
   gt_view = gt_reduced;

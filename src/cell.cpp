715b203bf495a2f0b1b5e40b0d2319a71cdb94be
GuySten <guyste@post.bgu.ac.il>
2026-07-22 20:21:53 +0300

    try to fix infinite loop

--- src/cell.cpp
+++ src/cell.cpp
@@ -1018,8 +1018,16 @@ std::pair<double, int32_t> Region::distance_complex(
     }
     if (min_dist == INFTY)
       break;
-    auto p = r + (min_dist + TINY_BIT) * u;
-    if (contains_complex(p, u, on_surface) != in_region)
+    // Search for next closest intersection
+    auto [i_surf_next, next_dist] =
+      distance_simple(r + min_dist * u, u, i_surf);
+    // If no next intersection we are done
+    if (next_dist == INFTY)
+      break;
+    // If there is next intersection we will check point containment in the
+    // middle to avoid close to boundary numerical errors
+    auto r_mid = r + (0.5 * (min_dist + next_dist)) * u;
+    if (contains_complex(r_mid, u, i_surf) != in_region)
       break;
     atleast = min_dist;
   }

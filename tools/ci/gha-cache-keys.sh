263c77cc7d55bb8fab291a3c672a771807997418
John Vincent Cauilan <64677361+johvincau@users.noreply.github.com>
2026-08-04 03:13:54 -0500

    Tweak repo commit hash getter script

--- tools/ci/gha-cache-keys.sh
+++ tools/ci/gha-cache-keys.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 set -ex
 
-echo "NJOY_HASH=$(git ls-remote https://github.com/njoy/NJOY2016.git HEAD \
-    --tags 2016.78 | head -c 15)" >> $GITHUB_ENV
+echo "NJOY_HASH=$(git ls-remote https://github.com/njoy/NJOY2016.git \
+    --tags 2016.78 )" >> $GITHUB_ENV
 
 if [[ "$DAGMC" == 'y' ]]; then
     echo "MOAB_HASH=$(git ls-remote https://bitbucket.org/fathomteam/moab.git \
-    --tags Version5.1.0 | head -c 15)" >> $GITHUB_ENV
+    --tags 5.5.1 )" >> $GITHUB_ENV
 
-    echo "DAGMC_HASH=$(git ls-remote https://github.com/svalinn/dagmc.git HEAD |\
-    head -c 15)" >> $GITHUB_ENV
+    echo "DAGMC_HASH=$(git ls-remote https://github.com/svalinn/dagmc.git \
+    --head develop )" >> $GITHUB_ENV
 fi
 
 if [[ "$LIBMESH" == 'y' ]]; then
     echo "LB_HASH=$(git ls-remote https://github.com/libmesh/libmesh \
-    --tags v1.7.1 | head -c 15)" >> $GITHUB_ENV
+    --tags v1.7.1 )" >> $GITHUB_ENV
 fi
 
 if [[ "$MPI" == 'y' ]]; then

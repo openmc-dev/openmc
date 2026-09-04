d28ba346ff2d29b1b34ccd792dc4ac0ad0d1a8fb
Joffrey Dorville <jdorville@anl.gov>
2026-09-04 16:21:09 -0500

    Return number of unique vertices for rectilinear and regular meshes

--- openmc/mesh.py
+++ openmc/mesh.py
@@ -1049,6 +1049,8 @@ class RegularMesh(StructuredMesh):
         The number of mesh cells in each direction (x, y, z).
     n_dimension : int
         Number of mesh dimensions.
+    n_vertices : int
+        Number of unique vertices.
     axis_labels : tuple of str
         Names of the mesh axes ('x', 'y', 'z'), truncated to the mesh
         dimensionality.
@@ -1093,6 +1095,13 @@ class RegularMesh(StructuredMesh):
             return len(self._dimension)
         else:
             return None
+
+    @property
+    def n_vertices(self):
+        n_vertices = 1
+        for d in self._dimension:
+            n_vertices *= d + 1
+        return n_vertices
 
     @property
     def axis_labels(self):
@@ -1579,6 +1588,8 @@ class RectilinearMesh(StructuredMesh):
         The number of mesh cells in each direction (x, y, z).
     n_dimension : int
         Number of mesh dimensions (always 3 for a RectilinearMesh).
+    n_vertices : int
+        Number of unique vertices.
     axis_labels : tuple of str
         Names of the mesh axes ('x', 'y', 'z').
     x_grid : numpy.ndarray
@@ -1612,6 +1623,13 @@ class RectilinearMesh(StructuredMesh):
     @property
     def n_dimension(self):
         return 3
+
+    @property
+    def n_vertices(self):
+        n_vertices = 1
+        for d in self._dimension:
+            n_vertices *= d + 1
+        return n_vertices
 
     @property
     def axis_labels(self):

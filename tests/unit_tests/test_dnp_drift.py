86b65f32030b027a4f639aa57dc1b14e40881977
Joffrey Dorville <jdorville@anl.gov>
2026-09-05 01:00:49 -0500

    Update dnp_drift python unit tests

--- tests/unit_tests/test_dnp_drift.py
+++ tests/unit_tests/test_dnp_drift.py
@@ -20,12 +20,22 @@ def mesh():
 
 def test_xml_serialization(mesh, run_in_tmpdir):
     """Test XML serialization."""
-    velocity_field = openmc.VelocityField(mesh=mesh, values=np.tile([0, 0, 10], (8, 1)))
+    velocity_field = openmc.VelocityField(
+        mesh=mesh, values=np.tile([0, 0, 10], (8, 1)), mapping="cell"
+    )
 
     dnp_drift = openmc.DNPDrift(
         velocity_field=velocity_field,
         boundary_map={"inlet": [1], "outlet": [2], "wall": [3, 4]},
-        mapping="nodal",
+        physical_group_map={
+            "face_ids": [
+                0, 24, 12, 36, 7, 31, 19, 43, 15, 39, 21, 45, 2,
+                26, 8,32, 4, 16, 10, 22, 29, 41, 35, 47
+            ],
+            "physical_groups": [
+                1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3,
+                3, 3, 3, 3, 3, 3, 3, 3
+            ]},
         integrator="RK4",
         integrator_dt=0.1,
         external_travel_time=2.0,
@@ -46,7 +56,15 @@ def test_xml_serialization(mesh, run_in_tmpdir):
         "outlet": [2],
         "wall": [3, 4],
     }
-    assert read_settings.dnp_drift.mapping == "nodal"
+    assert read_settings.dnp_drift.physical_group_map == {
+        "face_ids": [
+            0, 24, 12, 36, 7, 31, 19, 43, 15, 39, 21, 45, 2,
+            26, 8,32, 4, 16, 10, 22, 29, 41, 35, 47
+        ],
+        "physical_groups": [
+            1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3,
+            3, 3, 3, 3, 3, 3, 3, 3
+        ]}
     assert read_settings.dnp_drift.integrator == "RK4"
     assert read_settings.dnp_drift.integrator_dt == 0.1
     assert read_settings.dnp_drift.external_travel_time == 2.0
@@ -73,7 +91,11 @@ def test_missing_boundary_map_keys(mesh):
     velocity_field = openmc.VelocityField(mesh=mesh, values=np.tile([0, 0, 10], (8, 1)))
 
     with pytest.raises(ValueError):
-        openmc.DNPDrift(velocity_field=velocity_field, boundary_map={"inlet": [1]})
+        openmc.DNPDrift(
+            velocity_field=velocity_field,
+            boundary_map={"inlet": [1]},
+            physical_group_map={"face_ids": [0], "physical_groups": [1]}
+        )
 
 
 def test_invalid_boundary_map_key(mesh):
@@ -83,7 +105,8 @@ def test_invalid_boundary_map_key(mesh):
     with pytest.raises(ValueError):
         openmc.DNPDrift(
             velocity_field=velocity_field,
-            boundary_map={"inlet": [1], "outlet": [2], "dnp_drift": [3]}
+            boundary_map={"inlet": [1], "outlet": [2], "dnp_drift": [3]},
+            physical_group_map={"face_ids": [0], "physical_groups": [1]}
         )
 
 
@@ -95,6 +118,7 @@ def test_invalid_integrator_dt(mesh):
         openmc.DNPDrift(
             velocity_field=velocity_field,
             boundary_map={"inlet": [1], "outlet": [2]},
+            physical_group_map={"face_ids": [0], "physical_groups": [1]},
             integrator_dt=-0.1
         )
 
@@ -107,5 +131,6 @@ def test_invalid_external_travel_time(mesh):
         openmc.DNPDrift(
             velocity_field=velocity_field,
             boundary_map={"inlet": [1], "outlet": [2]},
+            physical_group_map={"face_ids": [0], "physical_groups": [1]},
             external_travel_time=-1.0
         )
